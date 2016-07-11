1adbe9f3765f5c321a0e54d3d116cc907ee6998b
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-11 14:33:33 +0900

    remove unnecessary _Pragma

--- src/kext/VirtualHIDDevice/share/diagnostic_macros.hpp
+++ src/kext/VirtualHIDDevice/share/diagnostic_macros.hpp
@@ -3,7 +3,6 @@
 #define BEGIN_IOKIT_INCLUDE        \
   _Pragma("clang diagnostic push") \
       _Pragma("clang diagnostic ignored \"-Winconsistent-missing-override\"")
-      _Pragma("clang diagnostic ignored \"-W#warnings\"")
 
 #define END_IOKIT_INCLUDE \
   _Pragma("clang diagnostic pop")
