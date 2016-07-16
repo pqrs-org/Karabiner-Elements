869551a891c1e1cb8a472296c6c1e9d0f177583b
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-17 01:19:56 +0900

    add grabber.hpp

---
+++ src/grabber/grabber/include/grabber.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <IOKit/hid/IOHIDDevice.h>
+#include <IOKit/hid/IOHIDElement.h>
+#include <IOKit/hid/IOHIDManager.h>
+#include <IOKit/hid/IOHIDQueue.h>
+#include <IOKit/hid/IOHIDValue.h>
+#include <mach/mach_time.h>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <unordered_map>
+
+#include "event_grabber.hpp"
+#include "human_interface_device.hpp"
--- src/grabber/grabber/main.cpp
+++ src/grabber/grabber/main.cpp
@@ -1,14 +1,5 @@
-#include <CoreGraphics/CoreGraphics.h>
-#include <IOKit/hid/IOHIDDevice.h>
-#include <IOKit/hid/IOHIDElement.h>
-#include <IOKit/hid/IOHIDManager.h>
-#include <IOKit/hid/IOHIDQueue.h>
-#include <IOKit/hidsystem/IOHIDShared.h>
-#include <IOKit/hidsystem/IOLLEvent.h>
-#include <IOKit/hidsystem/ev_keymap.h>
 #include <iostream>
-
-#include "event_grabber.hpp"
+#include "grabber.hpp"
 
 int main(int argc, const char* argv[]) {
   event_grabber observer;
