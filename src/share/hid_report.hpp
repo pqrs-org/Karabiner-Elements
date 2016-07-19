169e49f370da2747569b184e488eab2dc7337148
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-20 00:25:14 +0900

    UInt8 -> uint8_t

--- src/share/hid_report.hpp
+++ src/share/hid_report.hpp
@@ -3,8 +3,8 @@
 class hid_report {
 public:
   struct __attribute__((packed)) keyboard_input {
-    UInt8 modifiers;
-    UInt8 reserved;
-    UInt8 keys[6];
+    uint8_t modifiers;
+    uint8_t reserved;
+    uint8_t keys[6];
   };
 };
