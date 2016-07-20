fc3c91da106190031b05486ed827ec98d0b3e73c
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-21 00:06:22 +0900

    add hid_report::keyboard_input constructor

--- src/share/hid_report.hpp
+++ src/share/hid_report.hpp
@@ -2,7 +2,10 @@
 
 class hid_report {
 public:
-  struct __attribute__((packed)) keyboard_input {
+  class __attribute__((packed)) keyboard_input {
+  public:
+    keyboard_input(void) : modifiers(0), reserved(0), keys{0} {}
+
     uint8_t modifiers;
     uint8_t reserved;
     uint8_t keys[6];
