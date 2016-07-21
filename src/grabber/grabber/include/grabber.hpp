d4c79821a9aba26026df01576cdc4ecc3e8e8789
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-21 22:39:36 +0900

    update #include

--- src/grabber/grabber/include/grabber.hpp
+++ src/grabber/grabber/include/grabber.hpp
@@ -1,16 +1,20 @@
 #pragma once
 
+#include <IOKit/IOKitLib.h>
 #include <IOKit/hid/IOHIDDevice.h>
 #include <IOKit/hid/IOHIDElement.h>
 #include <IOKit/hid/IOHIDManager.h>
 #include <IOKit/hid/IOHIDQueue.h>
 #include <IOKit/hid/IOHIDValue.h>
 #include <mach/mach_time.h>
+
+#include <iostream>
 #include <memory>
 #include <string>
 #include <unordered_map>
-#include <unordered_map>
 
 #include "event_grabber.hpp"
-#include "human_interface_device.hpp"
 #include "hid_report.hpp"
+#include "human_interface_device.hpp"
+#include "user_client.hpp"
+#include "user_client_method.hpp"
