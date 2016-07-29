d4bc918914e76652432fe549e6580b9f6ef7e711
Takayama Fumihiko <tekezo@pqrs.org>
2016-07-29 14:05:21 +0900

    add VirtualHIDConsumer

--- src/kext/VirtualHIDDevice/scripts/unload.sh
+++ src/kext/VirtualHIDDevice/scripts/unload.sh
@@ -6,5 +6,6 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
 sudo kextunload -b org.pqrs.driver.VirtualHIDManager
 
 sudo kextunload -b org.pqrs.driver.VirtualHIDKeyboard
+sudo kextunload -b org.pqrs.driver.VirtualHIDConsumer
 
 exit 0
