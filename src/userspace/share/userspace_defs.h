2e997f2ca9d50329d8ffd0d525e3931505c2a403
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-05 00:04:02 +0900

    add krbn_op_type

--- src/userspace/share/userspace_defs.h
+++ src/userspace/share/userspace_defs.h
@@ -1,6 +1,11 @@
 #pragma once
 
-enum EV_TYPE {
-  EV_KEY_DOWN,
-  EV_KEY_UP,
+enum krbn_op_type {
+  KRBN_OP_TYPE_NONE,
+  KRBN_OP_TYPE_POST_MODIFIER_FLAGS,
+};
+
+enum krbn_ev_type {
+  KRBN_EV_TYPE_KEY_DOWN,
+  KRBN_EV_TYPE_KEY_UP,
 };
