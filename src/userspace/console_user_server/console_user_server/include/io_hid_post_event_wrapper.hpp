e15adcc1e8f8224826c7a76ca5428c71a2fe57fb
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-05 00:04:14 +0900

    add post_key

--- src/userspace/console_user_server/console_user_server/include/io_hid_post_event_wrapper.hpp
+++ src/userspace/console_user_server/console_user_server/include/io_hid_post_event_wrapper.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "user_client.hpp"
+#include "userspace_defs.h"
 
 class io_hid_post_event_wrapper final {
 public:
@@ -25,6 +26,36 @@ public:
     }
   }
 
+  void post_key(uint8_t key_code, enum krbn_ev_type ev_type, IOOptionBits flags, bool repeat) {
+    auto connect = user_client_.get_connect();
+    if (!connect) {
+      return;
+    }
+
+    NXEventData event;
+    memset(&event, 0, sizeof(event));
+    event.key.origCharCode = 0;
+    event.key.repeat = repeat;
+    event.key.charSet = NX_ASCIISET;
+    event.key.charCode = 0;
+    event.key.keyCode = key_code;
+    event.key.origCharSet = NX_ASCIISET;
+    event.key.keyboardType = 0;
+
+    IOGPoint loc = {0, 0};
+    auto kr = IOHIDPostEvent(connect,
+                             ev_type == KRBN_EV_TYPE_KEY_DOWN ? NX_KEYDOWN : NX_KEYUP,
+                             loc,
+                             &event,
+                             kNXEventDataVersion,
+                             flags,
+                             0);
+
+    if (KERN_SUCCESS != kr) {
+      std::cerr << "IOHIDPostEvent returned 0x" << std::hex << kr << std::dec << std::endl;
+    }
+  }
+
   void post_aux_key(uint8_t key_code, bool key_down, IOOptionBits flags) {
     auto connect = user_client_.get_connect();
     if (!connect) {
@@ -32,7 +63,7 @@ public:
     }
 
     NXEventData event;
-    bzero(&event, sizeof(event));
+    memset(&event, 0, sizeof(event));
     event.compound.subType = NX_SUBTYPE_AUX_CONTROL_BUTTONS;
     event.compound.misc.L[0] = (key_code << 16) | ((key_down ? NX_KEYDOWN : NX_KEYUP) << 8);
 
--- src/userspace/console_user_server/console_user_server/include/receiver.hpp
+++ src/userspace/console_user_server/console_user_server/include/receiver.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
 #include "constants.hpp"
+#include "io_hid_post_event_wrapper.hpp"
 #include "local_datagram_server.hpp"
+#include "userspace_defs.h"
 
 class receiver final {
 public:
-  receiver(void) : exit_loop_(false) {}
+  receiver(void) : exit_loop_(false) {
+    io_hid_post_event_wrapper_.start();
+  }
 
   void start(void) {
     if (server_) {
@@ -42,10 +46,16 @@ public:
       boost::system::error_code ec;
       std::size_t n = server_->receive(boost::asio::buffer(buffer_), boost::posix_time::seconds(1), ec);
 
-      if (ec) {
-        std::cout << "Receive error: " << ec.message() << "\n";
-      } else {
-        std::cout << n << std::endl;
+      if (!ec && n > 0) {
+        --n;
+        if (buffer_[0] == KRBN_OP_TYPE_POST_MODIFIER_FLAGS) {
+          if (n == sizeof(IOOptionBits)) {
+            __block IOOptionBits flags;
+            memcpy(&flags, &(buffer_[1]), sizeof(flags));
+            std::cout << "0x" << std::hex << flags << std::endl;
+            io_hid_post_event_wrapper_.post_modifier_flags(flags);
+          }
+        }
       }
     }
   }
@@ -58,4 +68,6 @@ private:
   std::unique_ptr<local_datagram_server> server_;
   std::thread thread_;
   volatile bool exit_loop_;
+
+  io_hid_post_event_wrapper io_hid_post_event_wrapper_;
 };
