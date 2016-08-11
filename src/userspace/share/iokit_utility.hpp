ffe12c4c7b49517452c5a7c0c87998e551639b80
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-12 02:07:56 +0900

    add iokit_utility::create_device_matching_dictionaries

--- src/userspace/share/iokit_utility.hpp
+++ src/userspace/share/iokit_utility.hpp
@@ -2,7 +2,10 @@
 
 #include <IOKit/IOKitLib.h>
 #include <IOKit/hid/IOHIDKeys.h>
+
+#include <cstdint>
 #include <string>
+#include <vector>
 
 class iokit_utility {
 public:
@@ -24,5 +27,56 @@ public:
     }
 
     return serial_number;
+  }
+
+  static CFDictionaryRef _Nonnull create_device_matching_dictionary(uint32_t usage_page, uint32_t usage) {
+    auto device_matching_dictionary = CFDictionaryCreateMutable(kCFAllocatorDefault, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    if (!device_matching_dictionary) {
+      goto finish;
+    }
+
+    // usage_page
+    if (!usage_page) {
+      goto finish;
+    } else {
+      auto number = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &usage_page);
+      if (!number) {
+        goto finish;
+      }
+      CFDictionarySetValue(device_matching_dictionary, CFSTR(kIOHIDDeviceUsagePageKey), number);
+      CFRelease(number);
+    }
+
+    // usage (The usage is only valid if the usage page is also defined)
+    if (!usage) {
+      goto finish;
+    } else {
+      auto number = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &usage);
+      if (!number) {
+        goto finish;
+      }
+      CFDictionarySetValue(device_matching_dictionary, CFSTR(kIOHIDDeviceUsageKey), number);
+      CFRelease(number);
+    }
+
+  finish:
+    return device_matching_dictionary;
+  }
+
+  static CFArrayRef _Nullable create_device_matching_dictionaries(const std::vector<std::pair<uint32_t, uint32_t>>& usage_pairs) {
+    auto device_matching_dictionaries = CFArrayCreateMutable(kCFAllocatorDefault, 0, &kCFTypeArrayCallBacks);
+    if (!device_matching_dictionaries) {
+      return nullptr;
+    }
+
+    for (const auto& usage_pair : usage_pairs) {
+      auto device_matching_dictionary = create_device_matching_dictionary(usage_pair.first, usage_pair.second);
+      if (device_matching_dictionary) {
+        CFArrayAppendValue(device_matching_dictionaries, device_matching_dictionary);
+        CFRelease(device_matching_dictionary);
+      }
+    }
+
+    return device_matching_dictionaries;
   }
 };
