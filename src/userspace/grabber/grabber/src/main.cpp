6930c77fbffef6d5eac0d217c46afffdfef27715
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-15 22:50:42 +0900

    set SIG_IGN for SIGUSR1 and SIGUSR2

--- src/userspace/grabber/grabber/src/main.cpp
+++ src/userspace/grabber/grabber/src/main.cpp
@@ -1,6 +1,9 @@
 #include "grabber.hpp"
 
 int main(int argc, const char* argv[]) {
+  signal(SIGUSR1, SIG_IGN);
+  signal(SIGUSR2, SIG_IGN);
+
   const char* version = "version "
 #include "version.hpp"
       ;
