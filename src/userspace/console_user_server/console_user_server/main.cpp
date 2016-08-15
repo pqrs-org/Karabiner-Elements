3c4f020f119c2b41a8e2ae10e5de809b478d39b4
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-16 00:17:01 +0900

    set SIG_IGN for SIGUSR1 and SIGUSR2

--- src/userspace/console_user_server/console_user_server/main.cpp
+++ src/userspace/console_user_server/console_user_server/main.cpp
@@ -1,11 +1,13 @@
-#include <CoreFoundation/CoreFoundation.h>
-
 #include "include_boost.hpp"
 
 #include "connection_manager.hpp"
 
 int main(int argc, const char* argv[]) {
+  signal(SIGUSR1, SIG_IGN);
+  signal(SIGUSR2, SIG_IGN);
+
   connection_manager manager;
+
   CFRunLoopRun();
   return 0;
 }
