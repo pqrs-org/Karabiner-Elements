2e1c471f1d736a96ba5dfc343cf34c1a85b7fffc
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-19 11:25:43 +0900

    set operation_type manually

--- src/userspace/grabber/grabber/include/console_user_client.hpp
+++ src/userspace/grabber/grabber/include/console_user_client.hpp
@@ -12,17 +12,20 @@ public:
 
   void stop_key_repeat(void) {
     krbn_operation_type_stop_key_repeat data;
+    data.operation_type = KRBN_OPERATION_TYPE_STOP_KEY_REPEAT;
     client_.send_to(reinterpret_cast<uint8_t*>(&data), sizeof(data));
   }
 
   void post_modifier_flags(void) {
     krbn_operation_type_post_modifier_flags data;
+    data.operation_type = KRBN_OPERATION_TYPE_POST_MODIFIER_FLAGS;
     data.flags = modifier_flag_manager_.get_io_option_bits();
     client_.send_to(reinterpret_cast<uint8_t*>(&data), sizeof(data));
   }
 
   void post_key(krbn_key_code key_code, krbn_event_type event_type) {
     krbn_operation_type_post_key data;
+    data.operation_type = KRBN_OPERATION_TYPE_POST_KEY;
     data.key_code = key_code;
     data.event_type = event_type;
     data.flags = modifier_flag_manager_.get_io_option_bits();
