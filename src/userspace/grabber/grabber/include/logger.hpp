be1cef35496750ae04f405d0c41d5d417270d72e
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-20 03:04:09 +0900

    add stdout_logger_mt

--- src/userspace/grabber/grabber/include/logger.hpp
+++ src/userspace/grabber/grabber/include/logger.hpp
@@ -8,7 +8,11 @@ public:
   static spdlog::logger& get_logger(void) {
     static std::shared_ptr<spdlog::logger> logger;
     if (!logger) {
+#if 0
       logger = spdlog::rotating_logger_mt("karabiner_grabber", "/var/log/karabiner_grabber_log", 5 * 1024 * 1024, 3, true);
+#else
+      logger = spdlog::stdout_logger_mt("karabiner_grabber", true);
+#endif
     }
     return *logger;
   }
