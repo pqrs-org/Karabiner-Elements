49c76ec928cf235986e60f4e2684da74ee0b5ffc
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-19 12:04:38 +0900

    add grabber_client_pid_monitor_

--- src/userspace/grabber/grabber/include/grabber_server.hpp
+++ src/userspace/grabber/grabber/include/grabber_server.hpp
@@ -1,12 +1,15 @@
 #pragma once
 
 #include "constants.hpp"
+#include "device_grabber.hpp"
 #include "local_datagram_server.hpp"
 #include "session.hpp"
 #include "userspace_defs.h"
 
 class grabber_server final {
 public:
+  grabber_server(void) : exit_loop_(false), grabber_client_pid_monitor_(0) {}
+
   void start(void) {
     const char* path = constants::get_grabber_socket_file_path();
     unlink(path);
@@ -18,22 +21,21 @@ public:
     }
     chmod(path, 0600);
 
+    exit_loop_ = false;
     thread_ = std::thread([this] { this->worker(); });
   }
 
   void stop(void) {
     unlink(constants::get_grabber_socket_file_path());
 
-    if (!thread_.joinable()) {
-      return;
-    }
-    if (!server_) {
-      return;
+    exit_loop_ = true;
+    if (thread_.joinable()) {
+      thread_.join();
     }
 
-    exit_loop_ = true;
-    thread_.join();
     server_.reset(nullptr);
+    release_grabber_client_pid_monitor();
+    device_grabber_.reset(nullptr);
   }
 
   void worker(void) {
@@ -52,7 +54,30 @@ public:
             logger::get_logger().error("invalid size for KRBN_OPERATION_TYPE_CONNECT");
           } else {
             auto p = reinterpret_cast<krbn_operation_type_connect*>(&(buffer_[0]));
-            std::cout << "pid " << std::dec << p->console_user_server_pid << std::endl;
+            auto pid = p->console_user_server_pid;
+
+            logger::get_logger().info("grabber_client is connected (pid:{0})", pid);
+
+            device_grabber_.reset(nullptr);
+            device_grabber_ = std::make_unique<device_grabber>();
+
+            // monitor the last process
+            release_grabber_client_pid_monitor();
+            grabber_client_pid_monitor_ = dispatch_source_create(DISPATCH_SOURCE_TYPE_PROC,
+                                                                 p->console_user_server_pid,
+                                                                 DISPATCH_PROC_EXIT,
+                                                                 dispatch_get_main_queue());
+            if (!grabber_client_pid_monitor_) {
+              logger::get_logger().error("{0}: failed to dispatch_source_create", __PRETTY_FUNCTION__);
+            } else {
+              dispatch_source_set_event_handler(grabber_client_pid_monitor_, ^{
+                  logger::get_logger().info("grabber_client is exited (pid:{0})", pid);
+
+                  device_grabber_.reset(nullptr);
+                  release_grabber_client_pid_monitor();
+                });
+              dispatch_resume(grabber_client_pid_monitor_);
+            }
           }
           break;
         }
@@ -61,6 +86,14 @@ public:
   }
 
 private:
+  void release_grabber_client_pid_monitor(void) {
+    if (grabber_client_pid_monitor_) {
+      dispatch_source_cancel(grabber_client_pid_monitor_);
+      dispatch_release(grabber_client_pid_monitor_);
+      grabber_client_pid_monitor_ = 0;
+    }
+  }
+
   enum {
     buffer_length = 8 * 1024 * 1024,
   };
@@ -68,4 +101,8 @@ private:
   std::unique_ptr<local_datagram_server> server_;
   std::thread thread_;
   volatile bool exit_loop_;
+
+  std::unique_ptr<device_grabber> device_grabber_;
+
+  dispatch_source_t grabber_client_pid_monitor_;
 };
