b92244c9f00c637a37a190483f8bb38c1d4d8f9b
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-20 22:12:45 +0900

    put all kexts into /Library/Extensions

--- src/kext/VirtualHIDDevice/scripts/load.sh
+++ src/kext/VirtualHIDDevice/scripts/load.sh
@@ -8,23 +8,18 @@ targetdir=/tmp/org.pqrs.driver.VirtualHIDDevice
 sudo rm -rf $targetdir
 mkdir $targetdir
 
-for d in \
-    build/Release/VirtualHIDKeyboard.kext \
-    build/Release/VirtualHIDPointing.kext \
-    build/Release/VirtualHIDManager.kext \
-    ; do
-    cp -R $d $targetdir
+for kext in VirtualHIDKeyboard.kext VirtualHIDManager.kext VirtualHIDPointing.kext; do
+    cp -R "build/Release/$kext" $targetdir
 done
 
 bash ../../../scripts/codesign.sh $targetdir
 sudo chown -R root:wheel $targetdir
 
-sudo rm -rf /Library/Extensions/org.pqrs.driver.VirtualHIDKeyboard.kext
-sudo rm -rf /Library/Extensions/org.pqrs.driver.VirtualHIDPointing.kext
-
-sudo mv $targetdir/VirtualHIDKeyboard.kext /Library/Extensions/org.pqrs.driver.VirtualHIDKeyboard.kext
-sudo mv $targetdir/VirtualHIDPointing.kext /Library/Extensions/org.pqrs.driver.VirtualHIDPointing.kext
+for kext in VirtualHIDKeyboard.kext VirtualHIDManager.kext VirtualHIDPointing.kext; do
+    sudo rm -rf "/Library/Extensions/org.pqrs.driver.$kext"
+    sudo mv "$targetdir/$kext" "/Library/Extensions/org.pqrs.driver.$kext"
+done
 
 sudo kextload /Library/Extensions/org.pqrs.driver.VirtualHIDKeyboard.kext
 sudo kextload /Library/Extensions/org.pqrs.driver.VirtualHIDPointing.kext
-sudo kextload $targetdir/VirtualHIDManager.kext
+sudo kextload /Library/Extensions/org.pqrs.driver.VirtualHIDManager.kext
