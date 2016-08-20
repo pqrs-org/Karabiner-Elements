3880538ced0fe372d298d83b9fbdb78b41f2e3d0
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-20 18:30:08 +0900

    move files

--- src/userspace/console_user_server/main.cpp
+++ src/userspace/console_user_server/main.cpp
@@ -1,8 +1,12 @@
 #include "connection_manager.hpp"
+#include "karabiner_version.h"
+#include "logger.hpp"
 
 int main(int argc, const char* argv[]) {
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
+
+  logger::get_logger().info("version {0}", karabiner_version);
 
   connection_manager manager;
 
