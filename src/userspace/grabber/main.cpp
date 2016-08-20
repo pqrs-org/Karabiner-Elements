62b606341d8af213b64ed2c9eef6f049fae625b1
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-20 18:22:53 +0900

    move files

--- src/userspace/grabber/main.cpp
+++ src/userspace/grabber/main.cpp
@@ -1,13 +1,11 @@
 #include "grabber.hpp"
+#include "karabiner_version.h"
 
 int main(int argc, const char* argv[]) {
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
 
-  const char* version = "version "
-#include "version.hpp"
-      ;
-  logger::get_logger().info(version);
+  logger::get_logger().info("version {0}", karabiner_version);
 
   userspace_connection_manager connection_manager;
 
