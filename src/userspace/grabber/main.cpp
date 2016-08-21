8d41319c2b708819adf3af183138742c3f3a6300
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-21 12:52:13 +0900

    check uid

--- src/userspace/grabber/main.cpp
+++ src/userspace/grabber/main.cpp
@@ -1,7 +1,14 @@
 #include "grabber.hpp"
 #include "karabiner_version.h"
+#include <iostream>
+#include <unistd.h>
 
 int main(int argc, const char* argv[]) {
+  if (getuid() != 0) {
+    std::cerr << "fatal: karabiner_grabber requires root privilege." << std::endl;
+    exit(1);
+  }
+
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
 
@@ -13,7 +20,7 @@ int main(int argc, const char* argv[]) {
   system("/sbin/kextload /Library/Extensions/org.pqrs.driver.VirtualHIDManager.kext");
 
   userspace_connection_manager connection_manager;
-
   CFRunLoopRun();
+
   return 0;
 }
