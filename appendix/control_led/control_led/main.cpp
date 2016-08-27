5f96b15f8b127c8963d24495762c8cfcf91ca145
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-27 22:38:33 +0900

    add control_led

---
+++ appendix/control_led/control_led/main.cpp
@@ -0,0 +1,162 @@
+#include "boost_defs.hpp"
+
+#include "human_interface_device.hpp"
+#include "iokit_utility.hpp"
+#include <CoreFoundation/CoreFoundation.h>
+#include <IOKit/IOKitLib.h>
+#include <IOKit/hid/IOHIDDevice.h>
+#include <IOKit/hid/IOHIDElement.h>
+#include <IOKit/hid/IOHIDManager.h>
+#include <IOKit/hid/IOHIDQueue.h>
+#include <IOKit/hid/IOHIDValue.h>
+#include <IOKit/hidsystem/IOHIDShared.h>
+#include <IOKit/hidsystem/ev_keymap.h>
+#include <boost/bind.hpp>
+#include <iostream>
+#include <mach/mach_time.h>
+
+class logger final {
+public:
+  static spdlog::logger& get_logger(void) {
+    static std::shared_ptr<spdlog::logger> logger;
+    if (!logger) {
+      logger = spdlog::stdout_logger_mt("control_led", true);
+    }
+    return *logger;
+  }
+};
+
+class control_led final {
+public:
+  control_led(void) {
+    manager_ = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
+    if (!manager_) {
+      return;
+    }
+
+    auto device_matching_dictionaries = iokit_utility::create_device_matching_dictionaries({
+        std::make_pair(kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard),
+    });
+    if (device_matching_dictionaries) {
+      IOHIDManagerSetDeviceMatchingMultiple(manager_, device_matching_dictionaries);
+      CFRelease(device_matching_dictionaries);
+
+      IOHIDManagerRegisterDeviceMatchingCallback(manager_, static_device_matching_callback, this);
+      IOHIDManagerRegisterDeviceRemovalCallback(manager_, static_device_removal_callback, this);
+
+      IOHIDManagerScheduleWithRunLoop(manager_, CFRunLoopGetMain(), kCFRunLoopDefaultMode);
+    }
+  }
+
+private:
+  static void static_device_matching_callback(void* _Nullable context, IOReturn result, void* _Nullable sender, IOHIDDeviceRef _Nonnull device) {
+    if (result != kIOReturnSuccess || !device) {
+      return;
+    }
+
+    auto self = static_cast<control_led*>(context);
+    if (!self) {
+      return;
+    }
+
+    self->device_matching_callback(device);
+  }
+
+  void device_matching_callback(IOHIDDeviceRef _Nonnull device) {
+    if (!device) {
+      return;
+    }
+
+    hids_[device] = std::make_unique<human_interface_device>(logger::get_logger(), device);
+    auto& dev = hids_[device];
+
+    logger::get_logger().info("matching device: "
+                              "manufacturer:{1}, "
+                              "product:{2}, "
+                              "vendor_id:0x{3:x}, "
+                              "product_id:0x{4:x}, "
+                              "location_id:0x{5:x}, "
+                              "serial_number:{6} "
+                              "@ {0}",
+                              __PRETTY_FUNCTION__,
+                              dev->get_manufacturer(),
+                              dev->get_product(),
+                              dev->get_vendor_id(),
+                              dev->get_product_id(),
+                              dev->get_location_id(),
+                              dev->get_serial_number_string());
+
+    dev->open();
+    dev->schedule();
+
+    auto caps_lock_led_state = dev->get_caps_lock_led_state();
+    switch (caps_lock_led_state) {
+      case krbn::led_state::none:
+        logger::get_logger().info("failed to get caps_lock_led_state.");
+        break;
+      case krbn::led_state::on:
+        logger::get_logger().info("caps_lock_led_state is on.");
+        break;
+      case krbn::led_state::off:
+        logger::get_logger().info("caps_lock_led_state is off.");
+        break;
+    }
+
+    // toggle led by escape
+    if (caps_lock_led_state == krbn::led_state::on) {
+      dev->set_caps_lock_led_state(krbn::led_state::off);
+    } else {
+      dev->set_caps_lock_led_state(krbn::led_state::on);
+    }
+  }
+
+  static void static_device_removal_callback(void* _Nullable context, IOReturn result, void* _Nullable sender, IOHIDDeviceRef _Nonnull device) {
+    if (result != kIOReturnSuccess || !device) {
+      return;
+    }
+
+    auto self = static_cast<control_led*>(context);
+    if (!self) {
+      return;
+    }
+
+    self->device_removal_callback(device);
+  }
+
+  void device_removal_callback(IOHIDDeviceRef _Nonnull device) {
+    if (!device) {
+      return;
+    }
+
+    auto it = hids_.find(device);
+    if (it != hids_.end()) {
+      auto& dev = it->second;
+      if (dev) {
+        logger::get_logger().info("removal device: "
+                                  "vendor_id:0x{1:x}, "
+                                  "product_id:0x{2:x}, "
+                                  "location_id:0x{3:x} "
+                                  "@ {0}",
+                                  __PRETTY_FUNCTION__,
+                                  dev->get_vendor_id(),
+                                  dev->get_product_id(),
+                                  dev->get_location_id());
+
+        hids_.erase(it);
+      }
+    }
+  }
+
+  IOHIDManagerRef _Nullable manager_;
+  std::unordered_map<IOHIDDeviceRef, std::unique_ptr<human_interface_device>> hids_;
+};
+
+int main(int argc, const char* argv[]) {
+  if (getuid() != 0) {
+    logger::get_logger().error("control_led requires root privilege.");
+  }
+
+  control_led d;
+  CFRunLoopRun();
+  return 0;
+}
