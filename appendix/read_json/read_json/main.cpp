eb91ec6574c2d284fa9351afa407e3ad8687f136
Takayama Fumihiko <tekezo@pqrs.org>
2016-08-31 13:12:22 +0900

    add #pragma clang diagnostic ignored "-Wshorten-64-to-32"

--- appendix/read_json/read_json/main.cpp
+++ appendix/read_json/read_json/main.cpp
@@ -2,6 +2,7 @@
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wshadow"
+#pragma clang diagnostic ignored "-Wshorten-64-to-32"
 #include <boost/property_tree/json_parser.hpp>
 #pragma clang diagnostic pop
 
