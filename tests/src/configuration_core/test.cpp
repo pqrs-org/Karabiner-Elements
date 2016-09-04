615a8fe04bb3d9c650a231bd7e9f2aa73427b493
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-05 02:06:06 +0900

    add tests

---
+++ tests/src/configuration_core/test.cpp
@@ -0,0 +1,43 @@
+#define CATCH_CONFIG_MAIN
+#include "../../vendor/catch/catch.hpp"
+
+#include "configuration_core.hpp"
+#include <iostream>
+#include <spdlog/spdlog.h>
+
+class logger final {
+public:
+  static spdlog::logger& get_logger(void) {
+    static std::shared_ptr<spdlog::logger> logger;
+    if (!logger) {
+      logger = spdlog::stdout_logger_mt("configuration_core", true);
+    }
+    return *logger;
+  }
+};
+
+TEST_CASE("valid") {
+  configuration_core configuration(logger::get_logger(), "json/example.json");
+
+  std::vector<std::pair<krbn::key_code, krbn::key_code>> expected{
+      std::make_pair(krbn::key_code(57), krbn::key_code(42)),
+      std::make_pair(krbn::key_code(41), krbn::key_code(44)),
+  };
+  REQUIRE(configuration.get_current_profile_simple_modifications() == expected);
+}
+
+TEST_CASE("broken.json") {
+  configuration_core configuration(logger::get_logger(), "json/broken.json");
+
+  std::vector<std::pair<krbn::key_code, krbn::key_code>> expected;
+  REQUIRE(configuration.get_current_profile_simple_modifications() == expected);
+}
+
+TEST_CASE("invalid_key_code_name.json") {
+  configuration_core configuration(logger::get_logger(), "json/invalid_key_code_name.json");
+
+  std::vector<std::pair<krbn::key_code, krbn::key_code>> expected{
+      std::make_pair(krbn::key_code(41), krbn::key_code(44)),
+  };
+  REQUIRE(configuration.get_current_profile_simple_modifications() == expected);
+}
---
+++ tests/vendor/README.md
@@ -0,0 +1,5 @@
+# Files
+
+* catch
+  * version: v1.5.6
+  * https://github.com/philsquared/Catch
