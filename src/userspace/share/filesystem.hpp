58ec8c8fcf76b777155e806dd4fb2331ea7a2b31
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-06 02:23:10 +0900

    add filesystem::normalize

--- src/userspace/share/filesystem.hpp
+++ src/userspace/share/filesystem.hpp
@@ -13,12 +13,149 @@ public:
     return false;
   }
 
-  static std::string realpath(const std::string& path) {
-    std::string result;
-    if (auto p = ::realpath(path.c_str(), nullptr)) {
-      result = p;
-      free(p);
+  static std::string dirname(const std::string& path) {
+    size_t pos = get_dirname_position(path);
+    if (pos == 0) {
+      return ".";
     }
-    return result;
+    return path.substr(0, pos);
+  }
+
+  static void normalize(std::string& path) {
+    if (path.empty()) {
+      path += '.';
+      return;
+    }
+
+    size_t end = path.size();
+    size_t dest = 1;
+    for (size_t src = 1; src < end; ++src) {
+      // Skip multiple slashes.
+      if (path[dest - 1] == '/' && path[src] == '/') {
+        continue;
+      }
+
+      // Handling . and ..
+      if (path[src] == '/') {
+        dest = process_dot(path, dest);
+        dest = process_dotdot(path, dest);
+
+        if (dest == 0) {
+          if (path[0] != '/') {
+            continue;
+          }
+        } else {
+          if (path[dest - 1] == '/') {
+            continue;
+          }
+        }
+      }
+
+      if (dest != src) {
+        path[dest] = path[src];
+      }
+
+      ++dest;
+    }
+
+    dest = process_dot(path, dest);
+    dest = process_dotdot(path, dest);
+
+    if (dest == 0) {
+      path[0] = '.';
+      dest = 1;
+    }
+
+    path.resize(dest);
+  }
+
+private:
+  static size_t get_dirname_position(const std::string& path, size_t pos = std::string::npos) {
+    if (path.empty()) return 0;
+
+    if (pos == std::string::npos) {
+      pos = path.size() - 1;
+    }
+
+    if (path.size() <= pos) return 0;
+
+    if (pos == 0) {
+      // We retain the first slash for dirname("/") == "/".
+      if (path[pos] == '/') {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+
+    if (path[pos] == '/') {
+      --pos;
+    }
+
+    size_t i = path.rfind('/', pos);
+    if (i == std::string::npos) {
+      return 0;
+    }
+    if (i == 0) {
+      // path starts with "/".
+      return 1;
+    }
+    return i;
+  }
+
+  static size_t process_dot(const std::string& path, size_t pos) {
+    if (path.empty()) return pos;
+
+    // foo/bar/./
+    //          ^
+    //         pos
+    //
+    if (pos > 2 &&
+        path[pos - 2] == '/' &&
+        path[pos - 1] == '.') {
+      return pos - 2;
+    }
+
+    // ./foo/bar
+    //  ^
+    //  pos
+    //
+    if (pos == 1 &&
+        path[0] == '.') {
+      return 0;
+    }
+
+    return pos;
+  }
+
+  static size_t process_dotdot(const std::string& path, size_t pos) {
+    // Ignore ../../
+    if (pos > 4 &&
+        path[pos - 5] == '.' &&
+        path[pos - 4] == '.' &&
+        path[pos - 3] == '/' &&
+        path[pos - 2] == '.' &&
+        path[pos - 1] == '.') {
+      return pos;
+    }
+
+    // foo/bar/../
+    //           ^
+    //          pos
+    //
+    if (pos > 2 &&
+        path[pos - 3] == '/' &&
+        path[pos - 2] == '.' &&
+        path[pos - 1] == '.') {
+      pos = get_dirname_position(path, pos - 3);
+
+      // foo/bar/../
+      //    ^
+      //   pos
+
+      return pos;
+    }
+
+    return pos;
   }
 };
---
+++ tests/src/filesystem/test.cpp
@@ -0,0 +1,124 @@
+#define CATCH_CONFIG_MAIN
+#include "../../vendor/catch/catch.hpp"
+
+#include <ostream>
+
+#include "filesystem.hpp"
+
+TEST_CASE("dirname") {
+  REQUIRE(filesystem::dirname("/usr/bin/ls") == "/usr/bin");
+  REQUIRE(filesystem::dirname("/usr/bin/ls/") == "/usr/bin");
+  REQUIRE(filesystem::dirname("/usr/bin/l") == "/usr/bin");
+  REQUIRE(filesystem::dirname("/usr/bin/l/") == "/usr/bin");
+  REQUIRE(filesystem::dirname("/usr") == "/");
+  REQUIRE(filesystem::dirname("/") == "/");
+  REQUIRE(filesystem::dirname("usr/bin/ls") == "usr/bin");
+  REQUIRE(filesystem::dirname("usr/bin/") == "usr");
+  REQUIRE(filesystem::dirname("usr") == ".");
+  REQUIRE(filesystem::dirname("usr/") == ".");
+  REQUIRE(filesystem::dirname("") == ".");
+}
+
+TEST_CASE("normalize") {
+  std::string file_path;
+
+  file_path = "";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == ".");
+
+  file_path = ".";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == ".");
+
+  file_path = "./";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == ".");
+
+  file_path = "..";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "..");
+
+  file_path = "../";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../");
+
+  file_path = "..//foo";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../foo");
+
+  file_path = "abcde";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "abcde");
+
+  file_path = "abcde/";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "abcde/");
+
+  file_path = "/foo//bar/../baz";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "/foo/baz");
+
+  file_path = "/../foo//bar/../baz";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "/foo/baz");
+
+  file_path = "foo/../bar";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "bar");
+
+  file_path = "foo/.../bar";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "foo/.../bar");
+
+  file_path = "a/../b/../c/d";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "c/d");
+
+  file_path = "a/./b/./c/d";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "a/b/c/d");
+
+  file_path = "foo/bar/..";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "foo");
+
+  file_path = "foo/bar/../";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "foo/");
+
+  file_path = "foo/bar/.";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "foo/bar");
+
+  file_path = "foo/bar/./";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "foo/bar/");
+
+  file_path = "../foo/bar";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../foo/bar");
+
+  file_path = "../../../foo/bar";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../../../foo/bar");
+
+  file_path = "./foo/bar";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "foo/bar");
+
+  file_path = "../foo/bar/..";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../foo");
+
+  file_path = "../foo/bar///...";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../foo/bar/...");
+
+  file_path = "../a/b/../c/../d///..";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../a");
+
+  file_path = "a/../..////../b/c";
+  filesystem::normalize(file_path);
+  REQUIRE(file_path == "../../b/c");
+}
