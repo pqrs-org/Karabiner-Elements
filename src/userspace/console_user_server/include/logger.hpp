6afc025eaac9bb22577e8070971c704a9dbe9d44
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-06 01:57:38 +0900

    change log location

--- src/userspace/console_user_server/include/logger.hpp
+++ src/userspace/console_user_server/include/logger.hpp
@@ -18,15 +18,9 @@ public:
         log_directory += "/log/";
         mkdir(log_directory.c_str(), 0700);
 
-        static char hostname[_POSIX_HOST_NAME_MAX + 1];
-        if (gethostname(hostname, sizeof(hostname)) == 0) {
-          log_directory += hostname;
-          mkdir(log_directory.c_str(), 0700);
-
-          if (filesystem::is_directory(log_directory)) {
-            std::string log_file_path = log_directory + "/karabiner_console_user_server_log";
-            logger = spdlog::rotating_logger_mt("karabiner_console_user_server", log_file_path.c_str(), 5 * 1024 * 1024, 3, true);
-          }
+        if (filesystem::is_directory(log_directory)) {
+          std::string log_file_path = log_directory + "/karabiner_console_user_server_log";
+          logger = spdlog::rotating_logger_mt("karabiner_console_user_server", log_file_path.c_str(), 5 * 1024 * 1024, 3, true);
         }
       }
 
