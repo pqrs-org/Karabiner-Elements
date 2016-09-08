13477939c8aee69ea30f1f96854b8e2e39cff17c
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-08 17:17:13 +0900

    call launchctl in PreferencesWindowController

--- src/userspace/apps/PreferencesWindow/PreferencesWindow/PreferencesWindowController.m
+++ src/userspace/apps/PreferencesWindow/PreferencesWindow/PreferencesWindowController.m
@@ -3,8 +3,8 @@
 
 @interface PreferencesWindowController ()
 
-@property (weak) IBOutlet LogFileTextViewController* grabberLogFileTextViewController;
-@property (weak) IBOutlet LogFileTextViewController* consoleUserServerLogFileTextViewController;
+@property(weak) IBOutlet LogFileTextViewController* grabberLogFileTextViewController;
+@property(weak) IBOutlet LogFileTextViewController* consoleUserServerLogFileTextViewController;
 
 @end
 
@@ -13,6 +13,8 @@
 - (void)setup {
   [self.grabberLogFileTextViewController monitor:@"/var/log/karabiner/grabber_log.txt"];
   [self.consoleUserServerLogFileTextViewController monitor:[NSString stringWithFormat:@"%@/.karabiner.d/log/console_user_server_log.txt", NSHomeDirectory()]];
+
+  [self launchctlConsoleUserServer:YES];
 }
 
 - (void)show {
@@ -27,6 +29,28 @@
 
 - (IBAction)openURL:(id)sender {
   [[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:[sender title]]];
+}
+
+- (IBAction)quitWithConfirmation:(id)sender {
+  NSAlert* alert = [NSAlert new];
+  alert.messageText = @"Are you sure you want to quit Karabiner-Elements?";
+  alert.informativeText = @"The changed key will be restored after Karabiner-Elements is quit.";
+  [alert addButtonWithTitle:@"Quit"];
+  [alert addButtonWithTitle:@"Cancel"];
+  if ([alert runModal] == NSAlertFirstButtonReturn) {
+    [self launchctlConsoleUserServer:NO];
+    [NSApp terminate:nil];
+  }
+}
+
+- (void)launchctlConsoleUserServer:(BOOL)load {
+  NSTask* task = [NSTask launchedTaskWithLaunchPath:@"/bin/launchctl"
+                                          arguments:@[
+                                            load ? @"load" : @"unload",
+                                            @"-w",
+                                            @"/Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist",
+                                          ]];
+  [task waitUntilExit];
 }
 
 @end
