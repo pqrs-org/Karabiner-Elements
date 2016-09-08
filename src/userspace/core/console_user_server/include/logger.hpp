bd28d116b589ee4767e2e30b96433c276901c127
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-08 16:55:45 +0900

    change log file name

--- src/userspace/core/console_user_server/include/logger.hpp
+++ src/userspace/core/console_user_server/include/logger.hpp
@@ -19,14 +19,14 @@ public:
         mkdir(log_directory.c_str(), 0700);
 
         if (filesystem::is_directory(log_directory)) {
-          std::string log_file_path = log_directory + "/karabiner_console_user_server_log";
-          logger = spdlog::rotating_logger_mt("karabiner_console_user_server", log_file_path.c_str(), 1024 * 1024, 3, true);
+          std::string log_file_path = log_directory + "/console_user_server_log";
+          logger = spdlog::rotating_logger_mt("console_user_server", log_file_path.c_str(), 1024 * 1024, 3, true);
         }
       }
 
       if (!logger) {
         // fallback
-        logger = spdlog::stdout_logger_mt("karabiner_console_user_server", true);
+        logger = spdlog::stdout_logger_mt("console_user_server", true);
       }
     }
     return *logger;
--- src/userspace/core/grabber/include/logger.hpp
+++ src/userspace/core/grabber/include/logger.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "filesystem.hpp"
 #include <memory>
 #include <spdlog/spdlog.h>
 
@@ -8,11 +9,17 @@ public:
   static spdlog::logger& get_logger(void) {
     static std::shared_ptr<spdlog::logger> logger;
     if (!logger) {
-#if 1
-      logger = spdlog::rotating_logger_mt("karabiner_grabber", "/var/log/karabiner_grabber_log", 1024 * 1024, 3, true);
-#else
-      logger = spdlog::stdout_logger_mt("karabiner_grabber", true);
-#endif
+      auto log_directory = "/var/log/karabiner";
+      mkdir(log_directory, 0755);
+
+      if (filesystem::is_directory(log_directory)) {
+        logger = spdlog::rotating_logger_mt("grabber", "/var/log/karabiner/grabber_log", 1024 * 1024, 3, true);
+      }
+
+      if (!logger) {
+        // fallback
+        logger = spdlog::stdout_logger_mt("grabber", true);
+      }
     }
     return *logger;
   }
