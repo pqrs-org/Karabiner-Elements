9d6c4f6aef26ff087ada5dc8ae8cf9beaa707531
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-13 01:48:22 +0900

    add libkrbn_get_distributed_notification_grabber_is_launched

--- src/apps/lib/libkrbn/libkrbn.cpp
+++ src/apps/lib/libkrbn/libkrbn.cpp
@@ -15,7 +15,8 @@ public:
       }
     }
 
-    v.clear();
+    v.resize(1);
+    v[0] = '\0';
     return false;
   }
 };
@@ -30,6 +31,21 @@ const char* libkrbn_get_distributed_notification_observed_object(void) {
   if (!once) {
     once = true;
     libkrbn::cfstring_to_cstring(result, constants::get_distributed_notification_observed_object());
+  }
+
+  return &(result[0]);
+}
+
+const char* libkrbn_get_distributed_notification_grabber_is_launched(void) {
+  static std::mutex mutex;
+  static bool once = false;
+  static std::vector<char> result;
+
+  std::lock_guard<std::mutex> guard(mutex);
+
+  if (!once) {
+    once = true;
+    libkrbn::cfstring_to_cstring(result, constants::get_distributed_notification_grabber_is_launched());
   }
 
   return &(result[0]);
--- src/apps/lib/libkrbn/libkrbn.h
+++ src/apps/lib/libkrbn/libkrbn.h
@@ -5,6 +5,7 @@ extern "C" {
 #endif
 
 const char* libkrbn_get_distributed_notification_observed_object(void);
+const char* libkrbn_get_distributed_notification_grabber_is_launched(void);
 const char* libkrbn_get_distributed_notification_console_user_socket_directory_is_ready(void);
 
 #ifdef __cplusplus
