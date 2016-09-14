215ba2a7ddb3cf2ca35fb4c5408f15f492921e86
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-14 12:52:12 +0900

    setuid in event_dispatcher

--- src/core/event_dispatcher/main.cpp
+++ src/core/event_dispatcher/main.cpp
@@ -1,12 +1,31 @@
+#include "constants.hpp"
 #include "karabiner_version.h"
 #include "logger.hpp"
-#include "constants.hpp"
+#include "session.hpp"
 
 int main(int argc, const char* argv[]) {
+  logger::get_logger().info("version {0}", karabiner_version);
+
+  // ----------------------------------------
+  if (getuid() != 0) {
+    logger::get_logger().error("karabiner_event_dispatcher requires root privilege.");
+    return 1;
+  }
+
+  auto uid = session::get_current_console_user_id();
+  if (!uid) {
+    logger::get_logger().error("session::get_current_console_user_id() error @ {0}", __PRETTY_FUNCTION__);
+    return 1;
+  }
+
+  if (setuid(*uid) != 0) {
+    logger::get_logger().error("setuid error @ {0}", __PRETTY_FUNCTION__);
+    return 1;
+  }
+
+  // ----------------------------------------
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
-
-  logger::get_logger().info("version {0}", karabiner_version);
 
   CFRunLoopRun();
   return 0;
