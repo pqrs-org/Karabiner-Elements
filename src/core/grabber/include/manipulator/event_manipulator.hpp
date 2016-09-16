0d347758655a1630bda5c655f2193b29ea9ea41b
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-17 00:32:44 +0900

    add manipulated_key

--- src/core/grabber/include/manipulator/event_manipulator.hpp
+++ src/core/grabber/include/manipulator/event_manipulator.hpp
@@ -7,6 +7,7 @@
 #include "types.hpp"
 #include <IOKit/hidsystem/ev_keymap.h>
 #include <boost/optional.hpp>
+#include <list>
 #include <thread>
 #include <unordered_map>
 
@@ -46,7 +47,7 @@ public:
   void post_modifier_flags(krbn::key_code key_code, IOOptionBits flags) {
     stop_key_repeat();
     if (auto mac_key = krbn::types::get_mac_key(key_code)) {
-//      hid_system_client_.post_modifier_flags(*mac_key, flags);
+      //      hid_system_client_.post_modifier_flags(*mac_key, flags);
     } else {
       logger::get_logger().error("unsupported key_code {1:#x} @ {0}", __PRETTY_FUNCTION__, static_cast<uint32_t>(key_code));
     }
@@ -93,30 +94,30 @@ public:
     }
 
     // ----------------------------------------
-//    uint8_t new_key_code = 0;
-//    boost::optional<hid_system_client::post_key_type> post_key_type;
+    //    uint8_t new_key_code = 0;
+    //    boost::optional<hid_system_client::post_key_type> post_key_type;
 
     {
       auto& map = krbn::types::get_mac_aux_control_button_map();
       auto it = map.find(key_code);
       if (it != map.end()) {
-//        new_key_code = it->second;
-//        post_key_type = hid_system_client::post_key_type::aux_control_button;
+        //        new_key_code = it->second;
+        //        post_key_type = hid_system_client::post_key_type::aux_control_button;
       }
     }
     {
       auto& map = krbn::types::get_mac_key_map();
       auto it = map.find(key_code);
       if (it != map.end()) {
-//        new_key_code = it->second;
-//        post_key_type = hid_system_client::post_key_type::key;
+        //        new_key_code = it->second;
+        //        post_key_type = hid_system_client::post_key_type::key;
       }
     }
 
-//    if (!post_key_type) {
-//      logger::get_logger().warn("key_code:{1:#x} is unsupported key @ {0}", __PRETTY_FUNCTION__, static_cast<uint32_t>(key_code));
-//      return;
-//    }
+    //    if (!post_key_type) {
+    //      logger::get_logger().warn("key_code:{1:#x} is unsupported key @ {0}", __PRETTY_FUNCTION__, static_cast<uint32_t>(key_code));
+    //      return;
+    //    }
 
     auto initial_key_repeat_milliseconds = system_preferences::get_initial_key_repeat_milliseconds();
     auto key_repeat_milliseconds = system_preferences::get_key_repeat_milliseconds();
@@ -131,7 +132,7 @@ public:
     }
 
     // post key
-//    hid_system_client_.post_key(*post_key_type, new_key_code, event_type, flags, false);
+    //    hid_system_client_.post_key(*post_key_type, new_key_code, event_type, flags, false);
 
     // set key repeat
     if (event_type == krbn::event_type::key_down) {
@@ -142,7 +143,7 @@ public:
       }
 
       if (repeat_target) {
-        key_repeat_timer_ = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, DISPATCH_TIMER_STRICT, dispatch_get_main_queue());
+        key_repeat_timer_ = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, DISPATCH_TIMER_STRICT, key_repeat_queue_);
         if (!key_repeat_timer_) {
           logger::get_logger().error("failed to dispatch_source_create");
         } else {
@@ -151,8 +152,8 @@ public:
                                     key_repeat_milliseconds * NSEC_PER_MSEC,
                                     0);
           dispatch_source_set_event_handler(key_repeat_timer_, ^{
-//            hid_system_client_.post_key(*post_key_type, new_key_code, event_type, flags, true);
-          });
+                                                //            hid_system_client_.post_key(*post_key_type, new_key_code, event_type, flags, true);
+                                            });
           dispatch_resume(key_repeat_timer_);
           key_repeat_key_code_ = key_code;
         }
@@ -161,10 +162,31 @@ public:
   }
 
 private:
-  dispatch_queue_t key_repeat_queue_;
+  class manipulated_key final {
+  public:
+    manipulated_key(uint64_t device_registry_entry_id,
+                    krbn::key_code from_key_code,
+                    krbn::key_code to_key_code) : device_registry_entry_id_(device_registry_entry_id),
+                                                  from_key_code_(from_key_code),
+                                                  to_key_code_(to_key_code) {
+    }
 
+    uint64_t get_device_registry_entry_id(void) const { return device_registry_entry_id_; }
+    krbn::key_code get_from_key_code(void) const { return from_key_code_; }
+    krbn::key_code get_to_key_code(void) const { return to_key_code_; }
+
+  private:
+    uint64_t device_registry_entry_id_;
+    krbn::key_code from_key_code_;
+    krbn::key_code to_key_code_;
+  };
+
+  dispatch_queue_t key_repeat_queue_;
   dispatch_source_t key_repeat_timer_;
+
   boost::optional<krbn::key_code> key_repeat_key_code_;
   std::unordered_map<krbn::key_code, krbn::key_code> fn_function_keys_;
   std::mutex fn_function_keys_mutex_;
+
+  std::list<manipulated_key> manipulated_keys_;
 };
