6a1ab61b312f1a7fa2c4569a8d6d9982309e9aff
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-16 11:32:26 +0900

    check root privilege at first

--- src/core/event_dispatcher/main.cpp
+++ src/core/event_dispatcher/main.cpp
@@ -8,12 +8,18 @@
 #include <memory>
 
 int main(int argc, const char* argv[]) {
-  logger::get_logger().info("version {0}", karabiner_version);
+  if (getuid() != 0) {
+    std::cerr << "fatal: karabiner_event_dispatcher requires root privilege." << std::endl;
+    exit(1);
+  }
 
   // ----------------------------------------
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
 
+  logger::get_logger().info("version {0}", karabiner_version);
+
+  // ----------------------------------------
   std::unique_ptr<receiver> r;
 
   while (true) {
@@ -26,11 +32,6 @@ int main(int argc, const char* argv[]) {
   }
 
   // ----------------------------------------
-  if (getuid() != 0) {
-    logger::get_logger().error("karabiner_event_dispatcher requires root privilege.");
-    return 1;
-  }
-
   auto uid = session::get_current_console_user_id();
   if (!uid) {
     logger::get_logger().error("session::get_current_console_user_id() error @ {0}", __PRETTY_FUNCTION__);
@@ -42,6 +43,7 @@ int main(int argc, const char* argv[]) {
     return 1;
   }
 
+  // ----------------------------------------
   CFRunLoopRun();
   return 0;
 }
