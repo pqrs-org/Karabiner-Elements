8bba85281acee30abbf3c05fcbff672f14486eb5
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-18 09:39:48 +0900

    remove keyboard_event_output_manager

--- src/core/console_user_server/include/configuration_manager.hpp
+++ src/core/console_user_server/include/configuration_manager.hpp
@@ -3,7 +3,6 @@
 #include "configuration_core.hpp"
 #include "filesystem.hpp"
 #include "grabber_client.hpp"
-#include "keyboard_event_output_manager.hpp"
 #include <CoreServices/CoreServices.h>
 #include <memory>
 
@@ -13,15 +12,13 @@ public:
 
   configuration_manager(spdlog::logger& logger,
                         const std::string& configuration_directory,
-                        grabber_client& grabber_client,
-                        keyboard_event_output_manager& keyboard_event_output_manager) : logger_(logger),
-                                                                                        configuration_directory_(configuration_directory),
-                                                                                        configuration_core_file_path_(configuration_directory_ + "/karabiner.json"),
-                                                                                        grabber_client_(grabber_client),
-                                                                                        keyboard_event_output_manager_(keyboard_event_output_manager),
-                                                                                        path_(nullptr),
-                                                                                        paths_(nullptr),
-                                                                                        stream_(nullptr) {
+                        grabber_client& grabber_client) : logger_(logger),
+                                                          configuration_directory_(configuration_directory),
+                                                          configuration_core_file_path_(configuration_directory_ + "/karabiner.json"),
+                                                          grabber_client_(grabber_client),
+                                                          path_(nullptr),
+                                                          paths_(nullptr),
+                                                          stream_(nullptr) {
     filesystem::normalize_file_path(configuration_directory_);
     filesystem::normalize_file_path(configuration_core_file_path_);
 
@@ -150,9 +147,9 @@ private:
       grabber_client_.add_simple_modification(pair.first, pair.second);
     }
 
-    keyboard_event_output_manager_.clear_fn_function_keys();
+    grabber_client_.clear_fn_function_keys();
     for (const auto& pair : configuration_core_->get_current_profile_fn_function_keys()) {
-      keyboard_event_output_manager_.add_fn_function_key(pair.first, pair.second);
+      grabber_client_.add_fn_function_key(pair.first, pair.second);
     }
   }
 
@@ -160,7 +157,6 @@ private:
   std::string configuration_directory_;
   std::string configuration_core_file_path_;
   grabber_client& grabber_client_;
-  keyboard_event_output_manager& keyboard_event_output_manager_;
 
   CFStringRef path_;
   CFArrayRef paths_;
--- src/core/console_user_server/include/receiver.hpp
+++ src/core/console_user_server/include/receiver.hpp
@@ -3,10 +3,8 @@
 #include "configuration_manager.hpp"
 #include "constants.hpp"
 #include "grabber_client.hpp"
-#include "keyboard_event_output_manager.hpp"
 #include "local_datagram_server.hpp"
 #include "logger.hpp"
-#include "process_monitor.hpp"
 #include "system_preferences_monitor.hpp"
 #include "types.hpp"
 #include <vector>
@@ -15,22 +13,11 @@ class receiver final {
 public:
   receiver(const receiver&) = delete;
 
-  receiver(void) : exit_loop_(false) {
-    const size_t buffer_length = 8 * 1024;
-    buffer_.resize(buffer_length);
-  }
+  receiver(void) {}
 
   void start(void) {
-    if (server_) {
-      logger::get_logger().error("receiver is already running");
-      return;
-    }
-
-    keyboard_event_output_manager_.stop_key_repeat();
-
     const char* path = constants::get_console_user_socket_file_path();
     unlink(path);
-    server_ = std::make_unique<local_datagram_server>(path);
     chmod(path, 0600);
 
     grabber_client_ = std::make_unique<grabber_client>();
@@ -42,11 +29,7 @@ public:
 
     configuration_manager_ = std::make_unique<configuration_manager>(logger::get_logger(),
                                                                      constants::get_configuration_directory(),
-                                                                     *grabber_client_,
-                                                                     keyboard_event_output_manager_);
-
-    exit_loop_ = false;
-    thread_ = std::thread([this] { this->worker(); });
+                                                                     *grabber_client_);
 
     logger::get_logger().info("receiver is started");
   }
@@ -54,78 +37,14 @@ public:
   void stop(void) {
     unlink(constants::get_console_user_socket_file_path());
 
-    if (!thread_.joinable()) {
-      return;
-    }
-    if (!server_) {
-      return;
-    }
-
-    exit_loop_ = true;
-    thread_.join();
-    grabber_process_monitor_.reset(nullptr);
     configuration_manager_.reset(nullptr);
     grabber_client_.reset(nullptr);
     system_preferences_monitor_.reset(nullptr);
-    server_.reset(nullptr);
-
-    keyboard_event_output_manager_.stop_key_repeat();
 
     logger::get_logger().info("receiver is stopped");
   }
 
-  void worker(void) {
-    if (!server_) {
-      return;
-    }
-
-    while (!exit_loop_) {
-      boost::system::error_code ec;
-      std::size_t n = server_->receive(boost::asio::buffer(buffer_), boost::posix_time::seconds(1), ec);
-
-      if (!ec && n > 0) {
-        switch (krbn::operation_type(buffer_[0])) {
-        case krbn::operation_type::connect_ack:
-          if (n != sizeof(krbn::operation_type_connect_ack_struct)) {
-            logger::get_logger().error("invalid size for krbn::operation_type::connect_ack");
-          } else {
-            auto p = reinterpret_cast<krbn::operation_type_connect_ack_struct*>(&(buffer_[0]));
-            logger::get_logger().info("connect_ack karabiner_grabber pid:{0}", p->pid);
-
-            grabber_process_monitor_ = nullptr;
-            grabber_process_monitor_ = std::make_unique<process_monitor>(logger::get_logger(),
-                                                                         p->pid,
-                                                                         std::bind(&receiver::grabber_exit_callback, this));
-          }
-          break;
-
-        case krbn::operation_type::stop_key_repeat:
-          keyboard_event_output_manager_.stop_key_repeat();
-          break;
-
-        case krbn::operation_type::post_key:
-          if (n != sizeof(krbn::operation_type_post_key_struct)) {
-            logger::get_logger().error("invalid size for krbn::operation_type::post_key");
-          } else {
-            auto p = reinterpret_cast<krbn::operation_type_post_key_struct*>(&(buffer_[0]));
-            keyboard_event_output_manager_.post_key(p->key_code, p->event_type, p->flags);
-          }
-          break;
-
-        default:
-          break;
-        }
-      }
-    }
-
-    keyboard_event_output_manager_.stop_key_repeat();
-  }
-
 private:
-  void grabber_exit_callback(void) {
-    keyboard_event_output_manager_.stop_key_repeat();
-  }
-
   void system_preferences_values_updated_callback(const system_preferences::values& values) {
     if (grabber_client_) {
       grabber_client_->system_preferences_values_updated(values);
@@ -133,14 +52,7 @@ private:
   }
 
   std::vector<uint8_t> buffer_;
-  std::unique_ptr<local_datagram_server> server_;
   std::unique_ptr<grabber_client> grabber_client_;
   std::unique_ptr<system_preferences_monitor> system_preferences_monitor_;
   std::unique_ptr<configuration_manager> configuration_manager_;
-  std::thread thread_;
-  std::atomic<bool> exit_loop_;
-
-  keyboard_event_output_manager keyboard_event_output_manager_;
-
-  std::unique_ptr<process_monitor> grabber_process_monitor_;
 };
