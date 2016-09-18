9051e993f5b7ccf5f3531df5eb6fe2c772c6b0ed
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-18 13:52:38 +0900

    use own queue_ in process_monitor

--- src/share/process_monitor.hpp
+++ src/share/process_monitor.hpp
@@ -10,11 +10,9 @@ public:
                   pid_t pid,
                   const exit_callback& exit_callback) : logger_(logger),
                                                         exit_callback_(exit_callback),
+                                                        queue_(dispatch_queue_create(nullptr, nullptr)),
                                                         monitor_(0) {
-    monitor_ = dispatch_source_create(DISPATCH_SOURCE_TYPE_PROC,
-                                      pid,
-                                      DISPATCH_PROC_EXIT,
-                                      dispatch_get_main_queue());
+    monitor_ = dispatch_source_create(DISPATCH_SOURCE_TYPE_PROC, pid, DISPATCH_PROC_EXIT, queue_);
     if (!monitor_) {
       logger_.error("failed to dispatch_source_create @ {0}", __PRETTY_FUNCTION__);
     } else {
@@ -33,6 +31,8 @@ public:
 
   ~process_monitor(void) {
     release();
+
+    dispatch_release(queue_);
   }
 
 private:
@@ -47,5 +47,6 @@ private:
   spdlog::logger& logger_;
   exit_callback exit_callback_;
 
+  dispatch_queue_t queue_;
   dispatch_source_t monitor_;
 };
