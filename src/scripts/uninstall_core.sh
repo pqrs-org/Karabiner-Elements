bf45316aa9d86903162677e238f1bfc28999d507
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-22 01:04:16 +0900

    add uninstall_core.sh

---
+++ src/scripts/uninstall_core.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
+
+# ----------------------------------------
+# unload before install
+
+if [ /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist ]; then
+    launchctl bootout system /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist
+fi
+
+# ----------------------------------------
+# uninstall
+rm -f  '/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist'
+rm -f  '/Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist'
+rm -rf '/Applications/Karabiner-Elements.app'
+rm -rf '/Applications/Karabiner-EventViewer.app'
+rm -rf '/Library/Application Support/org.pqrs/Karabiner-Elements'
+
+exit 0
