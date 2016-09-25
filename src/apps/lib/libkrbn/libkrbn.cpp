086697340d7fedef729679221136823463cdd451
Takayama Fumihiko <tekezo@pqrs.org>
2016-09-25 16:57:34 +0900

    add libkrbn_log_monitor

--- src/apps/lib/libkrbn/libkrbn.cpp
+++ src/apps/lib/libkrbn/libkrbn.cpp
@@ -1,6 +1,7 @@
 #include "libkrbn.h"
 #include "constants.hpp"
 #include "log_monitor.hpp"
+#include <spdlog/spdlog.h>
 #include <thread>
 #include <vector>
 
@@ -19,6 +20,18 @@ public:
     v.resize(1);
     v[0] = '\0';
     return false;
+  }
+
+  static spdlog::logger& get_logger(void) {
+    static std::mutex mutex;
+    static std::shared_ptr<spdlog::logger> logger;
+
+    std::lock_guard<std::mutex> guard(mutex);
+
+    if (!logger) {
+      logger = spdlog::stdout_logger_mt("libkrbn", true);
+    }
+    return *logger;
   }
 };
 
@@ -50,4 +63,78 @@ const char* libkrbn_get_distributed_notification_grabber_is_launched(void) {
   }
 
   return &(result[0]);
+}
+
+class libkrbn_log_monitor_class {
+public:
+  libkrbn_log_monitor_class(const libkrbn_log_monitor_class&) = delete;
+
+  libkrbn_log_monitor_class(libkrbn_log_monitor_callback callback) : callback_(callback) {
+    std::vector<std::string> targets = {
+        "/var/log/karabiner/grabber_log",
+        "/var/log/karabiner/event_dispatcher_log",
+    };
+    if (auto p = constants::get_home_dot_karabiner_directory()) {
+      targets.push_back(std::string(p) + "/log/console_user_server_log");
+    }
+
+    log_monitor_ = std::make_unique<log_monitor>(libkrbn::get_logger(),
+                                                 targets,
+                                                 std::bind(&libkrbn_log_monitor_class::cpp_callback, this, std::placeholders::_1));
+  }
+
+  const std::vector<std::pair<uint64_t, std::string>>& get_initial_lines(void) const {
+    return log_monitor_->get_initial_lines();
+  }
+
+private:
+  void cpp_callback(const std::string& line) {
+    if (callback_) {
+      callback_(line.c_str());
+    }
+  }
+
+  libkrbn_log_monitor_callback callback_;
+
+  std::unique_ptr<log_monitor> log_monitor_;
+};
+
+bool libkrbn_log_monitor_initialize(libkrbn_log_monitor** out, libkrbn_log_monitor_callback callback) {
+  if (!out) return false;
+  // return if already initialized.
+  if (*out) return false;
+
+  *out = reinterpret_cast<libkrbn_log_monitor*>(new libkrbn_log_monitor_class(callback));
+  return true;
+}
+
+void libkrbn_log_monitor_terminate(libkrbn_log_monitor** out) {
+  if (out && *out) {
+    delete reinterpret_cast<libkrbn_log_monitor_class*>(*out);
+    *out = nullptr;
+  }
+}
+
+size_t libkrbn_log_monitor_initial_lines_size(libkrbn_log_monitor* p) {
+  auto log_monitor = reinterpret_cast<libkrbn_log_monitor_class*>(p);
+  if (!log_monitor) {
+    return 0;
+  }
+
+  auto& initial_lines = log_monitor->get_initial_lines();
+  return initial_lines.size();
+}
+
+const char* libkrbn_log_monitor_initial_line(libkrbn_log_monitor* p, size_t index) {
+  auto log_monitor = reinterpret_cast<libkrbn_log_monitor_class*>(p);
+  if (!log_monitor) {
+    return 0;
+  }
+
+  auto& initial_lines = log_monitor->get_initial_lines();
+  if (index >= initial_lines.size()) {
+    return nullptr;
+  }
+
+  return initial_lines[index].second.c_str();
 }
--- src/apps/lib/libkrbn/libkrbn.h
+++ src/apps/lib/libkrbn/libkrbn.h
@@ -1,11 +1,20 @@
 #pragma once
 
+#include <stddef.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 const char* libkrbn_get_distributed_notification_observed_object(void);
 const char* libkrbn_get_distributed_notification_grabber_is_launched(void);
+
+typedef void libkrbn_log_monitor;
+typedef void (*libkrbn_log_monitor_callback)(const char* line);
+bool libkrbn_log_monitor_initialize(libkrbn_log_monitor** out, libkrbn_log_monitor_callback callback);
+void libkrbn_log_monitor_terminate(libkrbn_log_monitor** out);
+size_t libkrbn_log_monitor_initial_lines_size(libkrbn_log_monitor* p);
+const char* libkrbn_log_monitor_initial_line(libkrbn_log_monitor* p, size_t index);
 
 #ifdef __cplusplus
 }
