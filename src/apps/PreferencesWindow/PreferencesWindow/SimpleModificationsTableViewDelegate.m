f3b42e5f6086057b3228aea76886832e2db93f76
Takayama Fumihiko <tekezo@pqrs.org>
2016-10-10 20:25:07 +0900

    use SimpleModificationsTableCellView

--- src/apps/PreferencesWindow/PreferencesWindow/SimpleModificationsTableViewDelegate.m
+++ src/apps/PreferencesWindow/PreferencesWindow/SimpleModificationsTableViewDelegate.m
@@ -1,9 +1,12 @@
 #import "SimpleModificationsTableViewDelegate.h"
 #import "ConfigurationManager.h"
+#import "SimpleModificationsMenuManager.h"
+#import "SimpleModificationsTableCellView.h"
 
 @interface SimpleModificationsTableViewDelegate ()
 
 @property(weak) IBOutlet ConfigurationManager* configurationManager;
+@property(weak) IBOutlet SimpleModificationsMenuManager* simpleModificationsMenuManager;
 
 @end
 
@@ -11,12 +14,14 @@
 
 - (NSView*)tableView:(NSTableView*)tableView viewForTableColumn:(NSTableColumn*)tableColumn row:(NSInteger)row {
   if ([tableColumn.identifier isEqualToString:@"SimpleModificationsFromColumn"]) {
-    NSTableCellView* result = [tableView makeViewWithIdentifier:@"SimpleModificationsFromCellView" owner:self];
+    SimpleModificationsTableCellView* result = [tableView makeViewWithIdentifier:@"SimpleModificationsFromCellView" owner:self];
 
     NSArray<NSDictionary*>* simpleModifications = self.configurationManager.configurationCoreModel.simpleModifications;
     if (0 <= row && row < (NSInteger)(simpleModifications.count)) {
       result.textField.stringValue = simpleModifications[row][@"from"];
     }
+
+    result.popUpButton.menu = [self.simpleModificationsMenuManager.fromMenu copy];
 
     return result;
   }
