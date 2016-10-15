301b147b39a019190fa266538792a3e300c57a40
Takayama Fumihiko <tekezo@pqrs.org>
2016-10-15 16:04:20 +0900

    add cg_event_set_flags

---
+++ appendix/cg_event_set_flags/main.cpp
@@ -0,0 +1,61 @@
+#include <CoreGraphics/CoreGraphics.h>
+#include <iostream>
+
+static CFMachPortRef _Nullable event_tap;
+
+static CGEventRef _Nullable callback(CGEventTapProxy _Nullable proxy, CGEventType type, CGEventRef _Nullable event, void* _Nonnull refcon) {
+  if (type == kCGEventTapDisabledByTimeout) {
+    CGEventTapEnable(event_tap, true);
+    return event;
+  }
+
+  if (type == kCGEventRightMouseDown) {
+    exit(0);
+  }
+
+  if (event) {
+    CGEventSetFlags(event, kCGEventFlagMaskNonCoalesced | kCGEventFlagMaskCommand);
+  }
+  return event;
+}
+
+int main(int argc, const char* argv[]) {
+  // Observe all mouse events
+  auto mask = CGEventMaskBit(kCGEventLeftMouseDown) |
+              CGEventMaskBit(kCGEventLeftMouseUp) |
+              CGEventMaskBit(kCGEventRightMouseDown) |
+              CGEventMaskBit(kCGEventRightMouseUp) |
+              CGEventMaskBit(kCGEventMouseMoved) |
+              CGEventMaskBit(kCGEventLeftMouseDragged) |
+              CGEventMaskBit(kCGEventRightMouseDragged) |
+              CGEventMaskBit(kCGEventScrollWheel) |
+              CGEventMaskBit(kCGEventTabletPointer) |
+              CGEventMaskBit(kCGEventTabletProximity) |
+              CGEventMaskBit(kCGEventOtherMouseDown) |
+              CGEventMaskBit(kCGEventOtherMouseUp) |
+              CGEventMaskBit(kCGEventOtherMouseDragged);
+
+  event_tap = CGEventTapCreate(kCGHIDEventTap,
+                               kCGHeadInsertEventTap,
+                               kCGEventTapOptionDefault,
+                               mask,
+                               callback,
+                               nullptr);
+  if (event_tap) {
+    if (auto run_loop_source = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, event_tap, 0)) {
+      CFRunLoopAddSource(CFRunLoopGetMain(), run_loop_source, kCFRunLoopCommonModes);
+      CGEventTapEnable(event_tap, true);
+
+      std::cout << std::endl;
+      std::cout << std::endl;
+      std::cout << "Set mouse event flags to command." << std::endl;
+      std::cout << "You can exit by right click." << std::endl;
+      std::cout << std::endl;
+      std::cout << std::endl;
+    }
+  }
+
+  CFRunLoopRun();
+
+  return 0;
+}
