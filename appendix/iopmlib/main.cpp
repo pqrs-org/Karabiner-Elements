bdbad75cc790aba264c2c3a4c05b0a7ba82a0f79
Takayama Fumihiko <tekezo@pqrs.org>
2016-10-16 17:48:36 +0900

    add appendix/iopmlib

---
+++ appendix/iopmlib/main.cpp
@@ -0,0 +1,24 @@
+#include "iopm_client.hpp"
+
+class logger final {
+public:
+  static spdlog::logger& get_logger(void) {
+    static std::shared_ptr<spdlog::logger> logger;
+    if (!logger) {
+      logger = spdlog::stdout_logger_mt("iopmlib", true);
+    }
+    return *logger;
+  }
+};
+
+static void callback(uint32_t message_type) {
+  logger::get_logger().info("callback");
+}
+
+int main(int argc, const char* argv[]) {
+  iopm_client client(logger::get_logger(), std::bind(callback, std::placeholders::_1));
+
+  CFRunLoopRun();
+
+  return 0;
+}
