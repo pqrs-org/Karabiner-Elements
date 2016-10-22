adf53e6b70d1a1114a36262d5ad7a8b8950bd12d
Takayama Fumihiko <tekezo@pqrs.org>
2016-10-22 20:06:37 +0900

    flatten simple_modifications.json

--- src/apps/PreferencesWindow/PreferencesWindow/SimpleModificationsMenuManager.m
+++ src/apps/PreferencesWindow/PreferencesWindow/SimpleModificationsMenuManager.m
@@ -10,58 +10,6 @@
 
 @implementation SimpleModificationsMenuManager
 
-- (void)buildMenu:(NSArray*)array {
-  [array enumerateObjectsUsingBlock:^(id obj, NSUInteger index, BOOL* stop) {
-    NSString* category = obj[@"category"];
-    NSArray* children = obj[@"children"];
-    NSString* name = obj[@"name"];
-
-    if (category && children) {
-      [self.fromMenu addItem:[NSMenuItem separatorItem]];
-      [self.toMenu addItem:[NSMenuItem separatorItem]];
-
-      {
-        NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:category
-                                                      action:NULL
-                                               keyEquivalent:@""];
-        [self.fromMenu addItem:item];
-        item.enabled = NO;
-      }
-      {
-        NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:category
-                                                      action:NULL
-                                               keyEquivalent:@""];
-        [self.toMenu addItem:item];
-        item.enabled = NO;
-      }
-
-      [self buildMenu:children];
-
-    } else if (name) {
-      NSString* label = obj[@"label"];
-      if (!label) {
-        label = name;
-      }
-      label = [NSString stringWithFormat:@"  %@", label];
-
-      if (!obj[@"not_from"]) {
-        NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:label
-                                                      action:NULL
-                                               keyEquivalent:@""];
-        item.representedObject = name;
-        [self.fromMenu addItem:item];
-      }
-      if (!obj[@"not_to"]) {
-        NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:label
-                                                      action:NULL
-                                               keyEquivalent:@""];
-        item.representedObject = name;
-        [self.toMenu addItem:item];
-      }
-    }
-  }];
-}
-
 - (void)setup {
   NSString* jsonFilePath = [[NSBundle mainBundle] pathForResource:@"simple_modifications" ofType:@"json"];
   if (!jsonFilePath) {
@@ -76,7 +24,51 @@
     self.toMenu = [NSMenu new];
     self.toMenu.autoenablesItems = NO;
 
-    [self buildMenu:jsonObject];
+    for (NSDictionary* dict in jsonObject) {
+      NSString* category = dict[@"category"];
+      NSString* name = dict[@"name"];
+
+      if (category) {
+        [self.fromMenu addItem:[NSMenuItem separatorItem]];
+        [self.toMenu addItem:[NSMenuItem separatorItem]];
+
+        {
+          NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:category
+                                                        action:NULL
+                                                 keyEquivalent:@""];
+          [self.fromMenu addItem:item];
+          item.enabled = NO;
+        }
+        {
+          NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:category
+                                                        action:NULL
+                                                 keyEquivalent:@""];
+          [self.toMenu addItem:item];
+          item.enabled = NO;
+        }
+      } else if (name) {
+        NSString* label = dict[@"label"];
+        if (!label) {
+          label = name;
+        }
+        label = [NSString stringWithFormat:@"  %@", label];
+
+        if (!dict[@"not_from"]) {
+          NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:label
+                                                        action:NULL
+                                                 keyEquivalent:@""];
+          item.representedObject = name;
+          [self.fromMenu addItem:item];
+        }
+        if (!dict[@"not_to"]) {
+          NSMenuItem* item = [[NSMenuItem alloc] initWithTitle:label
+                                                        action:NULL
+                                                 keyEquivalent:@""];
+          item.representedObject = name;
+          [self.toMenu addItem:item];
+        }
+      }
+    }
   }
 }
 
