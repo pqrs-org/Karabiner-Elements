11b1e3e4ae08562500de7a405994e73ffe48101b
Takayama Fumihiko <tekezo@pqrs.org>
2016-10-22 20:19:58 +0900

    show hidsystem key name in EventViewer

--- src/apps/EventViewer/Classes/EventQueue.m
+++ src/apps/EventViewer/Classes/EventQueue.m
@@ -1,9 +1,12 @@
 #import "EventQueue.h"
+#import "JsonUtility.h"
 #import "PreferencesKeys.h"
+#include "libkrbn.h"
 
 @interface EventQueue ()
 
 @property NSMutableArray* queue;
+@property NSMutableDictionary* hidSystemKeyNames;
 @property(weak) IBOutlet NSTableView* view;
 
 @end
@@ -19,6 +22,25 @@ enum {
 
   if (self) {
     _queue = [NSMutableArray new];
+    _hidSystemKeyNames = [NSMutableDictionary new];
+
+    NSString* jsonFilePath = [[NSBundle mainBundle] pathForResource:@"simple_modifications" ofType:@"json"];
+    if (jsonFilePath) {
+      NSArray* jsonObject = [JsonUtility loadFile:jsonFilePath];
+      if (jsonObject) {
+        for (NSDictionary* dict in jsonObject) {
+          NSString* name = dict[@"name"];
+          if (name) {
+            uint8_t hid_system_key = 0;
+            if (libkrbn_get_hid_system_key(&hid_system_key, [name UTF8String])) {
+              if (!_hidSystemKeyNames[@(hid_system_key)]) {
+                _hidSystemKeyNames[@(hid_system_key)] = name;
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   return self;
@@ -55,110 +77,9 @@ enum {
 - (NSString*)specialKeycodeToString:(NSEvent*)event {
   unsigned short keycode = [event keyCode];
 
-  if (keycode == 0x35) return @"Escape";
-  if (keycode == 0x36) return @"Command_R";
-  if (keycode == 0x37) return @"Command_L";
-  if (keycode == 0x38) return @"Shift_L";
-  if (keycode == 0x39) return @"CapsLock";
-  if (keycode == 0x3a) return @"Option_L";
-  if (keycode == 0x3b) return @"Control_L";
-  if (keycode == 0x3c) return @"Shift_R";
-  if (keycode == 0x3d) return @"Option_R";
-  if (keycode == 0x3e) return @"Control_R";
-  if (keycode == 0x3f) return @"Fn";
-
-  if (keycode == 0x24) return @"Return";
-  if (keycode == 0x30) return @"Tab";
-  if (keycode == 0x31) return @"Space";
-  if (keycode == 0x33) return @"Delete";
-  if (keycode == 0x4c) return @"Enter";
-  if (keycode == 0x66) return @"LANG2";
-  if (keycode == 0x68) return @"LANG1";
-  if (keycode == 0x6e) return @"Application";
-
-  // ------------------------------------------------------------
-  @try {
-    NSString* characters = [event characters];
-    if ([characters length] > 0) {
-      unichar unichar = [characters characterAtIndex:0];
-
-      if (unichar == NSUpArrowFunctionKey) { return @"Up"; }
-      if (unichar == NSDownArrowFunctionKey) { return @"Down"; }
-      if (unichar == NSLeftArrowFunctionKey) { return @"Left"; }
-      if (unichar == NSRightArrowFunctionKey) { return @"Right"; }
-
-      if (unichar == NSF1FunctionKey) { return @"F1"; }
-      if (unichar == NSF2FunctionKey) { return @"F2"; }
-      if (unichar == NSF3FunctionKey) { return @"F3"; }
-      if (unichar == NSF4FunctionKey) { return @"F4"; }
-      if (unichar == NSF5FunctionKey) { return @"F5"; }
-      if (unichar == NSF6FunctionKey) { return @"F6"; }
-      if (unichar == NSF7FunctionKey) { return @"F7"; }
-      if (unichar == NSF8FunctionKey) { return @"F8"; }
-      if (unichar == NSF9FunctionKey) { return @"F9"; }
-      if (unichar == NSF10FunctionKey) { return @"F10"; }
-      if (unichar == NSF11FunctionKey) { return @"F11"; }
-      if (unichar == NSF12FunctionKey) { return @"F12"; }
-      if (unichar == NSF13FunctionKey) { return @"F13"; }
-      if (unichar == NSF14FunctionKey) { return @"F14"; }
-      if (unichar == NSF15FunctionKey) { return @"F15"; }
-      if (unichar == NSF16FunctionKey) { return @"F16"; }
-      if (unichar == NSF17FunctionKey) { return @"F17"; }
-      if (unichar == NSF18FunctionKey) { return @"F18"; }
-      if (unichar == NSF19FunctionKey) { return @"F19"; }
-      if (unichar == NSF20FunctionKey) { return @"F20"; }
-      if (unichar == NSF21FunctionKey) { return @"F21"; }
-      if (unichar == NSF22FunctionKey) { return @"F22"; }
-      if (unichar == NSF23FunctionKey) { return @"F23"; }
-      if (unichar == NSF24FunctionKey) { return @"F24"; }
-      if (unichar == NSF25FunctionKey) { return @"F25"; }
-      if (unichar == NSF26FunctionKey) { return @"F26"; }
-      if (unichar == NSF27FunctionKey) { return @"F27"; }
-      if (unichar == NSF28FunctionKey) { return @"F28"; }
-      if (unichar == NSF29FunctionKey) { return @"F29"; }
-      if (unichar == NSF30FunctionKey) { return @"F30"; }
-      if (unichar == NSF31FunctionKey) { return @"F31"; }
-      if (unichar == NSF32FunctionKey) { return @"F32"; }
-      if (unichar == NSF33FunctionKey) { return @"F33"; }
-      if (unichar == NSF34FunctionKey) { return @"F34"; }
-      if (unichar == NSF35FunctionKey) { return @"F35"; }
-
-      if (unichar == NSInsertFunctionKey) { return @"Insert"; }
-      if (unichar == NSDeleteFunctionKey) { return @"Forward Delete"; }
-      if (unichar == NSHomeFunctionKey) { return @"Home"; }
-      if (unichar == NSBeginFunctionKey) { return @"Begin"; }
-      if (unichar == NSEndFunctionKey) { return @"End"; }
-      if (unichar == NSPageUpFunctionKey) { return @"Page Up"; }
-      if (unichar == NSPageDownFunctionKey) { return @"Page Down"; }
-      if (unichar == NSPrintScreenFunctionKey) { return @"Print Screen"; }
-      if (unichar == NSScrollLockFunctionKey) { return @"Scroll Lock"; }
-      if (unichar == NSPauseFunctionKey) { return @"Pause"; }
-      if (unichar == NSSysReqFunctionKey) { return @"System Request"; }
-      if (unichar == NSBreakFunctionKey) { return @"Break"; }
-      if (unichar == NSResetFunctionKey) { return @"Reset"; }
-      if (unichar == NSStopFunctionKey) { return @"Stop"; }
-      if (unichar == NSMenuFunctionKey) { return @"Menu"; }
-      if (unichar == NSUserFunctionKey) { return @"User"; }
-      if (unichar == NSSystemFunctionKey) { return @"System"; }
-      if (unichar == NSPrintFunctionKey) { return @"Print"; }
-      if (unichar == NSClearLineFunctionKey) { return @"Clear/Num Lock"; }
-      if (unichar == NSClearDisplayFunctionKey) { return @"Clear Display"; }
-      if (unichar == NSInsertLineFunctionKey) { return @"Insert Line"; }
-      if (unichar == NSDeleteLineFunctionKey) { return @"Delete Line"; }
-      if (unichar == NSInsertCharFunctionKey) { return @"Insert Character"; }
-      if (unichar == NSDeleteCharFunctionKey) { return @"Delete Character"; }
-      if (unichar == NSPrevFunctionKey) { return @"Previous"; }
-      if (unichar == NSNextFunctionKey) { return @"Next"; }
-      if (unichar == NSSelectFunctionKey) { return @"Select"; }
-      if (unichar == NSExecuteFunctionKey) { return @"Execute"; }
-      if (unichar == NSUndoFunctionKey) { return @"Undo"; }
-      if (unichar == NSRedoFunctionKey) { return @"Redo"; }
-      if (unichar == NSFindFunctionKey) { return @"Find"; }
-      if (unichar == NSHelpFunctionKey) { return @"Help"; }
-      if (unichar == NSModeSwitchFunctionKey) { return @"Mode Switch"; }
-    }
-  }
-  @catch (NSException* exception) {
+  NSString* name = self.hidSystemKeyNames[@(keycode)];
+  if (name) {
+    return name;
   }
 
   return nil;
--- src/apps/EventViewer/main.m
+++ src/apps/EventViewer/main.m
@@ -1,5 +1,7 @@
 @import Cocoa;
+#include "libkrbn.h"
 
 int main(int argc, char* argv[]) {
+  libkrbn_initialize();
   return NSApplicationMain(argc, (const char**)argv);
 }
