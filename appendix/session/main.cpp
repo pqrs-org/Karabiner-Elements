8c5a5e5ce182b8f38a6e3990584d1ef7b41cf014
Takayama Fumihiko <tekezo@pqrs.org>
2016-11-06 01:51:20 +0900

    update session

--- appendix/session/main.cpp
+++ appendix/session/main.cpp
@@ -1,10 +1,15 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 #include "session.hpp"
 
 int main(int argc, const char* argv[]) {
-  if (auto current_console_user_id = session::get_current_console_user_id()) {
-    std::cout << "current_console_user_id: " << *current_console_user_id << std::endl;
+  while (true) {
+    if (auto current_console_user_id = session::get_current_console_user_id()) {
+      std::cout << "current_console_user_id: " << *current_console_user_id << std::endl;
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
 
   return 0;
