c5a0ab7030f8823318371eecbc0c2c44fa2f346a
Takayama Fumihiko <tekezo@pqrs.org>
2017-01-17 00:37:26 +0900

    update test

--- tests/src/spdlog_utility/test.cpp
+++ tests/src/spdlog_utility/test.cpp
@@ -5,8 +5,20 @@
 
 #include "spdlog_utility.hpp"
 #include "thread_utility.hpp"
+#include <boost/lexical_cast.hpp>
 #include <boost/optional/optional_io.hpp>
 #include <ostream>
+
+class logger final {
+public:
+  static spdlog::logger& get_logger(void) {
+    static std::shared_ptr<spdlog::logger> logger;
+    if (!logger) {
+      logger = spdlog::stdout_logger_mt("log_reducer", true);
+    }
+    return *logger;
+  }
+};
 
 TEST_CASE("get_timestamp_number") {
   {
@@ -22,6 +34,38 @@ TEST_CASE("get_timestamp_number") {
     auto actual = spdlog_utility::get_sort_key("[yyyy-mm-dd hh:mm:ss.mmm]");
     REQUIRE(actual == boost::none);
   }
+}
+
+TEST_CASE("log_reducer") {
+  spdlog_utility::log_reducer log_reducer(logger::get_logger());
+
+  // reduce
+  log_reducer.info("test1");
+  log_reducer.info("test1");
+  log_reducer.info("test1");
+  log_reducer.warn("test1");
+  log_reducer.warn("test1");
+  log_reducer.warn("test1");
+  log_reducer.error("test1");
+  log_reducer.error("test1");
+  log_reducer.error("test1");
+  log_reducer.info("test1");
+  log_reducer.warn("test1");
+  log_reducer.error("test1");
+
+  // reset
+  log_reducer.info("test2");
+  log_reducer.reset();
+  log_reducer.info("test2");
+  log_reducer.reset();
+  log_reducer.info("test2");
+
+  // old value will be removed after new items are pushed.
+  log_reducer.info("test3");
+  for (int i = 0; i < 100; ++i) {
+    log_reducer.info(std::string("dummy ") + boost::lexical_cast<std::string>(i));
+  }
+  log_reducer.info("test3");
 }
 
 int main(int argc, char* const argv[]) {
