84353c36dbdd440b6a4349d9727785c09380dec8
Takayama Fumihiko <tekezo@pqrs.org>
2017-01-30 01:59:27 +0900

    add Menu to package

--- make-package.sh
+++ make-package.sh
@@ -23,6 +23,7 @@ cp src/scripts/uninstaller.applescript "$basedir"
 cp src/scripts/uninstall.sh "$basedir"
 cp src/scripts/uninstall_core.sh "$basedir/uninstall_core.sh"
 cp -R "src/vendor/Karabiner-VirtualHIDDevice/dist" "$basedir/Karabiner-VirtualHIDDevice"
+cp -R "src/apps/Menu/build/Release/Karabiner-Menu.app" "$basedir"
 
 basedir="pkgroot/Library/Application Support/org.pqrs/Karabiner-Elements/bin"
 mkdir -p "$basedir"
