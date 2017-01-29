22f11cd6855ae630e9a19740f781c12e245e4aa8
Takayama Fumihiko <tekezo@pqrs.org>
2017-01-29 23:56:16 +0900

    add share/update_utility

---
+++ src/share/update_utility.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+class update_utility final {
+public:
+  static void check_for_updates_in_background(void) {
+    launch_updater("checkForUpdatesInBackground");
+  }
+
+  static void check_for_updates_stable_only(void) {
+    launch_updater("checkForUpdatesStableOnly");
+  }
+
+  static void check_for_updates_with_beta_version(void) {
+    launch_updater("checkForUpdatesWithBetaVersion");
+  }
+
+private:
+  static void launch_updater(const std::string& argument) {
+    auto command = std::string("open '/Library/Application Support/org.pqrs/Karabiner-Elements/updater/Karabiner-Elements.app' --args ") + argument;
+    system(command.c_str());
+  }
+};
