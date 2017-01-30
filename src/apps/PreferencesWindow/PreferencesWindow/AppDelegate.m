23416db0b1faed193dfe5370a9a632cf40c34144
Takayama Fumihiko <tekezo@pqrs.org>
2017-01-31 00:37:38 +0900

    remove share/Relauncher

--- src/apps/PreferencesWindow/PreferencesWindow/AppDelegate.m
+++ src/apps/PreferencesWindow/PreferencesWindow/AppDelegate.m
@@ -1,7 +1,6 @@
 #import "AppDelegate.h"
 #import "KarabinerKit/KarabinerKit.h"
 #import "PreferencesWindowController.h"
-#import "Relauncher.h"
 #import "SystemPreferencesManager.h"
 #import "libkrbn.h"
 
@@ -22,16 +21,6 @@
   [self.systemPreferencesManager setup];
 
   [self.preferencesWindowController setup];
-
-  [[NSDistributedNotificationCenter defaultCenter] addObserver:self
-                                                      selector:@selector(grabberIsLaunchedNotificationCallback:)
-                                                          name:[NSString stringWithUTF8String:libkrbn_get_distributed_notification_grabber_is_launched()]
-                                                        object:[NSString stringWithUTF8String:libkrbn_get_distributed_notification_observed_object()]
-                                            suspensionBehavior:NSNotificationSuspensionBehaviorDeliverImmediately];
-}
-
-- (void)dealloc {
-  [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)theApplication {
@@ -41,12 +30,6 @@
 - (BOOL)applicationShouldHandleReopen:(NSApplication*)theApplication hasVisibleWindows:(BOOL)flag {
   [self.preferencesWindowController show];
   return YES;
-}
-
-- (void)grabberIsLaunchedNotificationCallback:(NSNotification*)notification {
-  dispatch_async(dispatch_get_main_queue(), ^{
-    [Relauncher relaunch];
-  });
 }
 
 @end
