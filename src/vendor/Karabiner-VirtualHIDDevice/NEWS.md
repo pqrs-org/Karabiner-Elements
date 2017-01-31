b230b1267a350092b8de58796c5c605c54ea545b
Takayama Fumihiko <tekezo@pqrs.org>
2017-02-01 01:00:03 +0900

    update Karabiner-VirtualHIDDevice

--- src/vendor/Karabiner-VirtualHIDDevice/NEWS.md
+++ src/vendor/Karabiner-VirtualHIDDevice/NEWS.md
@@ -1,3 +1,9 @@
+# Version 4.2.0
+
+* Fixed issue:
+  * `update.sh` fails to copy kext when the kext file is not in /Library/Extensions and kext is already loaded.
+
+
 # Version 4.1.0
 
 * `caps_lock_delay_milliseconds` has been added into `pqrs::karabiner_virtual_hid_device::properties::keyboard_initialization`.
--- src/vendor/Karabiner-VirtualHIDDevice/dist/update.sh
+++ src/vendor/Karabiner-VirtualHIDDevice/dist/update.sh
@@ -5,21 +5,23 @@ PATH='/bin:/sbin:/usr/bin:/usr/sbin'; export PATH
 basedir=`dirname "$0"`
 cd "$basedir"
 
-bundle_identifier='org.pqrs.driver.Karabiner.VirtualHIDDevice.v040100'
+bundle_identifier='org.pqrs.driver.Karabiner.VirtualHIDDevice.v040200'
 
-# Skip if already installed
-if kextstat | grep -q "$bundle_identifier"; then
-    echo "$bundle_identifier is already installed"
-    exit 0
+if diff -qr org.pqrs.driver.Karabiner.VirtualHIDDevice.kext /Library/Extensions/org.pqrs.driver.Karabiner.VirtualHIDDevice.kext 1>/dev/null 2>/dev/null; then
+    echo "$bundle_identifier is already installed."
+else
+    # Copy kext
+    rm -rf /Library/Extensions/org.pqrs.driver.Karabiner.VirtualHIDDevice.kext
+    cp -R org.pqrs.driver.Karabiner.VirtualHIDDevice.kext /Library/Extensions
 fi
 
 # Unload kexts
 for kext in `kextstat | ruby -ne 'print $1,"\n" if /(org\.pqrs\.driver\.Karabiner\.VirtualHIDDevice.*?) /'`; do
-    kextunload -b "$kext" -q
+    if [ "$kext" != "$bundle_identifier" ]; then
+        echo "kextunload $kext"
+        kextunload -b "$kext" -q
+    fi
 done
-
-# Copy kext
-cp -R org.pqrs.driver.Karabiner.VirtualHIDDevice.kext /Library/Extensions
 
 # Load kext
 kextload /Library/Extensions/org.pqrs.driver.Karabiner.VirtualHIDDevice.kext
