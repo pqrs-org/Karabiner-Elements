ea44c3e543e081019b360793f979ff3ef8bc6b89
Takayama Fumihiko <tekezo@pqrs.org>
2017-02-02 01:23:58 +0900

    remove unnecessary make clean and rm *.dmg, add message for sudo

--- scripts/build-and-install.sh
+++ scripts/build-and-install.sh
@@ -9,12 +9,20 @@ set -e
 unset CPATH
 
 git pull --rebase
-make clean
-rm *.dmg
 make
 
 DMG=$(ls *.dmg)
 
 VOL=`hdiutil attach "$DMG" | grep -o "/Volumes/.*"`
+
+echo
+echo -ne '\033[33;40m'
+echo "========================================"
+echo "Trying to install `basename $DMG .dmg` with administrator privilege."
+echo "Please enter your password in order to run 'sudo installer ...' command."
+echo "========================================"
+echo -ne '\033[0m'
+echo
+
 sudo installer -package "$VOL"/*.pkg -target /
 hdiutil detach "$VOL"
