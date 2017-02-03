83833be2ec68d1e0d04b80f23ea27b079df3df94
Takayama Fumihiko <tekezo@pqrs.org>
2017-02-03 23:01:33 +0900

    show uninstalled message after uninstall_core.sh

--- src/scripts/uninstall.sh
+++ src/scripts/uninstall.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-/usr/bin/osascript -e 'display dialog "Karabiner-Elements has been uninstalled." buttons {"OK"}'
-
 /bin/sh '/Library/Application Support/org.pqrs/Karabiner-Elements/Karabiner-VirtualHIDDevice/uninstall.sh'
 /bin/sh '/Library/Application Support/org.pqrs/Karabiner-Elements/uninstall_core.sh'
 
 /usr/bin/killall karabiner_grabber
 /usr/bin/killall karabiner_console_user_server
 /usr/bin/killall Karabiner-Elements
+
+/usr/bin/osascript -e 'display dialog "Karabiner-Elements has been uninstalled.\nPlease restart your system." buttons {"OK"}'
 
 exit 0
