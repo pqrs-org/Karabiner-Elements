7a39eb409e779086d88a43deb657155c0671af63
Takayama Fumihiko <tekezo@pqrs.org>
2017-03-02 00:30:03 +0900

    remove unused method

--- src/lib/libkrbn/libkrbn.hpp
+++ src/lib/libkrbn/libkrbn.hpp
@@ -7,23 +7,8 @@
 #include <thread>
 #include <vector>
 
-class libkrbn {
+class libkrbn final {
 public:
-  static bool cfstring_to_cstring(std::vector<char>& v, CFStringRef string) {
-    if (string) {
-      if (auto cppstring = cf_utility::to_string(string)) {
-        auto length = cppstring->length() + 1;
-        v.resize(length);
-        strlcpy(&(v[0]), cppstring->c_str(), length);
-        return true;
-      }
-    }
-
-    v.resize(1);
-    v[0] = '\0';
-    return false;
-  }
-
   static spdlog::logger& get_logger(void) {
     static std::mutex mutex;
     static std::shared_ptr<spdlog::logger> logger;
