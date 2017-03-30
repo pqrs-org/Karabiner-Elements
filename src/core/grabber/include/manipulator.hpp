1246cbff3a99c9840e44fedc6aba8559c889e835
Takayama Fumihiko <tekezo@pqrs.org>
2017-03-30 23:16:02 +0900

    remove unused classes

--- src/core/grabber/include/manipulator.hpp
+++ src/core/grabber/include/manipulator.hpp
@@ -11,16 +11,5 @@ namespace krbn {
 namespace manipulator {
 enum class device_registry_entry_id : uint64_t {
 };
-enum class manipulator_type : uint32_t {
-  keytokey,
-};
-enum class autogen_id : uint64_t {
-  max_ = UINT64_MAX,
-};
-enum class add_data_type : uint32_t {
-  key_code,
-  modifier_flag,
-};
-enum class add_value : uint32_t {};
 } // namespace manipulator
 } // namespace krbn
