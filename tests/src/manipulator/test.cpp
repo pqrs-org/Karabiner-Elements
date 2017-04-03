3ef049787f1e2b13b91569025392edeee98361b3
Takayama Fumihiko <tekezo@pqrs.org>
2017-04-04 00:11:13 +0900

    update test

--- tests/src/manipulator/test.cpp
+++ tests/src/manipulator/test.cpp
@@ -1,14 +1,14 @@
 #define CATCH_CONFIG_RUNNER
 #include "../../vendor/catch/catch.hpp"
 
-#include "manipulator/details/types.hpp"
+#include "manipulator/manipulator_factory.hpp"
 #include "thread_utility.hpp"
 
 TEST_CASE("manipulator.details.event_definition") {
   {
     nlohmann::json json;
-    krbn::manipulator::detail::event_definition event_definition(json);
-    REQUIRE(event_definition.get_type() == krbn::manipulator::detail::event_definition::type::none);
+    krbn::manipulator::details::event_definition event_definition(json);
+    REQUIRE(event_definition.get_type() == krbn::manipulator::details::event_definition::type::none);
     REQUIRE(event_definition.get_modifiers().size() == 0);
   }
   {
@@ -20,13 +20,13 @@ TEST_CASE("manipulator.details.event_definition") {
                          },
         },
     });
-    krbn::manipulator::detail::event_definition event_definition(json);
-    REQUIRE(event_definition.get_type() == krbn::manipulator::detail::event_definition::type::key);
+    krbn::manipulator::details::event_definition event_definition(json);
+    REQUIRE(event_definition.get_type() == krbn::manipulator::details::event_definition::type::key);
     REQUIRE(event_definition.get_value() == "spacebar");
-    REQUIRE(event_definition.get_modifiers() == std::vector<krbn::manipulator::detail::event_definition::modifier>({
-                                                    krbn::manipulator::detail::event_definition::modifier::shift,
-                                                    krbn::manipulator::detail::event_definition::modifier::left_command,
-                                                    krbn::manipulator::detail::event_definition::modifier::any,
+    REQUIRE(event_definition.get_modifiers() == std::vector<krbn::manipulator::details::event_definition::modifier>({
+                                                    krbn::manipulator::details::event_definition::modifier::shift,
+                                                    krbn::manipulator::details::event_definition::modifier::left_command,
+                                                    krbn::manipulator::details::event_definition::modifier::any,
                                                 }));
   }
   {
@@ -36,10 +36,61 @@ TEST_CASE("manipulator.details.event_definition") {
             "modifiers", "dummy",
         },
     });
-    krbn::manipulator::detail::event_definition event_definition(json);
-    REQUIRE(event_definition.get_type() == krbn::manipulator::detail::event_definition::type::none);
+    krbn::manipulator::details::event_definition event_definition(json);
+    REQUIRE(event_definition.get_type() == krbn::manipulator::details::event_definition::type::none);
     REQUIRE(event_definition.get_value() == "");
     REQUIRE(event_definition.get_modifiers().size() == 0);
+  }
+}
+
+TEST_CASE("manipulator.manipulator_factory") {
+  {
+    nlohmann::json json;
+    auto manipulator = krbn::manipulator::manipulator_factory::make_manipulator(json);
+    REQUIRE(dynamic_cast<krbn::manipulator::details::nop*>(manipulator.get()) != nullptr);
+    REQUIRE(dynamic_cast<krbn::manipulator::details::basic*>(manipulator.get()) == nullptr);
+  }
+  {
+    nlohmann::json json({
+        {"type", "basic"},
+        {
+            "from", {
+                        {
+                            "key", "escape",
+                        },
+                        {
+                            "modifiers", {
+                                             "left_shift", "left_option", "any",
+                                         },
+                        },
+                    },
+        },
+        {
+            "to", {
+                      {
+                          {
+                              "pointing_button", "button1",
+                          },
+                      },
+                  },
+        },
+    });
+    auto manipulator = krbn::manipulator::manipulator_factory::make_manipulator(json);
+    REQUIRE(dynamic_cast<krbn::manipulator::details::basic*>(manipulator.get()) != nullptr);
+    REQUIRE(dynamic_cast<krbn::manipulator::details::nop*>(manipulator.get()) == nullptr);
+
+    auto basic = dynamic_cast<krbn::manipulator::details::basic*>(manipulator.get());
+    REQUIRE(basic->get_from().get_type() == krbn::manipulator::details::event_definition::type::key);
+    REQUIRE(basic->get_from().get_value() == "escape");
+    REQUIRE(basic->get_from().get_modifiers() == std::vector<krbn::manipulator::details::event_definition::modifier>({
+                                                     krbn::manipulator::details::event_definition::modifier::left_shift,
+                                                     krbn::manipulator::details::event_definition::modifier::left_option,
+                                                     krbn::manipulator::details::event_definition::modifier::any,
+                                                 }));
+    REQUIRE(basic->get_to().size() == 1);
+    REQUIRE(basic->get_to()[0].get_type() == krbn::manipulator::details::event_definition::type::pointing_button);
+    REQUIRE(basic->get_to()[0].get_value() == "button1");
+    REQUIRE(basic->get_to()[0].get_modifiers() == std::vector<krbn::manipulator::details::event_definition::modifier>());
   }
 }
 
