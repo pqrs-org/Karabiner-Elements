0971151d6dd10bbd89d2217794f6dcad1132fd97
Takayama Fumihiko <tekezo@pqrs.org>
2017-04-04 00:10:08 +0900

    add manipulator_factory

---
+++ src/core/grabber/include/manipulator/manipulator_factory.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "manipulator/details/base.hpp"
+#include "manipulator/details/basic.hpp"
+#include "manipulator/details/nop.hpp"
+#include "manipulator/details/types.hpp"
+#include <memory>
+
+namespace krbn {
+namespace manipulator {
+class manipulator_factory final {
+public:
+  static std::unique_ptr<details::base> make_manipulator(const nlohmann::json& json) {
+    {
+      const std::string key = "type";
+      if (json.find(key) != std::end(json) && json[key].is_string()) {
+        if (json[key] == "basic") {
+          return std::make_unique<details::basic>(json);
+        }
+      }
+    }
+    return std::make_unique<details::nop>();
+  }
+};
+} // namespace manipulator
+} // namespace krbn
