aeaf2d31af9297e592fbf042beea48226a73124a
Takayama Fumihiko <tekezo@pqrs.org>
2017-04-04 00:08:44 +0900

    add from_, to_

--- src/core/grabber/include/manipulator/details/basic.hpp
+++ src/core/grabber/include/manipulator/details/basic.hpp
@@ -1,26 +1,46 @@
 #pragma once
 
-#include "event_queue.hpp"
-#include "types.hpp"
+#include "manipulator/details/base.hpp"
+#include "manipulator/details/types.hpp"
+#include "manipulator/event_queue.hpp"
+#include <json/json.hpp>
 #include <unordered_map>
 
 namespace krbn {
 namespace manipulator {
-namespace detail {
+namespace details {
 class basic final : public base {
 public:
-  basic(const nlohmann::json& json) : base() {
+  basic(const nlohmann::json& json) : base(),
+                                      from_(json.find("from") != std::end(json) ? json["from"] : nlohmann::json()) {
+    {
+      const std::string key = "to";
+      if (json.find(key) != std::end(json) && json[key].is_array()) {
+        for (const auto& j : json[key]) {
+          to_.emplace_back(j);
+        }
+      }
+    }
   }
 
   virtual ~basic(void) {
   }
 
-  virtual manipulate(event_queue& event_queue) {
+  virtual void manipulate(event_queue& event_queue) {
+  }
+
+  const event_definition& get_from(void) const {
+    return from_;
+  }
+
+  const std::vector<event_definition>& get_to(void) const {
+    return to_;
   }
 
 private:
-
+  event_definition from_;
+  std::vector<event_definition> to_;
 };
-} // namespace detail
+} // namespace details
 } // namespace manipulator
 } // namespace krbn
