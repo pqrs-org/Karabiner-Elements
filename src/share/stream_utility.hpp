bee0ec30066a9f4803563af7bb1b885e1c207640
Takayama Fumihiko <tekezo@pqrs.org>
2017-05-12 11:57:42 +0900

    update output format

--- src/share/stream_utility.hpp
+++ src/share/stream_utility.hpp
@@ -7,8 +7,8 @@ namespace krbn {
 class stream_utility final {
 public:
   template <typename E>
-  static std::ostream& output_enum(std::ostream& stream, const std::string& name, const E& value) {
-    stream << name << " (" << static_cast<typename std::underlying_type<E>::type>(value) << ")";
+  static std::ostream& output_enum(std::ostream& stream, const E& value) {
+    stream << static_cast<typename std::underlying_type<E>::type>(value);
     return stream;
   }
 
@@ -17,16 +17,16 @@ public:
   template <typename E, template <class T, class A> class container>
   static std::ostream& output_enums(std::ostream& stream, const container<E, std::allocator<E>>& values) {
     bool first = true;
-    stream << "{ ";
+    stream << "[";
     for (const auto& v : values) {
       if (first) {
         first = false;
       } else {
-        stream << ", ";
+        stream << ",";
       }
       stream << v;
     }
-    stream << " }";
+    stream << "]";
     return stream;
   }
 
@@ -35,16 +35,16 @@ public:
   template <typename E, template <class T, class H, class K, class A> class container>
   static std::ostream& output_enums(std::ostream& stream, const container<E, std::hash<E>, std::equal_to<E>, std::allocator<E>>& values) {
     bool first = true;
-    stream << "{ ";
+    stream << "[";
     for (const auto& v : values) {
       if (first) {
         first = false;
       } else {
-        stream << ", ";
+        stream << ",";
       }
       stream << v;
     }
-    stream << " }";
+    stream << "]";
     return stream;
   }
 };
