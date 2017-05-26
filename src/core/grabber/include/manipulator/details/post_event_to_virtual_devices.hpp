8a67e3d3b427ed18b95958a99c326adc06f90eca
Takayama Fumihiko <tekezo@pqrs.org>
2017-05-26 11:33:24 +0900

    add methods for unit testing

--- src/core/grabber/include/manipulator/details/post_event_to_virtual_devices.hpp
+++ src/core/grabber/include/manipulator/details/post_event_to_virtual_devices.hpp
@@ -4,6 +4,7 @@
 
 #include "manipulator/details/base.hpp"
 #include "manipulator/details/types.hpp"
+#include "stream_utility.hpp"
 #include "types.hpp"
 #include "virtual_hid_device_client.hpp"
 #include <boost/optional.hpp>
@@ -74,6 +75,10 @@ public:
     };
 
     queue(void) {
+    }
+
+    const std::vector<event>& get_events(void) const {
+      return events_;
     }
 
     void emplace_back_event(const pqrs::karabiner_virtual_hid_device::hid_event_service::keyboard_event& keyboard_event,
@@ -209,9 +214,48 @@ public:
     // Do nothing
   }
 
+  void post_events(virtual_hid_device_client& virtual_hid_device_client) {
+    queue_.post_events(virtual_hid_device_client);
+  }
+
+  const queue& get_queue(void) const {
+    return queue_;
+  }
+
 private:
   queue queue_;
 };
+
+std::ostream& operator<<(std::ostream& stream, const post_event_to_virtual_devices::queue::event& event) {
+  stream << "{"
+         << "\"type\":";
+  stream_utility::output_enum(stream, event.get_type());
+
+  if (auto keyboard_event = event.get_keyboard_event()) {
+    stream << ",\"keyboard_event.usage\":" << static_cast<uint32_t>(keyboard_event->usage);
+    stream << ",\"keyboard_event.usage_page\":" << static_cast<uint32_t>(keyboard_event->usage_page);
+    stream << ",\"keyboard_event.value\":" << static_cast<uint32_t>(keyboard_event->value);
+  }
+
+  if (auto pointing_input = event.get_pointing_input()) {
+    stream << std::hex;
+    stream << ",\"pointing_input.buttons[0]\":0x" << static_cast<int>(pointing_input->buttons[0]);
+    stream << ",\"pointing_input.buttons[1]\":0x" << static_cast<int>(pointing_input->buttons[1]);
+    stream << ",\"pointing_input.buttons[2]\":0x" << static_cast<int>(pointing_input->buttons[2]);
+    stream << ",\"pointing_input.buttons[3]\":0x" << static_cast<int>(pointing_input->buttons[3]);
+    stream << std::dec;
+    stream << ",\"pointing_input.x\":" << static_cast<int>(pointing_input->x);
+    stream << ",\"pointing_input.y\":" << static_cast<int>(pointing_input->y);
+    stream << ",\"pointing_input.vertical_wheel\":" << static_cast<int>(pointing_input->vertical_wheel);
+    stream << ",\"pointing_input.horizontal_wheel\":" << static_cast<int>(pointing_input->horizontal_wheel);
+  }
+
+  stream << ",\"time_stamp\":" << event.get_time_stamp();
+
+  stream << "}";
+
+  return stream;
+}
 } // namespace details
 } // namespace manipulator
 } // namespace krbn
