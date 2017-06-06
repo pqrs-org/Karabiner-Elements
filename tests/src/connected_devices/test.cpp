54c1b8be18f81504210c1a587ec8a648b464c60e
Takayama Fumihiko <tekezo@pqrs.org>
2017-06-06 10:34:42 +0900

    add mutex to get_logger

--- tests/src/connected_devices/test.cpp
+++ tests/src/connected_devices/test.cpp
@@ -9,11 +9,15 @@
 class logger final {
 public:
   static spdlog::logger& get_logger(void) {
+    static std::mutex mutex;
+    std::lock_guard<std::mutex> guard(mutex);
+
     static std::shared_ptr<spdlog::logger> logger;
     if (!logger) {
       logger = spdlog::stdout_color_mt("connected_devices");
       logger->set_level(spdlog::level::off);
     }
+
     return *logger;
   }
 };
--- tests/src/file_monitor/test.cpp
+++ tests/src/file_monitor/test.cpp
@@ -12,11 +12,15 @@ namespace {
 class logger final {
 public:
   static spdlog::logger& get_logger(void) {
+    static std::mutex mutex;
+    std::lock_guard<std::mutex> guard(mutex);
+
     static std::shared_ptr<spdlog::logger> logger;
     if (!logger) {
       logger = spdlog::stdout_color_mt("file_monitor");
       logger->set_level(spdlog::level::off);
     }
+
     return *logger;
   }
 };
