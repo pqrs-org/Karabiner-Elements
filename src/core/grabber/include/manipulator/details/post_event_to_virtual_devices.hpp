ca62708ac86925e2ef253c8e9b797499d8b64610
Takayama Fumihiko <tekezo@pqrs.org>
2017-06-10 01:21:35 +0900

    add std::endl

--- src/core/grabber/include/manipulator/details/post_event_to_virtual_devices.hpp
+++ src/core/grabber/include/manipulator/details/post_event_to_virtual_devices.hpp
@@ -487,7 +487,8 @@ private:
 };
 
 inline std::ostream& operator<<(std::ostream& stream, const post_event_to_virtual_devices::queue::event& event) {
-  stream << "{"
+  stream << std::endl
+         << "{"
          << "\"type\":";
   stream_utility::output_enum(stream, event.get_type());
 
