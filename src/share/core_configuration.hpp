70c6fb4ce86756dbdf6e072901c4ef38499b4991
Takayama Fumihiko <tekezo@pqrs.org>
2017-06-20 01:46:06 +0900

    change log level of parse error (warn -> error)

--- src/share/core_configuration.hpp
+++ src/share/core_configuration.hpp
@@ -244,7 +244,7 @@ public:
             }
 
           } catch (std::exception& e) {
-            logger::get_logger().warn("parse error in {0}: {1}", file_path, e.what());
+            logger::get_logger().error("parse error in {0}: {1}", file_path, e.what());
             json_ = nlohmann::json();
             loaded_ = false;
           }
