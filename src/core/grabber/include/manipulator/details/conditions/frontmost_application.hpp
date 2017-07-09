8c2d5554eb34ca12e67f3b849bd2a367771c37c2
Takayama Fumihiko <tekezo@pqrs.org>
2017-07-09 20:49:37 +0900

    show error for unknown key

--- src/core/grabber/include/manipulator/details/conditions/frontmost_application.hpp
+++ src/core/grabber/include/manipulator/details/conditions/frontmost_application.hpp
@@ -20,46 +20,51 @@ public:
                                                       type_(type::frontmost_application_if),
                                                       cached_update_count_(0),
                                                       cached_result_(true) {
-    {
-      const std::string key = "type";
-      if (json.find(key) != std::end(json) && json[key].is_string()) {
-        if (json[key] == "frontmost_application_if") {
-          type_ = type::frontmost_application_if;
-        }
-        if (json[key] == "frontmost_application_unless") {
-          type_ = type::frontmost_application_unless;
-        }
-      }
-    }
-    {
-      const std::string key = "bundle_identifiers";
-      if (json.find(key) != std::end(json) && json[key].is_array()) {
-        for (const auto& j : json[key]) {
-          if (j.is_string()) {
-            std::string s = j;
-            try {
-              std::regex r(s);
-              bundle_identifiers_.push_back(r);
-            } catch (std::exception& e) {
-              logger::get_logger().error("regex error: \"{0}\" {1}", s, e.what());
+    if (json.is_object()) {
+      for (auto it = std::begin(json); it != std::end(json); std::advance(it, 1)) {
+        // it.key() is always std::string.
+        const auto& key = it.key();
+        const auto& value = it.value();
+
+        if (key == "type") {
+          if (json.find(key) != std::end(json) && json[key].is_string()) {
+            if (json[key] == "frontmost_application_if") {
+              type_ = type::frontmost_application_if;
+            }
+            if (json[key] == "frontmost_application_unless") {
+              type_ = type::frontmost_application_unless;
             }
           }
-        }
-      }
-    }
-    {
-      const std::string key = "file_paths";
-      if (json.find(key) != std::end(json) && json[key].is_array()) {
-        for (const auto& j : json[key]) {
-          if (j.is_string()) {
-            std::string s = j;
-            try {
-              std::regex r(s);
-              file_paths_.push_back(r);
-            } catch (std::exception& e) {
-              logger::get_logger().error("regex error: \"{0}\" {1}", s, e.what());
+        } else if (key == "bundle_identifiers") {
+          if (json.find(key) != std::end(json) && json[key].is_array()) {
+            for (const auto& j : json[key]) {
+              if (j.is_string()) {
+                std::string s = j;
+                try {
+                  std::regex r(s);
+                  bundle_identifiers_.push_back(r);
+                } catch (std::exception& e) {
+                  logger::get_logger().error("regex error: \"{0}\" {1}", s, e.what());
+                }
+              }
             }
           }
+        } else if (key == "file_paths") {
+          if (json.find(key) != std::end(json) && json[key].is_array()) {
+            for (const auto& j : json[key]) {
+              if (j.is_string()) {
+                std::string s = j;
+                try {
+                  std::regex r(s);
+                  file_paths_.push_back(r);
+                } catch (std::exception& e) {
+                  logger::get_logger().error("regex error: \"{0}\" {1}", s, e.what());
+                }
+              }
+            }
+          }
+        } else {
+          logger::get_logger().error("unknown key: {0} in {1}", key, json.dump());
         }
       }
     }
