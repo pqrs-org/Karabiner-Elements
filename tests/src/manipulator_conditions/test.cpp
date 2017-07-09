3b304d0957b7783e0b44d4cd07dbcb117ddd7c32
Takayama Fumihiko <tekezo@pqrs.org>
2017-07-09 22:14:46 +0900

    add tests

--- tests/src/manipulator_conditions/test.cpp
+++ tests/src/manipulator_conditions/test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_RUNNER
 #include "../../vendor/catch/catch.hpp"
 
-#include "manipulator/manipulator_factory.hpp"
+#include "manipulator/condition_manager.hpp"
 #include "thread_utility.hpp"
 #include <boost/optional/optional_io.hpp>
 
@@ -38,6 +38,51 @@ TEST_CASE("manipulator.manipulator_factory") {
     REQUIRE(dynamic_cast<krbn::manipulator::details::conditions::frontmost_application*>(condition.get()) != nullptr);
     REQUIRE(dynamic_cast<krbn::manipulator::details::conditions::nop*>(condition.get()) == nullptr);
   }
+}
+
+namespace {
+class actual_examples_helper final {
+public:
+  actual_examples_helper(const std::string& file_name) {
+    std::ifstream input(std::string("json/") + file_name);
+    auto json = nlohmann::json::parse(input);
+    for (const auto& j : json) {
+      condition_manager_.push_back_condition(j);
+    }
+  }
+
+  const krbn::manipulator::condition_manager& get_condition_manager(void) const {
+    return condition_manager_;
+  }
+
+private:
+  krbn::manipulator::condition_manager condition_manager_;
+};
+} // namespace
+
+TEST_CASE("manipulator.frontmost_application") {
+  actual_examples_helper helper("frontmost_application.json");
+  krbn::manipulator_environment manipulator_environment;
+
+  // bundle_identifiers matching
+  manipulator_environment.get_frontmost_application().set_bundle_identifier("com.apple.Terminal");
+  manipulator_environment.get_frontmost_application().set_file_path("/not_found");
+  REQUIRE(helper.get_condition_manager().is_fulfilled(manipulator_environment) == true);
+
+  // Test regex escape works properly
+  manipulator_environment.get_frontmost_application().set_bundle_identifier("com/apple/Terminal");
+  REQUIRE(helper.get_condition_manager().is_fulfilled(manipulator_environment) == false);
+
+  // file_path matching
+  manipulator_environment.get_frontmost_application().set_file_path("/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal");
+  REQUIRE(helper.get_condition_manager().is_fulfilled(manipulator_environment) == true);
+
+  // frontmost_application_not
+  manipulator_environment.get_frontmost_application().set_bundle_identifier("com.googlecode.iterm2");
+  manipulator_environment.get_frontmost_application().set_bundle_identifier("/Applications/iTerm.app");
+  REQUIRE(helper.get_condition_manager().is_fulfilled(manipulator_environment) == true);
+  manipulator_environment.get_frontmost_application().set_bundle_identifier("/Users/tekezo/Applications/iTerm.app");
+  REQUIRE(helper.get_condition_manager().is_fulfilled(manipulator_environment) == true);
 }
 
 int main(int argc, char* const argv[]) {
