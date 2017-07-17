9de5f53a1814b108e253d467b3452dbf61a43eb1
Takayama Fumihiko <tekezo@pqrs.org>
2017-07-17 22:54:55 +0900

    remove ;

--- src/core/grabber/include/manipulator/details/post_event_to_virtual_devices.hpp
+++ src/core/grabber/include/manipulator/details/post_event_to_virtual_devices.hpp
@@ -181,7 +181,7 @@ public:
         if (auto shell_command = e.get_shell_command()) {
           try {
             if (auto current_console_user_id = session::get_current_console_user_id()) {
-              console_user_server_client client(*current_console_user_id);;
+              console_user_server_client client(*current_console_user_id);
               client.shell_command_execution(*shell_command);
             }
           } catch (std::exception& e) {
