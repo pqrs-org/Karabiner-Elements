6dba39389ea16098076ce298e754face33b518a5
Takayama Fumihiko <tekezo@pqrs.org>
2017-08-27 19:46:27 +0900

    add simple_modifications::definition

--- src/share/core_configuration/profile/simple_modifications.hpp
+++ src/share/core_configuration/profile/simple_modifications.hpp
@@ -2,6 +2,59 @@
 
 class simple_modifications final {
 public:
+  class definition final {
+  public:
+    definition(const std::string& type,
+               const std::string& value) : type_(type),
+                                           value_(value) {
+    }
+
+    nlohmann::json to_json(void) const {
+      return nlohmann::json({
+          {type_, value_},
+      });
+    }
+
+    const std::string& get_type(void) const {
+      return type_;
+    }
+
+    void set_type(const std::string& value) {
+      type_ = value;
+    }
+
+    const std::string& get_value(void) const {
+      return value_;
+    }
+
+    void set_value(const std::string& value) {
+      value_ = value;
+    }
+
+    bool valid(void) const {
+      return !type_.empty() &&
+             !value_.empty();
+    }
+
+    bool compare(const definition& other) const {
+      if (type_ != other.type_) {
+        return SI::natural::compare<std::string>(type_, other.type_);
+      } else {
+        return SI::natural::compare<std::string>(value_, other.value_);
+      }
+    }
+
+    bool operator==(const definition& other) const {
+      // Do not compare `from_mandatory_modifiers_`.
+      return type_ == other.type_ &&
+             value_ == other.value_;
+    }
+
+  private:
+    std::string type_;
+    std::string value_;
+  };
+
   simple_modifications(const nlohmann::json& json) {
     if (json.is_object()) {
       for (auto it = json.begin(); it != json.end(); ++it) {
