28d0a6f6cb8ffced411556a8b1806dd1ce6bd451
Takayama Fumihiko <tekezo@pqrs.org>
2018-02-07 22:25:32 +0900

    add hash<std::pair>, hash<boost::variant> hash<boost::blank>

--- src/share/hash_utility.hpp
+++ src/share/hash_utility.hpp
@@ -2,14 +2,44 @@
 
 #include "boost_defs.hpp"
 
+#include <boost/blank.hpp>
 #include <boost/functional/hash.hpp>
+#include <boost/variant.hpp>
+#include <utility>
 #include <vector>
+
+namespace boost {
+std::size_t hash_value(const boost::blank& v) {
+  return 0;
+}
+} // namespace boost
 
 namespace std {
 template <typename T, typename A>
 struct hash<std::vector<T, A>> {
   std::size_t operator()(const std::vector<T, A>& v) const {
-    return boost::hash_range(v.begin(), v.end());
+    return boost::hash<std::vector<T, A>>(v.begin(), v.end());
+  }
+};
+
+template <typename A, typename B>
+struct hash<std::pair<A, B>> {
+  std::size_t operator()(const std::pair<A, B>& v) const {
+    return boost::hash<std::pair<A, B>>(v);
+  }
+};
+
+template <BOOST_VARIANT_ENUM_PARAMS(typename T)>
+struct hash<boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)>> {
+  std::size_t hash_value(const boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)>& v) {
+    return boost::hash<boost::variant<BOOST_VARIANT_ENUM_PARAMS(T)>>(v);
+  }
+};
+
+template <>
+struct hash<boost::blank> {
+  std::size_t operator()(const boost::blank& v) const {
+    return 0;
   }
 };
 } // namespace std
