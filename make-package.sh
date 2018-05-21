2fe7d3f98bc2e43044846c8ce80dc805978e4235
Takayama Fumihiko <tekezo@pqrs.org>
2018-05-21 20:25:30 +0900

    use cmake @ karabiner_cli

--- make-package.sh
+++ make-package.sh
@@ -30,7 +30,7 @@ cp -R "src/vendor/Karabiner-VirtualHIDDevice/dist/uninstall.sh" "$basedir/uninst
 
 basedir="pkgroot/Library/Application Support/org.pqrs/Karabiner-Elements/bin"
 mkdir -p "$basedir"
-cp src/bin/cli/build/Release/karabiner_cli "$basedir"
+cp src/bin/cli/build/karabiner_cli "$basedir"
 cp src/core/console_user_server/build/karabiner_console_user_server "$basedir"
 cp src/core/grabber/build/karabiner_grabber "$basedir"
 
