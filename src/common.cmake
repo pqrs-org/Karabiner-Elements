078527b14e8220cd8bee9ead9b13d48630700a41
Takayama Fumihiko <tekezo@pqrs.org>
2018-05-25 23:39:12 +0900

    move common configuration to common.cmake

--- src/common.cmake
+++ src/common.cmake
@@ -5,14 +5,26 @@ set(CMAKE_OSX_DEPLOYMENT_TARGET "10.12" CACHE STRING "Minimum OS X deployment ve
 set(CMAKE_CXX_STANDARD 14)
 set(CMAKE_CXX_STANDARD_REQUIRED on)
 
+include_directories(SYSTEM /opt/local/include)
+include_directories(SYSTEM /usr/local/include)
+include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/vendor)
+include_directories(${CMAKE_CURRENT_LIST_DIR}/share)
+
+add_compile_options(-Wall)
+add_compile_options(-Werror)
+add_compile_options(-O2)
+add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-std=c++14>)
+add_compile_options($<$<COMPILE_LANGUAGE:C>:-fobjc-arc>)
+add_compile_options($<$<COMPILE_LANGUAGE:C>:-fmodules>)
+
 # Version
 
 file(READ ${CMAKE_CURRENT_LIST_DIR}/../version KARABINER_VERSION)
 string(STRIP ${KARABINER_VERSION} KARABINER_VERSION)
 
 configure_file (
-  "${CMAKE_CURRENT_LIST_DIR}/share/karabiner_version.h.in"
-  "${CMAKE_CURRENT_LIST_DIR}/share/karabiner_version.h"
+  ${CMAKE_CURRENT_LIST_DIR}/share/karabiner_version.h.in
+  ${CMAKE_CURRENT_LIST_DIR}/share/karabiner_version.h
 )
 
 # Variables to avoid string replacement in Info.plist
