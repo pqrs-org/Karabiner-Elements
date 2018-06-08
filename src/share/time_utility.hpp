3c0571e54bdea102dd3885aacb06297b035c7720
Takayama Fumihiko <tekezo@pqrs.org>
2018-06-09 03:33:46 +0900

    add time_utility::make_current_local_yyyymmdd_string

--- src/share/time_utility.hpp
+++ src/share/time_utility.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include "logger.hpp"
 #include <mach/mach_time.h>
+#include <time.h>
 
 namespace krbn {
 class time_utility final {
@@ -19,6 +21,18 @@ public:
       return absolute_time * t.denom / t.numer;
     }
     return absolute_time;
+  }
+
+  static std::string make_current_local_yyyymmdd_string(void) {
+    auto t = time(nullptr);
+
+    tm tm;
+    localtime_r(&t, &tm);
+
+    return fmt::format("{0:04d}-{1:02d}-{2:02d}",
+                       tm.tm_year + 1900,
+                       tm.tm_mon + 1,
+                       tm.tm_mday);
   }
 
 private:
