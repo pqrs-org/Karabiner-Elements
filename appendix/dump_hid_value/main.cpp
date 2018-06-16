9c742a1aa2e6afaeecc14f73a85a06131dd3ba0b
Takayama Fumihiko <tekezo@pqrs.org>
2018-06-16 22:34:14 +0900

    use hid_manager

--- appendix/dump_hid_value/main.cpp
+++ appendix/dump_hid_value/main.cpp
@@ -1,21 +1,7 @@
 #include "boost_defs.hpp"
 
-#include "human_interface_device.hpp"
-#include "iokit_utility.hpp"
-#include "logger.hpp"
-#include <CoreFoundation/CoreFoundation.h>
-#include <IOKit/IOKitLib.h>
-#include <IOKit/hid/IOHIDDevice.h>
-#include <IOKit/hid/IOHIDElement.h>
-#include <IOKit/hid/IOHIDManager.h>
-#include <IOKit/hid/IOHIDQueue.h>
-#include <IOKit/hid/IOHIDValue.h>
-#include <IOKit/hidsystem/IOHIDShared.h>
-#include <IOKit/hidsystem/ev_keymap.h>
-#include <boost/bind.hpp>
+#include "hid_manager.hpp"
 #include <boost/optional/optional_io.hpp>
-#include <iostream>
-#include <mach/mach_time.h>
 
 namespace {
 class dump_hid_value final {
@@ -23,83 +9,22 @@ public:
   dump_hid_value(const dump_hid_value&) = delete;
 
   dump_hid_value(void) {
-    manager_ = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
-    if (!manager_) {
-      return;
-    }
+    hid_manager_.device_detected.connect([&](auto&& human_interface_device) {
+      human_interface_device.set_value_callback([&](auto&& human_interface_device,
+                                                    auto&& event_queue) {
+        value_callback(human_interface_device, event_queue);
+      });
+      human_interface_device.observe();
+    });
 
-    auto device_matching_dictionaries = krbn::iokit_utility::create_device_matching_dictionaries({
+    hid_manager_.start({
         std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_keyboard),
         std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_mouse),
         std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_pointer),
     });
-    if (device_matching_dictionaries) {
-      IOHIDManagerSetDeviceMatchingMultiple(manager_, device_matching_dictionaries);
-      CFRelease(device_matching_dictionaries);
-
-      IOHIDManagerRegisterDeviceMatchingCallback(manager_, static_device_matching_callback, this);
-      IOHIDManagerRegisterDeviceRemovalCallback(manager_, static_device_removal_callback, this);
-
-      IOHIDManagerScheduleWithRunLoop(manager_, CFRunLoopGetMain(), kCFRunLoopDefaultMode);
-    }
   }
 
 private:
-  static void static_device_matching_callback(void* _Nullable context, IOReturn result, void* _Nullable sender, IOHIDDeviceRef _Nonnull device) {
-    if (result != kIOReturnSuccess || !device) {
-      return;
-    }
-
-    auto self = static_cast<dump_hid_value*>(context);
-    if (!self) {
-      return;
-    }
-
-    self->device_matching_callback(device);
-  }
-
-  void device_matching_callback(IOHIDDeviceRef _Nonnull device) {
-    if (!device) {
-      return;
-    }
-
-    krbn::iokit_utility::log_matching_device(device);
-
-    hids_[device] = std::make_unique<krbn::human_interface_device>(device);
-    auto& dev = hids_[device];
-    dev->set_value_callback(boost::bind(&dump_hid_value::value_callback, this, _1, _2));
-    dev->observe();
-  }
-
-  static void static_device_removal_callback(void* _Nullable context, IOReturn result, void* _Nullable sender, IOHIDDeviceRef _Nonnull device) {
-    if (result != kIOReturnSuccess || !device) {
-      return;
-    }
-
-    auto self = static_cast<dump_hid_value*>(context);
-    if (!self) {
-      return;
-    }
-
-    self->device_removal_callback(device);
-  }
-
-  void device_removal_callback(IOHIDDeviceRef _Nonnull device) {
-    if (!device) {
-      return;
-    }
-
-    krbn::iokit_utility::log_removal_device(device);
-
-    auto it = hids_.find(device);
-    if (it != hids_.end()) {
-      auto& dev = it->second;
-      if (dev) {
-        hids_.erase(it);
-      }
-    }
-  }
-
   void value_callback(krbn::human_interface_device& device,
                       krbn::event_queue& event_queue) {
     for (const auto& queued_event : event_queue.get_events()) {
@@ -210,9 +135,7 @@ private:
     event_queue.clear_events();
   }
 
-  IOHIDManagerRef _Nullable manager_;
-  std::unordered_map<IOHIDDeviceRef, std::unique_ptr<krbn::human_interface_device>> hids_;
-  krbn::event_queue event_queue_;
+  krbn::hid_manager hid_manager_;
 };
 } // namespace
 
