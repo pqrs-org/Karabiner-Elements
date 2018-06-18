92cc1953fdf08231315648d56357f9931810d86e
Takayama Fumihiko <tekezo@pqrs.org>
2018-06-19 00:20:16 +0900

    use hid_manager

--- appendix/control_led/main.cpp
+++ appendix/control_led/main.cpp
@@ -1,20 +1,4 @@
-#include "boost_defs.hpp"
-
-#include "human_interface_device.hpp"
-#include "iokit_utility.hpp"
-#include "logger.hpp"
-#include <CoreFoundation/CoreFoundation.h>
-#include <IOKit/IOKitLib.h>
-#include <IOKit/hid/IOHIDDevice.h>
-#include <IOKit/hid/IOHIDElement.h>
-#include <IOKit/hid/IOHIDManager.h>
-#include <IOKit/hid/IOHIDQueue.h>
-#include <IOKit/hid/IOHIDValue.h>
-#include <IOKit/hidsystem/IOHIDShared.h>
-#include <IOKit/hidsystem/ev_keymap.h>
-#include <boost/bind.hpp>
-#include <iostream>
-#include <mach/mach_time.h>
+#include "hid_manager.hpp"
 
 namespace {
 class control_led final {
@@ -22,109 +6,43 @@ public:
   control_led(const control_led&) = delete;
 
   control_led(void) {
-    manager_ = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
-    if (!manager_) {
-      return;
-    }
+    hid_manager_.device_detected.connect([&](auto&& human_interface_device) {
+      auto r = human_interface_device.open();
+      if (r != kIOReturnSuccess) {
+        krbn::logger::get_logger().error("failed to open");
+        return;
+      }
 
-    auto device_matching_dictionaries = krbn::iokit_utility::create_device_matching_dictionaries({
+      if (auto caps_lock_led_state = human_interface_device.get_caps_lock_led_state()) {
+        switch (*caps_lock_led_state) {
+          case krbn::led_state::on:
+            krbn::logger::get_logger().info("caps_lock_led_state is on.");
+            break;
+          case krbn::led_state::off:
+            krbn::logger::get_logger().info("caps_lock_led_state is off.");
+            break;
+        }
+
+        if (caps_lock_led_state == krbn::led_state::on) {
+          human_interface_device.set_caps_lock_led_state(krbn::led_state::off);
+        } else {
+          human_interface_device.set_caps_lock_led_state(krbn::led_state::on);
+        }
+
+        krbn::logger::get_logger().info("set_caps_lock_led_state is called.");
+
+      } else {
+        krbn::logger::get_logger().info("failed to get caps_lock_led_state.");
+      }
+    });
+
+    hid_manager_.start({
         std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_keyboard),
     });
-    if (device_matching_dictionaries) {
-      IOHIDManagerSetDeviceMatchingMultiple(manager_, device_matching_dictionaries);
-      CFRelease(device_matching_dictionaries);
-
-      IOHIDManagerRegisterDeviceMatchingCallback(manager_, static_device_matching_callback, this);
-      IOHIDManagerRegisterDeviceRemovalCallback(manager_, static_device_removal_callback, this);
-
-      IOHIDManagerScheduleWithRunLoop(manager_, CFRunLoopGetMain(), kCFRunLoopDefaultMode);
-    }
   }
 
 private:
-  static void static_device_matching_callback(void* _Nullable context, IOReturn result, void* _Nullable sender, IOHIDDeviceRef _Nonnull device) {
-    if (result != kIOReturnSuccess || !device) {
-      return;
-    }
-
-    auto self = static_cast<control_led*>(context);
-    if (!self) {
-      return;
-    }
-
-    self->device_matching_callback(device);
-  }
-
-  void device_matching_callback(IOHIDDeviceRef _Nonnull device) {
-    if (!device) {
-      return;
-    }
-
-    krbn::iokit_utility::log_matching_device(device);
-
-    hids_[device] = std::make_unique<krbn::human_interface_device>(device);
-    auto& dev = hids_[device];
-
-    auto kr = dev->open();
-    if (kr != kIOReturnSuccess) {
-      krbn::logger::get_logger().error("failed to dev->open(). {0}", kr);
-    }
-    dev->schedule();
-
-    if (auto caps_lock_led_state = dev->get_caps_lock_led_state()) {
-      switch (*caps_lock_led_state) {
-        case krbn::led_state::on:
-          krbn::logger::get_logger().info("caps_lock_led_state is on.");
-          break;
-        case krbn::led_state::off:
-          krbn::logger::get_logger().info("caps_lock_led_state is off.");
-          break;
-      }
-
-      if (caps_lock_led_state == krbn::led_state::on) {
-        dev->set_caps_lock_led_state(krbn::led_state::off);
-      } else {
-        dev->set_caps_lock_led_state(krbn::led_state::on);
-      }
-
-      krbn::logger::get_logger().info("set_caps_lock_led_state is called.");
-
-    } else {
-      krbn::logger::get_logger().info("failed to get caps_lock_led_state.");
-    }
-  }
-
-  static void static_device_removal_callback(void* _Nullable context, IOReturn result, void* _Nullable sender, IOHIDDeviceRef _Nonnull device) {
-    if (result != kIOReturnSuccess || !device) {
-      return;
-    }
-
-    auto self = static_cast<control_led*>(context);
-    if (!self) {
-      return;
-    }
-
-    self->device_removal_callback(device);
-  }
-
-  void device_removal_callback(IOHIDDeviceRef _Nonnull device) {
-    if (!device) {
-      return;
-    }
-
-    krbn::iokit_utility::log_removal_device(device);
-
-    auto it = hids_.find(device);
-    if (it != hids_.end()) {
-      auto& dev = it->second;
-      if (dev) {
-        hids_.erase(it);
-      }
-    }
-  }
-
-  IOHIDManagerRef _Nullable manager_;
-  std::unordered_map<IOHIDDeviceRef, std::unique_ptr<krbn::human_interface_device>> hids_;
+  krbn::hid_manager hid_manager_;
 };
 } // namespace
 
