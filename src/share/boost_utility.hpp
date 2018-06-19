9e726e96795145c806c0c02e2035160a24c01535
Takayama Fumihiko <tekezo@pqrs.org>
2018-06-19 17:37:06 +0900

    add boost_utility.hpp

---
+++ src/share/boost_utility.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "boost_defs.hpp"
+
+#include <boost/signals2.hpp>
+
+namespace krbn {
+class boost_utility final {
+public:
+  struct signal2_combiner_call_while_true {
+    typedef bool result_type;
+
+    template <typename input_iterator>
+    result_type operator()(input_iterator first_observer, input_iterator last_observer) const {
+      result_type value = true;
+      for (; first_observer != last_observer && value; std::advance(first_observer, 1)) {
+        value = *first_observer;
+      }
+      return value;
+    }
+  };
+};
+} // namespace krbn
