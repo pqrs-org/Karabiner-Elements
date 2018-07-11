71340748161913391a320087d21c7244272c7854
Takayama Fumihiko <tekezo@pqrs.org>
2018-07-11 19:21:37 +0900

    use hid_observer

--- src/core/observer/include/device_observer.hpp
+++ src/core/observer/include/device_observer.hpp
@@ -6,6 +6,7 @@
 #include "grabbable_state_manager.hpp"
 #include "grabber_client.hpp"
 #include "hid_manager.hpp"
+#include "hid_observer.hpp"
 #include "logger.hpp"
 #include "types.hpp"
 
@@ -40,25 +41,30 @@ public:
                                       ungrabbable_temporarily_reason::none,
                                       mach_absolute_time());
 
-      human_interface_device->device_observed.connect([this](auto&& human_interface_device) {
-        logger::get_logger().info("{0} is observed.",
-                                  human_interface_device.get_name_for_log());
+      human_interface_device->values_arrived.connect([this](auto&& human_interface_device,
+                                                            auto&& event_queue) {
+        grabbable_state_manager_.update(event_queue);
+      });
 
-        if (auto state = grabbable_state_manager_.get_grabbable_state(human_interface_device.get_registry_entry_id())) {
+      auto observer = std::make_shared<hid_observer>(human_interface_device);
+      observer->device_observed.connect([this](auto&& human_interface_device) {
+        logger::get_logger().info("{0} is observed.",
+                                  human_interface_device->get_name_for_log());
+
+        if (auto state = grabbable_state_manager_.get_grabbable_state(human_interface_device->get_registry_entry_id())) {
           // Keep grabbable_state if the state is already changed by value_callback.
           if (state->first == grabbable_state::device_error) {
-            grabbable_state_manager_.update(human_interface_device.get_registry_entry_id(),
+            grabbable_state_manager_.update(human_interface_device->get_registry_entry_id(),
                                             grabbable_state::grabbable,
                                             ungrabbable_temporarily_reason::none,
                                             mach_absolute_time());
           }
         }
       });
-      human_interface_device->values_arrived.connect([this](auto&& human_interface_device,
-                                                            auto&& event_queue) {
-        grabbable_state_manager_.update(event_queue);
-      });
-      human_interface_device->observe();
+
+      observer->observe();
+
+      hid_observers_[human_interface_device->get_registry_entry_id()] = observer;
     });
 
     hid_manager_.start({
@@ -69,6 +75,7 @@ public:
   }
 
   ~device_observer(void) {
+    hid_observers_.clear();
     hid_manager_.stop();
   }
 
@@ -76,6 +83,7 @@ private:
   const grabber_client& grabber_client_;
 
   hid_manager hid_manager_;
+  std::unordered_map<registry_entry_id, std::shared_ptr<hid_observer>> hid_observers_;
   grabbable_state_manager grabbable_state_manager_;
 };
 } // namespace krbn
