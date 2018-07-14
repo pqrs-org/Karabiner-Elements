f2720d463ddffa815eb5f2e71e3aa1d2a21d7377
Takayama Fumihiko <tekezo@pqrs.org>
2018-07-14 17:17:09 +0900

    add event_tap_utility

---
+++ src/share/event_tap_utility.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "boost_defs.hpp"
+
+#include "event_queue.hpp"
+#include <CoreGraphics/CoreGraphics.h>
+#include <boost/optional.hpp>
+
+namespace krbn {
+class event_tap_utility final {
+public:
+  static boost::optional<std::pair<event_type, event_queue::queued_event::event>> make_event(CGEventType type,
+                                                                                             CGEventRef event) {
+    switch (type) {
+      case kCGEventLeftMouseDown:
+        return std::make_pair(event_type::key_down,
+                              event_queue::queued_event::event(pointing_button::button1));
+
+      case kCGEventLeftMouseUp:
+        return std::make_pair(event_type::key_up,
+                              event_queue::queued_event::event(pointing_button::button1));
+
+      case kCGEventRightMouseDown:
+        return std::make_pair(event_type::key_down,
+                              event_queue::queued_event::event(pointing_button::button2));
+
+      case kCGEventRightMouseUp:
+        return std::make_pair(event_type::key_up,
+                              event_queue::queued_event::event(pointing_button::button2));
+
+      case kCGEventOtherMouseDown:
+        return std::make_pair(event_type::key_down,
+                              event_queue::queued_event::event(pointing_button::button3));
+
+      case kCGEventOtherMouseUp:
+        return std::make_pair(event_type::key_up,
+                              event_queue::queued_event::event(pointing_button::button3));
+
+      case kCGEventMouseMoved:
+      case kCGEventLeftMouseDragged:
+      case kCGEventRightMouseDragged:
+      case kCGEventOtherMouseDragged:
+        return std::make_pair(event_type::single,
+                              event_queue::queued_event::event(pointing_motion()));
+
+      case kCGEventScrollWheel: {
+        // Set non-zero value for `manipulator::details::base::unset_alone_if_needed`.
+        pointing_motion pointing_motion;
+        pointing_motion.set_vertical_wheel(1);
+        return std::make_pair(event_type::single,
+                              event_queue::queued_event::event(pointing_motion));
+      }
+
+      case kCGEventNull:
+      case kCGEventKeyDown:
+      case kCGEventKeyUp:
+      case kCGEventFlagsChanged:
+      case kCGEventTabletPointer:
+      case kCGEventTabletProximity:
+      case kCGEventTapDisabledByTimeout:
+      case kCGEventTapDisabledByUserInput:
+        break;
+    }
+
+    return boost::none;
+  }
+};
+} // namespace krbn
---
+++ tests/src/event_tap_utility/test.cpp
@@ -0,0 +1,22 @@
+#define CATCH_CONFIG_MAIN
+#include "../../vendor/catch/catch.hpp"
+
+#include "event_tap_utility.hpp"
+#include "thread_utility.hpp"
+
+TEST_CASE("initialize") {
+  krbn::thread_utility::register_main_thread();
+}
+
+TEST_CASE("make_event") {
+  {
+    auto actual = krbn::event_tap_utility::make_event(kCGEventLeftMouseDown, nullptr);
+    REQUIRE(actual->first == krbn::event_type::key_down);
+    REQUIRE(actual->second == krbn::event_queue::queued_event::event(krbn::pointing_button::button1));
+  }
+  {
+    auto actual = krbn::event_tap_utility::make_event(kCGEventOtherMouseUp, nullptr);
+    REQUIRE(actual->first == krbn::event_type::key_up);
+    REQUIRE(actual->second == krbn::event_queue::queued_event::event(krbn::pointing_button::button3));
+  }
+}
