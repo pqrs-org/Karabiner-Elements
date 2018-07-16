def3d85db8fb102cfec1053767cda6a69c0e4c7d
Takayama Fumihiko <tekezo@pqrs.org>
2018-07-16 21:56:44 +0900

    add grabbable_state_queues_manager

---
+++ src/share/grabbable_state_queues_manager.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "device_detail.hpp"
+#include "event_queue.hpp"
+#include "grabbable_state_queue.hpp"
+#include <unordered_map>
+
+namespace krbn {
+class grabbable_state_queues_manager final {
+public:
+  std::shared_ptr<grabbable_state> find_current_grabbable_state(registry_entry_id registry_entry_id) const {
+    auto it = queues_.find(registry_entry_id);
+    if (it != std::end(queues_)) {
+      return it->second->find_current_grabbable_state();
+    }
+
+    return nullptr;
+  }
+
+  void clear(void) {
+    queues_.clear();
+  }
+
+  void update_grabbable_state(std::shared_ptr<grabbable_state> state) {
+    if (!state) {
+      return;
+    }
+
+    auto queue = find_or_create_queue(state->get_registry_entry_id());
+    queue->push_back_grabbable_state(state);
+  }
+
+  void update_first_grabbed_event_time_stamp(const event_queue& event_queue) {
+    for (const auto& queued_event : event_queue.get_events()) {
+      if (auto device_detail = types::find_device_detail(queued_event.get_device_id())) {
+        if (auto registry_entry_id = device_detail->get_registry_entry_id()) {
+          auto queue = find_or_create_queue(*registry_entry_id);
+          auto time_stamp = queued_event.get_event_time_stamp().get_time_stamp();
+          queue->update_first_grabbed_event_time_stamp(time_stamp);
+        }
+      }
+    }
+  }
+
+  void erase_queue(registry_entry_id registry_entry_id) {
+    queues_.erase(registry_entry_id);
+  }
+
+private:
+  std::shared_ptr<grabbable_state_queue> find_or_create_queue(registry_entry_id registry_entry_id) {
+    auto it = queues_.find(registry_entry_id);
+    if (it != std::end(queues_)) {
+      return it->second;
+    }
+
+    auto queue = std::make_shared<grabbable_state_queue>();
+    queues_[registry_entry_id] = queue;
+    return queue;
+  }
+
+  std::unordered_map<registry_entry_id, std::shared_ptr<grabbable_state_queue>> queues_;
+};
+} // namespace krbn
---
+++ tests/src/grabbable_state_queues_manager/test.cpp
@@ -0,0 +1,83 @@
+#define CATCH_CONFIG_MAIN
+#include "../../vendor/catch/catch.hpp"
+
+#include "grabbable_state_queues_manager.hpp"
+#include "thread_utility.hpp"
+
+namespace {
+auto device_id1 = krbn::device_id::zero;
+auto device_id2 = krbn::device_id::zero;
+auto registry_entry_id1 = krbn::registry_entry_id(1);
+auto registry_entry_id2 = krbn::registry_entry_id(2);
+} // namespace
+
+TEST_CASE("initialize") {
+  using namespace std::string_literals;
+
+  krbn::thread_utility::register_main_thread();
+
+  {
+    auto device_detail = std::make_shared<krbn::device_detail>(krbn::vendor_id(1001),
+                                                               krbn::product_id(2001),
+                                                               krbn::location_id(3001),
+                                                               "manufacturer_1"s,
+                                                               "product_1"s,
+                                                               "serial_number_1"s,
+                                                               "transport_1"s,
+                                                               registry_entry_id1,
+                                                               true,
+                                                               true);
+    device_id1 = krbn::types::make_new_device_id(device_detail);
+
+    auto actual = krbn::types::find_device_detail(device_id1)->get_registry_entry_id();
+    REQUIRE(*actual == registry_entry_id1);
+  }
+
+  {
+    auto device_detail = std::make_shared<krbn::device_detail>(krbn::vendor_id(1002),
+                                                               krbn::product_id(2002),
+                                                               krbn::location_id(3002),
+                                                               "manufacturer_2"s,
+                                                               "product_2"s,
+                                                               "serial_number_2"s,
+                                                               "transport_2"s,
+                                                               registry_entry_id2,
+                                                               true,
+                                                               true);
+    device_id2 = krbn::types::make_new_device_id(device_detail);
+
+    auto actual = krbn::types::find_device_detail(device_id2)->get_registry_entry_id();
+    REQUIRE(*actual == registry_entry_id2);
+  }
+}
+
+TEST_CASE("grabbable_state_queues_manager") {
+  {
+    krbn::grabbable_state_queues_manager manager;
+    REQUIRE(!manager.find_current_grabbable_state(registry_entry_id1));
+    REQUIRE(!manager.find_current_grabbable_state(registry_entry_id2));
+
+    for (auto time_stamp = 1000ULL; time_stamp < 10000ULL; time_stamp += 1000ULL) {
+      auto state = std::make_shared<krbn::grabbable_state>(
+          registry_entry_id1,
+          krbn::grabbable_state::state::grabbable,
+          krbn::grabbable_state::ungrabbable_temporarily_reason::none,
+          time_stamp);
+      manager.update_grabbable_state(state);
+      REQUIRE(manager.find_current_grabbable_state(registry_entry_id1)->get_time_stamp() == time_stamp);
+      REQUIRE(!manager.find_current_grabbable_state(registry_entry_id2));
+    }
+
+    {
+      krbn::event_queue event_queue;
+      event_queue.emplace_back_event(device_id1,
+                                     krbn::event_queue::queued_event::event_time_stamp(5000),
+                                     krbn::event_queue::queued_event::event(krbn::key_code::a),
+                                     krbn::event_type::key_down,
+                                     krbn::event_queue::queued_event::event(krbn::key_code::a));
+      manager.update_first_grabbed_event_time_stamp(event_queue);
+      REQUIRE(manager.find_current_grabbable_state(registry_entry_id1)->get_time_stamp() == 4000ULL);
+      REQUIRE(!manager.find_current_grabbable_state(registry_entry_id2));
+    }
+  }
+}
