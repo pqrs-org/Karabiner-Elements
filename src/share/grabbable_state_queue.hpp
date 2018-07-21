9e1416e25d1946580658ac90f699565346e3e8db
Takayama Fumihiko <tekezo@pqrs.org>
2018-07-21 23:52:29 +0900

    add grabbable_state_queue::grabbable_state_changed

--- src/share/grabbable_state_queue.hpp
+++ src/share/grabbable_state_queue.hpp
@@ -6,11 +6,19 @@
 #include "types.hpp"
 #include <boost/circular_buffer.hpp>
 #include <boost/optional.hpp>
+#include <boost/signals2.hpp>
 #include <memory>
 
 namespace krbn {
 class grabbable_state_queue final {
 public:
+  // Signals
+
+  boost::signals2::signal<void(boost::optional<grabbable_state>)>
+      grabbable_state_changed;
+
+  // Methods
+
   const int max_entries = 32;
 
   grabbable_state_queue(void) : grabbable_states_(max_entries) {
@@ -29,8 +37,14 @@ public:
   }
 
   void clear(void) {
+    auto old_state = find_current_grabbable_state();
+
     grabbable_states_.clear();
     first_grabbed_event_time_stamp_ = boost::none;
+
+    auto new_state = find_current_grabbable_state();
+
+    call_grabbable_state_changed_if_needed(old_state, new_state);
   }
 
   bool push_back_grabbable_state(const grabbable_state& state) {
@@ -40,11 +54,17 @@ public:
       return false;
     }
 
+    auto old_state = find_current_grabbable_state();
+
     if (grabbable_states_.full()) {
       grabbable_states_.pop_front();
     }
 
     grabbable_states_.push_back(state);
+
+    auto new_state = find_current_grabbable_state();
+
+    call_grabbable_state_changed_if_needed(old_state, new_state);
 
     return true;
   }
@@ -59,6 +79,8 @@ public:
     logger::get_logger().info("first grabbed event: time_stamp:{0}",
                               time_stamp);
 
+    auto old_state = find_current_grabbable_state();
+
     // Erase states after first_grabbed_event_time_stamp_.
     grabbable_states_.erase(std::remove_if(std::begin(grabbable_states_),
                                            std::end(grabbable_states_),
@@ -66,6 +88,10 @@ public:
                                              return s.get_time_stamp() >= time_stamp;
                                            }),
                             std::end(grabbable_states_));
+
+    auto new_state = find_current_grabbable_state();
+
+    call_grabbable_state_changed_if_needed(old_state, new_state);
 
     return true;
   }
@@ -75,10 +101,26 @@ public:
   }
 
 private:
+  void call_grabbable_state_changed_if_needed(boost::optional<grabbable_state> old_state,
+                                              boost::optional<grabbable_state> new_state) {
+    if (!old_state &&
+        !new_state) {
+      return;
+    }
+
+    if (old_state &&
+        new_state &&
+        old_state->equals_except_time_stamp(*new_state)) {
+      return;
+    }
+
+    grabbable_state_changed(new_state);
+  }
+
   // Keep multiple entries for when `push_back_entry` is called multiple times before `set_first_grabbed_event_time_stamp`.
   // (We should remove entries after first_grabbed_event_time_stamp_.)
-
   boost::circular_buffer<grabbable_state> grabbable_states_;
+
   boost::optional<uint64_t> first_grabbed_event_time_stamp_;
 };
 } // namespace krbn
--- tests/src/grabbable_state_queue/test.cpp
+++ tests/src/grabbable_state_queue/test.cpp
@@ -1,8 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include "../../vendor/catch/catch.hpp"
 
+#include "boost_defs.hpp"
+
 #include "grabbable_state_queue.hpp"
 #include "thread_utility.hpp"
+#include <boost/optional/optional_io.hpp>
 
 namespace {
 auto registry_entry_id1 = krbn::registry_entry_id(1);
@@ -16,8 +19,19 @@ TEST_CASE("grabbable_state_queue") {
   {
     krbn::grabbable_state_queue queue;
 
+    boost::optional<krbn::grabbable_state> last_changed_grabbable_state;
+    int grabbable_state_changed_count = 0;
+
+    queue.grabbable_state_changed.connect([&](auto&& grabbable_state) {
+      last_changed_grabbable_state = grabbable_state;
+      ++grabbable_state_changed_count;
+    });
+
     REQUIRE(!queue.find_current_grabbable_state());
     REQUIRE(!queue.get_first_grabbed_event_time_stamp());
+
+    queue.clear();
+    REQUIRE(grabbable_state_changed_count == 0);
 
     for (auto time_stamp = 1000ULL; time_stamp < 10000ULL; time_stamp += 1000ULL) {
       krbn::grabbable_state state(registry_entry_id1,
@@ -29,6 +43,16 @@ TEST_CASE("grabbable_state_queue") {
       REQUIRE(*(queue.find_current_grabbable_state()) == state);
     }
 
+    // Check `grabbable_state_changed` signal
+    {
+      krbn::grabbable_state expected(registry_entry_id1,
+                                     krbn::grabbable_state::state::grabbable,
+                                     krbn::grabbable_state::ungrabbable_temporarily_reason::none,
+                                     1000ULL);
+      REQUIRE(last_changed_grabbable_state == expected);
+      REQUIRE(grabbable_state_changed_count == 1);
+    }
+
     // Remove states after first_grabbed_event_time_stamp_.
     REQUIRE(queue.update_first_grabbed_event_time_stamp(5000ULL));
     REQUIRE(*(queue.get_first_grabbed_event_time_stamp()) == 5000ULL);
@@ -37,6 +61,8 @@ TEST_CASE("grabbable_state_queue") {
     REQUIRE(!queue.update_first_grabbed_event_time_stamp(3000ULL));
     REQUIRE(*(queue.get_first_grabbed_event_time_stamp()) == 5000ULL);
     REQUIRE(queue.find_current_grabbable_state()->get_time_stamp() == 4000ULL);
+
+    REQUIRE(grabbable_state_changed_count == 1);
 
     // Ignore events after first_grabbed_event_time_stamp_.
     {
@@ -49,11 +75,15 @@ TEST_CASE("grabbable_state_queue") {
     }
     {
       krbn::grabbable_state state(registry_entry_id1,
-                                  krbn::grabbable_state::state::grabbable,
-                                  krbn::grabbable_state::ungrabbable_temporarily_reason::none,
+                                  krbn::grabbable_state::state::ungrabbable_temporarily,
+                                  krbn::grabbable_state::ungrabbable_temporarily_reason::key_repeating,
                                   4500ULL);
       REQUIRE(queue.push_back_grabbable_state(state));
       REQUIRE(queue.find_current_grabbable_state()->get_time_stamp() == 4500ULL);
+
+      // Check `grabbable_state_changed` signal
+      REQUIRE(last_changed_grabbable_state == state);
+      REQUIRE(grabbable_state_changed_count == 2);
     }
 
     // Unset first_grabbed_event_time_stamp_.
@@ -63,5 +93,22 @@ TEST_CASE("grabbable_state_queue") {
     REQUIRE(queue.update_first_grabbed_event_time_stamp(3000ULL));
     REQUIRE(*(queue.get_first_grabbed_event_time_stamp()) == 3000ULL);
     REQUIRE(queue.find_current_grabbable_state()->get_time_stamp() == 2000ULL);
+
+    // Check `grabbable_state_changed` signal
+    {
+      krbn::grabbable_state expected(registry_entry_id1,
+                                     krbn::grabbable_state::state::grabbable,
+                                     krbn::grabbable_state::ungrabbable_temporarily_reason::none,
+                                     2000ULL);
+      REQUIRE(last_changed_grabbable_state == expected);
+      REQUIRE(grabbable_state_changed_count == 3);
+    }
+
+    // Clear
+    queue.clear();
+
+    // Check `grabbable_state_changed` signal
+    REQUIRE(last_changed_grabbable_state == boost::none);
+    REQUIRE(grabbable_state_changed_count == 4);
   }
 }
