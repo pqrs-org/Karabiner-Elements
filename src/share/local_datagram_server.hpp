ab1f55efde37fcf98eb1653774124c80b1ad27ad
Takayama Fumihiko <tekezo@pqrs.org>
2018-07-28 00:05:17 +0900

    unlink socket file in destructor

--- src/share/local_datagram_server.hpp
+++ src/share/local_datagram_server.hpp
@@ -7,21 +7,35 @@ BEGIN_BOOST_INCLUDE
 #include <boost/bind.hpp>
 END_BOOST_INCLUDE
 
+#include <unistd.h>
+
 namespace krbn {
 class local_datagram_server final {
 public:
+  // Signals
+
+  // Note: These signals are fired on non-main thread.
+
+  // Methods
+
   local_datagram_server(const local_datagram_server&) = delete;
 
-  local_datagram_server(const char* _Nonnull path) : endpoint_(path),
-                                                     io_service_(),
-                                                     socket_(io_service_, endpoint_),
-                                                     deadline_(io_service_) {
+  local_datagram_server(const std::string& path) : endpoint_(path),
+                                                   io_service_(),
+                                                   socket_(io_service_, endpoint_),
+                                                   deadline_(io_service_) {
     deadline_.expires_at(boost::posix_time::pos_infin);
     check_deadline();
   }
 
+  ~local_datagram_server(void) {
+    unlink(endpoint_.path().c_str());
+  }
+
   // from doc/html/boost_asio/example/cpp03/timeouts/blocking_udp_client.cpp
-  std::size_t receive(const boost::asio::mutable_buffer& buffer, boost::posix_time::time_duration timeout, boost::system::error_code& ec) {
+  std::size_t receive(const boost::asio::mutable_buffer& buffer,
+                      boost::posix_time::time_duration timeout,
+                      boost::system::error_code& ec) {
     deadline_.expires_from_now(timeout);
 
     ec = boost::asio::error::would_block;
@@ -38,7 +52,7 @@ public:
   }
 
 private:
-  void check_deadline() {
+  void check_deadline(void) {
     if (deadline_.expires_at() <= boost::asio::deadline_timer::traits_type::now()) {
       socket_.cancel();
       deadline_.expires_at(boost::posix_time::pos_infin);
@@ -47,8 +61,10 @@ private:
     deadline_.async_wait(boost::bind(&local_datagram_server::check_deadline, this));
   }
 
-  static void handle_receive(const boost::system::error_code& ec, std::size_t length,
-                             boost::system::error_code* _Nonnull out_ec, std::size_t* _Nonnull out_length) {
+  static void handle_receive(const boost::system::error_code& ec,
+                             std::size_t length,
+                             boost::system::error_code* _Nonnull out_ec,
+                             std::size_t* _Nonnull out_length) {
     *out_ec = ec;
     *out_length = length;
   }
