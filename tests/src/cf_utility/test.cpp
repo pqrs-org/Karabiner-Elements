7c90a15a627cee1fac6bc5477af9086a6743051d
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-08 23:50:37 +0900

    update tests

--- tests/src/cf_utility/test.cpp
+++ tests/src/cf_utility/test.cpp
@@ -9,34 +9,60 @@ TEST_CASE("initialize") {
   krbn::thread_utility::register_main_thread();
 }
 
-TEST_CASE("run_loop_thread") {
-  for (int i = 0; i < 5000; ++i) {
-    __block int count1 = 0;
-    __block int count2 = 0;
+namespace {
+class run_loop_thread_test final {
+public:
+  run_loop_thread_test(void) : count1_(0),
+                               count2_(0) {
+    for (int i = 0; i < 5000; ++i) {
+      count1_ = 0;
+      count2_ = 0;
 
-    auto thread1 = std::make_shared<krbn::cf_utility::run_loop_thread>();
-    auto thread2 = std::make_shared<krbn::cf_utility::run_loop_thread>();
+      thread1_ = std::make_shared<krbn::cf_utility::run_loop_thread>();
+      thread2_ = std::make_shared<krbn::cf_utility::run_loop_thread>();
 
-    // thread1
+      // thread1 (loop)
 
-    for (int j = 0; j < 5; ++j) {
-      thread1->enqueue(^{
-        ++count1;
-        // krbn::logger::get_logger().info("thread1 {0} {1}", j, count1);
-      });
+      for (int j = 0; j < 5; ++j) {
+        thread1_->enqueue(^{
+          ++count1_;
+          // krbn::logger::get_logger().info("thread1 {0} {1}", j, count1);
+        });
+      }
+
+      // thread2 (recursive)
+
+      enqueue2();
+
+      // Verify counts
+
+      thread1_ = nullptr;
+      thread2_ = nullptr;
+
+      REQUIRE(count1_ == 5);
+      REQUIRE(count2_ == 3);
     }
-
-    // thread2
-
-    thread2->enqueue(^{
-      ++count2;
-      // krbn::logger::get_logger().info("thread2 {0}", count2);
-    });
-
-    thread1 = nullptr;
-    thread2 = nullptr;
-
-    REQUIRE(count1 == 5);
-    REQUIRE(count2 == 1);
   }
+
+private:
+  void enqueue2(void) {
+    thread2_->enqueue(^{
+      ++count2_;
+      if (count2_ < 3) {
+        enqueue2();
+      }
+    });
+  }
+
+private:
+  std::shared_ptr<krbn::cf_utility::run_loop_thread> thread1_;
+  std::shared_ptr<krbn::cf_utility::run_loop_thread> thread2_;
+
+  int count1_;
+  int count2_;
+};
+} // namespace
+
+TEST_CASE("run_loop_thread") {
+  run_loop_thread_test();
 }
