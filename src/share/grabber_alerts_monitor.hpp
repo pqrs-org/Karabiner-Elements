00885b016c871f9e065d5b0e6482785c3576b882
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-10 00:51:21 +0900

    update grabber_alerts_monitor

--- src/share/grabber_alerts_monitor.hpp
+++ src/share/grabber_alerts_monitor.hpp
@@ -3,7 +3,6 @@
 #include "constants.hpp"
 #include "file_monitor.hpp"
 #include "filesystem.hpp"
-#include "gcd_utility.hpp"
 #include "json_utility.hpp"
 #include "logger.hpp"
 #include <fstream>
@@ -11,61 +10,61 @@
 namespace krbn {
 class grabber_alerts_monitor final {
 public:
-  typedef std::function<void(void)> callback;
+  // Signals
+
+  boost::signals2::signal<void(const nlohmann::json& alerts)> alerts_changed;
+
+  // Methods
 
   grabber_alerts_monitor(const grabber_alerts_monitor&) = delete;
 
-  grabber_alerts_monitor(const callback& callback) : callback_(callback) {
-    auto file_path = constants::get_grabber_alerts_json_file_path();
-    auto directory = filesystem::dirname(file_path);
-
-    std::vector<std::pair<std::string, std::vector<std::string>>> targets = {
-        {directory, {file_path}},
-    };
-    file_monitor_ = std::make_unique<file_monitor>(targets,
-                                                   [this](const std::string&) {
-                                                     auto file_path = constants::get_grabber_alerts_json_file_path();
-                                                     std::ifstream stream(file_path);
-                                                     if (stream) {
-                                                       try {
-                                                         auto json = nlohmann::json::parse(stream);
-
-                                                         // json example
-                                                         //
-                                                         // {
-                                                         //     "alerts": [
-                                                         //         "system_policy_prevents_loading_kext"
-                                                         //     ]
-                                                         // }
-
-                                                         if (auto v = json_utility::find_array(json, "alerts")) {
-                                                           if (!v->empty()) {
-                                                             auto s = json.dump();
-                                                             if (json_string_ != s) {
-                                                               json_string_ = s;
-                                                               if (callback_) {
-                                                                 callback_();
-                                                               }
-                                                             }
-                                                           }
-                                                         }
-                                                       } catch (std::exception& e) {
-                                                         logger::get_logger().error("parse error in {0}: {1}", file_path, e.what());
-                                                       }
-                                                     }
-                                                   });
+  grabber_alerts_monitor(const std::string& grabber_alerts_json_file_path) : grabber_alerts_json_file_path_(grabber_alerts_json_file_path) {
   }
 
-  ~grabber_alerts_monitor(void) {
-    gcd_utility::dispatch_sync_in_main_queue(^{
-      file_monitor_ = nullptr;
+  void start(void) {
+    std::lock_guard<std::mutex> lock(file_monitor_mutex_);
+
+    std::vector<std::string> targets = {
+        grabber_alerts_json_file_path_,
+    };
+
+    file_monitor_ = std::make_unique<file_monitor>(targets);
+
+    file_monitor_->file_changed.connect([this](const std::string& file_path) {
+      std::ifstream stream(file_path);
+      if (stream) {
+        try {
+          auto json = nlohmann::json::parse(stream);
+
+          // json example
+          //
+          // {
+          //     "alerts": [
+          //         "system_policy_prevents_loading_kext"
+          //     ]
+          // }
+
+          if (auto v = json_utility::find_array(json, "alerts")) {
+            auto s = v->dump();
+            if (last_json_string_ != s) {
+              last_json_string_ = s;
+              alerts_changed(*v);
+            }
+          }
+        } catch (std::exception& e) {
+          logger::get_logger().error("parse error in {0}: {1}", file_path, e.what());
+        }
+      }
     });
+
+    file_monitor_->start();
   }
 
 private:
-  callback callback_;
+  std::string grabber_alerts_json_file_path_;
+  boost::optional<std::string> last_json_string_;
 
-  std::string json_string_;
   std::unique_ptr<file_monitor> file_monitor_;
+  std::mutex file_monitor_mutex_;
 };
 } // namespace krbn
---
+++ tests/src/grabber_alerts_monitor/test.cpp
@@ -0,0 +1,82 @@
+#define CATCH_CONFIG_MAIN
+#include "../../vendor/catch/catch.hpp"
+
+#include "boost_defs.hpp"
+
+#include "grabber_alerts_monitor.hpp"
+#include "thread_utility.hpp"
+#include <boost/optional/optional_io.hpp>
+
+TEST_CASE("initialize") {
+  krbn::thread_utility::register_main_thread();
+}
+
+TEST_CASE("grabber_alerts_monitor") {
+  {
+    system("rm -rf target");
+    system("mkdir -p target");
+
+    std::string file_path = "target/karabiner_grabber_alerts.json";
+    krbn::grabber_alerts_monitor grabber_alerts_monitor(file_path);
+
+    boost::optional<std::string> last_alerts;
+
+    grabber_alerts_monitor.alerts_changed.connect([&](auto&& alerts) {
+      last_alerts = alerts.dump();
+    });
+
+    grabber_alerts_monitor.start();
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    REQUIRE(!last_alerts);
+
+    // ========================================
+    // Empty alerts
+    // ========================================
+
+    {
+      last_alerts = boost::none;
+
+      auto json = nlohmann::json::object({{"alerts", nlohmann::json::array()}});
+
+      system(fmt::format("echo '{0}' > {1}", json.dump(), file_path).c_str());
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+      REQUIRE(last_alerts);
+      REQUIRE(*last_alerts == json["alerts"].dump());
+    }
+
+    // ========================================
+    // Alerts
+    // ========================================
+
+    {
+      last_alerts = boost::none;
+
+      auto json = nlohmann::json::object({{"alerts", nlohmann::json::array({"example"})}});
+
+      system(fmt::format("echo '{0}' > {1}", json.dump(), file_path).c_str());
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+      REQUIRE(last_alerts);
+      REQUIRE(*last_alerts == json["alerts"].dump());
+    }
+
+    // ========================================
+    // Broken json
+    // ========================================
+
+    {
+      last_alerts = boost::none;
+
+      system(fmt::format("echo '[' > {0}", file_path).c_str());
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+      REQUIRE(!last_alerts);
+    }
+  }
+}
