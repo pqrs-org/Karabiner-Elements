3f5e062b9ad5bff8675699987a942ab9b91a8f2a
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-10 00:55:52 +0900

    start_monitor_to_stop_run_loop_when_version_changed -> start_monitor_to_stop_main_run_loop_when_version_changed

--- src/share/version_monitor_utility.hpp
+++ src/share/version_monitor_utility.hpp
@@ -5,10 +5,10 @@
 namespace krbn {
 class version_monitor_utility final {
 public:
-  static void start_monitor_to_stop_run_loop_when_version_changed(void) {
+  static void start_monitor_to_stop_main_run_loop_when_version_changed(void) {
     auto monitor = version_monitor::get_shared_instance();
 
-    monitor->changed.connect([] {
+    monitor->changed.connect([](auto&& version) {
       dispatch_async(dispatch_get_main_queue(), ^{
         CFRunLoopStop(CFRunLoopGetCurrent());
       });
