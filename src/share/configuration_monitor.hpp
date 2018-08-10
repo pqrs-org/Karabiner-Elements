9e7b554fefe010e7c5650d08282988b572cb0086
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-11 03:25:17 +0900

    update configuration_monitor

--- src/share/configuration_monitor.hpp
+++ src/share/configuration_monitor.hpp
@@ -1,71 +1,104 @@
 #pragma once
 
-#include "boost_defs.hpp"
-
 #include "core_configuration.hpp"
 #include "file_monitor.hpp"
-#include "filesystem.hpp"
 #include "logger.hpp"
 
 namespace krbn {
 class configuration_monitor final {
 public:
-  typedef std::function<void(std::shared_ptr<core_configuration> core_configuration)> core_configuration_updated_callback;
+  // Signals
+
+  boost::signals2::signal<void(std::shared_ptr<core_configuration> core_configuration)>
+      core_configuration_updated;
+
+  // Methods
 
   configuration_monitor(const std::string& user_core_configuration_file_path,
-                        const core_configuration_updated_callback& callback) : user_core_configuration_file_path_(user_core_configuration_file_path),
-                                                                               callback_(callback) {
-    std::vector<std::pair<std::string, std::vector<std::string>>> targets = {
-        {constants::get_system_configuration_directory(), {constants::get_system_core_configuration_file_path()}},
-        {filesystem::dirname(user_core_configuration_file_path), {user_core_configuration_file_path}},
-    };
-
-    file_monitor_ = std::make_unique<file_monitor>(targets,
-                                                   [this](const std::string&) {
-                                                     core_configuration_file_updated_callback();
-                                                   });
-
-    // file_monitor doesn't call the callback if target files are not exists.
-    // Thus, we call the callback manually at here if the callback is not called yet.
-    if (!core_configuration_) {
-      core_configuration_file_updated_callback();
-    }
+                        const std::string& system_core_configuration_file_path = constants::get_system_core_configuration_file_path()) : user_core_configuration_file_path_(user_core_configuration_file_path),
+                                                                                                                                         system_core_configuration_file_path_(system_core_configuration_file_path) {
   }
 
   ~configuration_monitor(void) {
-    // Release file_monitor_ in main thread to avoid callback invocations after object has been destroyed.
-    gcd_utility::dispatch_sync_in_main_queue(^{
-      file_monitor_ = nullptr;
+    file_monitor_ = nullptr;
+  }
+
+  void start() {
+    std::lock_guard<std::mutex> lock(file_monitor_mutex_);
+
+    std::vector<std::string> targets = {
+        user_core_configuration_file_path_,
+        system_core_configuration_file_path_,
+    };
+
+    file_monitor_ = std::make_unique<file_monitor>(targets);
+
+    file_monitor_->file_changed.connect([this](auto&& file_path) {
+      core_configuration_file_changed();
+    });
+
+    file_monitor_->start();
+
+    // `core_configuration_file_changed` is enqueued by `file_monitor::start` only if the file exists.
+    // We have to enqueue it manually for when the file does not exist.
+
+    file_monitor_->get_run_loop_thread()->enqueue(^{
+      {
+        std::lock_guard<std::mutex> lock(core_configuration_mutex_);
+
+        if (core_configuration_) {
+          // `core_configuration_file_changed` is already called.
+          // We do not need call the method again.
+          return;
+        }
+      }
+
+      core_configuration_file_changed();
     });
   }
 
   std::shared_ptr<core_configuration> get_core_configuration(void) const {
+    std::lock_guard<std::mutex> lock(core_configuration_mutex_);
+
     return core_configuration_;
   }
 
 private:
-  void core_configuration_file_updated_callback(void) {
-    logger::get_logger().info("Load karabiner.json...");
-
-    std::string file_path = constants::get_system_core_configuration_file_path();
+  void core_configuration_file_changed(void) {
+    std::string file_path = system_core_configuration_file_path_;
     if (filesystem::exists(user_core_configuration_file_path_)) {
       file_path = user_core_configuration_file_path_;
     }
 
+    logger::get_logger().info("Load {0}...", file_path);
+
     auto c = std::make_shared<core_configuration>(file_path);
-    if (!core_configuration_ || c->is_loaded()) {
+
+    {
+      std::lock_guard<std::mutex> lock(core_configuration_mutex_);
+
+      if (core_configuration_ && !c->is_loaded()) {
+        return;
+      }
+
+      if (core_configuration_ && core_configuration_->to_json() == c->to_json()) {
+        return;
+      }
+
       logger::get_logger().info("core_configuration is updated.");
       core_configuration_ = c;
-      if (callback_) {
-        callback_(core_configuration_);
-      }
     }
+
+    core_configuration_updated(c);
   }
 
   std::string user_core_configuration_file_path_;
-  core_configuration_updated_callback callback_;
+  std::string system_core_configuration_file_path_;
 
   std::unique_ptr<file_monitor> file_monitor_;
+  mutable std::mutex file_monitor_mutex_;
+
   std::shared_ptr<core_configuration> core_configuration_;
+  mutable std::mutex core_configuration_mutex_;
 };
 } // namespace krbn
---
+++ tests/src/configuration_monitor/test.cpp
@@ -0,0 +1,175 @@
+#define CATCH_CONFIG_MAIN
+#include "../../vendor/catch/catch.hpp"
+
+#include "configuration_monitor.hpp"
+#include "thread_utility.hpp"
+
+namespace {
+class test_configuration_monitor final {
+public:
+  test_configuration_monitor(void) : count_(0) {
+    configuration_monitor_ = std::make_unique<krbn::configuration_monitor>("target/user.json",
+                                                                           "target/system.json");
+
+    configuration_monitor_->core_configuration_updated.connect([this](auto&& core_configuration) {
+      ++count_;
+      last_core_configuration_ = core_configuration;
+    });
+
+    configuration_monitor_->start();
+
+    wait();
+  }
+
+  size_t get_count(void) const {
+    return count_;
+  }
+
+  std::shared_ptr<krbn::core_configuration> get_last_core_configuration(void) const {
+    return last_core_configuration_;
+  }
+
+  void wait(void) const {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  }
+
+private:
+  std::unique_ptr<krbn::configuration_monitor> configuration_monitor_;
+  size_t count_;
+  std::shared_ptr<krbn::core_configuration> last_core_configuration_;
+};
+} // namespace
+
+TEST_CASE("initialize") {
+  krbn::thread_utility::register_main_thread();
+}
+
+TEST_CASE("configuration_monitor") {
+  {
+    system("rm -rf target");
+    system("mkdir -p target");
+    system("echo '{}' > target/user.json");
+
+    test_configuration_monitor monitor;
+
+    REQUIRE(monitor.get_count() == 1);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == true);
+
+    // ============================================================
+    // Update user.json
+    // ============================================================
+
+    system("echo '{\"global\":{\"show_in_menu_bar\":false}}' > target/user.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 2);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == false);
+
+    // ============================================================
+    // Update system.json (ignored since user.json exists.)
+    // ============================================================
+
+    system("echo '{\"global\":{\"show_in_menu_bar\":true}}' > target/system.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 2);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == false);
+
+    // ============================================================
+    // Remove user.json (ignored file removal)
+    // ============================================================
+
+    system("rm target/user.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 2);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == false);
+
+    // ============================================================
+    // Update system.json
+    // ============================================================
+
+    system("echo '{\"global\":{\"show_in_menu_bar\":true}}' > target/system.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 3);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == true);
+
+    // ============================================================
+    // Update user.json
+    // ============================================================
+
+    system("echo '{\"global\":{\"show_in_menu_bar\":false}}' > target/user.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 4);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == false);
+  }
+
+  // ============================================================
+  // There are not user.json and system.json at start
+  // ============================================================
+
+  {
+    system("rm -rf target");
+    system("mkdir -p target");
+
+    test_configuration_monitor monitor;
+
+    REQUIRE(monitor.get_count() == 1);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == true);
+
+    // ============================================================
+    // Update user.json
+    // ============================================================
+
+    system("echo '{\"global\":{\"show_in_menu_bar\":false}}' > target/user.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 2);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == false);
+  }
+
+  // ============================================================
+  // Broken json
+  // ============================================================
+
+  {
+    system("rm -rf target");
+    system("mkdir -p target");
+    system("echo '[' > target/user.json");
+
+    test_configuration_monitor monitor;
+
+    REQUIRE(monitor.get_count() == 1);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == true);
+
+    // ============================================================
+    // Update user.json
+    // ============================================================
+
+    system("echo '{\"global\":{\"show_in_menu_bar\":false}}' > target/user.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 2);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == false);
+
+    // ============================================================
+    // Break user.json (ignored)
+    // ============================================================
+
+    system("echo '[' > target/user.json");
+
+    monitor.wait();
+
+    REQUIRE(monitor.get_count() == 2);
+    REQUIRE(monitor.get_last_core_configuration()->get_global_configuration().get_show_in_menu_bar() == false);
+  }
+}
