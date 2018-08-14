4967da3fb21d823816325dd23e3bed2f4c929624
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-15 00:54:56 +0900

    use thread_utility::timer in hid_observer

--- src/share/hid_observer.hpp
+++ src/share/hid_observer.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
-#include "gcd_utility.hpp"
+// `krbn::hid_observers_` can be used safely in a multi-threaded environment.
+
 #include "human_interface_device.hpp"
 #include "spdlog_utility.hpp"
+#include "thread_utility.hpp"
 
 namespace krbn {
 class hid_observer final {
@@ -17,87 +19,109 @@ public:
 
   hid_observer(std::shared_ptr<human_interface_device> human_interface_device) : human_interface_device_(human_interface_device),
                                                                                  observed_(false) {
+    call_slots_queue_ = std::make_unique<thread_utility::queue>();
   }
 
   ~hid_observer(void) {
     unobserve();
+
+    call_slots_queue_ = nullptr;
   }
 
   std::weak_ptr<human_interface_device> get_human_interface_device(void) {
     return human_interface_device_;
   }
 
-  bool get_observed(void) const {
-    return observed_;
-  }
-
   void observe(void) {
-    gcd_utility::dispatch_sync_in_main_queue(^{
-      log_reducer_.reset();
+    std::lock_guard<std::mutex> lock(timer_mutex_);
 
-      timer_ = nullptr;
+    if (timer_) {
+      return;
+    }
 
-      timer_ = std::make_unique<gcd_utility::fire_while_false_timer>(
-          3 * NSEC_PER_SEC,
-          ^{
-            if (auto hid = human_interface_device_.lock()) {
-              if (hid->get_removed()) {
-                return true;
-              }
+    log_reducer_.reset();
 
-              if (observed_) {
-                return true;
-              }
-
-              // ----------------------------------------
-              auto r = hid->open();
-              if (r != kIOReturnSuccess) {
-                auto message = fmt::format("IOHIDDeviceOpen error: {0} ({1}) {2}",
-                                           iokit_utility::get_error_name(r),
-                                           r,
-                                           hid->get_name_for_log());
-                log_reducer_.error(message);
-                return false;
-              }
-
-              // ----------------------------------------
-              observed_ = true;
-
-              device_observed(hid);
-
-              hid->queue_start();
-              hid->schedule();
+    timer_ = std::make_unique<thread_utility::timer>(
+        [](auto&& count) {
+          if (count == 0) {
+            return std::chrono::milliseconds(0);
+          } else {
+            return std::chrono::milliseconds(3000);
+          }
+        },
+        true,
+        [this] {
+          if (auto hid = human_interface_device_.lock()) {
+            if (hid->get_removed()) {
+              timer_->unset_repeats();
+              return;
             }
 
-            return true;
-          });
-    });
+            if (observed_) {
+              timer_->unset_repeats();
+              return;
+            }
+
+            // ----------------------------------------
+            auto r = hid->open();
+            if (r != kIOReturnSuccess) {
+              auto message = fmt::format("IOHIDDeviceOpen error: {0} ({1}) {2}",
+                                         iokit_utility::get_error_name(r),
+                                         r,
+                                         hid->get_name_for_log());
+              log_reducer_.error(message);
+              // Retry
+              return;
+            }
+
+            // ----------------------------------------
+            observed_ = true;
+
+            call_slots_queue_->push_back([this, hid] {
+              device_observed(hid);
+            });
+
+            hid->queue_start();
+            hid->schedule();
+          }
+
+          timer_->unset_repeats();
+        });
   }
 
   void unobserve(void) {
-    gcd_utility::dispatch_sync_in_main_queue(^{
+    {
+      std::lock_guard<std::mutex> lock(timer_mutex_);
+
+      if (!timer_) {
+        return;
+      }
+
       timer_ = nullptr;
 
-      if (auto hid = human_interface_device_.lock()) {
-        if (!observed_) {
-          return;
+      if (observed_) {
+        if (auto hid = human_interface_device_.lock()) {
+          hid->unschedule();
+          hid->queue_stop();
+          hid->close();
+
+          call_slots_queue_->push_back([this, hid] {
+            device_unobserved(hid);
+          });
         }
 
-        hid->unschedule();
-        hid->queue_stop();
-        hid->close();
-
         observed_ = false;
-
-        device_unobserved(hid);
       }
-    });
+    }
   }
 
 private:
   std::weak_ptr<human_interface_device> human_interface_device_;
+  std::unique_ptr<thread_utility::queue> call_slots_queue_;
   bool observed_;
-  std::unique_ptr<gcd_utility::fire_while_false_timer> timer_;
   spdlog_utility::log_reducer log_reducer_;
+
+  std::unique_ptr<thread_utility::timer> timer_;
+  mutable std::mutex timer_mutex_;
 };
 } // namespace krbn
