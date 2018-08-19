294d4281168d22a263dc55f6bf05fdacb73da02f
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-20 04:30:49 +0900

    add boost_utility::signals2_connections

--- tests/src/boost_utility/test.cpp
+++ tests/src/boost_utility/test.cpp
@@ -41,3 +41,46 @@ TEST_CASE("signals2_combiner_call_while_true") {
   REQUIRE(signal() == false);
   REQUIRE(counter == 3);
 }
+
+TEST_CASE("signals2_connections") {
+  boost::signals2::signal<void(void)> signal;
+  int counter = 0;
+
+  krbn::boost_utility::signals2_connections signals2_connections;
+
+  // ============================================================
+  // `wait_disconnect_all_connections` for empty connections.
+  // ============================================================
+
+  signals2_connections.wait_disconnect_all_connections();
+
+  // ============================================================
+  // `disconnect_all_connections` and `wait_disconnect_all_connections`.
+  // ============================================================
+
+  {
+    auto c = signal.connect([&] {
+      ++counter;
+    });
+    signals2_connections.push_back(c);
+  }
+
+  signal();
+  REQUIRE(counter == 1);
+
+  std::thread thread([&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+    signals2_connections.disconnect_all_connections();
+  });
+
+  signal();
+  REQUIRE(counter == 2);
+
+  signals2_connections.wait_disconnect_all_connections();
+
+  signal();
+  REQUIRE(counter == 2);
+
+  thread.join();
+}
