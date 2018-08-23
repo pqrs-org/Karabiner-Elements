ad46b1e0cd6055ad56cfdcca7db3833a59ea863f
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-23 22:17:32 +0900

    update menu_process_manager

--- src/core/console_user_server/include/menu_process_manager.hpp
+++ src/core/console_user_server/include/menu_process_manager.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "application_launcher.hpp"
+#include "boost_utility.hpp"
 #include "configuration_monitor.hpp"
 
 namespace krbn {
@@ -8,24 +9,42 @@ class menu_process_manager final {
 public:
   menu_process_manager(const menu_process_manager&) = delete;
 
-  menu_process_manager(std::shared_ptr<configuration_monitor> configuration_monitor) {
-    // core_configuration_updated
-
-    {
-      auto c = configuration_monitor->core_configuration_updated.connect([](auto&& core_configuration) {
-        if (core_configuration->get_global_configuration().get_show_in_menu_bar() ||
-            core_configuration->get_global_configuration().get_show_profile_name_in_menu_bar()) {
-          application_launcher::launch_menu();
-        } else {
-          application_launcher::kill_menu();
-        }
-      });
-
-      connections_.push_back(std::make_unique<boost::signals2::scoped_connection>(c));
+  menu_process_manager(std::weak_ptr<configuration_monitor> weak_configuration_monitor) : weak_configuration_monitor_(weak_configuration_monitor) {
+    if (auto configuration_monitor = weak_configuration_monitor_.lock()) {
+      // core_configuration_updated
+      {
+        auto c = configuration_monitor->core_configuration_updated.connect([](auto&& weak_core_configuration) {
+          if (auto core_configuration = weak_core_configuration.lock()) {
+            if (core_configuration->get_global_configuration().get_show_in_menu_bar() ||
+                core_configuration->get_global_configuration().get_show_profile_name_in_menu_bar()) {
+              application_launcher::launch_menu();
+            } else {
+              application_launcher::kill_menu();
+            }
+          }
+        });
+        configuration_monitor_connections_.push_back(c);
+      }
     }
   }
 
+  ~menu_process_manager(void) {
+    // Disconnect `configuration_monitor_connections_`.
+
+    if (auto configuration_monitor = weak_configuration_monitor_.lock()) {
+      configuration_monitor->get_run_loop_thread()->enqueue(^{
+        configuration_monitor_connections_.disconnect_all_connections();
+      });
+    } else {
+      configuration_monitor_connections_.disconnect_all_connections();
+    }
+
+    configuration_monitor_connections_.wait_disconnect_all_connections();
+  }
+
 private:
-  std::vector<std::unique_ptr<boost::signals2::scoped_connection>> connections_;
+  std::weak_ptr<configuration_monitor> weak_configuration_monitor_;
+
+  boost_utility::signals2_connections configuration_monitor_connections_;
 };
 } // namespace krbn
