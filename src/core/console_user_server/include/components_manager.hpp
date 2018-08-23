90c3bf1d773a6a1e22daa778aac4343cb6c4c498
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-23 22:58:41 +0900

    update console_user_server

--- src/core/console_user_server/include/components_manager.hpp
+++ src/core/console_user_server/include/components_manager.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+// `krbn::components_manager` can be used safely in a multi-threaded environment.
+
+#include "application_launcher.hpp"
 #include "configuration_monitor.hpp"
 #include "console_user_id_monitor.hpp"
 #include "constants.hpp"
 #include "frontmost_application_observer.hpp"
+#include "grabber_alerts_monitor.hpp"
 #include "grabber_client.hpp"
 #include "input_source_observer.hpp"
 #include "logger.hpp"
@@ -22,52 +26,90 @@ public:
   components_manager(const components_manager&) = delete;
 
   components_manager(void) {
+    queue_ = std::make_unique<thread_utility::queue>();
+
     version_monitor_ = version_monitor_utility::make_version_monitor_stops_main_run_loop_when_version_changed();
+    start_grabber_alerts_monitor();
 
-    console_user_id_monitor_.console_user_id_changed.connect([this](boost::optional<uid_t> uid) {
-      version_monitor_->manual_check();
+    console_user_id_monitor_ = std::make_unique<console_user_id_monitor>();
 
-      filesystem::create_directory_with_intermediate_directories(
-          constants::get_user_configuration_directory(),
-          0700);
+    console_user_id_monitor_->console_user_id_changed.connect([this](auto&& uid) {
+      queue_->push_back([this, uid] {
+        if (version_monitor_) {
+          version_monitor_->async_manual_check();
+        }
 
-      if (uid != getuid()) {
-        stop_grabber_client();
-        return;
-      }
+        filesystem::create_directory_with_intermediate_directories(
+            constants::get_user_configuration_directory(),
+            0700);
 
-      receiver_ = std::make_unique<receiver>();
+        if (uid != getuid()) {
+          stop_grabber_client();
+          return;
+        }
 
-      receiver_->bound.connect([this] {
-        stop_grabber_client();
-        start_grabber_client();
+        receiver_ = std::make_unique<receiver>();
+
+        receiver_->bound.connect([this] {
+          queue_->push_back([this] {
+            stop_grabber_client();
+            start_grabber_client();
+          });
+        });
+
+        receiver_->bind_failed.connect([this](auto&& error_code) {
+          queue_->push_back([this] {
+            stop_grabber_client();
+          });
+        });
+
+        receiver_->closed.connect([this] {
+          queue_->push_back([this] {
+            stop_grabber_client();
+          });
+        });
+
+        receiver_->start();
       });
-
-      receiver_->bind_failed.connect([this](auto&& error_code) {
-        stop_grabber_client();
-      });
-
-      receiver_->closed.connect([this] {
-        stop_grabber_client();
-      });
-
-      receiver_->start();
     });
-    console_user_id_monitor_.start();
+
+    console_user_id_monitor_->start();
   }
 
   ~components_manager(void) {
-    console_user_id_monitor_.stop();
+    queue_->push_back([this] {
+      stop_grabber_client();
 
-    receiver_ = nullptr;
+      console_user_id_monitor_ = nullptr;
+      receiver_ = nullptr;
+      grabber_alerts_monitor_ = nullptr;
+      version_monitor_ = nullptr;
+    });
 
-    stop_grabber_client();
+    queue_ = nullptr;
   }
 
 private:
-  void start_grabber_client(void) {
-    std::lock_guard<std::mutex> lock(grabber_client_mutex_);
+  void start_grabber_alerts_monitor(void) {
+    if (grabber_alerts_monitor_) {
+      return;
+    }
 
+    grabber_alerts_monitor_ = std::make_unique<grabber_alerts_monitor>(constants::get_grabber_alerts_json_file_path());
+
+    grabber_alerts_monitor_->alerts_changed.connect([this](auto&& alerts) {
+      queue_->push_back([alerts] {
+        logger::get_logger().info("karabiner_grabber_alerts.json is updated.");
+        if (!alerts.empty()) {
+          application_launcher::launch_preferences();
+        }
+      });
+    });
+
+    grabber_alerts_monitor_->async_start();
+  }
+
+  void start_grabber_client(void) {
     if (grabber_client_) {
       return;
     }
@@ -75,63 +117,64 @@ private:
     grabber_client_ = std::make_shared<grabber_client>();
 
     grabber_client_->connected.connect([this] {
-      version_monitor_->manual_check();
+      queue_->push_back([this] {
+        if (version_monitor_) {
+          version_monitor_->async_manual_check();
+        }
 
-      grabber_client_->connect_console_user_server();
+        if (grabber_client_) {
+          grabber_client_->connect_console_user_server();
+        }
 
-      stop_child_components();
-      start_child_components();
+        stop_child_components();
+        start_child_components();
+      });
     });
 
     grabber_client_->connect_failed.connect([this](auto&& error_code) {
-      version_monitor_->manual_check();
+      queue_->push_back([this] {
+        if (version_monitor_) {
+          version_monitor_->async_manual_check();
+        }
 
-      stop_child_components();
+        stop_child_components();
+      });
     });
 
     grabber_client_->closed.connect([this] {
-      version_monitor_->manual_check();
+      queue_->push_back([this] {
+        if (version_monitor_) {
+          version_monitor_->async_manual_check();
+        }
 
-      stop_child_components();
+        stop_child_components();
+      });
     });
 
     grabber_client_->start();
   }
 
   void stop_grabber_client(void) {
-    std::lock_guard<std::mutex> lock(grabber_client_mutex_);
-
     grabber_client_ = nullptr;
     stop_child_components();
   }
 
   void start_child_components(void) {
-    std::weak_ptr<grabber_client> grabber_client_weak_ptr;
-    {
-      std::lock_guard<std::mutex> lock(grabber_client_mutex_);
-      grabber_client_weak_ptr = grabber_client_;
-    }
+    configuration_monitor_ = std::make_shared<configuration_monitor>(
+        constants::get_user_core_configuration_file_path(),
+        constants::get_system_core_configuration_file_path());
 
-    {
-      std::lock_guard<std::mutex> lock(child_components_mutex_);
+    menu_process_manager_ = std::make_unique<menu_process_manager>(configuration_monitor_);
+    updater_process_manager_ = std::make_unique<updater_process_manager>(configuration_monitor_);
+    system_preferences_monitor_ = make_system_preferences_monitor(configuration_monitor_,
+                                                                  grabber_client_);
+    start_frontmost_application_observer();
+    start_input_source_observer();
 
-      configuration_monitor_ = std::make_shared<configuration_monitor>(
-          constants::get_user_core_configuration_file_path());
-
-      menu_process_manager_ = make_menu_process_manager(configuration_monitor_);
-      updater_process_manager_ = make_updater_process_manager(configuration_monitor_);
-      system_preferences_monitor_ = make_system_preferences_monitor(configuration_monitor_,
-                                                                    grabber_client_weak_ptr);
-      start_frontmost_application_observer();
-      start_input_source_observer();
-
-      configuration_monitor_->start();
-    }
+    configuration_monitor_->async_start();
   }
 
   void stop_child_components(void) {
-    std::lock_guard<std::mutex> lock(child_components_mutex_);
-
     menu_process_manager_ = nullptr;
     updater_process_manager_ = nullptr;
     system_preferences_monitor_ = nullptr;
@@ -139,14 +182,6 @@ private:
     stop_input_source_observer();
 
     configuration_monitor_ = nullptr;
-  }
-
-  static std::unique_ptr<menu_process_manager> make_menu_process_manager(std::shared_ptr<configuration_monitor> configuration_monitor) {
-    return std::make_unique<menu_process_manager>(configuration_monitor);
-  }
-
-  static std::unique_ptr<updater_process_manager> make_updater_process_manager(std::shared_ptr<configuration_monitor> configuration_monitor) {
-    return std::make_unique<updater_process_manager>(configuration_monitor);
   }
 
   static std::unique_ptr<system_preferences_monitor> make_system_preferences_monitor(std::shared_ptr<configuration_monitor> configuration_monitor,
@@ -173,8 +208,6 @@ private:
           }
 
           {
-            std::lock_guard<std::mutex> lock(grabber_client_mutex_);
-
             if (grabber_client_) {
               grabber_client_->frontmost_application_changed(bundle_identifier, file_path);
             }
@@ -193,8 +226,6 @@ private:
 
     input_source_observer_ = std::make_unique<input_source_observer>(
         [this](auto&& input_source_identifiers) {
-          std::lock_guard<std::mutex> lock(grabber_client_mutex_);
-
           if (grabber_client_) {
             grabber_client_->input_source_changed(input_source_identifiers);
           }
@@ -205,19 +236,18 @@ private:
     input_source_observer_ = nullptr;
   }
 
+  std::unique_ptr<thread_utility::queue> queue_;
+
   // Core components
 
   std::shared_ptr<version_monitor> version_monitor_;
+  std::unique_ptr<grabber_alerts_monitor> grabber_alerts_monitor_;
 
-  console_user_id_monitor console_user_id_monitor_;
+  std::unique_ptr<console_user_id_monitor> console_user_id_monitor_;
   std::unique_ptr<receiver> receiver_;
-
   std::shared_ptr<grabber_client> grabber_client_;
-  std::mutex grabber_client_mutex_;
 
   // Child components
-
-  std::mutex child_components_mutex_;
 
   std::shared_ptr<configuration_monitor> configuration_monitor_;
   std::unique_ptr<menu_process_manager> menu_process_manager_;
--- src/core/console_user_server/src/main.cpp
+++ src/core/console_user_server/src/main.cpp
@@ -1,91 +1,50 @@
 #include "components_manager.hpp"
 #include "constants.hpp"
 #include "filesystem.hpp"
-#include "grabber_alerts_monitor.hpp"
 #include "karabiner_version.h"
 #include "logger.hpp"
 #include "migration.hpp"
 #include "process_utility.hpp"
-#include "spdlog_utility.hpp"
 #include "thread_utility.hpp"
-#include <spdlog/async.h>
-#include <spdlog/sinks/rotating_file_sink.h>
-
-namespace krbn {
-class karabiner_console_user_server final {
-public:
-  karabiner_console_user_server(void) {
-    {
-      auto log_directory = constants::get_user_log_directory();
-      if (!log_directory.empty()) {
-        filesystem::create_directory_with_intermediate_directories(log_directory, 0700);
-
-        if (filesystem::is_directory(log_directory)) {
-          std::string log_file_path = log_directory + "/console_user_server.log";
-          auto l = spdlog::rotating_logger_mt<spdlog::async_factory>("console_user_server",
-                                                                     log_file_path.c_str(),
-                                                                     256 * 1024,
-                                                                     3);
-          l->flush_on(spdlog::level::info);
-          l->set_pattern(spdlog_utility::get_pattern());
-          logger::set_logger(l);
-        }
-      }
-    }
-
-    logger::get_logger().info("version {0}", karabiner_version);
-
-    if (!process_utility::lock_single_application_with_user_pid_file("karabiner_console_user_server.pid")) {
-      std::string message("Exit since another process is running.");
-      logger::get_logger().info(message);
-      std::cerr << message << std::endl;
-      exit(0);
-    }
-
-    // ========================================
-
-    grabber_alerts_monitor_ = std::make_unique<grabber_alerts_monitor>(constants::get_grabber_alerts_json_file_path());
-
-    grabber_alerts_monitor_->alerts_changed.connect([](auto&& alerts) {
-      logger::get_logger().info("karabiner_grabber_alerts.json is updated.");
-      if (!alerts.empty()) {
-        application_launcher::launch_preferences();
-      }
-    });
-
-    grabber_alerts_monitor_->start();
-
-    // ========================================
-
-    migration::migrate_v1();
-
-    // ========================================
-
-    krbn::filesystem::create_directory_with_intermediate_directories(constants::get_user_configuration_directory(), 0700);
-    krbn::filesystem::create_directory_with_intermediate_directories(constants::get_user_complex_modifications_assets_directory(), 0700);
-
-    components_manager_ = std::make_unique<components_manager>();
-  }
-
-  ~karabiner_console_user_server(void) {
-    components_manager_ = nullptr;
-    grabber_alerts_monitor_ = nullptr;
-  }
-
-private:
-  std::unique_ptr<grabber_alerts_monitor> grabber_alerts_monitor_;
-  std::unique_ptr<components_manager> components_manager_;
-};
-} // namespace krbn
 
 int main(int argc, const char* argv[]) {
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
   krbn::thread_utility::register_main_thread();
 
-  krbn::karabiner_console_user_server karabiner_console_user_server;
+  // Setup logger
+
+  if (!krbn::constants::get_user_log_directory().empty()) {
+    krbn::logger::set_async_rotating_logger("console_user_server",
+                                            krbn::constants::get_user_log_directory() + "/console_user_server.log",
+                                            0700);
+  }
+
+  krbn::logger::get_logger().info("version {0}", karabiner_version);
+
+  // Check another process
+
+  if (!krbn::process_utility::lock_single_application_with_user_pid_file("karabiner_console_user_server.pid")) {
+    krbn::logger::get_logger().info("Exit since another process is running.");
+    exit(0);
+  }
+
+  // Migrate old configuration file
+
+  krbn::migration::migrate_v1();
+
+  // Create directories
+
+  krbn::filesystem::create_directory_with_intermediate_directories(krbn::constants::get_user_configuration_directory(), 0700);
+  krbn::filesystem::create_directory_with_intermediate_directories(krbn::constants::get_user_complex_modifications_assets_directory(), 0700);
+
+  // Run components_manager
+
+  auto components_manager = std::make_unique<krbn::components_manager>();
 
   CFRunLoopRun();
+
+  components_manager = nullptr;
 
   return 0;
 }
