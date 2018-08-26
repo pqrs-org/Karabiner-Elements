12d716a49b0ab433b06aa4c795fd0c7846164864
Takayama Fumihiko <tekezo@pqrs.org>
2018-08-26 19:56:35 +0900

    use thread_utility::timer> in system_preferences_monitor

--- appendix/dump_system_preferences/main.cpp
+++ appendix/dump_system_preferences/main.cpp
@@ -4,21 +4,21 @@
 #include "system_preferences_monitor.hpp"
 #include <iostream>
 
-namespace {
-void system_preferences_updated_callback(const krbn::system_preferences& system_preferences) {
-  std::cout << "system_preferences_updated_callback:" << std::endl;
-  std::cout << "  com.apple.keyboard.fnState: " << system_preferences.get_keyboard_fn_state() << std::endl;
-  std::cout << "  com.apple.swipescrolldirection: " << system_preferences.get_swipe_scroll_direction() << std::endl;
-  std::cout << "  keyboard_type: " << static_cast<int>(system_preferences.get_keyboard_type()) << std::endl;
-}
-} // namespace
-
 int main(int argc, const char* argv[]) {
   krbn::thread_utility::register_main_thread();
 
-  auto configuration_monitor = std::make_shared<krbn::configuration_monitor>(krbn::constants::get_user_core_configuration_file_path(),
-                                                                             [](auto core_configuration) {});
-  krbn::system_preferences_monitor monitor(system_preferences_updated_callback, configuration_monitor);
+  auto configuration_monitor = std::make_shared<krbn::configuration_monitor>(krbn::constants::get_user_core_configuration_file_path());
+
+  krbn::system_preferences_monitor monitor(configuration_monitor);
+
+  monitor.system_preferences_changed.connect([](auto&& system_preferences) {
+    std::cout << "system_preferences_updated_callback:" << std::endl;
+    std::cout << "  com.apple.keyboard.fnState: " << system_preferences.get_keyboard_fn_state() << std::endl;
+    std::cout << "  com.apple.swipescrolldirection: " << system_preferences.get_swipe_scroll_direction() << std::endl;
+    std::cout << "  keyboard_type: " << static_cast<int>(system_preferences.get_keyboard_type()) << std::endl;
+  });
+
+  configuration_monitor->async_start();
 
   CFRunLoopRun();
 
--- src/share/system_preferences_monitor.hpp
+++ src/share/system_preferences_monitor.hpp
@@ -1,65 +1,113 @@
 #pragma once
 
-#include "boost_defs.hpp"
+// `krbn::system_preferences_monitor` can be used safely in a multi-threaded environment.
 
+#include "boost_utility.hpp"
 #include "configuration_monitor.hpp"
-#include "gcd_utility.hpp"
 #include "logger.hpp"
 #include "system_preferences_utility.hpp"
-#include <boost/optional.hpp>
+#include "thread_utility.hpp"
 
 namespace krbn {
 class system_preferences_monitor final {
 public:
-  typedef std::function<void(const system_preferences& value)> system_preferences_updated_callback;
+  // Signals
 
-  system_preferences_monitor(const system_preferences_updated_callback& callback,
-                             std::weak_ptr<configuration_monitor> configuration_monitor) : callback_(callback),
-                                                                                           configuration_monitor_(configuration_monitor) {
-    timer_ = std::make_unique<gcd_utility::main_queue_timer>(
-        dispatch_time(DISPATCH_TIME_NOW, 3.0 * NSEC_PER_SEC),
-        3.0 * NSEC_PER_SEC,
-        0,
-        ^{
-          auto v = make_system_preferences();
-          if (!system_preferences_ || *system_preferences_ != v) {
-            logger::get_logger().info("system_preferences is updated.");
+  boost::signals2::signal<void(const system_preferences& value)> system_preferences_changed;
 
-            system_preferences_ = v;
-            if (callback_) {
-              callback_(*system_preferences_);
-            }
+  // Methods
+
+  system_preferences_monitor(std::weak_ptr<configuration_monitor> weak_configuration_monitor) : weak_configuration_monitor_(weak_configuration_monitor) {
+    queue_ = std::make_unique<thread_utility::queue>();
+
+    if (auto configuration_monitor = weak_configuration_monitor_.lock()) {
+      // core_configuration_updated
+      {
+        auto c = configuration_monitor->core_configuration_updated.connect([this](auto&&) {
+          queue_->push_back([this] {
+            check_system_preferences();
+          });
+        });
+        configuration_monitor_connections_.push_back(c);
+      }
+    }
+
+    // Setup timer_
+
+    timer_ = std::make_unique<thread_utility::timer>(
+        [](auto&& count) {
+          if (count == 0) {
+            return std::chrono::milliseconds(0);
+          } else {
+            return std::chrono::milliseconds(3000);
           }
+        },
+        true,
+        [this] {
+          queue_->push_back([this] {
+            check_system_preferences();
+          });
         });
   }
 
   ~system_preferences_monitor(void) {
-    timer_ = nullptr;
+    // Disconnect `configuration_monitor_connections_`.
+
+    if (auto configuration_monitor = weak_configuration_monitor_.lock()) {
+      configuration_monitor->get_run_loop_thread()->enqueue(^{
+        configuration_monitor_connections_.disconnect_all_connections();
+      });
+    } else {
+      configuration_monitor_connections_.disconnect_all_connections();
+    }
+
+    configuration_monitor_connections_.wait_disconnect_all_connections();
+
+    // Destroy timer_
+
+    queue_->push_back([this] {
+      timer_ = nullptr;
+    });
+
+    // Destroy queue_
+
+    queue_ = nullptr;
   }
 
 private:
   system_preferences make_system_preferences(void) const {
     system_preferences system_preferences;
 
-    if (auto configuration_monitor_ptr = configuration_monitor_.lock()) {
-      auto core_configuration = configuration_monitor_ptr->get_core_configuration();
-      auto country_code = core_configuration->get_selected_profile().get_virtual_hid_keyboard().get_country_code();
+    if (auto configuration_monitor = weak_configuration_monitor_.lock()) {
+      if (auto core_configuration = configuration_monitor->get_core_configuration()) {
+        auto country_code = core_configuration->get_selected_profile().get_virtual_hid_keyboard().get_country_code();
 
-      system_preferences.set_keyboard_fn_state(system_preferences_utility::get_keyboard_fn_state());
-      system_preferences.set_swipe_scroll_direction(system_preferences_utility::get_swipe_scroll_direction());
-      system_preferences.set_keyboard_type(system_preferences_utility::get_keyboard_type(vendor_id(0x16c0),
-                                                                                         product_id(0x27db),
-                                                                                         country_code));
+        system_preferences.set_keyboard_fn_state(system_preferences_utility::get_keyboard_fn_state());
+        system_preferences.set_swipe_scroll_direction(system_preferences_utility::get_swipe_scroll_direction());
+        system_preferences.set_keyboard_type(system_preferences_utility::get_keyboard_type(vendor_id(0x16c0),
+                                                                                           product_id(0x27db),
+                                                                                           country_code));
+      }
     }
 
     return system_preferences;
   }
 
-  system_preferences_updated_callback callback_;
-  std::weak_ptr<configuration_monitor> configuration_monitor_;
+  void check_system_preferences(void) {
+    auto v = make_system_preferences();
+    if (!last_system_preferences_ || *last_system_preferences_ != v) {
+      logger::get_logger().info("system_preferences is updated.");
 
-  std::unique_ptr<gcd_utility::main_queue_timer> timer_;
+      last_system_preferences_ = v;
+      system_preferences_changed(v);
+    }
+  }
 
-  boost::optional<system_preferences> system_preferences_;
+  std::weak_ptr<configuration_monitor> weak_configuration_monitor_;
+
+  std::unique_ptr<thread_utility::queue> queue_;
+  boost_utility::signals2_connections configuration_monitor_connections_;
+  std::unique_ptr<thread_utility::timer> timer_;
+  boost::optional<system_preferences> last_system_preferences_;
 };
 } // namespace krbn
