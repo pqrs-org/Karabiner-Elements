8d2d2668915ac30a248092c13de747dbe0f64a41
Takayama Fumihiko <tekezo@pqrs.org>
2018-09-04 00:44:32 +0900

    remove gcd_utility::scoped_queue

--- src/share/gcd_utility.hpp
+++ src/share/gcd_utility.hpp
@@ -8,28 +8,6 @@
 namespace krbn {
 class gcd_utility final {
 public:
-  class scoped_queue final {
-  public:
-    scoped_queue(void) {
-      auto label = get_next_queue_label();
-      queue_ = dispatch_queue_create(label.c_str(), nullptr);
-    }
-
-    ~scoped_queue(void) {
-      dispatch_release(queue_);
-    }
-
-    dispatch_queue_t _Nonnull get(void) { return queue_; }
-
-    std::string get_label(void) {
-      auto p = dispatch_queue_get_label(queue_);
-      return p ? p : "";
-    }
-
-  private:
-    dispatch_queue_t _Nonnull queue_;
-  };
-
   static void dispatch_sync_in_main_queue(void (^_Nonnull block)(void)) {
     if (thread_utility::is_main_thread()) {
       block();
