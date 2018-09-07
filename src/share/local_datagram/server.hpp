bf9182aa4cf8a1c622dc7847df0a21b5f91900f0
Takayama Fumihiko <tekezo@pqrs.org>
2018-09-07 19:40:33 +0900

    use repeating timer in server.hpp

--- src/share/local_datagram/server.hpp
+++ src/share/local_datagram/server.hpp
@@ -11,8 +11,6 @@ class server final {
 public:
   // Signals
 
-  // Note: These signals are fired on non-main thread.
-
   boost::signals2::signal<void(void)> bound;
   boost::signals2::signal<void(const boost::system::error_code&)> bind_failed;
   boost::signals2::signal<void(void)> closed;
@@ -25,15 +23,14 @@ public:
   server(void) : io_service_(),
                  work_(std::make_unique<boost::asio::io_service::work>(io_service_)),
                  socket_(io_service_),
-                 bound_(false),
-                 server_check_enabled_(false) {
+                 bound_(false) {
     io_service_thread_ = std::thread([this] {
       (this->io_service_).run();
     });
   }
 
   ~server(void) {
-    close();
+    async_close();
 
     if (io_service_thread_.joinable()) {
       work_ = nullptr;
@@ -41,10 +38,10 @@ public:
     }
   }
 
-  void bind(const std::string& path,
-            size_t buffer_size,
-            boost::optional<std::chrono::milliseconds> server_check_interval) {
-    close();
+  void async_bind(const std::string& path,
+                  size_t buffer_size,
+                  boost::optional<std::chrono::milliseconds> server_check_interval) {
+    async_close();
 
     io_service_.post([this, path, buffer_size, server_check_interval] {
       bound_ = false;
@@ -80,8 +77,8 @@ public:
       bound_ = true;
       bound_path_ = path;
 
-      start_server_check_thread(path,
-                                server_check_interval);
+      start_server_check_timer(path,
+                               server_check_interval);
 
       bound();
 
@@ -92,29 +89,33 @@ public:
     });
   }
 
-  void close(void) {
+  void async_close(void) {
     io_service_.post([this] {
-      stop_server_check_thread();
-
-      // Close socket
-
-      boost::system::error_code error_code;
-
-      socket_.cancel(error_code);
-      socket_.close(error_code);
-
-      // Signal
-
-      if (bound_) {
-        bound_ = false;
-        unlink(bound_path_.c_str());
-
-        closed();
-      }
+      close();
     });
   }
 
 private:
+  void close(void) {
+    stop_server_check_timer();
+
+    // Close socket
+
+    boost::system::error_code error_code;
+
+    socket_.cancel(error_code);
+    socket_.close(error_code);
+
+    // Signal
+
+    if (bound_) {
+      bound_ = false;
+      unlink(bound_path_.c_str());
+
+      closed();
+    }
+  }
+
   void async_receive(void) {
     socket_.async_receive(boost::asio::buffer(buffer_),
                           [this](auto&& error_code, auto&& bytes_transferred) {
@@ -134,56 +135,54 @@ private:
                           });
   }
 
-  void start_server_check_thread(const std::string& path,
-                                 boost::optional<std::chrono::milliseconds> server_check_interval) {
-    stop_server_check_thread();
-
-    if (server_check_interval) {
-      server_check_enabled_ = true;
-
-      server_check_thread_ = std::thread([this, path, server_check_interval] {
-        std::unique_ptr<client> c;
-        bool client_connected = false;
-
-        while (server_check_enabled_) {
-          server_check_timer_ = std::make_shared<thread_utility::timer>(
-              *server_check_interval,
-              false,
-              [this, path, &c, &client_connected] {
-                // Skip if client is connecting
-                if (c && !client_connected) {
-                  return;
-                }
-
-                c = std::make_unique<client>();
-
-                c->connected.connect([&client_connected] {
-                  client_connected = true;
-                });
-
-                c->connect_failed.connect([this](auto&& error_code) {
-                  close();
-                });
-
-                c->connect(path, boost::none);
-              });
-
-          server_check_timer_->wait();
-        }
-      });
+  void start_server_check_timer(const std::string& path,
+                                boost::optional<std::chrono::milliseconds> server_check_interval) {
+    if (!server_check_interval) {
+      return;
     }
-  }
-
-  void stop_server_check_thread(void) {
-    server_check_enabled_ = false;
 
     if (server_check_timer_) {
-      server_check_timer_->cancel();
+      return;
     }
 
-    if (server_check_thread_.joinable()) {
-      server_check_thread_.join();
+    server_check_timer_ = std::make_unique<thread_utility::timer>(
+        *server_check_interval,
+        thread_utility::timer::mode::repeat,
+        [this, path] {
+          io_service_.post([this, path] {
+            // Skip if client is connecting
+            if (server_check_client_) {
+              return;
+            }
+
+            server_check_client_ = std::make_unique<client>();
+
+            server_check_client_->connected.connect([this] {
+              io_service_.post([this] {
+                server_check_client_ = nullptr;
+              });
+            });
+
+            server_check_client_->connect_failed.connect([this](auto&& error_code) {
+              io_service_.post([this] {
+                close();
+              });
+            });
+
+            server_check_client_->async_connect(path, boost::none);
+          });
+        });
+  }
+
+  void stop_server_check_timer(void) {
+    if (!server_check_timer_) {
+      return;
     }
+
+    server_check_timer_->cancel();
+    server_check_timer_ = nullptr;
+
+    server_check_client_ = nullptr;
   }
 
   boost::asio::io_service io_service_;
@@ -195,9 +194,8 @@ private:
 
   std::vector<uint8_t> buffer_;
 
-  std::thread server_check_thread_;
-  std::atomic<bool> server_check_enabled_;
-  std::shared_ptr<thread_utility::timer> server_check_timer_;
+  std::unique_ptr<thread_utility::timer> server_check_timer_;
+  std::unique_ptr<client> server_check_client_;
 };
 } // namespace local_datagram
 } // namespace krbn
