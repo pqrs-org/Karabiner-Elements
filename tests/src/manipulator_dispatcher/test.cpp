a769cf0aac3532f5b3bf35a8f2dcb097f3eba826
Takayama Fumihiko <tekezo@pqrs.org>
2018-09-12 23:20:01 +0900

    add manipulator_dispatcher

---
+++ tests/src/manipulator_dispatcher/test.cpp
@@ -0,0 +1,58 @@
+#define CATCH_CONFIG_MAIN
+#include "../../vendor/catch/catch.hpp"
+
+#include "manipulator/manipulator_dispatcher.hpp"
+#include "thread_utility.hpp"
+
+TEST_CASE("initialize") {
+  krbn::thread_utility::register_main_thread();
+}
+
+TEST_CASE("manipulator_dispatcher") {
+  std::cout << "manipulator_dispatcher" << std::endl;
+
+  auto manipulator_dispatcher = std::make_unique<krbn::manipulator::manipulator_dispatcher>();
+  int object;
+  auto manipulator_object_id = krbn::manipulator::make_manipulator_object_id(&object);
+  std::vector<int> result;
+
+  manipulator_dispatcher->async_attach(manipulator_object_id);
+
+  manipulator_dispatcher->enqueue(manipulator_object_id, [&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    result.push_back(1);
+  });
+  manipulator_dispatcher->enqueue(manipulator_object_id, [&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    result.push_back(2);
+  });
+  manipulator_dispatcher->enqueue(manipulator_object_id, [&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    result.push_back(3);
+  });
+  manipulator_dispatcher->enqueue(manipulator_object_id, [&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    result.push_back(4);
+  });
+  manipulator_dispatcher->enqueue(manipulator_object_id, [&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    result.push_back(5);
+  });
+
+  REQUIRE(result.size() == 0);
+
+  manipulator_dispatcher->detach(manipulator_object_id);
+
+  REQUIRE(result.size() == 5);
+
+  // Ignored after `detach`
+
+  manipulator_dispatcher->enqueue(manipulator_object_id, [&] {
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    result.push_back(6);
+  });
+
+  manipulator_dispatcher = nullptr;
+
+  REQUIRE(result.size() == 5);
+}
