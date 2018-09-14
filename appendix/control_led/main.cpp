1dc69a94e1d27873df5cf6b43ed99fe4c9265e6b
Takayama Fumihiko <tekezo@pqrs.org>
2018-09-15 01:42:49 +0900

    update appendix/control_led

--- appendix/control_led/main.cpp
+++ appendix/control_led/main.cpp
@@ -5,60 +5,66 @@ class control_led final {
 public:
   control_led(const control_led&) = delete;
 
-  control_led(void) : hid_manager_({
-                          std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_keyboard),
-                      }) {
-    hid_manager_.device_detected.connect([](auto&& human_interface_device) {
-      auto r = human_interface_device->open();
-      if (r != kIOReturnSuccess) {
-        krbn::logger::get_logger().error("failed to open");
-        return;
-      }
+  control_led(bool led_state) {
+    std::vector<std::pair<krbn::hid_usage_page, krbn::hid_usage>> targets({
+        std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_keyboard),
+    });
 
-      if (auto caps_lock_led_state = human_interface_device->get_caps_lock_led_state()) {
-        switch (*caps_lock_led_state) {
-          case krbn::led_state::on:
-            krbn::logger::get_logger().info("caps_lock_led_state is on.");
-            break;
-          case krbn::led_state::off:
-            krbn::logger::get_logger().info("caps_lock_led_state is off.");
-            break;
-        }
+    hid_manager_ = std::make_unique<krbn::hid_manager>(targets);
 
-        if (caps_lock_led_state == krbn::led_state::on) {
-          human_interface_device->set_caps_lock_led_state(krbn::led_state::off);
-        } else {
-          human_interface_device->set_caps_lock_led_state(krbn::led_state::on);
-        }
+    hid_manager_->device_detected.connect([led_state](auto&& weak_hid) {
+      if (auto hid = weak_hid.lock()) {
+        hid->opened.connect([led_state, weak_hid] {
+          if (auto hid = weak_hid.lock()) {
+            hid->async_set_caps_lock_led_state(led_state ? krbn::led_state::on : krbn::led_state::off);
+            krbn::logger::get_logger().info("async_set_caps_lock_led_state is called.");
+          }
+        });
 
-        krbn::logger::get_logger().info("set_caps_lock_led_state is called.");
+        hid->open_failed.connect([](auto&& error_code) {
+          krbn::logger::get_logger().error("failed to open");
+        });
 
-      } else {
-        krbn::logger::get_logger().info("failed to get caps_lock_led_state.");
+        hid->async_open();
       }
     });
 
-    hid_manager_.start();
+    hid_manager_->async_start();
   }
 
   ~control_led(void) {
-    hid_manager_.stop();
+    hid_manager_ = nullptr;
   }
 
 private:
-  krbn::hid_manager hid_manager_;
+  std::unique_ptr<krbn::hid_manager> hid_manager_;
 };
 } // namespace
 
 int main(int argc, const char* argv[]) {
   krbn::thread_utility::register_main_thread();
 
+  signal(SIGINT, [](int) {
+    CFRunLoopStop(CFRunLoopGetMain());
+  });
+
   if (getuid() != 0) {
     krbn::logger::get_logger().error("control_led requires root privilege.");
     return 1;
   }
 
-  control_led d;
+  if (argc == 1) {
+    krbn::logger::get_logger().error("Usage: control_led on|off");
+    krbn::logger::get_logger().error("  Example: control_led on");
+    krbn::logger::get_logger().error("  Example: control_led off");
+    return 1;
+  }
+
+  auto p = std::make_unique<control_led>(std::string(argv[1]) == "on");
+
   CFRunLoopRun();
+
+  p = nullptr;
+
   return 0;
 }
