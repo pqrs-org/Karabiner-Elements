bf92eecc684458511f36160ba911920d81abf004
Takayama Fumihiko <tekezo@pqrs.org>
2018-09-15 02:05:44 +0900

    update appendix/hid_set_report

--- appendix/hid_set_report/main.cpp
+++ appendix/hid_set_report/main.cpp
@@ -6,50 +6,60 @@ class hid_set_report final {
 public:
   hid_set_report(const hid_set_report&) = delete;
 
-  hid_set_report(void) : hid_manager_({
-                             std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_keyboard),
-                         }) {
-    hid_manager_.device_detected.connect([](auto&& human_interface_device) {
-      auto r = human_interface_device->open();
-      if (r != kIOReturnSuccess) {
-        krbn::logger::get_logger().error("failed to open");
-        return;
-      }
-
-      {
-        pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input keyboard_input;
-        const uint8_t* report = reinterpret_cast<const uint8_t*>(&keyboard_input);
-
-        auto kr = human_interface_device->set_report(kIOHIDReportTypeInput,
-                                                     report[0],
-                                                     report,
-                                                     sizeof(keyboard_input));
-        if (kr != kIOReturnSuccess) {
-          krbn::logger::get_logger().error("Failed to IOHIDDeviceSetReport: {0}",
-                                           krbn::iokit_utility::get_error_name(kr));
-          return;
-        }
-      }
-
-      human_interface_device->close();
+  hid_set_report(void) {
+    std::vector<std::pair<krbn::hid_usage_page, krbn::hid_usage>> targets({
+        std::make_pair(krbn::hid_usage_page::generic_desktop, krbn::hid_usage::gd_keyboard),
     });
 
-    hid_manager_.start();
+    hid_manager_ = std::make_unique<krbn::hid_manager>(targets);
+
+    hid_manager_->device_detected.connect([](auto&& weak_hid) {
+      if (auto hid = weak_hid.lock()) {
+        hid->opened.connect([weak_hid] {
+          if (auto hid = weak_hid.lock()) {
+            pqrs::karabiner_virtual_hid_device::hid_report::keyboard_input keyboard_input;
+            const uint8_t* report = reinterpret_cast<const uint8_t*>(&keyboard_input);
+
+            hid->async_set_report(kIOHIDReportTypeInput,
+                                  report[0],
+                                  report,
+                                  sizeof(keyboard_input));
+            krbn::logger::get_logger().info("async_set_report is called {0}", hid->get_name_for_log());
+          }
+        });
+
+        hid->open_failed.connect([weak_hid](auto&& error_code) {
+          if (auto hid = weak_hid.lock()) {
+            krbn::logger::get_logger().error("failed to open {0}", hid->get_name_for_log());
+          }
+        });
+
+        hid->async_open();
+      }
+    });
+
+    hid_manager_->async_start();
   }
 
   ~hid_set_report(void) {
-    hid_manager_.stop();
+    hid_manager_ = nullptr;
   }
 
 private:
-  krbn::hid_manager hid_manager_;
+  std::unique_ptr<krbn::hid_manager> hid_manager_;
 };
 } // namespace
 
 int main(int argc, const char* argv[]) {
   krbn::thread_utility::register_main_thread();
 
+  signal(SIGINT, [](int) {
+    CFRunLoopStop(CFRunLoopGetMain());
+  });
+
   hid_set_report hid_set_report;
+
   CFRunLoopRun();
+
   return 0;
 }
