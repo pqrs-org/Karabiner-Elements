ae1fc96b7e78b42c71e58b99faf1c2d1394627e6
Takayama Fumihiko <tekezo@pqrs.org>
2018-09-16 20:46:55 +0900

    add src/share/types/modifier_flag.hpp

--- src/share/types.hpp
+++ src/share/types.hpp
@@ -15,6 +15,7 @@
 #include "types/hid_usage.hpp"
 #include "types/hid_usage_page.hpp"
 #include "types/key_code.hpp"
+#include "types/modifier_flag.hpp"
 #include "types/pointing_button.hpp"
 #include <CoreFoundation/CoreFoundation.h>
 #include <IOKit/IOKitLib.h>
@@ -63,21 +64,6 @@ enum class pointing_event : uint32_t {
   y,
   vertical_wheel,
   horizontal_wheel,
-};
-
-enum class modifier_flag : uint32_t {
-  zero,
-  caps_lock,
-  left_control,
-  left_shift,
-  left_option,
-  left_command,
-  right_control,
-  right_shift,
-  right_option,
-  right_command,
-  fn,
-  end_,
 };
 
 enum class led_state : uint32_t {
@@ -1832,42 +1818,6 @@ KRBN_TYPES_STREAM_OUTPUT(product_id);
 KRBN_TYPES_STREAM_OUTPUT(location_id);
 
 #undef KRBN_TYPES_STREAM_OUTPUT
-
-inline std::ostream& operator<<(std::ostream& stream, const modifier_flag& value) {
-#define KRBN_MODIFIER_FLAG_STREAM_OUTPUT(NAME) \
-  case modifier_flag::NAME:                    \
-    stream << #NAME;                           \
-    break;
-
-  switch (value) {
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(zero);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(caps_lock);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(left_control);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(left_shift);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(left_option);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(left_command);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(right_control);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(right_shift);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(right_option);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(right_command);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(fn);
-    KRBN_MODIFIER_FLAG_STREAM_OUTPUT(end_);
-  }
-
-#undef KRBN_MODIFIER_FLAG_STREAM_OUTPUT
-
-  return stream;
-}
-
-template <template <class T, class A> class container>
-inline std::ostream& operator<<(std::ostream& stream, const container<modifier_flag, std::allocator<modifier_flag>>& values) {
-  return stream_utility::output_enums(stream, values);
-}
-
-template <template <class T, class H, class K, class A> class container>
-inline std::ostream& operator<<(std::ostream& stream, const container<modifier_flag, std::hash<modifier_flag>, std::equal_to<modifier_flag>, std::allocator<modifier_flag>>& values) {
-  return stream_utility::output_enums(stream, values);
-}
 
 inline std::ostream& operator<<(std::ostream& stream, const pointing_motion& value) {
   stream << "pointing_motion:" << value.to_json();
