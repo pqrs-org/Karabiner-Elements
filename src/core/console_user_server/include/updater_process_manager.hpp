fa8c9f42d00dfc05a1a751d39dfdf05e3ea8d0d5
Takayama Fumihiko <tekezo@pqrs.org>
2018-10-06 09:03:32 +0900

    use shared_dispatcher

--- src/core/console_user_server/include/updater_process_manager.hpp
+++ src/core/console_user_server/include/updater_process_manager.hpp
@@ -1,15 +1,17 @@
 #pragma once
 
 #include "boost_utility.hpp"
+#include "dispatcher.hpp"
 #include "monitor/configuration_monitor.hpp"
 #include "update_utility.hpp"
 
 namespace krbn {
-class updater_process_manager final {
+class updater_process_manager final : public pqrs::dispatcher::extra::dispatcher_client {
 public:
   updater_process_manager(const updater_process_manager&) = delete;
 
-  updater_process_manager(std::weak_ptr<configuration_monitor> weak_configuration_monitor) : weak_configuration_monitor_(weak_configuration_monitor),
+  updater_process_manager(std::weak_ptr<configuration_monitor> weak_configuration_monitor) : dispatcher_client(),
+                                                                                             weak_configuration_monitor_(weak_configuration_monitor),
                                                                                              checked_(false) {
     if (auto configuration_monitor = weak_configuration_monitor_.lock()) {
       // core_configuration_updated
@@ -32,17 +34,9 @@ public:
   }
 
   ~updater_process_manager(void) {
-    // Disconnect `configuration_monitor_connections_`.
-
-    if (auto configuration_monitor = weak_configuration_monitor_.lock()) {
-      configuration_monitor->get_run_loop_thread()->enqueue(^{
-        configuration_monitor_connections_.disconnect_all_connections();
-      });
-    } else {
+    detach_from_dispatcher([this] {
       configuration_monitor_connections_.disconnect_all_connections();
-    }
-
-    configuration_monitor_connections_.wait_disconnect_all_connections();
+    });
   }
 
 private:
