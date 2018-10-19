10ef0dfc8da5d97c30ff406a3416aae2ed38716d
Takayama Fumihiko <tekezo@pqrs.org>
2018-10-20 03:31:39 +0900

    add hid_system_client::caps_lock_state_changed

--- appendix/hid_system_client/main.cpp
+++ appendix/hid_system_client/main.cpp
@@ -5,11 +5,19 @@
 int main(int argc, const char* argv[]) {
   krbn::dispatcher_utility::initialize_dispatchers();
 
-  if (getuid() != 0) {
-    krbn::logger::get_logger().error("dump_caps_lock_state requires root privilege.");
-  }
+  signal(SIGINT, [](int) {
+    CFRunLoopStop(CFRunLoopGetMain());
+  });
 
   auto client = std::make_unique<krbn::hid_system_client>();
+  client->caps_lock_state_changed.connect([](auto&& state) {
+    if (!state) {
+      krbn::logger::get_logger().info("caps_lock_state_changed: boost::none");
+    } else {
+      krbn::logger::get_logger().info("caps_lock_state_changed: {0}", *state);
+    }
+  });
+  client->async_start_caps_lock_check_timer(std::chrono::milliseconds(100));
 
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
@@ -26,6 +34,8 @@ int main(int argc, const char* argv[]) {
   client->async_set_caps_lock_state(false);
 
   std::this_thread::sleep_for(std::chrono::milliseconds(500));
+
+  CFRunLoopRun();
 
   client = nullptr;
 
--- src/share/hid_system_client.hpp
+++ src/share/hid_system_client.hpp
@@ -1,13 +1,22 @@
 #pragma once
 
+#include "boost_defs.hpp"
+
 #include "iokit_utility.hpp"
 #include "logger.hpp"
 #include "monitor/service_monitor.hpp"
 #include "types.hpp"
+#include <boost/signals2.hpp>
 
 namespace krbn {
 class hid_system_client final : pqrs::dispatcher::extra::dispatcher_client {
 public:
+  // Signals (invoked from the shared dispatcher thread)
+
+  boost::signals2::signal<void(boost::optional<bool>)> caps_lock_state_changed;
+
+  // Methods
+
   hid_system_client(const hid_system_client&) = delete;
 
   // Note:
@@ -26,7 +35,8 @@ public:
 
   hid_system_client(void) : dispatcher_client(),
                             service_(IO_OBJECT_NULL),
-                            connect_(IO_OBJECT_NULL) {
+                            connect_(IO_OBJECT_NULL),
+                            caps_lock_state_check_timer_(*this) {
     if (auto matching_dictionary = IOServiceNameMatching(kIOHIDSystemClass)) {
       service_monitor_ = std::make_unique<monitor::service_monitor::monitor>(matching_dictionary);
 
@@ -56,9 +66,28 @@ public:
 
   virtual ~hid_system_client(void) {
     detach_from_dispatcher([this] {
+      caps_lock_state_check_timer_.stop();
       close_connection();
 
       service_monitor_ = nullptr;
+    });
+  }
+
+  void async_start_caps_lock_check_timer(std::chrono::milliseconds interval) {
+    enqueue_to_dispatcher([this, interval] {
+      last_caps_lock_state_ = boost::none;
+
+      caps_lock_state_check_timer_.start(
+          [this] {
+            auto s = get_modifier_lock_state(kIOHIDCapsLockState);
+            if (last_caps_lock_state_ != s) {
+              last_caps_lock_state_ = s;
+              enqueue_to_dispatcher([this, s] {
+                caps_lock_state_changed(s);
+              });
+            }
+          },
+          interval);
     });
   }
 
@@ -76,10 +105,11 @@ private:
 
     auto kr = IOServiceOpen(service_, mach_task_self(), kIOHIDParamConnectType, &connect_);
     if (kr == KERN_SUCCESS) {
-      logger::get_logger().info("IOServiceOpen is succeeded @ {0}", __PRETTY_FUNCTION__);
+      logger::get_logger().info("hid_system_client is opened.");
 
     } else {
-      logger::get_logger().error("IOServiceOpen error: {1} @ {0}", __PRETTY_FUNCTION__, kr);
+      logger::get_logger().error("hid_system_client::open_connection is failed: {0}",
+                                 iokit_utility::get_error_name(kr));
       connect_ = IO_OBJECT_NULL;
     }
   }
@@ -89,12 +119,13 @@ private:
     if (connect_) {
       auto kr = IOServiceClose(connect_);
       if (kr != kIOReturnSuccess) {
-        logger::get_logger().error("IOConnectRelease error: {1} @ {0}", __PRETTY_FUNCTION__, kr);
+        logger::get_logger().error("hid_system_client::close_connection error: {0}",
+                                   iokit_utility::get_error_name(kr));
       }
       connect_ = IO_OBJECT_NULL;
     }
 
-    logger::get_logger().info("IOServiceClose is succeeded @ {0}", __PRETTY_FUNCTION__);
+    logger::get_logger().info("hid_system_client is closed.");
 
     if (service_) {
       IOObjectRelease(service_);
@@ -103,20 +134,42 @@ private:
   }
 
   // This method is executed in the dispatcher thread.
+  boost::optional<bool> get_modifier_lock_state(int selector) {
+    if (!connect_) {
+      logger::get_logger().error("hid_system_client::get_modifier_lock_state connect_ is null.");
+      return boost::none;
+    }
+
+    bool state = false;
+    auto kr = IOHIDGetModifierLockState(connect_, selector, &state);
+    if (kr != KERN_SUCCESS) {
+      logger::get_logger().error("IOHIDGetModifierLockState is failed: {0}",
+                                 iokit_utility::get_error_name(kr));
+      return boost::none;
+    }
+
+    return state;
+  }
+
+  // This method is executed in the dispatcher thread.
   void set_modifier_lock_state(int selector, bool state) {
     if (!connect_) {
-      logger::get_logger().error("connect_ is null @ {0}", __PRETTY_FUNCTION__);
+      logger::get_logger().error("hid_system_client::set_modifier_lock_state connect_ is null.");
       return;
     }
 
     auto kr = IOHIDSetModifierLockState(connect_, selector, state);
     if (kr != KERN_SUCCESS) {
-      logger::get_logger().error("IOHIDSetModifierLockState error: {1} @ {0}", __PRETTY_FUNCTION__, kr);
+      logger::get_logger().error("IOHIDSetModifierLockState is failed: {0}",
+                                 iokit_utility::get_error_name(kr));
     }
   }
 
   std::unique_ptr<monitor::service_monitor::monitor> service_monitor_;
   io_service_t service_;
   io_connect_t connect_;
+
+  pqrs::dispatcher::extra::timer caps_lock_state_check_timer_;
+  boost::optional<bool> last_caps_lock_state_;
 };
 } // namespace krbn
