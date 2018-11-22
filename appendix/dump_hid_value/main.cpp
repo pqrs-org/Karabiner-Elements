d9cf146e4e5060c4bf1693e4223810166d6923ea
Takayama Fumihiko <tekezo@pqrs.org>
2018-11-22 21:55:56 +0900

    update appendix/dump_hid_value

--- appendix/dump_hid_value/main.cpp
+++ appendix/dump_hid_value/main.cpp
@@ -74,10 +74,6 @@ private:
     std::cout << entry.get_event_time_stamp().get_time_stamp() << " ";
 
     switch (entry.get_event().get_type()) {
-      case krbn::event_queue::event::type::none:
-        std::cout << "none" << std::endl;
-        break;
-
       case krbn::event_queue::event::type::key_code:
         if (auto key_code = entry.get_event().get_key_code()) {
           std::cout << "Key: " << std::dec << static_cast<uint32_t>(*key_code) << " "
@@ -108,66 +104,8 @@ private:
         }
         break;
 
-      case krbn::event_queue::event::type::shell_command:
-        std::cout << "shell_command" << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::select_input_source:
-        std::cout << "select_input_source" << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::set_variable:
-        std::cout << "set_variable" << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::mouse_key:
-        std::cout << "mouse_key" << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::stop_keyboard_repeat:
-        std::cout << "stop_keyboard_repeat" << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::device_keys_and_pointing_buttons_are_released:
-        std::cout << "device_keys_and_pointing_buttons_are_released: " << registry_entry_id << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::device_ungrabbed:
-        std::cout << "device_ungrabbed: " << registry_entry_id << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::caps_lock_state_changed:
-        if (auto integer_value = entry.get_event().get_integer_value()) {
-          std::cout << "caps_lock_state_changed " << *integer_value << std::endl;
-        }
-        break;
-
-      case krbn::event_queue::event::type::pointing_device_event_from_event_tap:
-        std::cout << "pointing_device_event_from_event_tap: " << registry_entry_id << std::endl;
-        break;
-
-      case krbn::event_queue::event::type::frontmost_application_changed:
-        if (auto frontmost_application = entry.get_event().get_frontmost_application()) {
-          std::cout << "frontmost_application_changed "
-                    << frontmost_application->get_bundle_identifier() << " "
-                    << frontmost_application->get_file_path() << std::endl;
-        }
-        break;
-
-      case krbn::event_queue::event::type::input_source_changed:
-        if (auto input_source_identifiers = entry.get_event().get_input_source_identifiers()) {
-          std::cout << "input_source_changed " << *input_source_identifiers << std::endl;
-        }
-        break;
-
-      case krbn::event_queue::event::type::keyboard_type_changed:
-        if (auto keyboard_type = entry.get_event().get_keyboard_type()) {
-          std::cout << "keyboard_type_changed " << *keyboard_type << std::endl;
-        }
-        break;
-
       default:
-        std::cout << std::endl;
+        break;
     }
   }
 
