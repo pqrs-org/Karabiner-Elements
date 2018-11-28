ffd532b928f6297ee8c6e701ca896937f1042624
Takayama Fumihiko <tekezo@pqrs.org>
2018-11-29 00:55:49 +0900

    update src/vendor

--- src/vendor/cget/cget/pkg/pqrs-org__cpp-osx-iokit_types/install/include/pqrs/osx/iokit_types.hpp
+++ src/vendor/cget/cget/pkg/pqrs-org__cpp-osx-iokit_types/install/include/pqrs/osx/iokit_types.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_types v2.3
+// pqrs::osx::iokit_types v2.4
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
@@ -12,3 +12,35 @@
 #include "iokit_types/iokit_hid_usage_page.hpp"
 #include "iokit_types/iokit_hid_vendor_id.hpp"
 #include "iokit_types/iokit_registry_entry_id.hpp"
+#include <tuple>
+#include <utility>
+
+namespace std {
+template <>
+struct hash<std::pair<pqrs::osx::iokit_hid_usage_page, pqrs::osx::iokit_hid_usage>> {
+  size_t operator()(const std::pair<pqrs::osx::iokit_hid_usage_page, pqrs::osx::iokit_hid_usage>& pair) const {
+    auto h1 = std::hash<int32_t>{}(type_safe::get(pair.first));
+    auto h2 = std::hash<int32_t>{}(type_safe::get(pair.second));
+    return h1 ^ (h2 << 16);
+  }
+};
+
+template <>
+struct hash<std::pair<pqrs::osx::iokit_hid_vendor_id, pqrs::osx::iokit_hid_product_id>> {
+  size_t operator()(const std::pair<pqrs::osx::iokit_hid_vendor_id, pqrs::osx::iokit_hid_product_id>& pair) const {
+    auto h1 = std::hash<int32_t>{}(type_safe::get(pair.first));
+    auto h2 = std::hash<int32_t>{}(type_safe::get(pair.second));
+    return h1 ^ (h2 << 16);
+  }
+};
+
+template <>
+struct hash<std::tuple<pqrs::osx::iokit_hid_vendor_id, pqrs::osx::iokit_hid_product_id, pqrs::osx::iokit_hid_location_id>> {
+  size_t operator()(const std::tuple<pqrs::osx::iokit_hid_vendor_id, pqrs::osx::iokit_hid_product_id, pqrs::osx::iokit_hid_location_id>& tuple) const {
+    auto h1 = std::hash<int32_t>{}(type_safe::get(std::get<0>(tuple)));
+    auto h2 = std::hash<int32_t>{}(type_safe::get(std::get<1>(tuple)));
+    auto h3 = std::hash<int32_t>{}(type_safe::get(std::get<2>(tuple)));
+    return h1 ^ (h2 << 16) ^ h3;
+  }
+};
+} // namespace std
