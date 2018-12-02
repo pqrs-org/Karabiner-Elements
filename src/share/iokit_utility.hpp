adda29e6683818074aacbec83f881d8a53bc1677
Takayama Fumihiko <tekezo@pqrs.org>
2018-12-03 00:01:56 +0900

    remove unused methods

--- src/share/iokit_utility.hpp
+++ src/share/iokit_utility.hpp
@@ -1,127 +1,39 @@
 #pragma once
 
-#include "boost_defs.hpp"
-
 #include "cf_utility.hpp"
 #include "logger.hpp"
 #include "types.hpp"
 #include <IOKit/IOKitLib.h>
 #include <IOKit/hid/IOHIDDevice.h>
 #include <IOKit/hid/IOHIDKeys.h>
-#include <boost/optional.hpp>
 #include <cstdint>
-#include <pqrs/osx/iokit_types.hpp>
+#include <pqrs/osx/iokit_hid_device.hpp>
+#include <pqrs/string.hpp>
 #include <string>
 #include <vector>
 
 namespace krbn {
 class iokit_utility final {
 public:
-  static boost::optional<std::string> find_string_property(io_service_t service, CFStringRef _Nonnull key) {
-    if (!service) {
-      return boost::none;
-    }
-
-    if (auto property = IORegistryEntrySearchCFProperty(service,
-                                                        kIOServicePlane,
-                                                        key,
-                                                        kCFAllocatorDefault,
-                                                        kIORegistryIterateRecursively | kIORegistryIterateParents)) {
-      auto value = cf_utility::to_string(property);
-      CFRelease(property);
-      return value;
-    }
-
-    return boost::none;
-  }
-
-  static boost::optional<std::string> find_manufacturer(io_service_t service) {
-    return find_string_property(service, CFSTR(kIOHIDManufacturerKey));
-  }
-
-  static boost::optional<std::string> find_product(io_service_t service) {
-    return find_string_property(service, CFSTR(kIOHIDProductKey));
-  }
-
-  static boost::optional<std::string> find_serial_number(io_service_t service) {
-    return find_string_property(service, CFSTR(kIOHIDSerialNumberKey));
-  }
-
-  static boost::optional<long> find_long_property(IOHIDDeviceRef _Nonnull device, CFStringRef _Nonnull key) {
-    auto property = IOHIDDeviceGetProperty(device, key);
-    if (!property) {
-      return boost::none;
-    }
-
-    if (CFNumberGetTypeID() != CFGetTypeID(property)) {
-      return boost::none;
-    }
-
-    long value = 0;
-    if (!CFNumberGetValue(static_cast<CFNumberRef>(property), kCFNumberLongType, &value)) {
-      return boost::none;
-    }
-
-    return value;
-  }
-
-  static boost::optional<std::string> find_string_property(IOHIDDeviceRef _Nonnull device, CFStringRef _Nonnull key) {
-    auto property = IOHIDDeviceGetProperty(device, key);
-    return cf_utility::to_string(property);
-  }
-
-  static boost::optional<long> find_max_input_report_size(IOHIDDeviceRef _Nonnull device) {
-    return find_long_property(device, CFSTR(kIOHIDMaxInputReportSizeKey));
-  }
-
-  static vendor_id find_vendor_id(IOHIDDeviceRef _Nonnull device) {
-    if (auto value = find_long_property(device, CFSTR(kIOHIDVendorIDKey))) {
-      return static_cast<vendor_id>(*value);
-    }
-    return vendor_id(0);
-  }
-
-  static product_id find_product_id(IOHIDDeviceRef _Nonnull device) {
-    if (auto value = find_long_property(device, CFSTR(kIOHIDProductIDKey))) {
-      return static_cast<product_id>(*value);
-    }
-    return product_id(0);
-  }
-
-  static boost::optional<location_id> find_location_id(IOHIDDeviceRef _Nonnull device) {
-    if (auto value = find_long_property(device, CFSTR(kIOHIDLocationIDKey))) {
-      return static_cast<location_id>(*value);
-    }
-    return boost::none;
-  }
-
-  static boost::optional<std::string> find_manufacturer(IOHIDDeviceRef _Nonnull device) {
-    return find_string_property(device, CFSTR(kIOHIDManufacturerKey));
-  }
-
-  static boost::optional<std::string> find_product(IOHIDDeviceRef _Nonnull device) {
-    return find_string_property(device, CFSTR(kIOHIDProductKey));
-  }
-
-  static boost::optional<std::string> find_serial_number(IOHIDDeviceRef _Nonnull device) {
-    return find_string_property(device, CFSTR(kIOHIDSerialNumberKey));
-  }
-
-  static boost::optional<std::string> find_transport(IOHIDDeviceRef _Nonnull device) {
-    return find_string_property(device, CFSTR(kIOHIDTransportKey));
-  }
-
   static bool is_keyboard(IOHIDDeviceRef _Nonnull device) {
-    return IOHIDDeviceConformsTo(device, kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard);
+    if (device) {
+      return IOHIDDeviceConformsTo(device, kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard);
+    }
+    return false;
   }
 
   static bool is_pointing_device(IOHIDDeviceRef _Nonnull device) {
-    return IOHIDDeviceConformsTo(device, kHIDPage_GenericDesktop, kHIDUsage_GD_Pointer) ||
-           IOHIDDeviceConformsTo(device, kHIDPage_GenericDesktop, kHIDUsage_GD_Mouse);
+    if (device) {
+      return IOHIDDeviceConformsTo(device, kHIDPage_GenericDesktop, kHIDUsage_GD_Pointer) ||
+             IOHIDDeviceConformsTo(device, kHIDPage_GenericDesktop, kHIDUsage_GD_Mouse);
+    }
+    return false;
   }
 
   static bool is_karabiner_virtual_hid_device(IOHIDDeviceRef _Nonnull device) {
-    if (auto manufacturer = find_manufacturer(device)) {
+    pqrs::osx::iokit_hid_device hid_device(device);
+
+    if (auto manufacturer = hid_device.find_manufacturer()) {
       if (*manufacturer == "pqrs.org") {
         return true;
       }
@@ -130,23 +42,52 @@ public:
     return false;
   }
 
+  static std::string make_device_name_for_log(device_id device_id,
+                                              IOHIDDeviceRef _Nonnull device) {
+    std::stringstream stream;
+    pqrs::osx::iokit_hid_device hid_device(device);
+
+    if (auto product_name = hid_device.find_product()) {
+      stream << pqrs::string::trim_copy(*product_name);
+    } else {
+      if (auto vendor_id = hid_device.find_vendor_id()) {
+        if (auto product_id = hid_device.find_product_id()) {
+          stream << std::hex
+                 << "(vendor_id:0x" << type_safe::get(*vendor_id)
+                 << ", product_id:0x" << type_safe::get(*product_id)
+                 << ")"
+                 << std::dec;
+        }
+      }
+    }
+
+    stream << " (device_id:" << type_safe::get(device_id) << ")";
+    return stream.str();
+  }
+
   static void log_matching_device(pqrs::osx::iokit_registry_entry_id registry_entry_id,
                                   IOHIDDeviceRef _Nonnull device) {
+    pqrs::osx::iokit_hid_device hid_device(device);
+
     logger::get_logger().info("matching device:");
     logger::get_logger().info("  registry_entry_id: {0}", type_safe::get(registry_entry_id));
 
-    if (auto manufacturer = find_manufacturer(device)) {
+    if (auto manufacturer = hid_device.find_manufacturer()) {
       logger::get_logger().info("  manufacturer: {0}", *manufacturer);
     }
-    if (auto product = find_product(device)) {
+    if (auto product = hid_device.find_product()) {
       logger::get_logger().info("  product: {0}", *product);
     }
-    logger::get_logger().info("  vendor_id: {0}", static_cast<uint32_t>(find_vendor_id(device)));
-    logger::get_logger().info("  product_id: {0}", static_cast<uint32_t>(find_product_id(device)));
-    if (auto location_id = find_location_id(device)) {
-      logger::get_logger().info("  location_id: {0:#x}", static_cast<uint32_t>(*location_id));
+    if (auto vendor_id = hid_device.find_vendor_id()) {
+      logger::get_logger().info("  vendor_id: {0}", type_safe::get(*vendor_id));
     }
-    if (auto serial_number = find_serial_number(device)) {
+    if (auto product_id = hid_device.find_product_id()) {
+      logger::get_logger().info("  product_id: {0}", type_safe::get(*product_id));
+    }
+    if (auto location_id = hid_device.find_location_id()) {
+      logger::get_logger().info("  location_id: {0:#x}", type_safe::get(*location_id));
+    }
+    if (auto serial_number = hid_device.find_serial_number()) {
       logger::get_logger().info("  serial_number: {0}", *serial_number);
     }
     logger::get_logger().info("  is_keyboard: {0}", is_keyboard(device));
