f3b6771fe3efdc31efb321344fb2e089b43cb00e
Takayama Fumihiko <tekezo@pqrs.org>
2018-12-02 19:30:37 +0900

    remove cf_utility::to_string

--- src/share/cf_utility.hpp
+++ src/share/cf_utility.hpp
@@ -16,33 +16,6 @@ public:
   // Converts
   // ========================================
 
-  static boost::optional<std::string> to_string(CFTypeRef _Nullable value) {
-    if (!value) {
-      return boost::none;
-    }
-
-    if (CFStringGetTypeID() != CFGetTypeID(value)) {
-      return boost::none;
-    }
-
-    auto cfstring = static_cast<CFStringRef>(value);
-
-    std::string string;
-    if (auto p = CFStringGetCStringPtr(cfstring, kCFStringEncodingUTF8)) {
-      string = p;
-    } else {
-      auto length = CFStringGetLength(cfstring);
-      auto max_size = CFStringGetMaximumSizeForEncoding(length, kCFStringEncodingUTF8) + 1;
-      char* buffer = new char[max_size];
-      if (CFStringGetCString(cfstring, buffer, max_size, kCFStringEncodingUTF8)) {
-        string = buffer;
-      }
-      delete[] buffer;
-    }
-
-    return string;
-  }
-
   static CFStringRef _Nullable create_cfstring(const std::string& string) {
     return CFStringCreateWithCString(kCFAllocatorDefault,
                                      string.c_str(),
