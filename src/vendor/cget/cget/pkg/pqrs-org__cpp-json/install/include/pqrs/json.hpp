d8f736e914564ba092b550850f50b8cfc614a889
Takayama Fumihiko <tekezo@pqrs.org>
2019-01-26 19:59:39 +0900

    update vendor

---
+++ src/vendor/cget/cget/pkg/pqrs-org__cpp-json/install/include/pqrs/json.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+// pqrs::json v1.0
+
+// (C) Copyright Takayama Fumihiko 2019.
+// Distributed under the Boost Software License, Version 1.0.
+// (See http://www.boost.org/LICENSE_1_0.txt)
+
+#include <nlohmann/json.hpp>
+#include <optional>
+
+namespace pqrs {
+namespace json {
+template <typename T>
+inline std::optional<T> find(const nlohmann::json& json,
+                             const std::string& key) {
+  auto it = json.find(key);
+  if (it != std::end(json)) {
+    try {
+      return it.value().get<T>();
+    } catch (std::exception&) {
+    }
+  }
+
+  return std::nullopt;
+}
+
+inline std::optional<nlohmann::json::const_iterator> find_array(const nlohmann::json& json,
+                                                                const std::string& key) {
+  auto it = json.find(key);
+  if (it != std::end(json)) {
+    if (it->is_array()) {
+      return it;
+    }
+  }
+  return std::nullopt;
+}
+
+inline std::optional<nlohmann::json::const_iterator> find_object(const nlohmann::json& json,
+                                                                 const std::string& key) {
+  auto it = json.find(key);
+  if (it != std::end(json)) {
+    if (it->is_object()) {
+      return it;
+    }
+  }
+  return std::nullopt;
+}
+
+inline std::optional<nlohmann::json::const_iterator> find_json(const nlohmann::json& json,
+                                                               const std::string& key) {
+  auto it = json.find(key);
+  if (it != std::end(json)) {
+    return it;
+  }
+  return std::nullopt;
+}
+
+inline nlohmann::json find_copy(const nlohmann::json& json,
+                                const std::string& key,
+                                const nlohmann::json& fallback_value) {
+  auto it = json.find(key);
+  if (it != std::end(json)) {
+    return it.value();
+  }
+  return fallback_value;
+}
+} // namespace json
+} // namespace pqrs
