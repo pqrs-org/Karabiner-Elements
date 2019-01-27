2801470da35fcb681e9f2a12ab8f9e88c211c255
Takayama Fumihiko <tekezo@pqrs.org>
2019-01-27 19:11:01 +0900

    update device_id

--- src/share/types/device_id.hpp
+++ src/share/types/device_id.hpp
@@ -1,33 +1,11 @@
 #pragma once
 
-#include <cstdint>
-#include <iostream>
 #include <pqrs/osx/iokit_types.hpp>
-#include <type_safe/strong_typedef.hpp>
-#include <unordered_set>
 
 namespace krbn {
-struct device_id : type_safe::strong_typedef<device_id, uint64_t>,
-                   type_safe::strong_typedef_op::equality_comparison<device_id>,
-                   type_safe::strong_typedef_op::relational_comparison<device_id> {
-  using strong_typedef::strong_typedef;
-};
-
-inline size_t hash_value(const device_id& value) {
-  return std::hash<uint64_t>{}(type_safe::get(value));
-}
-
-inline std::ostream& operator<<(std::ostream& stream, const device_id& value) {
-  return stream << type_safe::get(value);
-}
+using device_id = pqrs::osx::iokit_registry_entry_id;
 
 inline device_id make_device_id(const pqrs::osx::iokit_registry_entry_id& value) {
-  return device_id(type_safe::get(value));
+  return value;
 }
 } // namespace krbn
-
-namespace std {
-template <>
-struct hash<krbn::device_id> : type_safe::hashable<krbn::device_id> {
-};
-} // namespace std
