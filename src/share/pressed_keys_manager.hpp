0aac6bcdced96ef1d09c226a678831310940a054
Takayama Fumihiko <tekezo@pqrs.org>
2019-02-04 11:41:04 +0900

    use mpark::variant

--- src/share/pressed_keys_manager.hpp
+++ src/share/pressed_keys_manager.hpp
@@ -2,48 +2,13 @@
 
 // `krbn::pressed_keys_manager` can be used safely in a multi-threaded environment.
 
-#include "boost_defs.hpp"
-
 #include "types.hpp"
-#include <boost/functional/hash.hpp>
-#include <boost/variant.hpp>
-#include <pqrs/osx/iokit_types.hpp>
+#include <mpark/variant.hpp>
 #include <unordered_set>
 
 namespace krbn {
 class pressed_keys_manager {
 public:
-  void insert(key_code value) {
-    insert<key_code>(value);
-  }
-
-  void insert(consumer_key_code value) {
-    insert<consumer_key_code>(value);
-  }
-
-  void insert(pointing_button value) {
-    insert<pointing_button>(value);
-  }
-
-  void erase(key_code value) {
-    erase<key_code>(value);
-  }
-
-  void erase(consumer_key_code value) {
-    erase<consumer_key_code>(value);
-  }
-
-  void erase(pointing_button value) {
-    erase<pointing_button>(value);
-  }
-
-  bool empty(void) const {
-    std::lock_guard<std::mutex> lock(mutex_);
-
-    return entries_.empty();
-  }
-
-private:
   template <typename T>
   void insert(T value) {
     std::lock_guard<std::mutex> lock(mutex_);
@@ -58,11 +23,17 @@ private:
     entries_.erase(value);
   }
 
-  typedef boost::variant<key_code,
-                         consumer_key_code,
-                         pointing_button>
-      entry;
-  std::unordered_set<entry, boost::hash<entry>> entries_;
+  bool empty(void) const {
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    return entries_.empty();
+  }
+
+private:
+  using entry_t = mpark::variant<key_code,
+                                 consumer_key_code,
+                                 pointing_button>;
+  std::unordered_set<entry_t, std::hash<entry_t>> entries_;
 
   mutable std::mutex mutex_;
 };
--- tests/src/pressed_keys_manager/pressed_keys_manager_test.cpp
+++ tests/src/pressed_keys_manager/pressed_keys_manager_test.cpp
@@ -3,31 +3,90 @@
 #include "pressed_keys_manager.hpp"
 
 TEST_CASE("pressed_keys_manager") {
-  krbn::pressed_keys_manager manager;
+  // empty
 
-  REQUIRE(manager.empty());
+  {
+    krbn::pressed_keys_manager manager;
 
-  manager.insert(krbn::key_code::a);
-  REQUIRE(!manager.empty());
+    REQUIRE(manager.empty());
+  }
 
-  manager.insert(krbn::key_code::a);
-  REQUIRE(!manager.empty());
+  // key_code
 
-  manager.insert(krbn::consumer_key_code::mute);
-  REQUIRE(!manager.empty());
+  {
+    krbn::pressed_keys_manager manager;
 
-  manager.insert(krbn::pointing_button::button1);
-  REQUIRE(!manager.empty());
+    manager.insert(krbn::key_code::a);
+    REQUIRE(!manager.empty());
 
-  manager.erase(krbn::key_code::a);
-  REQUIRE(!manager.empty());
+    manager.erase(krbn::key_code::a);
+    REQUIRE(manager.empty());
+  }
 
-  manager.erase(krbn::consumer_key_code::mute);
-  REQUIRE(!manager.empty());
+  // Duplicated key_code
 
-  manager.erase(krbn::pointing_button::button10);
-  REQUIRE(!manager.empty());
+  {
+    krbn::pressed_keys_manager manager;
 
-  manager.erase(krbn::pointing_button::button1);
-  REQUIRE(manager.empty());
+    manager.insert(krbn::key_code::a);
+    manager.insert(krbn::key_code::a);
+    REQUIRE(!manager.empty());
+
+    manager.erase(krbn::key_code::a);
+    REQUIRE(manager.empty());
+  }
+
+  // consumer_key_code
+
+  {
+    krbn::pressed_keys_manager manager;
+
+    manager.insert(krbn::consumer_key_code::mute);
+    REQUIRE(!manager.empty());
+
+    manager.erase(krbn::consumer_key_code::mute);
+    REQUIRE(manager.empty());
+  }
+
+  // pointing_button
+
+  {
+    krbn::pressed_keys_manager manager;
+
+    manager.insert(krbn::pointing_button::button1);
+    REQUIRE(!manager.empty());
+
+    manager.erase(krbn::pointing_button::button1);
+    REQUIRE(manager.empty());
+  }
+
+  // combination
+
+  {
+    krbn::pressed_keys_manager manager;
+
+    manager.insert(krbn::key_code::a);
+    REQUIRE(!manager.empty());
+
+    manager.insert(krbn::key_code::a);
+    REQUIRE(!manager.empty());
+
+    manager.insert(krbn::consumer_key_code::mute);
+    REQUIRE(!manager.empty());
+
+    manager.insert(krbn::pointing_button::button1);
+    REQUIRE(!manager.empty());
+
+    manager.erase(krbn::key_code::a);
+    REQUIRE(!manager.empty());
+
+    manager.erase(krbn::consumer_key_code::mute);
+    REQUIRE(!manager.empty());
+
+    manager.erase(krbn::pointing_button::button10);
+    REQUIRE(!manager.empty());
+
+    manager.erase(krbn::pointing_button::button1);
+    REQUIRE(manager.empty());
+  }
 }
