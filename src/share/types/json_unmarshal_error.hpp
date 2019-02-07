a332f9f901b1f1e49cea9ab533e63bce2145270f
Takayama Fumihiko <tekezo@pqrs.org>
2019-02-08 03:07:41 +0900

    update json_unmarshal_error

--- src/share/types/json_unmarshal_error.hpp
+++ src/share/types/json_unmarshal_error.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
-#include <exception>
+#include <stdexcept>
 
 namespace krbn {
-class json_unmarshal_error : public std::exception {
+class json_unmarshal_error : public std::runtime_error {
+public:
+  json_unmarshal_error(const std::string& message) : std::runtime_error(message) {
+  }
 };
 } // namespace krbn
