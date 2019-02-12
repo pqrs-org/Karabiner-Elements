7aa659d68ce9bc44663c7d2004fe4c37f16a2233
Takayama Fumihiko <tekezo@pqrs.org>
2019-02-12 11:39:14 +0900

    update vendor

--- src/vendor/cget/cget/pkg/pqrs-org__cpp-osx-input_source_selector/install/include/pqrs/osx/input_source_selector.hpp
+++ src/vendor/cget/cget/pkg/pqrs-org__cpp-osx-input_source_selector/install/include/pqrs/osx/input_source_selector.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::input_source_selector v3.1
+// pqrs::osx::input_source_selector v3.2
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
