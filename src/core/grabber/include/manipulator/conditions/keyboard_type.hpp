b90aa0e765befb94ad2682c1287fe57dfb781c32
Takayama Fumihiko <tekezo@pqrs.org>
2019-02-17 19:16:23 +0900

    update error messages

--- src/core/grabber/include/manipulator/conditions/keyboard_type.hpp
+++ src/core/grabber/include/manipulator/conditions/keyboard_type.hpp
@@ -16,28 +16,46 @@ public:
 
   keyboard_type(const nlohmann::json& json) : base(),
                                               type_(type::keyboard_type_if) {
-    if (json.is_object()) {
-      for (auto it = std::begin(json); it != std::end(json); std::advance(it, 1)) {
-        // it.key() is always std::string.
-        const auto& key = it.key();
-        const auto& value = it.value();
+    if (!json.is_object()) {
+      throw pqrs::json::unmarshal_error(fmt::format("keyboard_type must be object, but is `{0}`", json.dump()));
+    }
 
-        if (key == "type") {
-          if (value.is_string()) {
-            if (value == "keyboard_type_if") {
-              type_ = type::keyboard_type_if;
-            }
-            if (value == "keyboard_type_unless") {
-              type_ = type::keyboard_type_unless;
-            }
-          }
-        } else if (key == "keyboard_types") {
-          for (const auto& j : value) {
-            keyboard_types_.push_back(j.get<std::string>());
-          }
-        } else {
-          logger::get_logger()->error("complex_modifications json error: Unknown key: {0} in {1}", key, json.dump());
+    for (const auto& [key, value] : json.items()) {
+      // key is always std::string.
+
+      if (key == "type") {
+        if (!value.is_string()) {
+          throw pqrs::json::unmarshal_error(fmt::format("{0} must be string, but is `{1}`", key, value.dump()));
         }
+
+        auto t = value.get<std::string>();
+
+        if (t == "keyboard_type_if") {
+          type_ = type::keyboard_type_if;
+        } else if (t == "keyboard_type_unless") {
+          type_ = type::keyboard_type_unless;
+        } else {
+          throw pqrs::json::unmarshal_error(fmt::format("unknown type `{0}`", t));
+        }
+
+      } else if (key == "keyboard_types") {
+        if (!value.is_array()) {
+          throw pqrs::json::unmarshal_error(fmt::format("`{0}` must be array, but is `{1}`", key, value.dump()));
+        }
+
+        for (const auto& j : value) {
+          if (!j.is_string()) {
+            throw pqrs::json::unmarshal_error(fmt::format("keyboard_types entry must be string, but is `{0}`", j.dump()));
+          }
+
+          keyboard_types_.push_back(j.get<std::string>());
+        }
+
+      } else if (key == "description") {
+        // Do nothing
+
+      } else {
+        throw pqrs::json::unmarshal_error(fmt::format("unknown key `{0}` in `{1}`", key, json.dump()));
       }
     }
   }
