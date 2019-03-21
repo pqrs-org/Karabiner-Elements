02c056f1844c03ab4e2ab7b0ae554ac7032c8e91
Takayama Fumihiko <tekezo@pqrs.org>
2019-03-21 17:03:04 +0900

    Set immutable flag to Karabiner-Elements.app,Karabiner-EventViewer.app

--- src/scripts/uninstall_core.sh
+++ src/scripts/uninstall_core.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
+PATH=/bin:/sbin:/usr/bin:/usr/sbin
+export PATH
 
 # ----------------------------------------
-# unload before install
+# Unload before install
 
 if [ /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist ]; then
     launchctl bootout system /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist
@@ -14,11 +15,18 @@ if [ /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_observer.plist ]; then
 fi
 
 # ----------------------------------------
-# uninstall
+# Clear immutable flag
+
+chflags nouchg,noschg /Applications/Karabiner-Elements.app
+chflags nouchg,noschg /Applications/Karabiner-EventViewer.app
+
+# ----------------------------------------
+# Uninstall
+
 bash '/Library/Application Support/org.pqrs/Karabiner-Elements/scripts/uninstall-Karabiner-VirtualHIDDevice.sh'
-rm -f  '/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist'
-rm -f  '/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_observer.plist'
-rm -f  '/Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist'
+rm -f '/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_grabber.plist'
+rm -f '/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_observer.plist'
+rm -f '/Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist'
 rm -rf '/Applications/Karabiner-Elements.app'
 rm -rf '/Applications/Karabiner-EventViewer.app'
 rm -rf '/Library/Application Support/org.pqrs/Karabiner-Elements'
