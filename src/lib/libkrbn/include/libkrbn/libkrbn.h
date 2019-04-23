50dd7165f2b5281232d7f8e4eee2ccb11b11d7de
Takayama Fumihiko <tekezo@pqrs.org>
2019-04-23 18:08:29 +0900

    add comment

--- src/lib/libkrbn/include/libkrbn/libkrbn.h
+++ src/lib/libkrbn/include/libkrbn/libkrbn.h
@@ -209,6 +209,7 @@ bool libkrbn_connected_devices_get_device_identifiers(libkrbn_connected_devices*
 bool libkrbn_connected_devices_get_is_built_in_keyboard(libkrbn_connected_devices* p, size_t index);
 bool libkrbn_connected_devices_get_is_built_in_trackpad(libkrbn_connected_devices* p, size_t index);
 
+// You have to call `libkrbn_connected_devices_terminate(&initialized_connected_devices)`.
 typedef void (*libkrbn_connected_devices_monitor_callback)(libkrbn_connected_devices* initialized_connected_devices,
                                                            void* refcon);
 void libkrbn_enable_connected_devices_monitor(libkrbn_connected_devices_monitor_callback callback,
