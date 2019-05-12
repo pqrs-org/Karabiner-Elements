0568302e0f107f4b8a6fa2dea522a39291512252
Takayama Fumihiko <tekezo@pqrs.org>
2019-05-12 10:00:08 +0900

    update tests

--- tests/src/orphan_key_up_events_manager/src/orphan_key_up_events_manager_test.cpp
+++ tests/src/orphan_key_up_events_manager/src/orphan_key_up_events_manager_test.cpp
@@ -70,3 +70,25 @@ TEST_CASE("orphan_key_up_events_manager") {
            krbn::absolute_time_point(500));
   REQUIRE(m.find_orphan_key_up_event() == krbn::key_down_up_valued_event(krbn::key_code::a));
 }
+
+TEST_CASE("orphan_key_up_events_manager key_down is dropped") {
+  krbn::orphan_key_up_events_manager m;
+
+  // ----------------------------------------
+  // key_down, key_up, key_up
+
+  m.update(krbn::key_down_up_valued_event(krbn::key_code::a),
+           krbn::event_type::key_down,
+           krbn::absolute_time_point(1000));
+  REQUIRE(m.find_orphan_key_up_event() == std::nullopt);
+
+  m.update(krbn::key_down_up_valued_event(krbn::key_code::a),
+           krbn::event_type::key_up,
+           krbn::absolute_time_point(1010));
+  REQUIRE(m.find_orphan_key_up_event() == std::nullopt);
+
+  m.update(krbn::key_down_up_valued_event(krbn::key_code::a),
+           krbn::event_type::key_up,
+           krbn::absolute_time_point(1020));
+  REQUIRE(m.find_orphan_key_up_event() == std::nullopt);
+}
