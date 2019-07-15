93538337c783b094d714fa730b67bbccce18b982
Takayama Fumihiko <tekezo@pqrs.org>
2019-07-16 00:10:19 +0900

    refactor launchctl_utility

--- src/share/launchctl_utility.hpp
+++ src/share/launchctl_utility.hpp
@@ -7,71 +7,85 @@
 #include <string>
 
 namespace krbn {
-class launchctl_utility final {
-public:
-  static void manage_session_monitor(void) {
-    uid_t uid = geteuid();
-    auto domain_target = (std::stringstream() << "gui/" << uid).str();
-    auto service_target = (std::stringstream() << "gui/" << uid << "/org.pqrs.karabiner.karabiner_session_monitor").str();
-    auto service_path = "/Library/LaunchAgents/org.pqrs.karabiner.karabiner_session_monitor.plist";
+namespace launchctl_utility {
+inline void enable_agent(const std::string& service_name,
+                         const std::string& service_path) {
+  uid_t uid = geteuid();
+  auto domain_target = (std::stringstream() << "gui/" << uid).str();
+  auto service_target = (std::stringstream() << "gui/" << uid << "/" << service_name).str();
 
-    {
-      auto command = std::string("/bin/launchctl enable ") + service_target;
-      system(command.c_str());
-    }
-    {
-      auto command = std::string("/bin/launchctl bootstrap ") + domain_target + " " + service_path;
-      system(command.c_str());
-    }
-    {
-      auto command = std::string("/bin/launchctl enable ") + service_target;
-      system(command.c_str());
-    }
+  // If service_path is already bootstrapped and disabled, launchctl bootstrap will fail until it is enabled again.
+  // So we should enable it first, and then bootstrap and enable it.
+
+  {
+    auto command = std::string("/bin/launchctl enable ") + service_target;
+    system(command.c_str());
   }
-
-  static void manage_console_user_server(bool load) {
-    uid_t uid = geteuid();
-    auto domain_target = (std::stringstream() << "gui/" << uid).str();
-    auto service_target = (std::stringstream() << "gui/" << uid << "/org.pqrs.karabiner.karabiner_console_user_server").str();
-    auto service_path = "/Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist";
-
-    if (load) {
-      // If service_path is already bootstrapped and disabled, launchctl bootstrap will fail until it is enabled again.
-      // So we should enable it first, and then bootstrap and enable it.
-
-      {
-        auto command = std::string("/bin/launchctl enable ") + service_target;
-        system(command.c_str());
-      }
-      {
-        auto command = std::string("/bin/launchctl bootstrap ") + domain_target + " " + service_path;
-        system(command.c_str());
-      }
-      {
-        auto command = std::string("/bin/launchctl enable ") + service_target;
-        system(command.c_str());
-      }
-
-    } else {
-      {
-        auto command = std::string("/bin/launchctl bootout ") + domain_target + " " + service_path;
-        system(command.c_str());
-      }
-      {
-        auto command = std::string("/bin/launchctl disable ") + service_target;
-        system(command.c_str());
-      }
-
-      apple_notification_center::post_distributed_notification(constants::get_distributed_notification_console_user_server_is_disabled());
-    }
+  {
+    auto command = std::string("/bin/launchctl bootstrap ") + domain_target + " " + service_path;
+    system(command.c_str());
   }
+  {
+    auto command = std::string("/bin/launchctl enable ") + service_target;
+    system(command.c_str());
+  }
+}
 
-  static void restart_console_user_server(void) {
-    uid_t uid = geteuid();
-    auto service_target = (std::stringstream() << "gui/" << uid << "/org.pqrs.karabiner.karabiner_console_user_server").str();
+inline void disable_agent(const std::string& service_name,
+                          const std::string& service_path) {
+  uid_t uid = geteuid();
+  auto domain_target = (std::stringstream() << "gui/" << uid).str();
+  auto service_target = (std::stringstream() << "gui/" << uid << "/" << service_name).str();
 
+  {
+    auto command = std::string("/bin/launchctl bootout ") + domain_target + " " + service_path;
+    system(command.c_str());
+  }
+  {
+    auto command = std::string("/bin/launchctl disable ") + service_target;
+    system(command.c_str());
+  }
+}
+
+inline void restart_agent(const std::string& service_name) {
+  uid_t uid = geteuid();
+  auto service_target = (std::stringstream() << "gui/" << uid << "/" << service_name).str();
+
+  {
     auto command = std::string("/bin/launchctl kickstart -k ") + service_target;
     system(command.c_str());
   }
-};
+}
+
+inline void manage_observer_agent(void) {
+  std::string service_name("org.pqrs.karabiner.agent.karabiner_observer");
+  std::string service_path("/Library/LaunchAgents/org.pqrs.karabiner.agent.karabiner_observer.plist");
+
+  enable_agent(service_name, service_path);
+  restart_agent(service_name);
+}
+
+inline void manage_session_monitor(void) {
+  enable_agent("org.pqrs.karabiner.karabiner_session_monitor",
+               "/Library/LaunchAgents/org.pqrs.karabiner.karabiner_session_monitor.plist");
+}
+
+inline void manage_console_user_server(bool load) {
+  std::string service_name("org.pqrs.karabiner.karabiner_console_user_server");
+  std::string service_path("/Library/LaunchAgents/org.pqrs.karabiner.karabiner_console_user_server.plist");
+
+  if (load) {
+    enable_agent(service_name, service_path);
+  } else {
+    disable_agent(service_name, service_path);
+    apple_notification_center::post_distributed_notification(constants::get_distributed_notification_console_user_server_is_disabled());
+  }
+}
+
+inline void restart_console_user_server(void) {
+  std::string service_name("org.pqrs.karabiner.karabiner_console_user_server");
+
+  restart_agent(service_name);
+}
+} // namespace launchctl_utility
 } // namespace krbn
