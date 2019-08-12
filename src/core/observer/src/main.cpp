26fb5653680544a3d8ae9caff3f7abce5123f1da
Takayama Fumihiko <tekezo@pqrs.org>
2019-08-12 14:49:47 +0900

    KeepAlive karabiner_observer (from LaunchAgents)

--- src/core/observer/src/main.cpp
+++ src/core/observer/src/main.cpp
@@ -18,8 +18,57 @@ int main(int argc, const char* argv[]) {
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
 
+  //
+  // Check euid
+  // (observer is launched from LaunchDaemons (root) and LaunchAgents (user).)
+  //
+
+  bool root = (geteuid() == 0);
+
+  //
+  // Setup logger
+  //
+
+  if (root) {
+    krbn::logger::set_async_rotating_logger("observer",
+                                            "/var/log/karabiner/observer.log",
+                                            0755);
+  } else {
+    krbn::logger::set_async_rotating_logger("observer_agent",
+                                            krbn::constants::get_user_log_directory() + "/observer_agent.log",
+                                            0700);
+  }
+
+  krbn::logger::get_logger()->info("version {0}", karabiner_version);
+
+  //
+  // Check another process
+  //
+
+  {
+    bool another_process_running = false;
+
+    if (root) {
+      std::string pid_file_path = krbn::constants::get_pid_directory() + "/karabiner_observer.pid";
+      another_process_running = !krbn::process_utility::lock_single_application(pid_file_path);
+    } else {
+      another_process_running = !krbn::process_utility::lock_single_application_with_user_pid_file("karabiner_observer_agent.pid");
+    }
+
+    if (another_process_running) {
+      auto message = "Exit since another process is running.";
+      krbn::logger::get_logger()->info(message);
+      std::cerr << message << std::endl;
+      return 1;
+    }
+  }
+
+  //
+  // Prepare state_json_writer
+  //
+
   std::shared_ptr<krbn::state_json_writer> state_json_writer;
-  if (geteuid() == 0) {
+  if (root) {
     state_json_writer = std::make_shared<krbn::state_json_writer>(
         krbn::constants::get_observer_state_json_file_path());
   }
@@ -33,59 +82,38 @@ int main(int argc, const char* argv[]) {
     if (state_json_writer) {
       state_json_writer->set("hid_device_open_permitted", false);
     }
-
-    return 0;
-  }
-
-  if (state_json_writer) {
-    state_json_writer->set("hid_device_open_permitted", true);
-  }
-
-  //
-  // Check euid
-  //
-
-  if (geteuid() != 0) {
-    return 0;
-  }
-
-  // Setup logger
-
-  krbn::logger::set_async_rotating_logger("observer",
-                                          "/var/log/karabiner/observer.log",
-                                          0755);
-
-  krbn::logger::get_logger()->info("version {0}", karabiner_version);
-
-  // Check another process
-
-  {
-    std::string pid_file_path = krbn::constants::get_pid_directory() + "/karabiner_observer.pid";
-    if (!krbn::process_utility::lock_single_application(pid_file_path)) {
-      auto message = "Exit since another process is running.";
-      krbn::logger::get_logger()->info(message);
-      std::cerr << message << std::endl;
-      return 1;
+  } else {
+    if (state_json_writer) {
+      state_json_writer->set("hid_device_open_permitted", true);
     }
   }
 
+  //
   // Run components_manager
+  //
 
-  std::shared_ptr<krbn::observer::components_manager> components_manager;
+  // We have to use raw pointer (not smart pointer) to delete it in `dispatch_async`.
+  krbn::observer::components_manager* components_manager = nullptr;
 
   auto version_monitor = std::make_shared<krbn::version_monitor>(krbn::constants::get_version_file_path());
 
-  version_monitor->changed.connect([&](auto&& version) {
+  version_monitor->changed.connect([&components_manager](auto&& version) {
     dispatch_async(dispatch_get_main_queue(), ^{
-      components_manager = nullptr;
+      if (components_manager) {
+        delete components_manager;
+      }
       CFRunLoopStop(CFRunLoopGetCurrent());
     });
   });
 
-  components_manager = std::make_shared<krbn::observer::components_manager>(version_monitor);
+  if (root) {
+    components_manager = new krbn::observer::components_manager(version_monitor);
+  }
 
   version_monitor->async_start();
-  components_manager->async_start();
+  if (components_manager) {
+    components_manager->async_start();
+  }
 
   CFRunLoopRun();
 
