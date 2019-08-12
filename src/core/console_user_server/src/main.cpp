016682a01b9b7b6ca40fc6758ae5abaee0f95cae
Takayama Fumihiko <tekezo@pqrs.org>
2019-08-12 17:20:04 +0900

    fix console_user_server at version changed

--- src/core/console_user_server/src/main.cpp
+++ src/core/console_user_server/src/main.cpp
@@ -10,14 +10,18 @@
 #include <pqrs/filesystem.hpp>
 
 int main(int argc, const char* argv[]) {
+  //
   // Initialize
+  //
 
   auto scoped_dispatcher_manager = krbn::dispatcher_utility::initialize_dispatchers();
 
   signal(SIGUSR1, SIG_IGN);
   signal(SIGUSR2, SIG_IGN);
 
+  //
   // Setup logger
+  //
 
   if (!krbn::constants::get_user_log_directory().empty()) {
     krbn::logger::set_async_rotating_logger("console_user_server",
@@ -27,43 +31,59 @@ int main(int argc, const char* argv[]) {
 
   krbn::logger::get_logger()->info("version {0}", karabiner_version);
 
+  //
   // Check another process
+  //
 
   if (!krbn::process_utility::lock_single_application_with_user_pid_file("karabiner_console_user_server.pid")) {
-    krbn::logger::get_logger()->info("Exit since another process is running.");
+    auto message = "Exit since another process is running.";
+    krbn::logger::get_logger()->info(message);
+    std::cerr << message << std::endl;
     return 1;
   }
 
+  //
   // Manage launchctl
+  //
 
   krbn::launchctl_utility::manage_session_monitor();
   krbn::launchctl_utility::manage_observer_agent();
   krbn::launchctl_utility::manage_grabber_agent();
   krbn::launchctl_utility::manage_console_user_server(true);
 
+  //
   // Migrate old configuration file
+  //
 
   krbn::console_user_server::migration::migrate_v1();
 
+  //
   // Create directories
+  //
 
   pqrs::filesystem::create_directory_with_intermediate_directories(krbn::constants::get_user_configuration_directory(), 0700);
   pqrs::filesystem::create_directory_with_intermediate_directories(krbn::constants::get_user_complex_modifications_assets_directory(), 0700);
 
+  //
   // Run components_manager
+  //
 
-  std::shared_ptr<krbn::console_user_server::components_manager> components_manager;
+  // We have to use raw pointer (not smart pointer) to delete it in `dispatch_async`.
+  krbn::console_user_server::components_manager* components_manager = nullptr;
 
   auto version_monitor = std::make_shared<krbn::version_monitor>(krbn::constants::get_version_file_path());
 
   version_monitor->changed.connect([&](auto&& version) {
     dispatch_async(dispatch_get_main_queue(), ^{
-      components_manager = nullptr;
+      pqrs::gcd::scoped_running_on_main_queue_marker marker;
+
+      delete components_manager;
+
       CFRunLoopStop(CFRunLoopGetCurrent());
     });
   });
 
-  components_manager = std::make_shared<krbn::console_user_server::components_manager>(version_monitor);
+  components_manager = new krbn::console_user_server::components_manager(version_monitor);
 
   version_monitor->async_start();
   components_manager->async_start();
