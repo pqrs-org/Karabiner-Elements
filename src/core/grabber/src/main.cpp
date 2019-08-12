79eb93d64216c51ff9232129f6a4c7c2bba9520f
Takayama Fumihiko <tekezo@pqrs.org>
2019-08-12 17:38:14 +0900

    use pqrs::gcd::scoped_running_on_main_queue_marker

--- src/core/grabber/src/main.cpp
+++ src/core/grabber/src/main.cpp
@@ -120,9 +120,12 @@ int main(int argc, const char* argv[]) {
 
   version_monitor->changed.connect([&components_manager](auto&& version) {
     dispatch_async(dispatch_get_main_queue(), ^{
+      pqrs::gcd::scoped_running_on_main_queue_marker marker;
+
       if (components_manager) {
         delete components_manager;
       }
+
       CFRunLoopStop(CFRunLoopGetCurrent());
     });
   });
