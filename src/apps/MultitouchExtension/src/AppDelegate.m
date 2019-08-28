1a9ba18dd85cd8f84174c3e6a4cc41c9400d5ee1
Takayama Fumihiko <tekezo@pqrs.org>
2019-08-29 00:10:17 +0900

    useMultitouchPrivate.h

--- src/apps/MultitouchExtension/src/AppDelegate.m
+++ src/apps/MultitouchExtension/src/AppDelegate.m
@@ -3,6 +3,7 @@
 #import "FingerStatus.h"
 #import "IgnoredAreaView.h"
 #import "KarabinerKit/KarabinerKit.h"
+#import "MultitouchPrivate.h"
 #import "PreferencesController.h"
 #import "PreferencesKeys.h"
 #import <pqrs/weakify.h>
@@ -50,43 +51,6 @@ static NSTimer* reset_timer_;
 }
 
 // ------------------------------------------------------------
-typedef struct {
-  float x;
-  float y;
-} mtPoint;
-
-typedef struct {
-  mtPoint position;
-  mtPoint velocity;
-} mtReadout;
-
-typedef struct {
-  int frame;
-  double timestamp;
-  int identifier;
-  int state;
-  int unknown1;
-  int unknown2;
-  mtReadout normalized;
-  float size;
-  int unknown3;
-  float angle;
-  float majorAxis;
-  float minorAxis;
-  mtReadout unknown4;
-  int unknown5[2];
-  float unknown6;
-} Finger;
-
-typedef void* MTDeviceRef;
-typedef int (*MTContactCallbackFunction)(int, Finger*, int, double, int);
-
-CFMutableArrayRef MTDeviceCreateList(void);
-void MTRegisterContactFrameCallback(MTDeviceRef, MTContactCallbackFunction);
-void MTUnregisterContactFrameCallback(MTDeviceRef, MTContactCallbackFunction);
-void MTDeviceStart(MTDeviceRef, int);
-void MTDeviceStop(MTDeviceRef, int);
-
 static AppDelegate* global_self_ = nil;
 static IgnoredAreaView* global_ignoredAreaView_ = nil;
 
