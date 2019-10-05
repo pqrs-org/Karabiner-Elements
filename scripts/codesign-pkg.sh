298a888ac19f9b0f29e47561f8fe40d91b82a846
Takayama Fumihiko <tekezo@pqrs.org>
2019-10-05 23:01:32 +0900

    apply shfmt

--- scripts/codesign-pkg.sh
+++ scripts/codesign-pkg.sh
@@ -3,8 +3,9 @@
 set -e
 
 readonly CODESIGN_IDENTITY='C86BB5F7830071C7B0B07D168A9A9375CC2D02C5'
-readonly PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
-readonly LOGFILE="`dirname $0`/productsign.log"
+readonly PATH=/bin:/sbin:/usr/bin:/usr/sbin
+export PATH
+readonly LOGFILE="$(dirname $0)/productsign.log"
 
 err() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
@@ -16,8 +17,11 @@ main() {
         exit 1
     fi
 
+    #
     # Sign with codesign
-    if 1>"$LOGFILE" 2>&1 productsign --sign "$CODESIGN_IDENTITY" "$1" "$1".signed; then
+    #
+
+    if productsign 1>"$LOGFILE" 2>&1 --sign "$CODESIGN_IDENTITY" "$1" "$1".signed; then
         cat $LOGFILE
         mv "$1".signed "$1"
     else
@@ -30,4 +34,3 @@ main() {
 }
 
 main "$1"
-
