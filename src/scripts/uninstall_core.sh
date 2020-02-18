7aee7c369754b382ce7e2e49338a3ad0ae25b3fc
Takayama Fumihiko <tekezo@pqrs.org>
2020-02-18 13:49:04 +0900

    run kextcache -prune-staging in uninstall_core.sh

--- src/scripts/uninstall_core.sh
+++ src/scripts/uninstall_core.sh
@@ -3,8 +3,9 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
 
-# ----------------------------------------
+#
 # Unload before install
+#
 
 if [ /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_kextd.plist ]; then
     launchctl bootout system /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_kextd.plist
@@ -18,14 +19,16 @@ if [ /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_observer.plist ]; then
     launchctl bootout system /Library/LaunchDaemons/org.pqrs.karabiner.karabiner_observer.plist
 fi
 
-# ----------------------------------------
+#
 # Clear immutable flag
+#
 
 chflags nouchg,noschg /Applications/Karabiner-Elements.app
 chflags nouchg,noschg /Applications/Karabiner-EventViewer.app
 
-# ----------------------------------------
+#
 # Uninstall
+#
 
 bash '/Library/Application Support/org.pqrs/Karabiner-Elements/scripts/uninstall-Karabiner-VirtualHIDDevice.sh'
 rm -f '/Library/LaunchDaemons/org.pqrs.karabiner.karabiner_kextd.plist'
@@ -36,5 +39,11 @@ rm -f '/Library/LaunchAgents/org.pqrs.karabiner.karabiner_session_monitor.plist'
 rm -rf '/Applications/Karabiner-Elements.app'
 rm -rf '/Applications/Karabiner-EventViewer.app'
 rm -rf '/Library/Application Support/org.pqrs/Karabiner-Elements'
+
+#
+# Remove '/Library/StagedExtensions/Library/Application Support/org.pqrs/Karabiner-VirtualHIDDevice'
+#
+
+kextcache -prune-staging
 
 exit 0
