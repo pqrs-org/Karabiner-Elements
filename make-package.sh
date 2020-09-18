5790c5c1655a093a5a759fc1926ac1d1e8a635aa
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-18 22:45:32 +0900

    cmake -> xcodegen @ src/apps/MultitouchExtension

--- make-package.sh
+++ make-package.sh
@@ -24,7 +24,7 @@ cp src/scripts/uninstall.sh "$basedir"
 cp src/scripts/uninstall_core.sh "$basedir"
 cp files/complex_modifications_rules_example.json "$basedir"
 cp -R "src/apps/Menu/build/Release/Karabiner-Menu.app" "$basedir"
-cp -R "src/apps/MultitouchExtension/build_xcode/build/Release/Karabiner-MultitouchExtension.app" "$basedir"
+cp -R "src/apps/MultitouchExtension/build/Release/Karabiner-MultitouchExtension.app" "$basedir"
 cp -R "src/apps/NotificationWindow/build/Release/Karabiner-NotificationWindow.app" "$basedir"
 
 basedir="pkgroot/Library/Application Support/org.pqrs/Karabiner-Elements/scripts"
