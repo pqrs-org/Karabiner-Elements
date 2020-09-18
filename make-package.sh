a4b209d71fe28584100ee84f1a087cd5df0751ad
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-18 22:32:10 +0900

    cmake -> xcodegen @ src/apps/EventViewer

--- make-package.sh
+++ make-package.sh
@@ -52,7 +52,7 @@ cp -R files/LaunchAgents "pkgroot/Library"
 basedir="pkgroot/Applications"
 mkdir -p "$basedir"
 cp -R "src/apps/PreferencesWindow/build_xcode/build/Release/Karabiner-Elements.app" "$basedir"
-cp -R "src/apps/EventViewer/build_xcode/build/Release/Karabiner-EventViewer.app" "$basedir"
+cp -R "src/apps/EventViewer/build/Release/Karabiner-EventViewer.app" "$basedir"
 
 set +e
 
