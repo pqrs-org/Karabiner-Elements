9fc4fbc4f6340bd19ed6e292905238d75a5f4d26
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-21 08:12:21 +0900

    Read json file in background thread @ EventViewer/DevicesController

--- src/apps/EventViewer/src/DevicesController.swift
+++ src/apps/EventViewer/src/DevicesController.swift
@@ -8,13 +8,14 @@ private func callback(_ filePath: UnsafePointer<Int8>?, _ context: UnsafeMutable
   let path = String(cString: filePath!)
   let obj: DevicesController? = unsafeBitCast(context, to: DevicesController.self)
 
+  guard let message = try? String(contentsOfFile: path, encoding: .utf8) else { return }
+
   DispatchQueue.main.async { [weak obj] in
     guard let obj = obj else { return }
-    guard let string = try? String(contentsOfFile: path, encoding: .utf8) else { return }
 
     guard let textStorage = obj.textView!.textStorage else { return }
     guard let font = NSFont(name: "Menlo", size: 11) else { return }
-    let attributedString = NSAttributedString(string: string, attributes: [
+    let attributedString = NSAttributedString(string: message, attributes: [
       NSAttributedString.Key.font: font,
       NSAttributedString.Key.foregroundColor: NSColor.textColor,
     ])
