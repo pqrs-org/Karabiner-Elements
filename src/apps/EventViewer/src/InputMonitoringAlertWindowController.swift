8ea4ca0585e43df4d9c58d17353f96b7c3c97377
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-20 21:07:45 +0900

    EventViewer/InputMonitoringAlertWindowController.m -> swift

---
+++ src/apps/EventViewer/src/InputMonitoringAlertWindowController.swift
@@ -0,0 +1,26 @@
+import Cocoa
+
+@objc
+public class InputMonitoringAlertWindowController: NSWindowController {
+  @IBOutlet var parentWindow: NSWindow?
+
+  @objc
+  public func show() {
+    let frame = NSMakeRect(
+      parentWindow!.frame.origin.x + (parentWindow!.frame.size.width / 2) - (window!.frame.size.width / 2),
+      parentWindow!.frame.origin.y + (parentWindow!.frame.size.height / 2) - (window!.frame.size.height / 2),
+      window!.frame.size.width,
+      window!.frame.size.height
+    )
+
+    window!.setFrame(frame, display: false)
+    parentWindow!.addChildWindow(window!, ordered: .above)
+  }
+
+  @IBAction func openSystemPreferencesSecurity(_: NSButton) {
+    NSApplication.shared.miniaturizeAll(self)
+
+    let url = URL(string: "x-apple.systempreferences:com.apple.preference.security?Privacy")!
+    NSWorkspace.shared.open(url)
+  }
+}
