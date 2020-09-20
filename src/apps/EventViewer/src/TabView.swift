245e100183af9703c9e1f3614d645702fe24234d
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-21 08:24:44 +0900

    EventViewer/src/TabView.m -> swift

---
+++ src/apps/EventViewer/src/TabView.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+class TabView: NSTabView {
+  override var acceptsFirstResponder: Bool {
+    // Disable to become key view in order to avoid
+    // grabbing control-tab when Full Keyboard Access is enabled.
+
+    return false
+  }
+}
