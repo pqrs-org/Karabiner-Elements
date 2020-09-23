010dfaf6cb3749dbc9ed52fa1bb22982d4da07d5
Takayama Fumihiko <tekezo@pqrs.org>
2020-09-23 22:49:58 +0900

    Update EventViewer/FrontmostApplicationController (update obj.text in main queue

--- src/apps/EventViewer/src/FrontmostApplicationController.swift
+++ src/apps/EventViewer/src/FrontmostApplicationController.swift
@@ -13,33 +13,33 @@ private func callback(_ bundleIdentifier: UnsafePointer<Int8>?,
 
   if identifier == "org.pqrs.Karabiner-EventViewer" { return }
 
-  // Update obj.text
-
-  guard let font = NSFont(name: "Menlo", size: 11) else { return }
-  let attributes = [
-    NSAttributedString.Key.font: font,
-    NSAttributedString.Key.foregroundColor: NSColor.textColor,
-  ]
-
-  if obj.text.length > 4 * 1024 {
-    obj.text.setAttributedString(NSAttributedString(
-      string: "", attributes: attributes
-    ))
-  }
-
-  obj.text.append(NSAttributedString(
-    string: "Bundle Identifier:  \(identifier)\n",
-    attributes: attributes
-  ))
-  obj.text.append(NSAttributedString(
-    string: "File Path:          \(path)\n\n",
-    attributes: attributes
-  ))
-
-  // Update obj.textView
-
   DispatchQueue.main.async { [weak obj] in
     guard let obj = obj else { return }
+
+    // Update obj.text
+
+    guard let font = NSFont(name: "Menlo", size: 11) else { return }
+    let attributes = [
+      NSAttributedString.Key.font: font,
+      NSAttributedString.Key.foregroundColor: NSColor.textColor,
+    ]
+
+    if obj.text.length > 4 * 1024 {
+      obj.text.setAttributedString(NSAttributedString(
+        string: "", attributes: attributes
+      ))
+    }
+
+    obj.text.append(NSAttributedString(
+      string: "Bundle Identifier:  \(identifier)\n",
+      attributes: attributes
+    ))
+    obj.text.append(NSAttributedString(
+      string: "File Path:          \(path)\n\n",
+      attributes: attributes
+    ))
+
+    // Update obj.textView
 
     guard let textStorage = obj.textView.textStorage else { return }
 
