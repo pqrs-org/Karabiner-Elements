df1ba28526966677cfb359123d00d18447683296
Takayama Fumihiko <tekezo@pqrs.org>
2020-10-17 23:37:43 +0900

    Add observer_state_json_writer

---
+++ src/core/observer/include/observer/observer_state_json_writer.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+// `observer_state_json_writer` can be used safely in a multi-threaded environment.
+
+#include "constants.hpp"
+#include "state_json_writer.hpp"
+
+namespace krbn {
+namespace observer {
+class observer_state_json_writer final {
+public:
+  observer_state_json_writer(void) : state_json_writer_(constants::get_observer_state_json_file_path()) {
+    set_hid_device_open_permitted(false);
+  }
+
+  void set_hid_device_open_permitted(bool value) {
+    state_json_writer_.set("hid_device_open_permitted", value);
+  }
+
+private:
+  krbn::state_json_writer state_json_writer_;
+};
+} // namespace observer
+} // namespace krbn
