2d34645acb3db1e3d04df1d7515f617c9b79a5a6
Takayama Fumihiko <tekezo@pqrs.org>
2020-10-18 23:49:19 +0900

    Add AlertWindowsManager.showInputMonitoringPermissionsAlertWindow

--- src/apps/PreferencesWindow/src/AlertWindowsManager.swift
+++ src/apps/PreferencesWindow/src/AlertWindowsManager.swift
@@ -6,6 +6,7 @@ import SwiftUI
 public class AlertWindowsManager: NSObject {
     @IBOutlet var parentWindow: NSWindow!
     var driverNotLoadedAlertWindow: NSWindow?
+    var inputMonitoringPermissionsAlertWindow: NSWindow?
 
     @objc
     public func showDriverNotLoadedAlertWindow() {
@@ -20,12 +21,34 @@ public class AlertWindowsManager: NSObject {
                 backing: .buffered,
                 defer: false
             )
-            driverNotLoadedAlertWindow!.title = "Driver Alert"
+            driverNotLoadedAlertWindow!.title = DriverNotLoadedAlertView.title
             driverNotLoadedAlertWindow!.contentView = NSHostingView(rootView: DriverNotLoadedAlertView())
             driverNotLoadedAlertWindow!.centerToOtherWindow(parentWindow)
             parentWindow.addChildWindow(driverNotLoadedAlertWindow!, ordered: .above)
         }
 
         driverNotLoadedAlertWindow!.makeKeyAndOrderFront(nil)
+    }
+
+    @objc
+    public func showInputMonitoringPermissionsAlertWindow() {
+        if inputMonitoringPermissionsAlertWindow == nil {
+            inputMonitoringPermissionsAlertWindow = NSPanel(
+                contentRect: .zero,
+                styleMask: [
+                    .titled,
+                    .closable,
+                    .fullSizeContentView,
+                ],
+                backing: .buffered,
+                defer: false
+            )
+            inputMonitoringPermissionsAlertWindow!.title = InputMonitoringPermissionsAlertView.title
+            inputMonitoringPermissionsAlertWindow!.contentView = NSHostingView(rootView: InputMonitoringPermissionsAlertView())
+            inputMonitoringPermissionsAlertWindow!.centerToOtherWindow(parentWindow)
+            parentWindow.addChildWindow(inputMonitoringPermissionsAlertWindow!, ordered: .above)
+        }
+
+        inputMonitoringPermissionsAlertWindow!.makeKeyAndOrderFront(nil)
     }
 }
