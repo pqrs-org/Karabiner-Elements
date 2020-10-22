443b49f4a96d7e5eda3462af51460e144ebb276c
Takayama Fumihiko <tekezo@pqrs.org>
2020-10-23 00:41:38 +0900

    Update DriverVersionNotMatchedAlertView

--- src/apps/PreferencesWindow/src/View/DriverVersionNotMatchedAlertView.swift
+++ src/apps/PreferencesWindow/src/View/DriverVersionNotMatchedAlertView.swift
@@ -15,6 +15,8 @@ struct DriverVersionNotMatchedAlertView: View {
             Text("The current virtual keyboard and mouse driver is outdated.\nPlease deactivate driver and restart macOS to upgrade the driver.")
                 .fixedSize(horizontal: false, vertical: true)
 
+            Text("How to upgrade the driver:")
+
             VStack(alignment: .leading, spacing: 10.0) {
                 Text("1. Press the following button to deactivate driver.\n(The administrator password will be required.)")
                     .fixedSize(horizontal: false, vertical: true)
