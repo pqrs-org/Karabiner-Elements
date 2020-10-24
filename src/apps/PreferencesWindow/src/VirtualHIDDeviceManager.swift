78176f6c606581cc0c10ca7dbd8a5a1fc158e36b
Takayama Fumihiko <tekezo@pqrs.org>
2020-10-24 22:22:53 +0900

    Update VirtualHIDDeviceManager (Add completion argument)

--- src/apps/PreferencesWindow/src/VirtualHIDDeviceManager.swift
+++ src/apps/PreferencesWindow/src/VirtualHIDDeviceManager.swift
@@ -3,18 +3,27 @@ import Foundation
 class VirtualHIDDeviceManager {
     static let shared = VirtualHIDDeviceManager()
 
-    func deactivateDriver() {
-        run("deactivate")
+    func deactivateDriver(completion: @escaping (Int32) -> Void) {
+        run(argument: "deactivate",
+            completion: completion)
     }
 
-    func activateDriver() {
-        run("activate")
+    func activateDriver(completion: @escaping (Int32) -> Void) {
+        run(argument: "activate",
+            completion: completion)
     }
 
-    private func run(_ argument: String) {
-        let task = Process()
-        task.executableURL = URL(fileURLWithPath: "/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager")
-        task.arguments = [argument]
-        try? task.run()
+    private func run(argument: String, completion: @escaping (Int32) -> Void) {
+        DispatchQueue.global().async {
+            let task = Process()
+            task.executableURL = URL(fileURLWithPath: "/Applications/.Karabiner-VirtualHIDDevice-Manager.app/Contents/MacOS/Karabiner-VirtualHIDDevice-Manager")
+            task.arguments = [argument]
+
+            try? task.run()
+
+            task.waitUntilExit()
+            let status = task.terminationStatus
+            completion(status)
+        }
     }
 }
