b49ccca0391dae752a47bc285a89426f0dea678d
Takayama Fumihiko <tekezo@pqrs.org>
2020-10-24 22:17:19 +0900

    Add alert to ActivateDriverButton, DeactivateDriverButton

--- src/apps/PreferencesWindow/src/View/ActivateDriverButton.swift
+++ src/apps/PreferencesWindow/src/View/ActivateDriverButton.swift
@@ -1,12 +1,35 @@
 import SwiftUI
 
 struct ActivateDriverButton: View {
+    @State private var showingAlert: Bool = false
+    @State private var status: Int32 = 0
+
     var body: some View {
-        Button(action: { VirtualHIDDeviceManager.shared.activateDriver() }) {
+        Button(action: {
+            VirtualHIDDeviceManager.shared.activateDriver(completion: { status in
+                DispatchQueue.main.async {
+                    self.status = status
+                    self.showingAlert = true
+                }
+            })
+        }) {
             Image(decorative: "ic_star_rate_18pt")
                 .resizable()
                 .frame(width: GUISize.buttonIconWidth, height: GUISize.buttonIconHeight)
             Text("Activate driver")
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text(
+                self.status == 0
+                    ? "Activation Result"
+                    : "Activation Error"
+            ),
+            message: Text(
+                self.status == 0
+                    ? "Driver was successfully activated."
+                    : "Activation was failed. (error: \(self.status))"
+            ),
+            dismissButton: .default(Text("Close")))
         }
     }
 }
--- src/apps/PreferencesWindow/src/View/DeactivateDriverButton.swift
+++ src/apps/PreferencesWindow/src/View/DeactivateDriverButton.swift
@@ -1,12 +1,35 @@
 import SwiftUI
 
 struct DeactivateDriverButton: View {
+    @State private var showingAlert: Bool = false
+    @State private var status: Int32 = 0
+
     var body: some View {
-        Button(action: { VirtualHIDDeviceManager.shared.deactivateDriver() }) {
+        Button(action: {
+            VirtualHIDDeviceManager.shared.deactivateDriver(completion: { status in
+                DispatchQueue.main.async {
+                    self.status = status
+                    self.showingAlert = true
+                }
+            })
+        }) {
             Image(decorative: "ic_star_rate_18pt")
                 .resizable()
                 .frame(width: GUISize.buttonIconWidth, height: GUISize.buttonIconHeight)
             Text("Deactivate driver")
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text(
+                self.status == 0
+                    ? "Deactivation Result"
+                    : "Deactivation Error"
+            ),
+            message: Text(
+                self.status == 0
+                    ? "Driver was successfully deactivated."
+                    : "Deactivation was failed. (error: \(self.status))"
+            ),
+            dismissButton: .default(Text("Close")))
         }
     }
 }
