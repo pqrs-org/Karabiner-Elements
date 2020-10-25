8004576332abfbd2da4e9c068658f30ade869ec6
Takayama Fumihiko <tekezo@pqrs.org>
2020-10-25 20:24:15 +0900

    Add showingProgress to ActivateDriverButton, DeactivateDriverButton

--- src/apps/PreferencesWindow/src/View/ActivateDriverButton.swift
+++ src/apps/PreferencesWindow/src/View/ActivateDriverButton.swift
@@ -1,15 +1,20 @@
 import SwiftUI
 
 struct ActivateDriverButton: View {
+    @State private var showingProgress: Bool = false
     @State private var showingResult: Bool = false
     @State private var status: Int32 = 0
 
     var body: some View {
         VStack(alignment: .leading) {
             Button(action: {
+                self.showingProgress = true
+                self.showingResult = false
+
                 VirtualHIDDeviceManager.shared.activateDriver(completion: { status in
                     DispatchQueue.main.async {
                         self.status = status
+                        self.showingProgress = false
                         self.showingResult = true
                     }
                 })
@@ -19,6 +24,12 @@ struct ActivateDriverButton: View {
                     .frame(width: GUISize.buttonIconWidth, height: GUISize.buttonIconHeight)
                 Text("Activate driver")
             }
+
+            if self.showingProgress {
+                Text("Activating ...")
+                    .padding(.bottom, 20)
+            }
+
             if self.showingResult {
                 VStack(alignment: .leading) {
                     if self.status == 0 {
--- src/apps/PreferencesWindow/src/View/DeactivateDriverButton.swift
+++ src/apps/PreferencesWindow/src/View/DeactivateDriverButton.swift
@@ -1,15 +1,20 @@
 import SwiftUI
 
 struct DeactivateDriverButton: View {
+    @State private var showingProgress: Bool = false
     @State private var showingResult: Bool = false
     @State private var status: Int32 = 0
 
     var body: some View {
         VStack(alignment: .leading) {
             Button(action: {
+                self.showingProgress = true
+                self.showingResult = false
+
                 VirtualHIDDeviceManager.shared.deactivateDriver(completion: { status in
                     DispatchQueue.main.async {
                         self.status = status
+                        self.showingProgress = false
                         self.showingResult = true
                     }
                 })
@@ -19,6 +24,12 @@ struct DeactivateDriverButton: View {
                     .frame(width: GUISize.buttonIconWidth, height: GUISize.buttonIconHeight)
                 Text("Deactivate driver")
             }
+
+            if self.showingProgress {
+                Text("Deactivating ...")
+                    .padding(.bottom, 20)
+            }
+
             if self.showingResult {
                 VStack(alignment: .leading) {
                     if self.status == 0 {
