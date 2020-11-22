ecdbc5d6ddb91ddee3a04aafd887539ec2aa88fb
Takayama Fumihiko <tekezo@pqrs.org>
2020-11-22 15:09:07 +0900

    c++17 -> c++2a

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_OSX_DEPLOYMENT_TARGET "10.15" CACHE STRING "Minimum OS X deployment version")
 
-set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD 20)
 
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../src/vendor)
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../src/vendor/cget/include)
