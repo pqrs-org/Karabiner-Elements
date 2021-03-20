3881fd51743013e7b71dfe42242e2b21acd18e42
Takayama Fumihiko <tekezo@pqrs.org>
2021-03-20 21:01:33 +0900

    Update GracefulShutdownCoordinator

--- src/apps/GracefulShutdownCoordinator/src/AppDelegate.swift
+++ src/apps/GracefulShutdownCoordinator/src/AppDelegate.swift
@@ -2,22 +2,26 @@ import AppKit
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-    var timer: Timer?
-    var count: Int = 0
-    
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        print("applicationDidFinishLaunching")
-    }
-
     public func applicationShouldTerminate(_: NSApplication) -> NSApplication.TerminateReply {
-        print("applicationShouldTerminate")
-        
-        for count in 1...10 {
-            print("i \(count)")
+        // macOS Big Sur 11.0 - 11.2 have an issue that causes kernel panic  if virtual HID devices exist at shutdown.
+        // So we have to detach these devices before shutdown.
+        //
+        // Shutting down console_user_server performs the device termination.
+
+        libkrbn_launchctl_bootout_console_user_server()
+
+        for _ in 1 ... 10 {
+            // Wait until the device is detached.
+
+            if !libkrbn_virtual_hid_keyboard_exists(),
+               !libkrbn_virtual_hid_pointing_exists()
+            {
+                break
+            }
+
             sleep(1)
         }
 
         return .terminateNow
     }
 }
-
