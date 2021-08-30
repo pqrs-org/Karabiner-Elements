cd54bf193f1fb87a8c4a7ad04b616602e223b7bb
Takayama Fumihiko <tekezo@pqrs.org>
2021-08-30 23:47:31 +0900

    Change notification window opacity when hovered

--- src/apps/NotificationWindow/src/NotificationView.swift
+++ src/apps/NotificationWindow/src/NotificationView.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 struct NotificationView: View {
     @ObservedObject var notificationMessage = NotificationMessage.shared
+    @State var hover = false
     let window: NSWindow!
 
     var body: some View {
@@ -16,7 +17,9 @@ struct NotificationView: View {
                     .multilineTextAlignment(.leading)
                     .frame(width: 340.0, height: 64.0, alignment: .leading)
                 Spacer()
-            }.padding(4.0)
+            }
+            .padding(4.0)
+            .opacity(hover ? 0 : 1.0)
 
             Button(
                 action: { window.orderOut(self) }
@@ -25,11 +28,17 @@ struct NotificationView: View {
                     .resizable()
                     .frame(width: 24.0, height: 24.0)
                     .foregroundColor(Color.gray)
-            }.buttonStyle(PlainButtonStyle())
-        }.background(
+            }
+            .buttonStyle(PlainButtonStyle())
+            // Do not set opacity to Button because the mouse click will be ignored.
+        }
+        .background(
             RoundedRectangle(cornerRadius: 12)
-                .fill(Color(NSColor.windowBackgroundColor))
+                .fill(Color(NSColor.windowBackgroundColor).opacity(hover ? 0.3 : 1.0))
         )
+        .whenHovered { hover in
+            self.hover = hover
+        }
     }
 }
 
