12b43c9c34ca9470c610744509dad2273abad28c
Takayama Fumihiko <tekezo@pqrs.org>
2021-09-20 22:35:42 +0900

    Show EventQueue.queue @ EventViewer

--- src/apps/EventViewer/src/View/MainView.swift
+++ src/apps/EventViewer/src/View/MainView.swift
@@ -1,9 +1,58 @@
 import SwiftUI
 
 struct MainView: View {
+    @ObservedObject var eventQueue = EventQueue.shared
+
     var body: some View {
-        Text("MainView")
-            .padding()
+        VStack(alignment: .leading, spacing: 25.0) {
+            GroupBox(label: Text("Keyboard & pointing events")) {
+                VStack(alignment: .leading, spacing: 10.0) {
+                    ScrollViewReader { proxy in
+                        // swiftformat:disable:next unusedArguments
+                        List($eventQueue.queue) { $entry in
+                            VStack {
+                                HStack(alignment: .center, spacing: 0) {
+                                    Text(entry.eventType)
+                                        .font(.title)
+                                        .frame(width: 70, alignment: .leading)
+
+                                    VStack(spacing: 0) {
+                                        Text(entry.name)
+                                            .truncationMode(.tail)
+                                            .lineLimit(1)
+                                        Text(entry.misc)
+                                    }
+
+                                    Spacer()
+
+                                    VStack(alignment: .trailing, spacing: 0) {
+                                        HStack(alignment: .bottom, spacing: 0) {
+                                            Text("usage page: ").font(.caption)
+                                            Text(entry.usagePage)
+                                        }
+                                        HStack(alignment: .bottom, spacing: 0) {
+                                            Text("usage: ").font(.caption)
+                                            Text(entry.usage)
+                                        }
+                                    }
+                                    .frame(alignment: .leading)
+                                }
+                                .padding(.horizontal, 8)
+
+                                Divider()
+                            }
+                        }
+                        .onChange(of: eventQueue.queue) { newQueue in
+                            if let last = newQueue.last {
+                                proxy.scrollTo(last.id)
+                            }
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
+        .padding()
     }
 }
 
