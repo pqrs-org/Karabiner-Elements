bc2c71f1ee74f1a114bd95171bec51d2c80ba6b6
Takayama Fumihiko <tekezo@pqrs.org>
2021-09-21 08:48:21 +0900

    Add Text input test field to MainView

--- src/apps/EventViewer/src/View/MainView.swift
+++ src/apps/EventViewer/src/View/MainView.swift
@@ -2,11 +2,18 @@ import SwiftUI
 
 struct MainView: View {
     @ObservedObject var eventQueue = EventQueue.shared
+    @State private var textInput: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 25.0) {
+        VStack(alignment: .leading, spacing: 12.0) {
             GroupBox(label: Text("Keyboard & pointing events")) {
-                VStack(alignment: .leading, spacing: 10.0) {
+                VStack(alignment: .leading, spacing: 6.0) {
+                    Button(action: {
+                        EventQueue.shared.clear()
+                    }) {
+                        Label("Clear", systemImage: "clear")
+                    }
+
                     ScrollViewReader { proxy in
                         // swiftformat:disable:next unusedArguments
                         List($eventQueue.queue) { $entry in
@@ -16,23 +23,28 @@ struct MainView: View {
                                         .font(.title)
                                         .frame(width: 70, alignment: .leading)
 
-                                    VStack(spacing: 0) {
+                                    VStack(alignment: .leading, spacing: 0) {
                                         Text(entry.name)
-                                            .truncationMode(.tail)
-                                            .lineLimit(1)
                                         Text(entry.misc)
+                                            .font(.caption)
                                     }
 
                                     Spacer()
 
                                     VStack(alignment: .trailing, spacing: 0) {
-                                        HStack(alignment: .bottom, spacing: 0) {
-                                            Text("usage page: ").font(.caption)
-                                            Text(entry.usagePage)
+                                        if entry.usagePage.count > 0 {
+                                            HStack(alignment: .bottom, spacing: 0) {
+                                                Text("usage page: ")
+                                                    .font(.caption)
+                                                Text(entry.usagePage)
+                                            }
                                         }
-                                        HStack(alignment: .bottom, spacing: 0) {
-                                            Text("usage: ").font(.caption)
-                                            Text(entry.usage)
+                                        if entry.usage.count > 0 {
+                                            HStack(alignment: .bottom, spacing: 0) {
+                                                Text("usage: ")
+                                                    .font(.caption)
+                                                Text(entry.usage)
+                                            }
                                         }
                                     }
                                     .frame(alignment: .leading)
@@ -44,12 +56,23 @@ struct MainView: View {
                         }
                         .onChange(of: eventQueue.queue) { newQueue in
                             if let last = newQueue.last {
-                                proxy.scrollTo(last.id)
+                                proxy.scrollTo(last.id, anchor: .bottom)
                             }
                         }
                     }
                 }
-                .padding()
+                .padding(6.0)
+            }
+
+            GroupBox(label: Text("Text input test field")) {
+                VStack {
+                    TextEditor(
+                        text: $textInput
+                    )
+                    .frame(height: 60)
+                    .disableAutocorrection(true)
+                }
+                .padding(6.0)
             }
         }
         .padding()
