d0917eb2eb847083a9df94e4805f458d2234a0d3
Takayama Fumihiko <tekezo@pqrs.org>
2021-09-22 08:46:37 +0900

    Implement FrontmostApplicationView

--- src/apps/EventViewer/src/View/FrontmostApplicationView.swift
+++ src/apps/EventViewer/src/View/FrontmostApplicationView.swift
@@ -1,9 +1,38 @@
 import SwiftUI
 
 struct FrontmostApplicationView: View {
+    @ObservedObject var frontmostApplicationHistory = FrontmostApplicationHistory.shared
+
     var body: some View {
-        Text("FrontmostApplicationView")
-            .padding()
+        VStack(alignment: .leading, spacing: 12.0) {
+            GroupBox(label: Text("Frontmost application history")) {
+                VStack(alignment: .leading, spacing: 12.0) {
+                    Text("You can investigate a bundle identifier and a file path of the frontmost application.")
+
+                    ScrollViewReader { proxy in
+                        ScrollView {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(frontmostApplicationHistory.text)
+                                        .lineLimit(nil)
+                                        .font(.custom("Menlo", size: 11.0))
+
+                                    Text("").id("frontmostApplicationHistoryButtom")
+                                }
+
+                                Spacer()
+                            }
+                            .background(Color(NSColor.textBackgroundColor))
+                        }
+                        .onReceive(frontmostApplicationHistory.$text) { _ in
+                            proxy.scrollTo("frontmostApplicationHistoryButtom", anchor: .bottom)
+                        }
+                    }
+                }
+                .padding(6.0)
+            }
+        }
+        .padding()
     }
 }
 
