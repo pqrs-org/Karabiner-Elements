573a4ed8d91cbd7214a73ebce0c82a290534b121
Takayama Fumihiko <tekezo@pqrs.org>
2021-10-31 00:09:21 +0900

    Use snake case

--- scripts/update-version.py
+++ scripts/update-version.py
@@ -6,43 +6,44 @@ import sys
 from pathlib import Path
 from itertools import chain
 
-topDirectory = Path(__file__).resolve(True).parents[1]
+top_directory = Path(__file__).resolve(True).parents[1]
 
-with topDirectory.joinpath('version').open() as versionFile:
-    version = versionFile.readline().strip()
+with top_directory.joinpath('version').open() as version_file:
+    version = version_file.readline().strip()
 
-    for templateFilePath in chain(topDirectory.rglob('*.h.in'),
-                                  topDirectory.rglob('*.hpp.in'),
-                                  topDirectory.rglob('*.plist.in'),
-                                  topDirectory.rglob('*.xml.in')):
+    for template_file_path in chain(top_directory.rglob('*.h.in'),
+                                    top_directory.rglob('*.hpp.in'),
+                                    top_directory.rglob('*.plist.in'),
+                                    top_directory.rglob('*.xml.in')):
         # Skip vendor directory
-        if re.search(r'/vendor/', str(templateFilePath)):
+        if re.search(r'/vendor/', str(template_file_path)):
             continue
 
-        replacedFilePath = Path(re.sub(r'\.in$', '', str(templateFilePath)))
-        needsUpdate = False
+        replaced_file_path = Path(
+            re.sub(r'\.in$', '', str(template_file_path)))
+        needs_update = False
 
-        with templateFilePath.open('r') as templateFile:
-            templateLines = templateFile.readlines()
-            replacedLines = []
+        with template_file_path.open('r') as template_file:
+            template_lines = template_file.readlines()
+            replaced_lines = []
 
-            if replacedFilePath.exists():
-                with replacedFilePath.open('r') as replacedFile:
-                    replacedLines = replacedFile.readlines()
-                    while len(replacedLines) < len(templateLines):
-                        replacedLines.append('')
+            if replaced_file_path.exists():
+                with replaced_file_path.open('r') as replaced_file:
+                    replaced_lines = replaced_file.readlines()
+                    while len(replaced_lines) < len(template_lines):
+                        replaced_lines.append('')
             else:
-                replacedLines = templateLines
+                replaced_lines = template_lines
 
-            for index, templateLine in enumerate(templateLines):
-                line = templateLine
+            for index, template_line in enumerate(template_lines):
+                line = template_line
                 line = line.replace('@VERSION@', version)
 
-                if (replacedLines[index] != line):
-                    needsUpdate = True
-                    replacedLines[index] = line
+                if (replaced_lines[index] != line):
+                    needs_update = True
+                    replaced_lines[index] = line
 
-        if needsUpdate:
-            with replacedFilePath.open('w') as replacedFile:
-                print("Update " + str(replacedFilePath))
-                replacedFile.write(''.join(replacedLines))
+        if needs_update:
+            with replaced_file_path.open('w') as replaced_file:
+                print("Update " + str(replaced_file_path))
+                replaced_file.write(''.join(replaced_lines))
