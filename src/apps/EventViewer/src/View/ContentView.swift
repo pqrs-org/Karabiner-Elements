099d3a51fdfe2d2922ef00e1566c4d57581526af
Takayama Fumihiko <tekezo@pqrs.org>
2021-10-31 23:56:44 +0900

    Show InputMonitoringAlertView directly instead of using sheet in order to avoid sheet prevents app termination even if preventsApplicationTerminationWhenModal is false

--- src/apps/EventViewer/src/View/ContentView.swift
+++ src/apps/EventViewer/src/View/ContentView.swift
@@ -8,78 +8,64 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            NavigationView {
-                VStack(alignment: .leading, spacing: 0) {
-                    List {
-                        NavigationLink(destination: MainView(),
-                                       tag: "Main",
-                                       selection: $selection) {
-                            Label("Main", systemImage: "magnifyingglass")
-                        }
-                        .padding(10)
-
-                        NavigationLink(destination: FrontmostApplicationView(),
-                                       tag: "FrontmostApplication",
-                                       selection: $selection) {
-                            Label("Frontmost Application", systemImage: "triangle.circle")
-                        }
-                        .padding(10)
-
-                        NavigationLink(destination: VariablesView(),
-                                       tag: "Variables",
-                                       selection: $selection) {
-                            Label("Variables", systemImage: "cube")
-                        }
-                        .padding(10)
-
-                        NavigationLink(destination: DevicesView(),
-                                       tag: "Devices",
-                                       selection: $selection) {
-                            Label("Devices", systemImage: "keyboard")
-                        }
-                        .padding(10)
-
-                        NavigationLink(destination: UnknownEventsView(),
-                                       tag: "UnknownEvents",
-                                       selection: $selection) {
-                            Label("Unknown Events", systemImage: "questionmark.square.dashed")
-                        }
-                        .padding(10)
-
-                        NavigationLink(destination: PreferencesView(),
-                                       tag: "Preferences",
-                                       selection: $selection) {
-                            Label("Preferences", systemImage: "gearshape")
-                        }
-                        .padding(10)
-                    }
-                    .listStyle(SidebarListStyle())
-                    .frame(width: 250)
-
-                    Spacer()
-                }
-            }
-        }
-        .frame(minWidth: 1100,
-               maxWidth: .infinity,
-               minHeight: 650,
-               maxHeight: .infinity)
-        .sheet(isPresented: $inputMonitoringAlertData.showing) {
-            ZStack(alignment: .topLeading) {
+            if inputMonitoringAlertData.showing {
                 InputMonitoringAlertView()
+            } else {
+                NavigationView {
+                    VStack(alignment: .leading, spacing: 0) {
+                        List {
+                            NavigationLink(destination: MainView(),
+                                           tag: "Main",
+                                           selection: $selection) {
+                                Label("Main", systemImage: "magnifyingglass")
+                            }
+                            .padding(10)
 
-                Button(
-                    action: {
-                        inputMonitoringAlertData.showing = false
+                            NavigationLink(destination: FrontmostApplicationView(),
+                                           tag: "FrontmostApplication",
+                                           selection: $selection) {
+                                Label("Frontmost Application", systemImage: "triangle.circle")
+                            }
+                            .padding(10)
+
+                            NavigationLink(destination: VariablesView(),
+                                           tag: "Variables",
+                                           selection: $selection) {
+                                Label("Variables", systemImage: "cube")
+                            }
+                            .padding(10)
+
+                            NavigationLink(destination: DevicesView(),
+                                           tag: "Devices",
+                                           selection: $selection) {
+                                Label("Devices", systemImage: "keyboard")
+                            }
+                            .padding(10)
+
+                            NavigationLink(destination: UnknownEventsView(),
+                                           tag: "UnknownEvents",
+                                           selection: $selection) {
+                                Label("Unknown Events", systemImage: "questionmark.square.dashed")
+                            }
+                            .padding(10)
+
+                            NavigationLink(destination: PreferencesView(),
+                                           tag: "Preferences",
+                                           selection: $selection) {
+                                Label("Preferences", systemImage: "gearshape")
+                            }
+                            .padding(10)
+                        }
+                        .listStyle(SidebarListStyle())
+                        .frame(width: 250)
+
+                        Spacer()
                     }
-                ) {
-                    Image(systemName: "xmark.circle")
-                        .resizable()
-                        .frame(width: 24.0, height: 24.0)
-                        .foregroundColor(Color(NSColor.textColor))
                 }
-                .buttonStyle(PlainButtonStyle())
-                .offset(x: 10, y: 10)
+                .frame(minWidth: 1100,
+                       maxWidth: .infinity,
+                       minHeight: 650,
+                       maxHeight: .infinity)
             }
         }
     }
