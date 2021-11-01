655ae62fc22f0badf8fdb35758e8d6de8075cdb8
Takayama Fumihiko <tekezo@pqrs.org>
2021-11-02 08:23:38 +0900

    Add showingAdvanced @ DriverNotLoadedAlertView

--- src/apps/PreferencesWindow/src/View/DriverNotLoadedAlertView.swift
+++ src/apps/PreferencesWindow/src/View/DriverNotLoadedAlertView.swift
@@ -1,18 +1,20 @@
 import SwiftUI
 
 struct DriverNotLoadedAlertView: View {
+    @State private var showingAdvanced = false
+
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .center) {
             Label("Please allow Karabiner-VirtualHIDDevice-Manager system software",
                   systemImage: "lightbulb")
                 .font(.system(size: 24))
 
             HStack(alignment: .top, spacing: 40.0) {
                 GroupBox {
-                    VStack(alignment: .leading, spacing: 20.0) {
-                        VStack(alignment: .leading, spacing: 0) {
+                    VStack(alignment: .center, spacing: 20.0) {
+                        VStack(alignment: .center, spacing: 0) {
                             Text("The virtual keyboard and mouse driver is not loaded.")
-                            Text("Please allow system software from \".Karabiner-VirtualHIDDevice-Manager\" on Security & Privacy System Preferences.")
+                            Text("Please allow \".Karabiner-VirtualHIDDevice-Manager\" on Security & Privacy System Preferences.")
                         }
 
                         Button(action: { openSystemPreferencesSecurity() }) {
@@ -25,47 +27,55 @@ struct DriverNotLoadedAlertView: View {
                             .aspectRatio(contentMode: .fit)
                             .frame(height: 300)
                             .border(Color.gray, width: 1)
-                    }.padding()
-                }.frame(width: 400)
 
-                GroupBox(label: Text("Advanced")) {
-                    VStack(alignment: .leading, spacing: 20.0) {
-                        VStack(alignment: .leading, spacing: 0) {
-                            Text("If macOS failed to load the driver in the early stage, the allow button might be not shown on Security & Privacy System Preferences.")
-                            Text("In this case, you need to reinstall the driver in order for the button to appear.")
-                        }
-
-                        Text("How to reinstall driver:")
-
-                        VStack(alignment: .leading, spacing: 10.0) {
-                            Text("1. Press the following button to deactivate driver.\n(The administrator password will be required.)")
-                                .fixedSize(horizontal: false, vertical: true)
-
-                            DeactivateDriverButton()
-                                .padding(.vertical, 10)
-                                .padding(.leading, 20)
-
-                            Text("2. Restart macOS.")
-                                .fontWeight(.bold)
-                                .fixedSize(horizontal: false, vertical: true)
-
-                            Text("3. Press the following button to activate driver.")
-                                .fixedSize(horizontal: false, vertical: true)
-
-                            ActivateDriverButton()
-                                .padding(.vertical, 10)
-                                .padding(.leading, 20)
-
-                            Text("4. \"System Extension Blocked\" alert is shown.")
-                                .fixedSize(horizontal: false, vertical: true)
-
-                            Text("5. Open Security Preferences and press the allow button.")
-                                .fixedSize(horizontal: false, vertical: true)
+                        Button(action: { showingAdvanced = true }) {
+                            Label("If the Allow button is not displayed on Security & Privacy.",
+                                  systemImage: "questionmark.circle")
                         }
                     }.padding()
-                }.frame(width: 400)
+                }.frame(width: showingAdvanced ? 400 : 800)
+
+                if showingAdvanced {
+                    GroupBox(label: Text("Advanced")) {
+                        VStack(alignment: .leading, spacing: 20.0) {
+                            VStack(alignment: .leading, spacing: 0) {
+                                Text("If macOS failed to load the driver in the early stage, the allow button might be not shown on Security & Privacy System Preferences.")
+                                Text("In this case, you need to reinstall the driver in order for the button to appear.")
+                            }
+
+                            Text("How to reinstall driver:")
+
+                            VStack(alignment: .leading, spacing: 10.0) {
+                                Text("1. Press the following button to deactivate driver.\n(The administrator password will be required.)")
+                                    .fixedSize(horizontal: false, vertical: true)
+
+                                DeactivateDriverButton()
+                                    .padding(.vertical, 10)
+                                    .padding(.leading, 20)
+
+                                Text("2. Restart macOS.")
+                                    .fontWeight(.bold)
+                                    .fixedSize(horizontal: false, vertical: true)
+
+                                Text("3. Press the following button to activate driver.")
+                                    .fixedSize(horizontal: false, vertical: true)
+
+                                ActivateDriverButton()
+                                    .padding(.vertical, 10)
+                                    .padding(.leading, 20)
+
+                                Text("4. \"System Extension Blocked\" alert is shown.")
+                                    .fixedSize(horizontal: false, vertical: true)
+
+                                Text("5. Open Security Preferences and press the allow button.")
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
+                        }.padding()
+                    }.frame(width: 400)
+                }
             }
         }
+        .frame(width: 850)
         .padding()
     }
 
