826020ba31d7a2c506cef69386e5caa38cfcc668
Takayama Fumihiko <tekezo@pqrs.org>
2021-12-26 22:07:26 +0900

    Add UpdateView

--- src/apps/PreferencesWindow2/src/View/ContentView.swift
+++ src/apps/PreferencesWindow2/src/View/ContentView.swift
@@ -12,75 +12,92 @@ struct ContentView: View {
             NavigationView {
                 VStack(alignment: .leading, spacing: 0) {
                     List {
-                        NavigationLink(destination: SimpleModificationsView(),
-                                       tag: "SimpleModifications",
-                                       selection: $selection) {
-                            Label("Simple Modifications", systemImage: "gearshape")
-                        }
-                        .padding(10)
+                        Group {
+                            NavigationLink(destination: SimpleModificationsView(),
+                                           tag: "SimpleModifications",
+                                           selection: $selection) {
+                                Label("Simple Modifications", systemImage: "gearshape")
+                            }
+                            .padding(10)
 
-                        NavigationLink(destination: FunctionKeysView(),
-                                       tag: "FunctionKeys",
-                                       selection: $selection) {
-                            Label("Function Keys", systemImage: "speaker.wave.2.circle")
-                        }
-                        .padding(10)
+                            NavigationLink(destination: FunctionKeysView(),
+                                           tag: "FunctionKeys",
+                                           selection: $selection) {
+                                Label("Function Keys", systemImage: "speaker.wave.2.circle")
+                            }
+                            .padding(10)
 
-                        NavigationLink(destination: ComplexModificationsView(),
-                                       tag: "ComplexModifications",
-                                       selection: $selection) {
-                            Label("Complex Modifications", systemImage: "gearshape.2")
+                            NavigationLink(destination: ComplexModificationsView(),
+                                           tag: "ComplexModifications",
+                                           selection: $selection) {
+                                Label("Complex Modifications", systemImage: "gearshape.2")
+                            }
+                            .padding(10)
                         }
-                        .padding(10)
 
-                        NavigationLink(destination: DevicesView(),
-                                       tag: "Devices",
-                                       selection: $selection) {
-                            Label("Devices", systemImage: "keyboard")
-                        }
-                        .padding(10)
+                        Divider()
 
-                        NavigationLink(destination: VirtualKeyboardView(),
-                                       tag: "VirtualKeyboard",
-                                       selection: $selection) {
-                            Label("Virtual Keyboard", systemImage: "puzzlepiece")
-                        }
-                        .padding(10)
+                        Group {
+                            NavigationLink(destination: DevicesView(),
+                                           tag: "Devices",
+                                           selection: $selection) {
+                                Label("Devices", systemImage: "keyboard")
+                            }
+                            .padding(10)
 
-                        NavigationLink(destination: ProfilesView(),
-                                       tag: "Profiles",
-                                       selection: $selection) {
-                            Label("Profiles", systemImage: "person.3")
-                        }
-                        .padding(10)
+                            NavigationLink(destination: VirtualKeyboardView(),
+                                           tag: "VirtualKeyboard",
+                                           selection: $selection) {
+                                Label("Virtual Keyboard", systemImage: "puzzlepiece")
+                            }
+                            .padding(10)
 
-                        NavigationLink(destination: MiscView(),
-                                       tag: "Misc",
-                                       selection: $selection) {
-                            Label("Misc", systemImage: "leaf")
+                            NavigationLink(destination: ProfilesView(),
+                                           tag: "Profiles",
+                                           selection: $selection) {
+                                Label("Profiles", systemImage: "person.3")
+                            }
+                            .padding(10)
                         }
-                        .padding(10)
 
-                        NavigationLink(destination: AdvancedView(),
-                                       tag: "Advanced",
-                                       selection: $selection) {
-                            Label("Advanced", systemImage: "leaf.fill")
-                        }
-                        .padding(10)
+                        Divider()
 
-                        NavigationLink(destination: LogView(),
-                                       tag: "Log",
-                                       selection: $selection) {
-                            Label("Log", systemImage: "doc.plaintext")
-                        }
-                        .padding(10)
+                        Group {
+                            NavigationLink(destination: UpdateView(),
+                                           tag: "Update",
+                                           selection: $selection) {
+                                Label("Update", systemImage: "network")
+                            }
+                            .padding(10)
 
-                        NavigationLink(destination: UninstallView(),
-                                       tag: "Uninstall",
-                                       selection: $selection) {
-                            Label("Uninstall", systemImage: "trash")
+                            NavigationLink(destination: MiscView(),
+                                           tag: "Misc",
+                                           selection: $selection) {
+                                Label("Misc", systemImage: "leaf")
+                            }
+                            .padding(10)
+
+                            NavigationLink(destination: AdvancedView(),
+                                           tag: "Advanced",
+                                           selection: $selection) {
+                                Label("Advanced", systemImage: "leaf.fill")
+                            }
+                            .padding(10)
+
+                            NavigationLink(destination: LogView(),
+                                           tag: "Log",
+                                           selection: $selection) {
+                                Label("Log", systemImage: "doc.plaintext")
+                            }
+                            .padding(10)
+
+                            NavigationLink(destination: UninstallView(),
+                                           tag: "Uninstall",
+                                           selection: $selection) {
+                                Label("Uninstall", systemImage: "trash")
+                            }
+                            .padding(10)
                         }
-                        .padding(10)
                     }
                     .listStyle(SidebarListStyle())
                     .frame(width: 250)
