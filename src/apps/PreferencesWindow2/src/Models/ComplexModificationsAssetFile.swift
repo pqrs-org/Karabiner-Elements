166885a196bcd546f73a6402d763d27a26c07ae1
Takayama Fumihiko <tekezo@pqrs.org>
2022-01-26 00:05:06 +0900

    Add ComplexModificationsAssetFiles

---
+++ src/apps/PreferencesWindow2/src/Models/ComplexModificationsAssetFile.swift
@@ -0,0 +1,19 @@
+class ComplexModificationsAssetFile: Identifiable {
+  var id = UUID()
+  var index: Int
+  var title: String
+  var userFile: Bool
+  var assetRules: [ComplexModificationsAssetRule]
+
+  init(
+    _ index: Int,
+    _ title: String,
+    _ userFile: Bool,
+    _ assetRules: [ComplexModificationsAssetRule]
+  ) {
+    self.index = index
+    self.title = title
+    self.userFile = userFile
+    self.assetRules = assetRules
+  }
+}
