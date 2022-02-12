c93352d565106cc4d1d1bc00aaa511b895fed9cc
Takayama Fumihiko <tekezo@pqrs.org>
2022-02-13 00:15:56 +0900

    Add sidebarButtonLabelStyle

--- src/apps/share/swift/View+Extensions.swift
+++ src/apps/share/swift/View+Extensions.swift
@@ -1,10 +1,16 @@
 import SwiftUI
 
-extension Label {
+extension View {
   func buttonLabelStyle() -> some View {
     self
       .padding(.horizontal, 8)
       .padding(.vertical, 2)
+      .contentShape(Rectangle())
+  }
+
+  func sidebarButtonLabelStyle() -> some View {
+    self
+      .padding(10.0)
       .contentShape(Rectangle())
   }
 }
