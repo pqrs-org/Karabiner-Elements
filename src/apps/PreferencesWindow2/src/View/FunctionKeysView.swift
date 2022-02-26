21c948827b4a7aa32442a88547ec23d8fce6f168
Takayama Fumihiko <tekezo@pqrs.org>
2022-02-26 23:13:49 +0900

    Update FunctionKeysView.swift

--- src/apps/PreferencesWindow2/src/View/FunctionKeysView.swift
+++ src/apps/PreferencesWindow2/src/View/FunctionKeysView.swift
@@ -1,11 +1,91 @@
 import SwiftUI
 
 struct FunctionKeysView: View {
+  @ObservedObject private var settings = Settings.shared
+  @State private var selectedDevice: ConnectedDevice? = nil
+
   var body: some View {
     VStack(alignment: .leading, spacing: 12.0) {
-      Text("Function Keys")
+      HStack(alignment: .top, spacing: 12.0) {
+        List {
+          VStack(alignment: .leading, spacing: 0) {
+            Button(action: {
+              selectedDevice = nil
+            }) {
+              HStack {
+                Text("For all devices")
+
+                Spacer()
+              }
+              .sidebarButtonLabelStyle()
+            }
+            .sidebarButtonStyle(selected: selectedDevice == nil)
+
+            Divider()
+
+            ForEach($settings.connectedDeviceSettings) { $connectedDeviceSetting in
+              Button(action: {
+                selectedDevice = connectedDeviceSetting.connectedDevice
+              }) {
+                HStack {
+                  Text(
+                    "\(connectedDeviceSetting.connectedDevice.productName) (\(connectedDeviceSetting.connectedDevice.manufacturerName)) [\(String(connectedDeviceSetting.connectedDevice.vendorId)),\(String(connectedDeviceSetting.connectedDevice.productId))]"
+                  )
+
+                  Spacer()
+
+                  VStack {
+                    if connectedDeviceSetting.connectedDevice.isKeyboard {
+                      Image(systemName: "keyboard")
+                    }
+                    if connectedDeviceSetting.connectedDevice.isPointingDevice {
+                      Image(systemName: "capsule.portrait")
+                    }
+                  }
+                }
+                .sidebarButtonLabelStyle()
+              }
+              .sidebarButtonStyle(
+                selected: selectedDevice?.id == connectedDeviceSetting.connectedDevice.id)
+
+              Divider()
+            }
+
+            Spacer()
+          }
+          .background(Color(NSColor.textBackgroundColor))
+        }
+        .frame(width: 250)
+
+        VStack {
+          FnFunctionKeysView(
+            fnFunctionKeys: selectedDevice == nil
+              ? settings.fnFunctionKeys
+              : settings.findConnectedDeviceSetting(selectedDevice!)?.fnFunctionKeys ?? []
+          )
+
+          Spacer()
+        }
+      }
     }
     .padding()
+  }
+
+  struct FnFunctionKeysView: View {
+    @State var fnFunctionKeys: [SimpleModification]
+
+    var body: some View {
+      List {
+        VStack(alignment: .leading, spacing: 0) {
+          ForEach($fnFunctionKeys) { $fnFunctionKey in
+            HStack {
+              Text(fnFunctionKey.fromJsonString)
+              Text(fnFunctionKey.toJsonString)
+            }
+          }
+        }
+      }
+    }
   }
 }
 
