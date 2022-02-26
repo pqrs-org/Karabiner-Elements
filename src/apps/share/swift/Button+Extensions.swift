6dd1542b465abfd4144628d0c309f51a0c1017fd
Takayama Fumihiko <tekezo@pqrs.org>
2022-02-26 23:11:51 +0900

    Add sidebarButtonStyle

--- src/apps/share/swift/Button+Extensions.swift
+++ src/apps/share/swift/Button+Extensions.swift
@@ -28,4 +28,19 @@ extension Button {
       .cornerRadius(5)
       .shadow(radius: 1)
   }
+
+  func sidebarButtonStyle(selected: Bool) -> some View {
+    // Do not put padding here.
+    // The padding area ignores click.
+    // Use `buttonLabelStyle` in order to set padding.
+
+    self
+      .buttonStyle(PlainButtonStyle())
+      .if(selected) {
+        $0
+          .background(Color.blue)
+          .foregroundColor(.white)
+      }
+      .cornerRadius(5)
+  }
 }
