252dc3c5b1be6b686e2b37a9419b14f70d6bc337
Takayama Fumihiko <tekezo@pqrs.org>
2022-04-23 22:09:26 +0900

    Check simple_modifications.json from data array size

--- tests/src/simple_modifications_json/src/simple_modifications_json_test.cpp
+++ tests/src/simple_modifications_json/src/simple_modifications_json_test.cpp
@@ -8,8 +8,24 @@ TEST_CASE("simple_modifications.json") {
   auto json = krbn::unit_testing::json_helper::load_jsonc("../../../src/apps/PreferencesWindow/Resources/simple_modifications.json");
   for (const auto& entry : json) {
     if (auto data = pqrs::json::find_array(entry, "data")) {
+      //
+      // Check `data` is valid
+      //
+
       for (const auto& d : **data) {
         krbn::manipulator::to_event_definition e(d);
+      }
+
+      //
+      // Check `from` data array size == 1
+      //
+
+      auto not_from = false;
+      if (auto nf = pqrs::json::find<bool>(entry, "not_from")) {
+        not_from = *nf;
+      }
+      if (!not_from) {
+        REQUIRE((**data).size() == 1);
       }
     }
   }
