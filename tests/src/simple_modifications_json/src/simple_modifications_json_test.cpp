c3c33c75b2e6dbe205792d0abdb739e495b5fb0f
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-01 13:51:07 +0900

    Check label uniqueness

--- tests/src/simple_modifications_json/src/simple_modifications_json_test.cpp
+++ tests/src/simple_modifications_json/src/simple_modifications_json_test.cpp
@@ -6,6 +6,8 @@
 
 TEST_CASE("simple_modifications.json") {
   auto json = krbn::unit_testing::json_helper::load_jsonc("../../../src/apps/PreferencesWindow/Resources/simple_modifications.json");
+  auto label_set = std::unordered_set<std::string>();
+
   for (const auto& entry : json) {
     if (auto data = pqrs::json::find_array(entry, "data")) {
       //
@@ -27,6 +29,24 @@ TEST_CASE("simple_modifications.json") {
       if (!not_from) {
         REQUIRE((**data).size() == 1);
       }
+
+      //
+      // Check `label` uniqueness
+      //
+
+      if (auto label = pqrs::json::find<std::string>(entry, "label")) {
+        std::cout << *label << std::endl;
+        if (label_set.contains(*label) == true) {
+          std::cout << std::endl
+                    << *label << " is duplicated." << std::endl
+                    << std::endl;
+          REQUIRE(false);
+        }
+
+        label_set.insert(*label);
+      }
     }
   }
+
+  REQUIRE(label_set.size() > 0);
 }
