a09664ec1e4863b7a09470649ed8732d95cc4a0f
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-02 20:20:13 +0900

    Remove unused code

--- src/apps/lib/KarabinerKit/include/KarabinerKit/KarabinerKit.h
+++ src/apps/lib/KarabinerKit/include/KarabinerKit/KarabinerKit.h
@@ -1,13 +1,8 @@
 // -*- mode: objective-c -*-
 
 @import Foundation;
-#import "KarabinerKit/ComplexModificationsAssetsFileModel.h"
-#import "KarabinerKit/ComplexModificationsAssetsManager.h"
-#import "KarabinerKit/ComplexModificationsAssetsRuleModel.h"
 #import "KarabinerKit/ConfigurationManager.h"
-#import "KarabinerKit/ConnectedDevices.h"
 #import "KarabinerKit/CoreConfigurationModel.h"
-#import "KarabinerKit/DeviceManager.h"
 #import "KarabinerKit/JsonUtility.h"
 #import "KarabinerKit/NotificationKeys.h"
 #import "KarabinerKit/SmartObserverContainer.h"
