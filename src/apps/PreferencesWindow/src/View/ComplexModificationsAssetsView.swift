80abae85551dd34ce5138d8fe6816445ea2b97c0
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-08 00:46:58 +0900

    Update ComplexModificationsAssetsView layout

--- src/apps/PreferencesWindow/src/View/ComplexModificationsAssetsView.swift
+++ src/apps/PreferencesWindow/src/View/ComplexModificationsAssetsView.swift
@@ -8,65 +8,68 @@ struct ComplexModificationsAssetsView: View {
     VStack(alignment: .center, spacing: 12.0) {
       List {
         ForEach($assetFiles.files) { $assetFile in
-          VStack(alignment: .leading, spacing: 4.0) {
-            GroupBox(
-              label:
-                HStack(alignment: .bottom, spacing: 16.0) {
-                  Text(assetFile.title)
-                    .font(.title)
+          GroupBox(
+            label:
+              HStack(alignment: .bottom, spacing: 16.0) {
+                Text(assetFile.title)
+                  .font(.title)
 
-                  Spacer()
+                Spacer()
+
+                Button(action: {
+                  LibKrbn.Settings.shared.addComplexModificationRules(assetFile)
+                  contentViewStates.complexModificationsViewSheetPresented = false
+                }) {
+                  Label("Enable All", systemImage: "plus.circle.fill")
+                    .font(.caption)
+                }
+
+                if assetFile.userFile {
+                  Button(action: {
+                    assetFiles.removeFile(assetFile)
+                  }) {
+                    Image(systemName: "trash.fill")
+                      .buttonLabelStyle()
+                  }
+                  .deleteButtonStyle()
+                }
+              }
+          ) {
+            VStack(alignment: .leading, spacing: 8.0) {
+              ForEach($assetFile.assetRules) { $assetRule in
+                if assetRule.ruleIndex > 0 {
+                  Divider()
+                    .padding(.vertical, 4.0)
+                }
+
+                HStack(alignment: .center, spacing: 16.0) {
+                  Text(assetRule.description)
 
                   Button(action: {
-                    LibKrbn.Settings.shared.addComplexModificationRules(assetFile)
+                    LibKrbn.Settings.shared.addComplexModificationRule(assetRule)
                     contentViewStates.complexModificationsViewSheetPresented = false
                   }) {
-                    Label("Enable All", systemImage: "plus.circle.fill")
-                      .font(.caption)
+                    // Use `Image` and `Text` instead of `Label` to set icon color like `Button` in `List`.
+                    Image(systemName: "plus.circle.fill").foregroundColor(.blue)
+                    Text("Enable")
                   }
 
-                  if assetFile.userFile {
-                    Button(action: {
-                      assetFiles.removeFile(assetFile)
-                    }) {
-                      Image(systemName: "trash.fill")
-                        .buttonLabelStyle()
-                    }
-                    .deleteButtonStyle()
-                  }
-                }
-            ) {
-              VStack(alignment: .leading, spacing: 8.0) {
-                ForEach($assetFile.assetRules) { $assetRule in
-                  HStack(alignment: .center, spacing: 16.0) {
-                    Text(assetRule.description)
-
-                    Button(action: {
-                      LibKrbn.Settings.shared.addComplexModificationRule(assetRule)
-                      contentViewStates.complexModificationsViewSheetPresented = false
-                    }) {
-                      // Use `Image` and `Text` instead of `Label` to set icon color like `Button` in `List`.
-                      Image(systemName: "plus.circle.fill").foregroundColor(.blue)
-                      Text("Enable")
-                    }
-
-                    Spacer()
-                  }
+                  Spacer()
                 }
               }
-              .padding()
             }
-
-            HStack(alignment: .bottom) {
-              Spacer()
-
-              if assetFile.userFile {
-                Text("Imported at \(assetFile.importedAt)")
-                  .font(.caption)
-              }
-            }
-            .padding(.bottom, 32.0)
+            .padding()
           }
+
+          HStack(alignment: .bottom) {
+            Spacer()
+
+            if assetFile.userFile {
+              Text("Imported at \(assetFile.importedAt)")
+                .font(.caption)
+            }
+          }
+          .padding(.bottom, 32.0)
         }
       }
       .background(Color(NSColor.textBackgroundColor))
