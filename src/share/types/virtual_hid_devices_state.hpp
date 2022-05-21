867256ff18805ad7728dd6a83f08a63c66b694b2
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-21 19:21:35 +0900

    Update virtual_hid_devices_state

--- src/share/types/virtual_hid_devices_state.hpp
+++ src/share/types/virtual_hid_devices_state.hpp
@@ -39,6 +39,26 @@ inline void to_json(nlohmann::json& json, const virtual_hid_devices_state& s) {
   json["virtual_hid_keyboard_ready"] = s.get_virtual_hid_keyboard_ready();
   json["virtual_hid_pointing_ready"] = s.get_virtual_hid_pointing_ready();
 }
+
+inline void from_json(const nlohmann::json& json, virtual_hid_devices_state& s) {
+  pqrs::json::requires_object(json, "json");
+
+  for (const auto& [key, value] : json.items()) {
+    if (key == "virtual_hid_keyboard_ready") {
+      pqrs::json::requires_boolean(value, "`" + key + "`");
+
+      s.set_virtual_hid_keyboard_ready(value.get<bool>());
+
+    } else if (key == "virtual_hid_pointing_ready") {
+      pqrs::json::requires_boolean(value, "`" + key + "`");
+
+      s.set_virtual_hid_pointing_ready(value.get<bool>());
+
+    } else {
+      throw pqrs::json::unmarshal_error(fmt::format("unknown key: `{0}`", key));
+    }
+  }
+}
 } // namespace krbn
 
 namespace std {
