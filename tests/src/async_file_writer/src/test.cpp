2f591ce3f582a2eea5b6977a514adb7a02d2d870
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 10:48:14 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/async_file_writer/src/test.cpp
+++ tests/src/async_file_writer/src/test.cpp
@@ -1,5 +1,21 @@
-#include "test_runner.hpp"
+#include "async_file_writer.hpp"
+#include <boost/ut.hpp>
 
-int main(int argc, char* argv[]) {
-  return run_tests(argc, argv);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  auto scoped_dispatcher_manager = krbn::dispatcher_utility::initialize_dispatchers();
+
+  "async_file_writer"_test = [] {
+    krbn::async_file_writer::enqueue("tmp/example", "example1", 0755, 0600);
+    krbn::async_file_writer::enqueue("tmp/example", "example2", 0755, 0600);
+    krbn::async_file_writer::enqueue("tmp/example", "example3", 0755, 0600);
+    krbn::async_file_writer::enqueue("tmp/mode666", "mode666", 0755, 0666);
+    krbn::async_file_writer::enqueue("tmp/mode644", "mode644", 0755, 0644);
+    krbn::async_file_writer::enqueue("tmp/not_found/example", "example", 0755, 0600);
+    krbn::async_file_writer::wait();
+  };
+
+  return 0;
 }
