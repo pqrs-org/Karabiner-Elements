8ad042de7e3d0877f6c83024867c2ceb36ed08d8
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-29 15:54:38 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/event_tap_utility/src/test.cpp
+++ tests/src/event_tap_utility/src/test.cpp
@@ -1,5 +1,24 @@
-#include "test_runner.hpp"
+#include "event_tap_utility.hpp"
+#include <boost/ut.hpp>
 
-int main(int argc, char* argv[]) {
-  return run_tests(argc, argv);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+
+  "make_event"_test = [] {
+    {
+      auto actual = krbn::event_tap_utility::make_event(kCGEventLeftMouseDown, nullptr);
+      expect(actual->first == krbn::event_type::key_down);
+      expect(actual->second == krbn::event_queue::event(krbn::momentary_switch_event(pqrs::hid::usage_page::button,
+                                                                                     pqrs::hid::usage::button::button_1)));
+    }
+    {
+      auto actual = krbn::event_tap_utility::make_event(kCGEventOtherMouseUp, nullptr);
+      expect(actual->first == krbn::event_type::key_up);
+      expect(actual->second == krbn::event_queue::event(krbn::momentary_switch_event(pqrs::hid::usage_page::button,
+                                                                                     pqrs::hid::usage::button::button_3)));
+    }
+  };
+
+  return 0;
 }
