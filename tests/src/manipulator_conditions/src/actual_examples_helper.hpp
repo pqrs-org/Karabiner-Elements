b335e7d965c81815aeacd4aa93390e1bebec2ffa
Takayama Fumihiko <tekezo@pqrs.org>
2022-08-23 08:38:20 +0900

    Update tests

---
+++ tests/src/manipulator_conditions/src/actual_examples_helper.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "../../share/json_helper.hpp"
+#include "manipulator/condition_manager.hpp"
+#include "manipulator/manipulator_factory.hpp"
+#include <boost/ut.hpp>
+
+namespace {
+using namespace boost::ut;
+using namespace boost::ut::literals;
+
+class actual_examples_helper final {
+public:
+  actual_examples_helper(const std::string& file_name) {
+    auto json = krbn::unit_testing::json_helper::load_jsonc(std::string("json/") + file_name);
+    for (const auto& j : json) {
+      try {
+        condition_manager_.push_back_condition(krbn::manipulator::manipulator_factory::make_condition(j));
+      } catch (pqrs::json::unmarshal_error& e) {
+        error_messages_.push_back(e.what());
+      } catch (std::exception& e) {
+        expect(false);
+      }
+    }
+  }
+
+  const krbn::manipulator::condition_manager& get_condition_manager(void) const {
+    return condition_manager_;
+  }
+
+  const std::vector<std::string> get_error_messages(void) const {
+    return error_messages_;
+  }
+
+private:
+  krbn::manipulator::condition_manager condition_manager_;
+  std::vector<std::string> error_messages_;
+};
+} // namespace
--- tests/src/manipulator_conditions/src/manipulator_conditions_test.hpp
+++ tests/src/manipulator_conditions/src/manipulator_conditions_test.hpp
@@ -1,41 +1,7 @@
-#include "../../share/json_helper.hpp"
+#pragma once
+
 #include "../../share/manipulator_conditions_helper.hpp"
-#include "manipulator/condition_manager.hpp"
-#include "manipulator/manipulator_factory.hpp"
-#include <boost/ut.hpp>
-
-namespace {
-using namespace boost::ut;
-using namespace boost::ut::literals;
-
-class actual_examples_helper final {
-public:
-  actual_examples_helper(const std::string& file_name) {
-    auto json = krbn::unit_testing::json_helper::load_jsonc(std::string("json/") + file_name);
-    for (const auto& j : json) {
-      try {
-        condition_manager_.push_back_condition(krbn::manipulator::manipulator_factory::make_condition(j));
-      } catch (pqrs::json::unmarshal_error& e) {
-        error_messages_.push_back(e.what());
-      } catch (std::exception& e) {
-        expect(false);
-      }
-    }
-  }
-
-  const krbn::manipulator::condition_manager& get_condition_manager(void) const {
-    return condition_manager_;
-  }
-
-  const std::vector<std::string> get_error_messages(void) const {
-    return error_messages_;
-  }
-
-private:
-  krbn::manipulator::condition_manager condition_manager_;
-  std::vector<std::string> error_messages_;
-};
-} // namespace
+#include "actual_examples_helper.hpp"
 
 void run_manipulator_conditions_test(void) {
   using namespace boost::ut;
