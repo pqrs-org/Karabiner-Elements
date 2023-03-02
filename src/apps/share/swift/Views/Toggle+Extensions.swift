1b54c009504df67a720c02b5cefdefcb90f370f5
Takayama Fumihiko <tekezo@pqrs.org>
2023-03-02 23:27:35 +0900

    Add controlSize, font args into switchToggleStyle

--- src/apps/share/swift/Views/Toggle+Extensions.swift
+++ src/apps/share/swift/Views/Toggle+Extensions.swift
@@ -1,10 +1,13 @@
 import SwiftUI
 
 extension Toggle {
-  func switchToggleStyle() -> some View {
+  func switchToggleStyle(
+    controlSize: ControlSize = .small,
+    font: Font = .body
+  ) -> some View {
     self
       .toggleStyle(.switch)
-      .controlSize(.small)
-      .font(.body)
+      .controlSize(controlSize)
+      .font(font)
   }
 }
