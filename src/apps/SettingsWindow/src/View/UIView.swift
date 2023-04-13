1f162dac3ceab94eb0d014a346bfe4b98bb5a6ce
Takayama Fumihiko <tekezo@pqrs.org>
2023-04-14 08:49:58 +0900

    Move UI settings to UIView

--- src/apps/SettingsWindow/src/View/UIView.swift
+++ src/apps/SettingsWindow/src/View/UIView.swift
@@ -2,9 +2,57 @@ import SwiftUI
 
 struct UIView: View {
   @ObservedObject private var appIcons = AppIcons.shared
+  @ObservedObject private var settings = LibKrbn.Settings.shared
 
   var body: some View {
     VStack(alignment: .leading, spacing: 24.0) {
+      GroupBox(label: Text("Menu bar")) {
+        VStack(alignment: .leading, spacing: 12.0) {
+          HStack {
+            Toggle(isOn: $settings.showIconInMenuBar) {
+              Text("Show icon in menu bar (Default: on)")
+            }
+            .switchToggleStyle()
+
+            Spacer()
+          }
+
+          HStack {
+            Toggle(isOn: $settings.showProfileNameInMenuBar) {
+              Text("Show profile name in menu bar (Default: off)")
+            }
+            .switchToggleStyle()
+
+            Spacer()
+          }
+
+          HStack {
+            Toggle(isOn: $settings.askForConfirmationBeforeQuitting) {
+              Text("Ask for confirmation when quitting (Default: on)")
+            }
+            .switchToggleStyle()
+
+            Spacer()
+          }
+
+        }
+        .padding(6.0)
+      }
+
+      GroupBox(label: Text("Sticky modifier keys")) {
+        VStack(alignment: .leading, spacing: 12.0) {
+          HStack {
+            Toggle(isOn: $settings.virtualHIDKeyboardIndicateStickyModifierKeysState) {
+              Text("Indicate sticky modifier keys state (Default: on)")
+            }
+            .switchToggleStyle()
+
+            Spacer()
+          }
+        }
+        .padding(6.0)
+      }
+
       GroupBox(label: Text("App icon")) {
         VStack(alignment: .leading, spacing: 12.0) {
           VStack {
