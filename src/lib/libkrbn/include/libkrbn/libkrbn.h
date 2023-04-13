e9bb7161faf8ef429ed7ced39dcdf6ea635c87e8
Takayama Fumihiko <tekezo@pqrs.org>
2023-04-14 01:02:51 +0900

    Add libkrbn_get_system_app_icon_configuration_file_path

--- src/lib/libkrbn/include/libkrbn/libkrbn.h
+++ src/lib/libkrbn/include/libkrbn/libkrbn.h
@@ -26,6 +26,7 @@ const char* libkrbn_get_distributed_notification_observed_object(void);
 const char* libkrbn_get_distributed_notification_console_user_server_is_disabled(void);
 const char* libkrbn_get_user_configuration_directory(void);
 const char* libkrbn_get_user_complex_modifications_assets_directory(void);
+const char* libkrbn_get_system_app_icon_configuration_file_path(void);
 
 bool libkrbn_lock_single_application_with_user_pid_file(const char* pid_file_name);
 void libkrbn_unlock_single_application(void);
