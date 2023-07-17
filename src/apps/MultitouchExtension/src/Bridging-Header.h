bfb5e62a6d92fd7fbb61f3bc507b138390ef7349
Takayama Fumihiko <tekezo@pqrs.org>
2023-07-17 19:22:56 +0900

    Add src/Bridging-Header.h

---
+++ src/apps/MultitouchExtension/src/Bridging-Header.h
@@ -0,0 +1,14 @@
+// -*- mode: objective-c -*-
+
+#import "KarabinerKit/KarabinerKit.h"
+#import "libkrbn/libkrbn.h"
+
+#import "FingerCount.h"
+#import "FingerStatusEntry.h"
+#import "FingerStatusManager.h"
+#import "IgnoredAreaView.h"
+#import "MultitouchDeviceManager.h"
+#import "MultitouchPrivate.h"
+#import "NotificationKeys.h"
+#import "PreferencesController.h"
+#import "PreferencesKeys.h"
