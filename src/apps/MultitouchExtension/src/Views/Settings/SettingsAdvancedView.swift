4c3eb9043a6ab6a0b1dae827d7929a641de8c577
Takayama Fumihiko <tekezo@pqrs.org>
2023-07-22 18:23:54 +0900

    Update SettingsAdvancedView

--- src/apps/MultitouchExtension/src/Views/Settings/SettingsAdvancedView.swift
+++ src/apps/MultitouchExtension/src/Views/Settings/SettingsAdvancedView.swift
@@ -5,6 +5,40 @@ struct SettingsAdvancedView: View {
 
   var body: some View {
     VStack(alignment: .leading, spacing: 25.0) {
+      GroupBox(label: Text("Sleep handling")) {
+        VStack(alignment: .leading, spacing: 30.0) {
+          VStack(alignment: .leading) {
+            HStack {
+              Toggle(isOn: $userSettings.relaunchAfterWakeUpFromSleep) {
+                Text("Relaunch after wake from sleep")
+              }
+              .switchToggleStyle()
+
+              Text("(Default: on)")
+
+              Spacer()
+            }
+
+            HStack {
+              Text("Wait before relaunch:")
+
+              IntTextField(
+                value: $userSettings.relaunchWait,
+                range: 0...10,
+                step: 1,
+                width: 40
+              )
+              .disabled(!userSettings.relaunchAfterWakeUpFromSleep)
+
+              Text("seconds (Default: 3)")
+
+              Spacer()
+            }
+          }
+        }
+        .padding(6.0)
+      }
+
       GroupBox(label: Text("Delay")) {
         VStack(alignment: .leading, spacing: 30.0) {
           VStack(alignment: .leading) {
