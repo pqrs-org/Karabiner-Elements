62e9690b8e500d852421ca729ad1c7078c69bd9d
Takayama Fumihiko <tekezo@pqrs.org>
2023-09-10 08:44:17 +0900

    Fix swiftlint warnings

--- src/apps/share/swift/NSWindowExtension.swift
+++ src/apps/share/swift/NSWindowExtension.swift
@@ -3,11 +3,11 @@ import AppKit
 extension NSWindow {
   public func centerToOtherWindow(_ other: NSWindow) {
     setFrame(
-      NSMakeRect(
-        other.frame.origin.x + (other.frame.size.width / 2) - (frame.size.width / 2),
-        other.frame.origin.y + (other.frame.size.height / 2) - (frame.size.height / 2),
-        frame.size.width,
-        frame.size.height
+      NSRect(
+        x: other.frame.origin.x + (other.frame.size.width / 2) - (frame.size.width / 2),
+        y: other.frame.origin.y + (other.frame.size.height / 2) - (frame.size.height / 2),
+        width: frame.size.width,
+        height: frame.size.height
       ),
       display: false
     )
