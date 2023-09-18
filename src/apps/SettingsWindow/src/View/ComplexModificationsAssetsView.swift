c3b9f5dea6f9f19bdf28d766164b41a60af5c452
Takayama Fumihiko <tekezo@pqrs.org>
2023-09-18 10:07:57 +0900

    Use SheetCloseButton

--- src/apps/SettingsWindow/src/View/ComplexModificationsAssetsView.swift
+++ src/apps/SettingsWindow/src/View/ComplexModificationsAssetsView.swift
@@ -13,112 +13,108 @@ struct ComplexModificationsAssetsView: View {
   }
 
   var body: some View {
-    VStack(alignment: .center, spacing: 12.0) {
-      HStack {
-        Image(systemName: "magnifyingglass")
-          .foregroundColor(.gray)
+    ZStack(alignment: .topLeading) {
+      VStack(alignment: .center, spacing: 12.0) {
+        Button(
+          action: {
+            NSWorkspace.shared.open(URL(string: "https://ke-complex-modifications.pqrs.org/")!)
+          },
+          label: {
+            AccentColorIconLabel(
+              title: "Import more rules from the Internet (Open a web browser)",
+              systemImage: "icloud.and.arrow.down.fill")
+          }
+        )
+        .padding(.top, 10.0)
 
-        TextField("Search", text: $search)
-      }
-      .padding(.vertical, 10.0)
+        HStack(alignment: .center) {
+          Image(systemName: "magnifyingglass")
+            .foregroundColor(.gray)
 
-      List {
-        ForEach($assetFiles.files) { $assetFile in
-          if search == "" || assetFile.match(search) {
-            GroupBox(
-              label:
-                HStack(alignment: .bottom, spacing: 16.0) {
-                  Text(assetFile.title)
-                    .font(.title)
+          TextField("Search", text: $search)
+        }
 
-                  Spacer()
-                }
-            ) {
-              VStack(alignment: .leading, spacing: 8.0) {
-                ForEach($assetFile.assetRules) { $assetRule in
-                  HStack(alignment: .center, spacing: 16.0) {
-                    Text(assetRule.description)
-
-                    Button(
-                      action: {
-                        LibKrbn.Settings.shared.addComplexModificationRule(assetRule)
-                        contentViewStates.complexModificationsViewSheetPresented = false
-                      },
-                      label: {
-                        // Use `Image` and `Text` instead of `Label` to set icon color like `Button` in `List`.
-                        Image(systemName: "plus.circle.fill").foregroundColor(.blue)
-                        Text("Enable")
-                      })
+        List {
+          ForEach($assetFiles.files) { $assetFile in
+            if search == "" || assetFile.match(search) {
+              GroupBox(
+                label:
+                  HStack(alignment: .bottom, spacing: 16.0) {
+                    Text(assetFile.title)
+                      .font(.title)
 
                     Spacer()
                   }
+              ) {
+                VStack(alignment: .leading, spacing: 8.0) {
+                  ForEach($assetFile.assetRules) { $assetRule in
+                    HStack(alignment: .center, spacing: 16.0) {
+                      Text(assetRule.description)
 
-                  Divider()
-                    .padding(.vertical, 4.0)
-                }
+                      Button(
+                        action: {
+                          LibKrbn.Settings.shared.addComplexModificationRule(assetRule)
+                          contentViewStates.complexModificationsViewSheetPresented = false
+                        },
+                        label: {
+                          // Use `Image` and `Text` instead of `Label` to set icon color like `Button` in `List`.
+                          Image(systemName: "plus.circle.fill").foregroundColor(.blue)
+                          Text("Enable")
+                        })
 
-                HStack {
-                  Spacer()
+                      Spacer()
+                    }
 
-                  if assetFile.userFile {
-                    Text(
-                      "Imported at \(formatter.string(from: assetFile.importedAt))"
-                    )
-                    .font(.caption)
+                    Divider()
+                      .padding(.vertical, 4.0)
                   }
 
-                  Button(
-                    action: {
-                      LibKrbn.Settings.shared.addComplexModificationRules(assetFile)
-                      contentViewStates.complexModificationsViewSheetPresented = false
-                    },
-                    label: {
-                      Text("Enable All")
-                        .font(.caption)
-                    })
+                  HStack {
+                    Spacer()
 
-                  if assetFile.userFile {
+                    if assetFile.userFile {
+                      Text(
+                        "Imported at \(formatter.string(from: assetFile.importedAt))"
+                      )
+                      .font(.caption)
+                    }
+
                     Button(
                       action: {
-                        assetFiles.removeFile(assetFile)
+                        LibKrbn.Settings.shared.addComplexModificationRules(assetFile)
+                        contentViewStates.complexModificationsViewSheetPresented = false
                       },
                       label: {
-                        Image(systemName: "trash.fill")
-                          .buttonLabelStyle()
-                      }
-                    )
-                    .deleteButtonStyle()
+                        Text("Enable All")
+                          .font(.caption)
+                      })
+
+                    if assetFile.userFile {
+                      Button(
+                        action: {
+                          assetFiles.removeFile(assetFile)
+                        },
+                        label: {
+                          Image(systemName: "trash.fill")
+                            .buttonLabelStyle()
+                        }
+                      )
+                      .deleteButtonStyle()
+                    }
                   }
                 }
+                .padding()
               }
-              .padding()
+              .padding(.bottom, 32.0)
             }
-            .padding(.bottom, 32.0)
           }
         }
+        .background(Color(NSColor.textBackgroundColor))
       }
-      .background(Color(NSColor.textBackgroundColor))
 
-      Spacer()
-
-      Button(
-        action: {
-          NSWorkspace.shared.open(URL(string: "https://ke-complex-modifications.pqrs.org/")!)
-        },
-        label: {
-          Label(
-            "Import more rules from the Internet (Open a web browser)",
-            systemImage: "icloud.and.arrow.down.fill")
-        })
-
-      Button(
-        action: {
-          contentViewStates.complexModificationsViewSheetPresented = false
-        },
-        label: {
-          Label("Close", systemImage: "xmark")
-            .frame(minWidth: 0, maxWidth: .infinity)
-        })
+      SheetCloseButton {
+        contentViewStates.complexModificationsViewSheetPresented = false
+      }
     }
     .padding()
     .frame(width: 1000, height: 600)
