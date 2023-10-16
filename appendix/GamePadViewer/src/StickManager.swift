b3622411556f41962ffcf05623e4fabc9b7ac912
Takayama Fumihiko <tekezo@pqrs.org>
2023-10-16 22:10:14 +0900

    Update GamePadViewer

--- appendix/GamePadViewer/src/StickManager.swift
+++ appendix/GamePadViewer/src/StickManager.swift
@@ -4,12 +4,6 @@ import SwiftUI
 public class StickManager: ObservableObject {
   public static let shared = StickManager()
 
-  struct History {
-    let timeStamp: Date
-    let radian: Double
-    var magnitude: Double
-  }
-
   class StickSensor: ObservableObject {
     @Published var lastDoubleValue = 0.0
 
@@ -35,7 +29,6 @@ public class StickManager: ObservableObject {
     @Published var deadzoneEnteredAt = Date()
     @Published var deadzoneLeftAt = Date()
     @Published var strokeAcceleration = 0.0
-    var histories: [History] = []
     let remainDeadzoneThresholdMilliseconds: UInt64 = 100
     let strokeAccelerationMeasurementTime = 0.05  // 50 ms
 
@@ -59,7 +52,6 @@ public class StickManager: ObservableObject {
 
               deadzoneEnteredAt = now
 
-              histories.removeAll()
               strokeAcceleration = 0.0
             } catch {
               print("cancelled")
@@ -75,27 +67,9 @@ public class StickManager: ObservableObject {
         }
       }
 
-      histories.removeAll(where: {
-        if $0.timeStamp < deadzoneLeftAt {
-          return true
-        }
-
-        return now.timeIntervalSince($0.timeStamp) > strokeAccelerationMeasurementTime
-      })
-
-      histories.append(
-        History(
-          timeStamp: now,
-          radian: radian,
-          magnitude: magnitude))
-
       if now.timeIntervalSince(deadzoneLeftAt) < strokeAccelerationMeasurementTime {
-        let minMagnitude = histories.min(by: { $0.magnitude < $1.magnitude })
-        let maxMagnitude = histories.max(by: { $0.magnitude < $1.magnitude })
-        let a = (maxMagnitude?.magnitude ?? 0) - (minMagnitude?.magnitude ?? 0)
-
-        if a > strokeAcceleration {
-          strokeAcceleration = a
+        if strokeAcceleration < magnitude {
+          strokeAcceleration = magnitude
         }
       }
     }
