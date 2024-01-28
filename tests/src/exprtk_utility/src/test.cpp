6d8055b01a88cf2baee20ad49e1d2fd9ec6da473
Takayama Fumihiko <tekezo@pqrs.org>
2024-01-28 11:17:17 +0900

    Update tests/src/exprtk_utility

--- tests/src/exprtk_utility/src/test.cpp
+++ tests/src/exprtk_utility/src/test.cpp
@@ -7,29 +7,38 @@ int main(void) {
   using namespace std::literals;
 
   "exprtk_utility::eval"_test = [] {
-    auto actual = krbn::exprtk_utility::eval("cos(radian) * magnitude",
+    auto actual = krbn::exprtk_utility::eval("cos(radian) * delta_magnitude",
                                              {
                                                  {"radian", 0.78539816339},
-                                                 {"magnitude", 3.5},
+                                                 {"delta_magnitude", 3.5},
                                              });
     expect(2.47487_d == actual);
   };
 
   "exprtk_utility::compile"_test = [] {
     double radian = 0.0;
-    double magnitude = 1.0;
+    double delta_magnitude = 1.0;
 
-    auto expression = krbn::exprtk_utility::compile("cos(radian) * magnitude",
+    auto expression = krbn::exprtk_utility::compile("cos(radian) * delta_magnitude",
                                                     {},
                                                     {
                                                         {"radian", radian},
-                                                        {"magnitude", magnitude},
+                                                        {"delta_magnitude", delta_magnitude},
                                                     });
     expect(1.0_d == expression.value());
 
     radian = 0.78539816339;
-    magnitude = 3.5;
+    delta_magnitude = 3.5;
     expect(2.47487_d == expression.value());
+  };
+
+  "exprtk_utility::eval error"_test = [] {
+    auto actual = krbn::exprtk_utility::eval("cos(unknown)",
+                                             {
+                                                 {"radian", 0.78539816339},
+                                                 {"delta_magnitude", 3.5},
+                                             });
+    expect(std::isnan(actual));
   };
 
   return 0;
