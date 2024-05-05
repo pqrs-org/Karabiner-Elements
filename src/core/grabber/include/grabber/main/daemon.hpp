e7b5621615fa0d125e011fdf1da340a50a36a855
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-05 22:40:39 +0900

    Remove touch LaunchAgents/*.plist, LaunchDaemons/*.plist

--- src/core/grabber/include/grabber/main/daemon.hpp
+++ src/core/grabber/include/grabber/main/daemon.hpp
@@ -84,22 +84,6 @@ int daemon(void) {
   }
 
   //
-  // Touch /Library/LaunchAgents/*.plist and /Library/LaunchDaemons/*.plist to refresh the name in Login Items System Settings.
-  //
-  // Note:
-  // Login Items maybe refer the Launch Services database before the applications registration is completed.
-  // If the application registration of AssociatedBundleIdentifiers is not completed at the time,
-  // Login Items displays the developer name instead of the application name.
-  // And the result is cached, the developer name will be displayed permanently.
-  // (We have confirmed this behaivor on macOS 13.1)
-  //
-  // In order to refresh the cache, we update the modification time of *.plist.
-  //
-
-  system("/usr/bin/touch /Library/LaunchAgents/org.pqrs.karabiner.*");
-  system("/usr/bin/touch /Library/LaunchDaemons/org.pqrs.karabiner.*");
-
-  //
   // Prepare state_json_writer
   //
 
