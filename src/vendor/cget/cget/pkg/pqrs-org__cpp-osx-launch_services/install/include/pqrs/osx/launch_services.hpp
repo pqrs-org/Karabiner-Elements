f13d561ee719c631cf87f5905aefaed4a077f756
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-05 19:11:12 +0900

    Update vendor/cget

--- src/vendor/cget/cget/pkg/pqrs-org__cpp-osx-launch_services/install/include/pqrs/osx/launch_services.hpp
+++ src/vendor/cget/cget/pkg/pqrs-org__cpp-osx-launch_services/install/include/pqrs/osx/launch_services.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-// pqrs::osx::launch_services v1.0
+// pqrs::osx::launch_services v1.1
 
 // (C) Copyright Takayama Fumihiko 2022.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "launch_services/status.hpp"
 #include <pqrs/cf/string.hpp>
@@ -13,13 +13,13 @@ namespace pqrs {
 namespace osx {
 namespace launch_services {
 
-inline status register_application(const std::string& path) {
+inline status register_application(const std::string& path, bool update = true) {
   if (auto cf_path = pqrs::cf::make_cf_string(path)) {
     if (auto url = CFURLCreateWithFileSystemPath(nullptr,
                                                  *cf_path,
                                                  kCFURLPOSIXPathStyle,
                                                  true)) {
-      auto s = LSRegisterURL(url, true);
+      auto s = LSRegisterURL(url, update);
       CFRelease(url);
 
       return s;
