06b1844729beb0154551d425e5ca28c5053d338e
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-06 17:56:33 +0900

    Call karabiner_cli --lsregister-karabiner-elements in repair.sh

--- src/scripts/repair.sh
+++ src/scripts/repair.sh
@@ -7,4 +7,6 @@ if [ ! -d /Applications/Karabiner-Elements.app ]; then
     echo "Restore /Applications/Karabiner-Elements.app"
 
     tar -C /Applications -xf '/Library/Application Support/org.pqrs/Karabiner-Elements/Karabiner-Elements.app.tar.gz'
+
+    '/Library/Application Support/org.pqrs/Karabiner-Elements/bin/karabiner_cli' --lsregister-karabiner-elements
 fi
