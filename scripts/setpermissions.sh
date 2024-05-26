c72b1ce063590dcc7a535703189e6f56abec6682
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-26 20:30:04 +0900

    Use trap in scripts/setpermissions.sh

--- scripts/setpermissions.sh
+++ scripts/setpermissions.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-set -e
+set -u # forbid undefined variables
+set -e # forbid command failure
 
-readonly PATH=/bin:/sbin:/usr/bin:/usr/sbin; export PATH
+readonly PATH=/bin:/sbin:/usr/bin:/usr/sbin
+export PATH
 
-err() {
-    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
-}
+trap "echo -ne '\033[0m'" EXIT
+echo -ne '\033[33;40m'
 
 test_mach_o() {
-    if `file -b "$1" | grep 'executable ' | grep -sq 'Mach-O '`; then
+    if $(file -b "$1" | grep 'executable ' | grep -sq 'Mach-O '); then
         # This is Mach-O file.
         exit 0
     fi
@@ -17,27 +18,23 @@ test_mach_o() {
     exit 1
 }
 
-main() {
-    if [ ! -e "$1" ]; then
-        err "Invalid argument: '$1'"
-        exit 1
-    fi
+if [ ! -e "$1" ]; then
+    echo "Invalid argument: '$1'"
+    exit 1
+fi
 
-    find "$1" -print0 | while read -d $'\0' filepath; do
-        if [ -d "$filepath" ]; then
+find "$1" -print0 | while read -d $'\0' filepath; do
+    if [ -d "$filepath" ]; then
+        chmod -h 755 "$filepath"
+    else
+        extension=${filepath##*.}
+        if [ "$extension" = 'sh' ]; then
             chmod -h 755 "$filepath"
+        elif $(test_mach_o "$filepath"); then
+            # Preserve the set-user-ID-on-execution bits.
+            chmod -h u+rwx,go=rx "$filepath"
         else
-            extension=${filepath##*.}
-            if [ "$extension" = 'sh' ]; then
-                chmod -h 755 "$filepath"
-            elif `test_mach_o "$filepath"`; then
-                # Preserve the set-user-ID-on-execution bits.
-                chmod -h u+rwx,go=rx "$filepath"
-            else
-                chmod -h 644 "$filepath"
-            fi
+            chmod -h 644 "$filepath"
         fi
-    done
-}
-
-main "$1"
+    fi
+done
