d66d3118e6922c5e81964d9c66a34d06cc07be29
Takayama Fumihiko <tekezo@pqrs.org>
2024-05-26 21:27:25 +0900

    Use trap in scripts/codesign.sh

--- scripts/codesign.sh
+++ scripts/codesign.sh
@@ -6,6 +6,9 @@ set -e # forbid command failure
 readonly PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH
 
+trap "echo -ne '\033[0m'" EXIT
+echo -ne '\033[33;40m'
+
 readonly CODE_SIGN_IDENTITY=$(bash $(dirname $0)/get-codesign-identity.sh)
 
 if [[ -z $CODE_SIGN_IDENTITY ]]; then
@@ -13,88 +16,51 @@ if [[ -z $CODE_SIGN_IDENTITY ]]; then
     exit 0
 fi
 
-#
-# Define err()
-#
-
-err() {
-    echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
-}
-
-#
-# Define do_codesign
-#
-
 do_codesign() {
-    echo -ne '\033[31;40m'
-
-    set +e # allow command failure
-
     codesign \
         --force \
         --deep \
         --options runtime \
         --sign "$CODE_SIGN_IDENTITY" \
-        "$1" 2>&1 |
-        grep -v ': replacing existing signature'
-
-    set -e # forbid command failure
-
-    echo -ne '\033[0m'
+        "$1"
 }
 
-#
-# Define main()
-#
+if [[ ! -e "$1" ]]; then
+    echo "Invalid argument: '$1'"
+    exit 1
+fi
 
-main() {
-    if [[ ! -e "$1" ]]; then
-        err "Invalid argument: '$1'"
-        exit 1
-    fi
+if [[ -d "$1" ]]; then
+    #
+    # Sign with codesign
+    #
 
-    if [[ -d "$1" ]]; then
+    cd "$1"
+    find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
         #
-        # Sign with codesign
-        #
-
-        cd "$1"
-        find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
-            #
-            # output message
-            #
-
-            echo -ne '\033[33;40m'
-            echo "code sign $f"
-            echo -ne '\033[0m'
-
-            #
-            # codesign
-            #
-
-            do_codesign "$f"
-        done
-
-        #
-        # Verify nested codesign (--deep)
+        # output message
         #
 
-        find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
-            echo -ne '\033[31;40m'
-            codesign --verify --deep "$f"
-            echo -ne '\033[0m'
-        done
-    else
+        echo "code sign $f"
+
         #
-        # Sign a file
+        # codesign
         #
 
-        do_codesign "$1"
-    fi
-}
+        do_codesign "$f"
+    done
 
-#
-# Run
-#
+    #
+    # Verify nested codesign (--deep)
+    #
 
-main "$1"
+    find * -name '*.app' -or -path '*/bin/*' | sort -r | while read f; do
+        codesign --verify --deep "$f"
+    done
+else
+    #
+    # Sign a file
+    #
+
+    do_codesign "$1"
+fi
