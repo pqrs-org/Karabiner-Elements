bb05757bb6087323111855f91e7028e659088aef
Takayama Fumihiko <tekezo@pqrs.org>
2024-07-21 21:45:58 +0900

    Add extra apps to AppIconSwitcher targets

--- src/apps/AppIconSwitcher/src/main.swift
+++ src/apps/AppIconSwitcher/src/main.swift
@@ -1,12 +1,21 @@
 import AppKit
 
 for argument in CommandLine.arguments {
-  if let icon = NSImage(named: "\(argument)-KarabinerElements.icns") {
-    let result = NSWorkspace.shared.setIcon(
-      icon,
-      forFile: "/Applications/Karabiner-Elements.app",
-      options: [])
-    print("setIcon Karabiner-Elements.app: \(result)")
+  for file in [
+    "/Applications/Karabiner-Elements.app",
+    "/Applications/Karabiner-Elements.app/Contents/Frameworks/Sparkle.framework/Updater.app",
+    "/Library/Application Support/org.pqrs//Karabiner-Elements/Karabiner-Elements Non-Privileged Agents.app",
+    "/Library/Application Support/org.pqrs//Karabiner-Elements/Karabiner-Elements Privileged Daemons.app",
+    "/Library/Application Support/org.pqrs//Karabiner-Elements/Karabiner-Menu.app",
+    "/Library/Application Support/org.pqrs//Karabiner-Elements/Karabiner-NotificationWindow.app",
+  ] {
+    if let icon = NSImage(named: "\(argument)-KarabinerElements.icns") {
+      let result = NSWorkspace.shared.setIcon(
+        icon,
+        forFile: file,
+        options: [])
+      print("setIcon Karabiner-Elements.app: \(result)")
+    }
   }
 
   if let icon = NSImage(named: "\(argument)-EventViewer.icns") {
