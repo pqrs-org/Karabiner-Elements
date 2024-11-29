b19d587f8f2f952ae0923f04aa942695610ea2fd
Takayama Fumihiko <tekezo@pqrs.org>
2024-11-29 09:48:42 +0900

    Add hash<gsl::not_null<std::shared_ptr<T>>>

--- src/share/memory_utility.hpp
+++ src/share/memory_utility.hpp
@@ -18,3 +18,17 @@ inline std::weak_ptr<T> make_weak(gsl::not_null<std::shared_ptr<T>> p) {
 
 } // namespace memory_utility
 } // namespace krbn
+
+namespace std {
+// This definition is required to prevent the following error:
+//
+// ```
+// error: call to deleted constructor of 'gsl::not_null_hash<gsl::not_null<shared_ptr<T>>>'
+// ```
+template <typename T>
+struct hash<gsl::not_null<std::shared_ptr<T>>> final {
+  std::size_t operator()(const gsl::not_null<std::shared_ptr<T>>& value) const {
+    return std::hash<T>{}(*value);
+  }
+};
+} // namespace std
