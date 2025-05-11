1af78e2f6478ecab07e7c96108b5c9978d2d3077
Takayama Fumihiko <tekezo@pqrs.org>
2025-05-11 19:13:53 +0900

    Migrating NotificationWindow to the SwiftUI life cycle

---
+++ src/apps/NotificationWindow/src/KarabinerNotificationWindowApp.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+@main
+struct KarabinerNotificationWindowApp: App {
+  @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+  init() {
+    libkrbn_initialize()
+  }
+
+  var body: some Scene {
+    // The main window is manually managed by NotificationWindowManager.
+
+    // Provide an empty Settings to prevent build errors.
+    Settings {}
+  }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+  public func applicationDidFinishLaunching(_: Notification) {
+    NotificationWindowManager.shared.start()
+  }
+
+  public func applicationWillTerminate(_: Notification) {
+    libkrbn_terminate()
+  }
+}
