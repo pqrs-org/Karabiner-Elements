654ea1d065087eb1f086761db0cd29c30d8964b8
Takayama Fumihiko <tekezo@pqrs.org>
2025-06-22 22:26:10 +0900

    Update vendor

--- vendor/vendor/include/pqrs/osx/frontmost_application_monitor.hpp
+++ vendor/vendor/include/pqrs/osx/frontmost_application_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::frontmost_application_monitor v5.1
+// pqrs::osx::frontmost_application_monitor v5.2
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
--- vendor/vendor/src/pqrs/osx/frontmost_application_monitor/PQRSOSXFrontmostApplicationMonitorImpl.swift
+++ vendor/vendor/src/pqrs/osx/frontmost_application_monitor/PQRSOSXFrontmostApplicationMonitorImpl.swift
@@ -5,67 +5,83 @@
 import AppKit
 import os
 
-private class PQRSOSXFrontmostApplicationMonitor {
+// We can add @Sendable since the `frontmost_application_monitor::static_cpp_callback` is sendable.
+public typealias PQRSOSXFrontmostApplicationMonitorCallback =
+  @Sendable @convention(c) (
+    UnsafePointer<CChar>?,
+    UnsafePointer<CChar>?,
+    UnsafePointer<CChar>?,
+    pid_t
+  ) -> Void
+
+private struct FrontmostApplication: Sendable {
+  let bundleIdentifier: String
+  let bundlePath: String
+  let filePath: String
+  let processIdentifier: pid_t
+
+  init(_ runningApplication: NSRunningApplication) {
+    bundleIdentifier = runningApplication.bundleIdentifier ?? ""
+    bundlePath = runningApplication.bundleURL?.path ?? ""
+    filePath = runningApplication.executableURL?.path ?? ""
+    processIdentifier = runningApplication.processIdentifier
+  }
+}
+
+private actor PQRSOSXFrontmostApplicationMonitor {
   static let shared = PQRSOSXFrontmostApplicationMonitor()
 
-  var callback: pqrs_osx_frontmost_application_monitor_callback?
-  let lock = OSAllocatedUnfairLock()
+  private var callback: PQRSOSXFrontmostApplicationMonitorCallback?
+  private var notificationsTask: Task<Void, Never>?
 
-  init() {
-    let sharedWorkspace = NSWorkspace.shared
-    let notificationCenter = sharedWorkspace.notificationCenter
+  func setCallback(_ callback: PQRSOSXFrontmostApplicationMonitorCallback) {
+    self.callback = callback
 
-    notificationCenter.addObserver(
-      forName: NSWorkspace.didActivateApplicationNotification,
-      object: sharedWorkspace,
-      queue: nil
-    ) { note in
-      guard let userInfo = note.userInfo else {
-        print("Missing notification info on NSWorkspace.didActivateApplicationNotification")
-        return
+    if notificationsTask == nil {
+      notificationsTask = Task {
+        await listenLoop()
       }
-
-      guard
-        let runningApplication = userInfo[NSWorkspace.applicationUserInfoKey]
-          as? NSRunningApplication
-      else {
-        print("Missing runningApplication on NSWorkspace.didActivateApplicationNotification")
-        return
-      }
-
-      self.runCallback(runningApplication)
-    }
-  }
-
-  func setCallback(_ callback: pqrs_osx_frontmost_application_monitor_callback) {
-    lock.withLock {
-      self.callback = callback
     }
   }
 
   func unsetCallback() {
-    lock.withLock {
-      callback = nil
+    callback = nil
+
+    notificationsTask?.cancel()
+    notificationsTask = nil
+  }
+
+  private func listenLoop() async {
+    let stream = NSWorkspace.shared.notificationCenter.notifications(
+      named: NSWorkspace.didActivateApplicationNotification
+    )
+    .compactMap { note -> FrontmostApplication? in
+      guard
+        let userInfo = note.userInfo,
+        let runningApplication = userInfo[NSWorkspace.applicationUserInfoKey]
+          as? NSRunningApplication
+      else { return nil }
+
+      return FrontmostApplication(runningApplication)
+    }
+
+    for await frontmostApplication in stream {
+      runCallback(frontmostApplication)
     }
   }
 
-  func runCallback(_ runningApplication: NSRunningApplication) {
-    let bundleIdentifier = runningApplication.bundleIdentifier ?? ""
-    let bundlePath = runningApplication.bundleURL?.path ?? ""
-    let filePath = runningApplication.executableURL?.path ?? ""
-    let processIdentifier = runningApplication.processIdentifier
+  func runCallback(_ frontmostApplication: FrontmostApplication) {
+    guard let callback = callback else { return }
 
-    lock.withLock {
-      bundleIdentifier.utf8CString.withUnsafeBufferPointer { bundleIdentifierPtr in
-        bundlePath.utf8CString.withUnsafeBufferPointer { bundlePathPtr in
-          filePath.utf8CString.withUnsafeBufferPointer { filePathPtr in
-            callback?(
-              bundleIdentifierPtr.baseAddress,
-              bundlePathPtr.baseAddress,
-              filePathPtr.baseAddress,
-              processIdentifier
-            )
-          }
+    frontmostApplication.bundleIdentifier.withCString { bundleIdentifierPtr in
+      frontmostApplication.bundlePath.withCString { bundlePathPtr in
+        frontmostApplication.filePath.withCString { filePathPtr in
+          callback(
+            bundleIdentifierPtr,
+            bundlePathPtr,
+            filePathPtr,
+            frontmostApplication.processIdentifier
+          )
         }
       }
     }
@@ -73,24 +89,45 @@ private class PQRSOSXFrontmostApplicationMonitor {
 
   func runCallbackWithFrontmostApplication() {
     if let runningApplication = NSWorkspace.shared.frontmostApplication {
-      runCallback(runningApplication)
+      runCallback(FrontmostApplication(runningApplication))
     }
   }
 }
 
+// If SetCallback and Trigger are called back-to-back,
+// just using regular Tasks doesn't guarantee execution order within the actor.
+// There's a chance Trigger might run before SetCallback.
+// To enforce the correct call order,methods invoked from C should
+// wait for execution to complete before returning to the caller.
+@inline(__always)
+private func syncCall(_ operation: @Sendable @escaping () async -> Void) {
+  let sem = DispatchSemaphore(value: 0)
+  Task {
+    await operation()
+    sem.signal()
+  }
+  sem.wait()
+}
+
 @_cdecl("pqrs_osx_frontmost_application_monitor_set_callback")
 func PQRSOSXFrontmostApplicationMonitorSetCallback(
-  _ callback: pqrs_osx_frontmost_application_monitor_callback
+  _ callback: PQRSOSXFrontmostApplicationMonitorCallback
 ) {
-  PQRSOSXFrontmostApplicationMonitor.shared.setCallback(callback)
+  syncCall {
+    await PQRSOSXFrontmostApplicationMonitor.shared.setCallback(callback)
+  }
 }
 
 @_cdecl("pqrs_osx_frontmost_application_monitor_unset_callback")
 func PQRSOSXFrontmostApplicationMonitorUnsetCallback() {
-  PQRSOSXFrontmostApplicationMonitor.shared.unsetCallback()
+  syncCall {
+    await PQRSOSXFrontmostApplicationMonitor.shared.unsetCallback()
+  }
 }
 
 @_cdecl("pqrs_osx_frontmost_application_monitor_trigger")
 func PQRSOSXFrontmostApplicationMonitorTrigger() {
-  PQRSOSXFrontmostApplicationMonitor.shared.runCallbackWithFrontmostApplication()
+  syncCall {
+    await PQRSOSXFrontmostApplicationMonitor.shared.runCallbackWithFrontmostApplication()
+  }
 }
