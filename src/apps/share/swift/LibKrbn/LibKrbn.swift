1b7b7fcee52ebd35d749fd7935e0f275dbd21873
Takayama Fumihiko <tekezo@pqrs.org>
2025-06-22 14:48:04 +0900

    String(cString) -> String(utf8String)

--- src/apps/share/swift/LibKrbn/LibKrbn.swift
+++ src/apps/share/swift/LibKrbn/LibKrbn.swift
@@ -2,24 +2,24 @@ public struct LibKrbn {
   public static func devicesJsonFilePath() -> String {
     var buffer = [Int8](repeating: 0, count: 1024)
     libkrbn_get_devices_json_file_path(&buffer, buffer.count)
-    return String(cString: buffer)
+    return String(utf8String: buffer) ?? ""
   }
 
   public static func grabberStateJsonFilePath() -> String {
     var buffer = [Int8](repeating: 0, count: 1024)
     libkrbn_get_grabber_state_json_file_path(&buffer, buffer.count)
-    return String(cString: buffer)
+    return String(utf8String: buffer) ?? ""
   }
 
   public static func manipulatorEnvironmentJsonFilePath() -> String {
     var buffer = [Int8](repeating: 0, count: 1024)
     libkrbn_get_manipulator_environment_json_file_path(&buffer, buffer.count)
-    return String(cString: buffer)
+    return String(utf8String: buffer) ?? ""
   }
 
   public static func notificationMessageJsonFilePath() -> String {
     var buffer = [Int8](repeating: 0, count: 1024)
     libkrbn_get_notification_message_json_file_path(&buffer, buffer.count)
-    return String(cString: buffer)
+    return String(utf8String: buffer) ?? ""
   }
 }
