168f50585b84081ab027bca2df949c989abfded5
Takayama Fumihiko <tekezo@pqrs.org>
2025-07-13 18:27:32 +0900

    Read karabiner_grabber_manipulator_environment.json to get useFkeysAsStandardFunctionKeys instead of using libkrbn_enable_system_preferences_monitor

--- src/apps/SettingsWindow/src/SystemPreferences.swift
+++ src/apps/SettingsWindow/src/SystemPreferences.swift
@@ -4,7 +4,30 @@ import SwiftUI
 
 private func callback() {
   Task { @MainActor in
-    SystemPreferences.shared.updateProperties()
+    if let jsonData = try? Data(
+      contentsOf: URL(
+        fileURLWithPath: SystemPreferences.shared.manipulatorEnvironmentJsonFilePath))
+    {
+      guard
+        let any = try? JSONSerialization.jsonObject(
+          with: jsonData,
+          options: [.allowFragments]
+        )
+      else {
+        return
+      }
+
+      guard let root = any as? [String: Any] else {
+        return
+      }
+
+      if let variables = root["variables"] as? [String: Any],
+        let useFkeysAsStandardFunctionKeys = variables["system.use_fkeys_as_standard_function_keys"]
+          as? Bool
+      {
+        SystemPreferences.shared.useFkeysAsStandardFunctionKeys = useFkeysAsStandardFunctionKeys
+      }
+    }
   }
 }
 
@@ -12,29 +35,26 @@ private func callback() {
 final class SystemPreferences: ObservableObject {
   static let shared = SystemPreferences()
 
-  private var didSetEnabled = false
-
-  private init() {
-    didSetEnabled = true
-  }
+  let manipulatorEnvironmentJsonFilePath = LibKrbn.manipulatorEnvironmentJsonFilePath()
 
   // We register the callback in the `start` method rather than in `init`.
   // If libkrbn_register_*_callback is called within init, there is a risk that `init` could be invoked again from the callback through `shared` before the initial `init` completes.
 
   public func start() {
-    libkrbn_enable_system_preferences_monitor()
+    libkrbn_enable_file_monitors()
 
-    libkrbn_register_system_preferences_updated_callback(callback)
+    libkrbn_register_file_updated_callback(
+      manipulatorEnvironmentJsonFilePath.cString(using: .utf8),
+      callback)
     libkrbn_enqueue_callback(callback)
   }
 
-  public func updateProperties() {
-    didSetEnabled = false
+  public func stop() {
+    libkrbn_unregister_file_updated_callback(
+      manipulatorEnvironmentJsonFilePath.cString(using: .utf8),
+      callback)
 
-    useFkeysAsStandardFunctionKeys =
-      libkrbn_system_preferences_properties_get_use_fkeys_as_standard_function_keys()
-
-    didSetEnabled = true
+    // We don't call `libkrbn_disable_file_monitors` because the file monitors may be used elsewhere.
   }
 
   @Published var useFkeysAsStandardFunctionKeys: Bool = false
