d649e34d0e4f23c75ddf900a1f4f7a92eecf3ef0
Takayama Fumihiko <tekezo@pqrs.org>
2025-07-27 18:08:28 +0900

    Use InfoBorder in MultitouchExtension

--- src/apps/MultitouchExtension/src/Views/Settings/SettingsActionView.swift
+++ src/apps/MultitouchExtension/src/Views/Settings/SettingsActionView.swift
@@ -5,28 +5,23 @@ struct SettingsActionView: View {
     VStack(alignment: .leading, spacing: 25.0) {
       GroupBox(label: Text("Action")) {
         VStack(alignment: .leading, spacing: 16) {
-          HStack {
-            Button(
-              action: {
-                // MultitouchExtension will be relaunched by launchd.
-                NSApplication.shared.terminate(self)
-              },
-              label: {
-                Label("Restart app", systemImage: "arrow.clockwise")
-              })
+          Button(
+            action: {
+              // MultitouchExtension will be relaunched by launchd.
+              NSApplication.shared.terminate(self)
+            },
+            label: {
+              Label("Restart app", systemImage: "arrow.clockwise")
+            })
 
-            Spacer()
-          }
-
-          VStack {
-            Text(
-              "To disable the Multitouch Extension, configure it in the Misc tab of the Karabiner-Elements settings."
-            )
-          }
-          .padding()
-          .foregroundColor(Color.infoForeground)
-          .background(Color.infoBackground)
-        }.padding()
+          Label(
+            "To disable the Multitouch Extension, configure it in the Misc tab of the Karabiner-Elements settings.",
+            systemImage: "lightbulb"
+          )
+          .modifier(InfoBorder())
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
       }
     }
   }
