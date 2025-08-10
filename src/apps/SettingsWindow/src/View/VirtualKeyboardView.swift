48dd6b7f31e3738b882e0d60bf45ace752593c84
Takayama Fumihiko <tekezo@pqrs.org>
2025-08-10 16:39:46 +0900

    Add ScrollView to VirtualKeyboardView

--- src/apps/SettingsWindow/src/View/VirtualKeyboardView.swift
+++ src/apps/SettingsWindow/src/View/VirtualKeyboardView.swift
@@ -4,35 +4,35 @@ struct VirtualKeyboardView: View {
   @ObservedObject private var settings = LibKrbn.Settings.shared
 
   var body: some View {
-    VStack(alignment: .leading, spacing: 24.0) {
-      GroupBox(label: Text("Keyboard Type")) {
-        VStack(alignment: .leading, spacing: 6.0) {
-          KeyboardTypeSelectorView()
-        }
-        .padding()
-      }
-
-      GroupBox(label: Text("Mouse key")) {
-        VStack(alignment: .leading, spacing: 12.0) {
-          HStack {
-            Text("Cursor speed:")
-
-            IntTextField(
-              value: $settings.virtualHIDKeyboardMouseKeyXYScale,
-              range: 0...100_000,
-              step: 10,
-              width: 50)
-
-            Text("%")
-
-            Spacer()
+    ScrollView {
+      VStack(alignment: .leading, spacing: 24.0) {
+        GroupBox(label: Text("Keyboard Type")) {
+          VStack(alignment: .leading, spacing: 6.0) {
+            KeyboardTypeSelectorView()
           }
+          .padding()
+          .frame(maxWidth: .infinity, alignment: .leading)
         }
-        .padding()
-      }
 
-      Spacer()
+        GroupBox(label: Text("Mouse key")) {
+          VStack(alignment: .leading, spacing: 12.0) {
+            HStack {
+              Text("Cursor speed:")
+
+              IntTextField(
+                value: $settings.virtualHIDKeyboardMouseKeyXYScale,
+                range: 0...100_000,
+                step: 10,
+                width: 50)
+
+              Text("%")
+            }
+          }
+          .padding()
+          .frame(maxWidth: .infinity, alignment: .leading)
+        }
+      }
+      .padding()
     }
-    .padding()
   }
 }
