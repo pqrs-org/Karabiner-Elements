67ae54be7e4e0d8254b633fb5de136497104bcc8
Takayama Fumihiko <tekezo@pqrs.org>
2025-08-11 11:49:08 +0900

    Adjust UnknownEventsView layout

--- src/apps/EventViewer/src/View/UnknownEventsView.swift
+++ src/apps/EventViewer/src/View/UnknownEventsView.swift
@@ -4,84 +4,92 @@ struct UnknownEventsView: View {
   @ObservedObject var eventHistory = EventHistory.shared
 
   var body: some View {
-    VStack(alignment: .leading, spacing: 12.0) {
-      GroupBox(label: Text("Showing events that are not supported by Karabiner-Elements")) {
-        VStack(alignment: .leading, spacing: 12.0) {
-          if eventHistory.unknownEventEntries.count >= 0 {
-            HStack(alignment: .center, spacing: 12.0) {
-              Button(
-                action: {
-                  eventHistory.copyToPasteboardUnknownEvents()
-                },
-                label: {
-                  Label("Copy to pasteboard", systemImage: "arrow.right.doc.on.clipboard")
-                })
+    VStack {
+      VStack(alignment: .leading, spacing: 12.0) {
+        Text("Showing events that are not supported by Karabiner-Elements")
+          .font(.title)
 
-              Button(
-                action: {
-                  eventHistory.clearUnknownEvents()
-                },
-                label: {
-                  Label("Clear", systemImage: "clear")
-                })
+        HStack(alignment: .center, spacing: 12.0) {
+          Button(
+            action: {
+              eventHistory.copyToPasteboardUnknownEvents()
+            },
+            label: {
+              Label("Copy to pasteboard", systemImage: "arrow.right.doc.on.clipboard")
             }
+          )
+          .disabled(eventHistory.unknownEventEntries.isEmpty)
 
-            ScrollViewReader { proxy in
-              ScrollView {
-                VStack(alignment: .leading, spacing: 0.0) {
-                  ForEach($eventHistory.unknownEventEntries) { $entry in
-                    HStack(alignment: .center, spacing: 0) {
-                      Text(entry.eventType)
-                        .font(.title)
-                        .frame(width: 70, alignment: .leading)
+          Button(
+            action: {
+              eventHistory.clearUnknownEvents()
+            },
+            label: {
+              Label("Clear", systemImage: "clear")
+            }
+          )
+          .disabled(eventHistory.unknownEventEntries.isEmpty)
+        }
+      }
+      .padding()
+      .frame(maxWidth: .infinity, alignment: .leading)
 
-                      Spacer()
+      ScrollViewReader { proxy in
+        ScrollView {
+          if eventHistory.unknownEventEntries.count == 0 {
+            Text("Unknown events are not being sent.")
+              .padding(.horizontal, 12.0)
+              .frame(maxWidth: .infinity, alignment: .leading)
 
-                      VStack(alignment: .trailing, spacing: 0) {
-                        if entry.usagePage.count > 0 {
-                          HStack(alignment: .bottom, spacing: 0) {
-                            Text("usage page: ")
-                              .font(.caption)
-                            Text(entry.usagePage)
-                              .font(.custom("Menlo", size: 11.0))
-                          }
-                        }
-                        if entry.usage.count > 0 {
-                          HStack(alignment: .bottom, spacing: 0) {
-                            Text("usage: ")
-                              .font(.caption)
-                            Text(entry.usage)
-                              .font(.custom("Menlo", size: 11.0))
-                          }
-                        }
+          } else {
+            VStack(alignment: .leading, spacing: 0.0) {
+              ForEach($eventHistory.unknownEventEntries) { $entry in
+                HStack(alignment: .center, spacing: 0) {
+                  Text(entry.eventType)
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                  VStack(alignment: .trailing, spacing: 0) {
+                    if entry.usagePage.count > 0 {
+                      HStack(alignment: .bottom, spacing: 0) {
+                        Text("usage page: ")
+                          .font(.caption)
+                        Text(entry.usagePage)
+                          .font(.custom("Menlo", size: 11.0))
                       }
-                      .frame(alignment: .leading)
                     }
-                    .padding(.horizontal, 12.0)
-
-                    Divider().id("divider \(entry.id)")
+                    if entry.usage.count > 0 {
+                      HStack(alignment: .bottom, spacing: 0) {
+                        Text("usage: ")
+                          .font(.caption)
+                        Text(entry.usage)
+                          .font(.custom("Menlo", size: 11.0))
+                      }
+                    }
                   }
+                  .frame(alignment: .leading)
                 }
-              }
-              .background(Color(NSColor.textBackgroundColor))
-              .onAppear {
-                if let last = eventHistory.unknownEventEntries.last {
-                  proxy.scrollTo("divider \(last.id)", anchor: .bottom)
-                }
-              }
-              .onChange(of: eventHistory.unknownEventEntries) { newEntries in
-                if let last = newEntries.last {
-                  proxy.scrollTo("divider \(last.id)", anchor: .bottom)
-                }
+                .padding(.horizontal, 12.0)
+
+                Divider().id("divider \(entry.id)")
               }
             }
           }
         }
-        .padding()
-        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(Color(NSColor.textBackgroundColor))
+        .onAppear {
+          if let last = eventHistory.unknownEventEntries.last {
+            proxy.scrollTo("divider \(last.id)", anchor: .bottom)
+          }
+        }
+        .onChange(of: eventHistory.unknownEventEntries) { newEntries in
+          if let last = newEntries.last {
+            proxy.scrollTo("divider \(last.id)", anchor: .bottom)
+          }
+        }
       }
     }
-    .padding()
+    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     .task {
       eventHistory.start()
       eventHistory.pause(false)
