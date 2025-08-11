99af1c57bcbd4e7c66c45800f65846263c49a146
Takayama Fumihiko <tekezo@pqrs.org>
2025-08-11 14:13:02 +0900

    Adjust FrontmostApplicationView layout

--- src/apps/EventViewer/src/View/FrontmostApplicationView.swift
+++ src/apps/EventViewer/src/View/FrontmostApplicationView.swift
@@ -4,97 +4,79 @@ struct FrontmostApplicationView: View {
   @ObservedObject var frontmostApplicationHistory = FrontmostApplicationHistory.shared
 
   var body: some View {
-    VStack(alignment: .leading, spacing: 12.0) {
-      GroupBox(label: Text("Frontmost application history")) {
-        VStack(alignment: .leading, spacing: 12.0) {
-          Text(
-            "You can investigate a bundle identifier and a file path of the frontmost application.")
+    VStack {
+      VStack(alignment: .leading, spacing: 12.0) {
+        Label(
+          "Switch to the app whose bundle identifier you want to check.",
+          systemImage: InfoBorder.icon
+        )
+        .modifier(InfoBorder())
 
-          Label(
-            "Please switch to apps which you want to know Bundle Identifier.",
-            systemImage: "info.circle.fill")
+        Button(
+          action: {
+            frontmostApplicationHistory.clear()
+          },
+          label: {
+            Label("Clear", systemImage: "clear")
+          })
+      }
+      .padding()
+      .frame(maxWidth: .infinity, alignment: .leading)
 
-          if frontmostApplicationHistory.entries.count == 0 {
-            Divider()
-            Spacer()
-          } else {
-            HStack(alignment: .center, spacing: 12.0) {
-              Button(
-                action: {
-                  frontmostApplicationHistory.clear()
-                },
-                label: {
-                  Label("Clear", systemImage: "clear")
-                })
-
-              Spacer()
-            }
-
-            ScrollViewReader { proxy in
-              ScrollView {
+      ScrollViewReader { proxy in
+        ScrollView {
+          VStack(alignment: .leading, spacing: 0.0) {
+            ForEach($frontmostApplicationHistory.entries) { $entry in
+              HStack {
                 VStack(alignment: .leading, spacing: 0.0) {
-                  ForEach($frontmostApplicationHistory.entries) { $entry in
-                    HStack {
-                      VStack {
-                        HStack(alignment: .center, spacing: 0) {
-                          Text("Bundle Identifier: ")
-                            .font(.caption)
+                  HStack(alignment: .center, spacing: 0) {
+                    Text("Bundle Identifier: ")
+                      .font(.caption)
 
-                          Text(entry.bundleIdentifier)
-                            .textSelection(.enabled)
-
-                          Spacer()
-                        }
-
-                        HStack(alignment: .center, spacing: 0) {
-                          Text("File Path: ")
-                            .font(.caption)
-
-                          Text(entry.filePath)
-                            .textSelection(.enabled)
-
-                          Spacer()
-                        }
-                      }
-
-                      Spacer()
-
-                      if entry.bundleIdentifier.count > 0 || entry.filePath.count > 0 {
-                        Button(
-                          action: {
-                            entry.copyToPasteboard()
-                          },
-                          label: {
-                            Label("Copy to pasteboard", systemImage: "arrow.right.doc.on.clipboard")
-                          })
-                      }
-                    }
-                    .padding(.vertical, 6.0)
-                    .padding(.horizontal, 12.0)
-
-                    Divider().id("divider \(entry.id)")
+                    Text(entry.bundleIdentifier)
+                      .textSelection(.enabled)
                   }
 
-                  Spacer()
+                  HStack(alignment: .center, spacing: 0) {
+                    Text("File Path: ")
+                      .font(.caption)
+
+                    Text(entry.filePath)
+                      .textSelection(.enabled)
+                  }
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+
+                if entry.bundleIdentifier.count > 0 || entry.filePath.count > 0 {
+                  Button(
+                    action: {
+                      entry.copyToPasteboard()
+                    },
+                    label: {
+                      Label("Copy to pasteboard", systemImage: "arrow.right.doc.on.clipboard")
+                    })
                 }
               }
-              .background(Color(NSColor.textBackgroundColor))
-              .onAppear {
-                if let last = frontmostApplicationHistory.entries.last {
-                  proxy.scrollTo("divider \(last.id)", anchor: .bottom)
-                }
-              }
-              .onChange(of: frontmostApplicationHistory.entries) { newEntries in
-                if let last = newEntries.last {
-                  proxy.scrollTo("divider \(last.id)", anchor: .bottom)
-                }
-              }
+              .padding(.vertical, 6.0)
+              .padding(.horizontal, 12.0)
+
+              Divider().id("divider \(entry.id)")
             }
           }
         }
-        .padding()
+        .background(Color(NSColor.textBackgroundColor))
+        .onAppear {
+          if let last = frontmostApplicationHistory.entries.last {
+            proxy.scrollTo("divider \(last.id)", anchor: .bottom)
+          }
+        }
+        .onChange(of: frontmostApplicationHistory.entries) { newEntries in
+          if let last = newEntries.last {
+            proxy.scrollTo("divider \(last.id)", anchor: .bottom)
+          }
+        }
       }
     }
-    .padding()
+    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
   }
 }
