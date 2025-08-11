be2c1ee974fae3396202077d460f0a4df8086b3f
Takayama Fumihiko <tekezo@pqrs.org>
2025-08-11 12:57:13 +0900

    Adjust VariablesView layout

--- src/apps/EventViewer/src/View/VariablesView.swift
+++ src/apps/EventViewer/src/View/VariablesView.swift
@@ -4,33 +4,33 @@ struct VariablesView: View {
   @ObservedObject var variablesJsonString = VariablesJsonString.shared
 
   var body: some View {
-    VStack(alignment: .leading, spacing: 12.0) {
-      GroupBox(label: Text("Internal variables of Karabiner-Elements")) {
-        VStack(alignment: .leading, spacing: 12.0) {
-          Button(
-            action: {
-              let pboard = NSPasteboard.general
-              pboard.clearContents()
-              pboard.writeObjects([variablesJsonString.text as NSString])
-            },
-            label: {
-              Label("Copy to pasteboard", systemImage: "arrow.right.doc.on.clipboard")
-            })
+    VStack {
+      VStack(alignment: .leading, spacing: 12.0) {
+        Text("Internal variables of Karabiner-Elements")
+          .font(.title)
 
-          ScrollView {
-            Text(variablesJsonString.text)
-              .lineLimit(nil)
-              .font(.custom("Menlo", size: 11.0))
-              .padding(5)
-              .textSelection(.enabled)
-          }
-          .frame(maxWidth: .infinity, alignment: .leading)
-          .background(Color(NSColor.textBackgroundColor))
-        }
-        .padding()
-        .frame(maxWidth: .infinity, alignment: .leading)
+        Button(
+          action: {
+            let pboard = NSPasteboard.general
+            pboard.clearContents()
+            pboard.writeObjects([variablesJsonString.text as NSString])
+          },
+          label: {
+            Label("Copy to pasteboard", systemImage: "arrow.right.doc.on.clipboard")
+          })
       }
+      .padding()
+      .frame(maxWidth: .infinity, alignment: .leading)
+
+      ScrollView {
+        Text(variablesJsonString.text)
+          .lineLimit(nil)
+          .font(.custom("Menlo", size: 11.0))
+          .padding(5)
+          .textSelection(.enabled)
+      }
+      .frame(maxWidth: .infinity, alignment: .leading)
+      .background(Color(NSColor.textBackgroundColor))
     }
-    .padding()
   }
 }
