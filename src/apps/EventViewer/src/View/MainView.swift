0057ae05413a767d6705cb0833c0fba88e315893
Takayama Fumihiko <tekezo@pqrs.org>
2025-08-12 18:51:03 +0900

    Update layout

--- src/apps/EventViewer/src/View/MainView.swift
+++ src/apps/EventViewer/src/View/MainView.swift
@@ -8,17 +8,19 @@ struct MainView: View {
   var body: some View {
     VStack(alignment: .leading, spacing: 0.0) {
       VStack(alignment: .leading, spacing: 12.0) {
-        Label(
-          "Please enter text in the field below.",
-          systemImage: InfoBorder.icon
-        )
-        .modifier(InfoBorder())
+        VStack(alignment: .leading, spacing: 12.0) {
+          Label(
+            "Please enter text in the field below.",
+            systemImage: InfoBorder.icon
+          )
 
-        TextEditor(
-          text: $textInput
-        )
-        .frame(height: 60)
-        .disableAutocorrection(true)
+          TextEditor(
+            text: $textInput
+          )
+          .frame(height: 60)
+          .disableAutocorrection(true)
+        }
+        .modifier(InfoBorder())
 
         HStack(alignment: .center, spacing: 12.0) {
           Button(
