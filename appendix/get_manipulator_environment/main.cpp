3cb51ed57e07d58858ff64a1477cb087f272dc10
Takayama Fumihiko <tekezo@pqrs.org>
2025-09-21 10:34:11 +0900

    Use libkrbn in get_manipulator_environment

--- appendix/get_manipulator_environment/main.cpp
+++ appendix/get_manipulator_environment/main.cpp
@@ -1,11 +1,41 @@
-#include "dispatcher_utility.hpp"
-#include "grabber_client.hpp"
-#include "json_utility.hpp"
+#include "../../src/lib/libkrbn/include/libkrbn/libkrbn.h"
 #include <iostream>
+#include <pqrs/thread_wait.hpp>
+#include <thread>
 
 namespace {
-auto global_wait = pqrs::make_thread_wait();
+void status_changed_callback(void) {
+  switch (libkrbn_grabber_client_get_status()) {
+    case libkrbn_grabber_client_status_connected:
+      std::cerr << "grabber_client connected" << std::endl;
+
+      libkrbn_grabber_client_async_connect_event_viewer();
+
+      // At this stage, the connection is typically not verified yet,
+      // so the `get_manipulator_environment` request is ignored.
+      libkrbn_grabber_client_async_get_manipulator_environment();
+
+      break;
+
+    case libkrbn_grabber_client_status_connect_failed:
+      std::cerr << "grabber_client connect_failed" << std::endl;
+      break;
+
+    case libkrbn_grabber_client_status_closed:
+      std::cerr << "grabber_client closed" << std::endl;
+      break;
+
+    case libkrbn_grabber_client_status_none:
+      break;
+  }
 }
+
+void manipulator_environment_received_callback(const char* json_string) {
+  std::cout << json_string << std::endl;
+}
+
+auto global_wait = pqrs::make_thread_wait();
+} // namespace
 
 int main(int argc, const char* argv[]) {
   std::cout << std::endl
@@ -15,65 +45,33 @@ int main(int argc, const char* argv[]) {
             << "the process must be code-signed with the same Team ID as karabiner_grabber." << std::endl
             << std::endl;
 
-  auto scoped_dispatcher_manager = krbn::dispatcher_utility::initialize_dispatchers();
+  libkrbn_initialize();
+  libkrbn_load_custom_environment_variables();
 
   signal(SIGINT, [](int) {
     global_wait->notify();
   });
 
-  auto grabber_client = std::make_shared<krbn::grabber_client>("appendix_get_me");
+  libkrbn_enable_grabber_client("appendix_get_me");
 
-  grabber_client->connected.connect([&] {
-    grabber_client->async_connect_event_viewer();
+  libkrbn_register_grabber_client_status_changed_callback(status_changed_callback);
+  libkrbn_register_grabber_client_manipulator_environment_received_callback(manipulator_environment_received_callback);
 
-    // At this stage, the connection is typically not verified yet,
-    // so the `get_manipulator_environment` request is ignored.
-    grabber_client->async_get_manipulator_environment();
-  });
-
-  grabber_client->connect_failed.connect([](auto&& error_code) {
-    std::cerr << "connect_failed: " << error_code << std::endl;
-  });
-
-  grabber_client->received.connect([](auto&& buffer, auto&& sender_endpoint) {
-    if (buffer) {
-      if (buffer->empty()) {
-        return;
-      }
-
-      try {
-        nlohmann::json json = nlohmann::json::from_msgpack(*buffer);
-        switch (json.at("operation_type").get<krbn::operation_type>()) {
-          case krbn::operation_type::manipulator_environment:
-            std::cout << krbn::json_utility::dump(json.at("manipulator_environment")) << std::endl;
-            break;
-
-          default:
-            break;
-        }
-      } catch (std::exception& e) {
-        std::cerr << "received data is corrupted" << std::endl;
-      }
-    }
-  });
-
-  grabber_client->async_start();
+  libkrbn_grabber_client_async_start();
 
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
-  grabber_client->async_get_manipulator_environment();
+  libkrbn_grabber_client_async_get_manipulator_environment();
 
   std::thread thread([] {
     global_wait->wait_notice();
+
+    libkrbn_terminate();
   });
 
   thread.join();
 
-  grabber_client = nullptr;
-
-  scoped_dispatcher_manager = nullptr;
-
   std::cout << "finished" << std::endl;
 
   return 0;
-}
+} // namespace
