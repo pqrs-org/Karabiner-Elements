206da0d8bf718143fe27b843e1cd917377b71e86
Takayama Fumihiko <tekezo@pqrs.org>
2025-10-12 22:30:12 +0900

    Add json_utility::unmarshal_string

--- src/share/json_utility.hpp
+++ src/share/json_utility.hpp
@@ -2,6 +2,7 @@
 
 #include <nlohmann/json.hpp>
 #include <pqrs/json.hpp>
+#include <spdlog/fmt/fmt.h>
 
 namespace krbn {
 namespace json_utility {
@@ -49,6 +50,37 @@ inline std::string dump(const T& json) {
            "game_pad_stick_x_formula",
            "game_pad_stick_y_formula",
        }});
+}
+
+std::string unmarshal_string(const std::string& key,
+                             const nlohmann::json& value) {
+  if (value.is_string()) {
+    return value.get<std::string>();
+
+  } else if (value.is_array()) {
+    std::stringstream ss;
+
+    for (const auto& j : value) {
+      if (!j.is_string()) {
+        goto error;
+      }
+
+      ss << j.get<std::string>() << '\n';
+    }
+
+    auto s = ss.str();
+    // Remove the last newline.
+    if (!s.empty()) {
+      s.pop_back();
+    }
+
+    return s;
+  }
+
+error:
+  throw pqrs::json::unmarshal_error(fmt::format("`{0}` must be array of string, or string, but is `{1}`",
+                                                key,
+                                                value));
 }
 }; // namespace json_utility
 } // namespace krbn
---
+++ tests/src/json_utility/src/test.cpp
@@ -0,0 +1,80 @@
+#include "json_utility.hpp"
+#include <boost/ut.hpp>
+
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
+  using namespace std::literals;
+
+  "json_utility::unmarshal_string"_test = [] {
+    //
+    // string
+    //
+
+    {
+      auto json = R"(
+
+"hello"
+
+)"_json;
+      expect("hello"s == krbn::json_utility::unmarshal_string("string_key", json));
+    }
+
+    //
+    // array
+    //
+
+    {
+      auto json = R"(
+
+[
+  "hello",
+  "world"
+]
+
+)"_json;
+      expect("hello\nworld"s == krbn::json_utility::unmarshal_string("array_key", json));
+    }
+
+    //
+    // unsupported type
+    //
+
+    {
+      auto json = R"(
+
+42
+
+)"_json;
+      try {
+        krbn::json_utility::unmarshal_string("number_key", json);
+        expect(false) << "json must throw unmarshal_error for unsupported type";
+      } catch (const pqrs::json::unmarshal_error& e) {
+        expect("`number_key` must be array of string, or string, but is `42`"s == e.what());
+      }
+    }
+
+    //
+    // unsupported type in array
+    //
+
+    {
+      auto json = R"(
+
+[
+      "hello",
+      42
+]
+
+)"_json;
+      try {
+        krbn::json_utility::unmarshal_string("number_array_key", json);
+        expect(false) << "json must throw unmarshal_error for unsupported type";
+      } catch (const pqrs::json::unmarshal_error& e) {
+        expect(R"(`number_array_key` must be array of string, or string, but is `["hello",42]`)"s == e.what());
+      }
+    }
+  };
+
+  return 0;
+}
