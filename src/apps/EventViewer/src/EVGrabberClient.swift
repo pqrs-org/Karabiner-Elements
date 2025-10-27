9f4bd12abf4c7e67dad75aa6bcd1f3cd22f52952
Takayama Fumihiko <tekezo@pqrs.org>
2025-10-28 07:52:14 +0900

    Add EVGrabberClient::temporarilyIgnoreAllDevices

--- src/apps/EventViewer/src/EVGrabberClient.swift
+++ src/apps/EventViewer/src/EVGrabberClient.swift
@@ -1,7 +1,13 @@
 import Combine
 
+private func callback() {
+  Task { @MainActor in
+    EVGrabberClient.shared.temporarilyIgnoreAllDevices = false
+  }
+}
+
 @MainActor
-final class EVGrabberClient {
+final class EVGrabberClient: ObservableObject {
   static let shared = EVGrabberClient()
 
   // We register the callback in the `start` method rather than in `init`.
@@ -18,6 +24,14 @@ final class EVGrabberClient {
 
     libkrbn_enable_grabber_client("ev_grb_clnt")
 
+    libkrbn_register_grabber_client_status_changed_callback(callback)
+
     libkrbn_grabber_client_async_start()
+  }
+
+  @Published var temporarilyIgnoreAllDevices: Bool = false {
+    didSet {
+      libkrbn_grabber_client_async_temporarily_ignore_all_devices(temporarilyIgnoreAllDevices)
+    }
   }
 }
