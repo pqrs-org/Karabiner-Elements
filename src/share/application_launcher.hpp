21b7fe35876583c870a37fe5dfbb6c26db0024cc
Takayama Fumihiko <tekezo@pqrs.org>
2025-11-24 22:11:08 +0900

    Add application_launcher::wait_for_process_termination

--- src/share/application_launcher.hpp
+++ src/share/application_launcher.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
 #include <cstdlib>
+#include <iostream>
 #include <pqrs/osx/workspace.hpp>
+#include <pqrs/process.hpp>
 #include <spdlog/fmt/fmt.h>
 #include <sstream>
+#include <sys/wait.h>
 
 namespace krbn {
 class application_launcher final {
@@ -29,6 +32,31 @@ public:
 
   static void killall_system_settings(void) {
     system("/usr/bin/killall 'System Settings'");
+    wait_for_process_termination("System Settings");
+  }
+
+  static void wait_for_process_termination(const std::string_view name) {
+    auto pgrep = fmt::format("/usr/bin/pgrep -x -U {0} '{1}'",
+                             getuid(),
+                             name);
+    for (int i = 0; i < 5; ++i) {
+      if (auto exit_code = pqrs::process::system(pgrep)) {
+        // exit_codes
+        //
+        // - 0: One or more processes were matched.
+        // - 1: No processes were matched.
+        // - 2: Invalid options were specified on the command line.
+        // - 3: An internal error occurred.
+        if (*exit_code == 1) {
+          break;
+        }
+      }
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    // Since Launch Services may still hold onto entries right after the process ends, we wait briefly.
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
   }
 
   static void launch_uninstaller(void) {
