028177deb912c23e5377298db88461cd985458a7
Takayama Fumihiko <tekezo@pqrs.org>
2025-11-24 20:29:29 +0900

    Update vendor

--- vendor/vendor/include/pqrs/process.hpp
+++ vendor/vendor/include/pqrs/process.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::process v1.4
+// pqrs::process v1.5
 
 // (C) Copyright Takayama Fumihiko 2019.
 // Distributed under the Boost Software License, Version 1.0.
@@ -8,3 +8,32 @@
 
 #include "process/execute.hpp"
 #include "process/process.hpp"
+#include <cstdlib>
+#include <optional>
+#include <string_view>
+
+namespace pqrs {
+namespace process {
+
+inline std::optional<int> system(std::string_view command) {
+  auto status = std::system(command.data());
+
+  if (status == -1) {
+    return std::nullopt;
+  }
+
+  if (WIFEXITED(status)) {
+    auto exit_code = WEXITSTATUS(status);
+    if (exit_code == 127) {
+      // The execution of the shell failed
+      return std::nullopt;
+    }
+
+    return exit_code;
+  }
+
+  return std::nullopt;
+}
+
+} // namespace process
+} // namespace pqrs
