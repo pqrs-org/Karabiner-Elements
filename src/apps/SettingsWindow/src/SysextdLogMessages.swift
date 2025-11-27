3d2742becd845bdaf81182d77ffc73e9fe30c96a
Takayama Fumihiko <tekezo@pqrs.org>
2025-11-27 22:48:36 +0900

    Add SysextdLogMessages.swift

---
+++ src/apps/SettingsWindow/src/SysextdLogMessages.swift
@@ -0,0 +1,28 @@
+import Foundation
+import SwiftUI
+
+@MainActor
+public class SysextdLogMessages {
+  public static let shared = SysextdLogMessages()
+
+  let streamer = RealtimeCommandStreamer()
+  var stream: RealtimeTextStream { streamer.stream }
+
+  public func update() {
+    streamer.start(
+      launchPath: "/usr/bin/log",
+      arguments: [
+        "show",
+        "--predicate", "sender == \"sysextd\" or sender CONTAINS \"org.pqrs\"",
+        "--info",
+        "--debug",
+        "--signpost",
+        "--loss",
+        "--last", "2h",
+      ],
+      environment: [
+        "LC_ALL": "C"
+      ]
+    )
+  }
+}
