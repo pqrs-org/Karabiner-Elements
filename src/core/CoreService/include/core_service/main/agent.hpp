39f274a48f739e8461c4d6ef04751efb04644a91
Takayama Fumihiko <tekezo@pqrs.org>
2025-12-26 21:09:47 +0900

    Open karabiner.json in core_service::main::agent

--- src/core/CoreService/include/core_service/main/agent.hpp
+++ src/core/CoreService/include/core_service/main/agent.hpp
@@ -8,6 +8,10 @@ namespace krbn {
 namespace core_service {
 namespace main {
 int agent(void) {
+  // The agent opens karabiner.json to trigger the disk-access permission prompt,
+  // in case ~/.config/karabiner is a symlink and karabiner.json lives under Documents or similar.
+  std::ifstream input(krbn::constants::get_user_core_configuration_file_path());
+
   IOHIDRequestAccess(kIOHIDRequestTypeListenEvent);
 
   core_service_utility::wait_until_input_monitoring_granted();
