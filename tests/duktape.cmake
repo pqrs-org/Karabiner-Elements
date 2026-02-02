a0004c10493a0fffbebfdc6344e4c0583aec283c
Takayama Fumihiko <tekezo@pqrs.org>
2026-02-02 22:14:27 +0900

    Use vendor/duktape-src/build/Release/libduktape.a

--- tests/duktape.cmake
+++ tests/duktape.cmake
@@ -6,25 +6,5 @@ include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../vendor/duktape-src)
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../vendor/duktape-2.7.0/extras/console)
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../vendor/duktape-2.7.0/extras/module-node)
 
-set(DUKTAPE_LIB_PATH ${CMAKE_CURRENT_LIST_DIR}/build/libduktape_lib.a)
-
-if (EXISTS ${DUKTAPE_LIB_PATH})
-  add_library(duktape_lib STATIC IMPORTED)
-  set_target_properties(duktape_lib PROPERTIES IMPORTED_LOCATION ${DUKTAPE_LIB_PATH})
-else ()
-  add_library(duktape_lib STATIC
-    ${CMAKE_CURRENT_LIST_DIR}/../vendor/duktape-src/duktape.c
-    ${CMAKE_CURRENT_LIST_DIR}/../vendor/duktape-2.7.0/extras/console/duk_console.c
-    ${CMAKE_CURRENT_LIST_DIR}/../vendor/duktape-2.7.0/extras/module-node/duk_module_node.c
-  )
-
-  set_target_properties(duktape_lib PROPERTIES ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}/build)
-
-  # Suppress duktape warnings for duktape_lib
-  target_compile_options(
-    duktape_lib
-    PRIVATE
-      -Wno-deprecated-declarations
-      -Wno-unused-but-set-variable
-  )
-endif ()
+add_library(libduktape STATIC IMPORTED)
+set_target_properties(libduktape PROPERTIES IMPORTED_LOCATION ${CMAKE_CURRENT_LIST_DIR}/../vendor/duktape-src/build/Release/libduktape.a)
