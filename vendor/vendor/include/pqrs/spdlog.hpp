686370c6ba2fcc35cb5b423bd22b61c6c1fbd6d7
Takayama Fumihiko <tekezo@pqrs.org>
2026-02-08 21:56:10 +0900

    Update vendor

--- vendor/vendor/include/pqrs/spdlog.hpp
+++ vendor/vendor/include/pqrs/spdlog.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-// pqrs::spdlog v2.3
+// pqrs::spdlog v2.4
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
-// (See http://www.boost.org/LICENSE_1_0.txt)
+// (See https://www.boost.org/LICENSE_1_0.txt)
 
 #include "spdlog/factory.hpp"
 #include "spdlog/filesystem.hpp"
