cc41ae1fef0842c5202dd7de9a5859019c30daf6
Takayama Fumihiko <tekezo@pqrs.org>
2026-02-09 22:47:59 +0900

    c++20 -> c++23

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,6 +1,6 @@
 set(CMAKE_OSX_DEPLOYMENT_TARGET "13.0" CACHE STRING "Minimum OS X deployment version")
 
-set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD 23)
 
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../vendor/Karabiner-DriverKit-VirtualHIDDevice/include)
 include_directories(SYSTEM ${CMAKE_CURRENT_LIST_DIR}/../vendor/vendor/include)
