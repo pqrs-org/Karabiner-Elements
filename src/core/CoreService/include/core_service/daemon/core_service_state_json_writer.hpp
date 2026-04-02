3ada99fc5fdf0ade4384b76e1986b35ebb6bfbdd
Takayama Fumihiko <tekezo@pqrs.org>
2026-04-02 23:45:23 +0900

    Remove state_json_writer.hpp

--- src/core/CoreService/include/core_service/daemon/core_service_state_json_writer.hpp
+++ src/core/CoreService/include/core_service/daemon/core_service_state_json_writer.hpp
@@ -3,7 +3,7 @@
 // `core_service_state_json_writer` can be used safely in a multi-threaded environment.
 
 #include "constants.hpp"
-#include "state_json_writer.hpp"
+#include <mutex>
 #include <optional>
 
 namespace krbn {
@@ -11,41 +11,78 @@ namespace core_service {
 namespace daemon {
 class core_service_state_json_writer final {
 public:
-  core_service_state_json_writer(void) : state_json_writer_(constants::get_core_service_state_json_file_path()) {
+  core_service_state_json_writer(void) : state_(nlohmann::json::object()) {
     set_hid_device_open_permitted(std::nullopt);
     set_accessibility_process_trusted(std::nullopt);
     set_virtual_hid_device_service_client_connected(std::nullopt);
     set_driver_activated(std::nullopt);
     set_driver_connected(std::nullopt);
     set_driver_version_mismatched(std::nullopt);
+
+    sync_save();
   }
 
   void set_hid_device_open_permitted(std::optional<bool> value) {
-    state_json_writer_.set("hid_device_open_permitted", value);
+    set("hid_device_open_permitted", value);
   }
 
   void set_accessibility_process_trusted(std::optional<bool> value) {
-    state_json_writer_.set("accessibility_process_trusted", value);
+    set("accessibility_process_trusted", value);
   }
 
   void set_virtual_hid_device_service_client_connected(std::optional<bool> value) {
-    state_json_writer_.set("virtual_hid_device_service_client_connected", value);
+    set("virtual_hid_device_service_client_connected", value);
   }
 
   void set_driver_activated(std::optional<bool> value) {
-    state_json_writer_.set("driver_activated", value);
+    set("driver_activated", value);
   }
 
   void set_driver_connected(std::optional<bool> value) {
-    state_json_writer_.set("driver_connected", value);
+    set("driver_connected", value);
   }
 
   void set_driver_version_mismatched(std::optional<bool> value) {
-    state_json_writer_.set("driver_version_mismatched", value);
+    set("driver_version_mismatched", value);
   }
 
 private:
-  krbn::state_json_writer state_json_writer_;
+  template <typename T>
+  void set(const std::string& key,
+           const std::optional<T>& value) {
+    std::lock_guard<std::mutex> guard(mutex_);
+
+    if (value) {
+      if (state_[key] == *value) {
+        return;
+      }
+
+      state_[key] = *value;
+      sync_save();
+
+      logger::get_logger()->info("core_service_state {}: {}", key, *value);
+
+    } else {
+      if (!state_.contains(key)) {
+        return;
+      }
+
+      state_.erase(key);
+      sync_save();
+
+      logger::get_logger()->info("core_service_state {}: nullopt", key);
+    }
+  }
+
+  void sync_save(void) {
+    json_writer::sync_save_to_file(state_,
+                                   constants::get_core_service_state_json_file_path(),
+                                   0755,
+                                   0644);
+  }
+
+  nlohmann::json state_;
+  std::mutex mutex_;
 };
 } // namespace daemon
 } // namespace core_service
