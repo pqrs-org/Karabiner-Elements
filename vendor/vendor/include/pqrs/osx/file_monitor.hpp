19ae829c4a8964a49f0c6738d8f4a0ebe0e9f669
Takayama Fumihiko <tekezo@pqrs.org>
2026-04-26 21:31:51 +0900

    Update vendor

--- vendor/vendor/include/pqrs/osx/file_monitor.hpp
+++ vendor/vendor/include/pqrs/osx/file_monitor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::file_monitor v1.10.0
+// pqrs::osx::file_monitor v2.0.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
@@ -49,7 +49,6 @@ public:
 
   // Signals (invoked from the dispatcher thread)
 
-  nod::signal<void(const std::string& watched_directory, availability availability)> watched_directory_availability_changed;
   nod::signal<void(const std::string& watched_file, availability availability)> watched_file_availability_changed;
   nod::signal<void(const std::string& changed_file_path, std::shared_ptr<std::vector<uint8_t>> changed_file_body)> file_changed;
   nod::signal<void(const std::string&)> error_occurred;
@@ -154,39 +153,6 @@ private:
       return;
     }
 
-    // ----------------------------------------
-    // File System Events API does not call the callback if the root directory and files are moved at the same time.
-    //
-    // Example:
-    //
-    //   FSEventStreamCreate(... ,{"target/file1", "target/file2"})
-    //
-    //   $ mkdir target.new
-    //   $ echo  target.new/file1
-    //   $ mv    target.new target
-    //
-    //   In this case, the callback will not be called.
-    //
-    // Thus, we should signal manually once.
-
-    update_watched_directory_availabilities();
-
-    for (const auto& file_path : files_) {
-      update_stream_file_paths(file_path);
-
-      auto [updated, file_body, availability] = update_file_bodies(file_path);
-      if (availability) {
-        emit_watched_file_availability_changed(file_path,
-                                               *availability);
-      }
-      if (updated) {
-        emit_file_changed(file_path,
-                          file_body);
-      }
-    }
-
-    // ----------------------------------------
-
     FSEventStreamContext context{0};
     context.info = this;
 
@@ -219,11 +185,14 @@ private:
       FSEventStreamSetDispatchQueue(stream_,
                                     queue_);
       if (!FSEventStreamStart(stream_)) {
+        cleanup_stream_registration_failure();
+
         enqueue_to_dispatcher([this] {
           error_occurred("FSEventStreamStart is failed.");
         });
       } else {
         ready_ = true;
+        emit_initial_state();
       }
     }
   }
@@ -232,6 +201,49 @@ private:
   void unregister_stream(bool emit_unavailable_signal = true) {
     ready_ = false;
 
+    clear_stream_state(emit_unavailable_signal);
+    release_stream(true);
+  }
+
+  void cleanup_stream_registration_failure() {
+    ready_ = false;
+
+    clear_stream_state(false);
+    release_stream(false);
+  }
+
+  void emit_initial_state() {
+    // File System Events API does not call the callback if the root directory and files are moved at the same time.
+    //
+    // Example:
+    //
+    //   FSEventStreamCreate(... ,{"target/file1", "target/file2"})
+    //
+    //   $ mkdir target.new
+    //   $ echo  target.new/file1
+    //   $ mv    target.new target
+    //
+    //   In this case, the callback will not be called.
+    //
+    // Thus, we should signal manually once after the stream is started.
+    update_watched_directory_availabilities();
+
+    for (const auto& file_path : files_) {
+      update_stream_file_paths(file_path);
+
+      auto [updated, file_body, availability] = update_file_bodies(file_path);
+      if (availability) {
+        emit_watched_file_availability_changed(file_path,
+                                               *availability);
+      }
+      if (updated) {
+        emit_file_changed(file_path,
+                          file_body);
+      }
+    }
+  }
+
+  void clear_stream_state(bool emit_unavailable_signal) {
     if (emit_unavailable_signal) {
       for (const auto& [file_path, file_body] : file_bodies_) {
         if (file_body) {
@@ -239,21 +251,18 @@ private:
                                                  availability::unavailable);
         }
       }
-
-      for (const auto& [directory_path, current_availability] : directory_availabilities_) {
-        if (current_availability == availability::available) {
-          emit_watched_directory_availability_changed(directory_path,
-                                                      availability::unavailable);
-        }
-      }
     }
 
     stream_file_paths_.clear();
     directory_availabilities_.clear();
     file_bodies_.clear();
+  }
 
+  void release_stream(bool stop_stream) {
     if (stream_) {
-      FSEventStreamStop(stream_);
+      if (stop_stream) {
+        FSEventStreamStop(stream_);
+      }
       FSEventStreamInvalidate(stream_);
       FSEventStreamRelease(stream_);
       stream_ = nullptr;
@@ -368,14 +377,6 @@ private:
     });
   }
 
-  void emit_watched_directory_availability_changed(const std::string& directory_path,
-                                                   availability availability) {
-    enqueue_to_dispatcher([this, directory_path, availability] {
-      watched_directory_availability_changed(directory_path,
-                                             availability);
-    });
-  }
-
   void update_stream_file_paths(const std::string& file_path) {
     std::erase_if(stream_file_paths_,
                   [&](const auto& pair) {
@@ -396,8 +397,6 @@ private:
       if (current_available != previous_available) {
         auto availability = current_available ? availability::available : availability::unavailable;
         directory_availabilities_[directory_path] = availability;
-        emit_watched_directory_availability_changed(directory_path,
-                                                    availability);
         if (current_available) {
           reevaluate_watched_files_in_directory(directory_path);
         }
