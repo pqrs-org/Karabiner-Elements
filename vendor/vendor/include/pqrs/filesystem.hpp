2b2b0189012dff6c537460d8bfd6986a05648505
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-27 19:37:26 +0900

    Update vendor

--- vendor/vendor/include/pqrs/filesystem.hpp
+++ vendor/vendor/include/pqrs/filesystem.hpp
@@ -1,29 +1,28 @@
 #pragma once
 
-// pqrs::filesystem v1.3.0
+// pqrs::filesystem v2.0.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
 // (See https://www.boost.org/LICENSE_1_0.txt)
 
-#include "filesystem/impl.hpp"
-#include <array>
-#include <climits>
-#include <fstream>
 #include <optional>
+#include <string>
 #include <sys/stat.h>
-#include <vector>
 
 namespace pqrs::filesystem {
-
-[[nodiscard]] inline bool exists(const std::string& path) noexcept {
-  struct stat s;
-  return (stat(path.c_str(), &s) == 0);
-}
 
 [[nodiscard]] inline std::optional<uid_t> uid(const std::string& path) noexcept {
   struct stat s;
   if (stat(path.c_str(), &s) == 0) {
+    return s.st_uid;
+  }
+  return std::nullopt;
+}
+
+[[nodiscard]] inline std::optional<uid_t> symlink_uid(const std::string& path) noexcept {
+  struct stat s;
+  if (lstat(path.c_str(), &s) == 0) {
     return s.st_uid;
   }
   return std::nullopt;
@@ -37,86 +36,20 @@ namespace pqrs::filesystem {
   return std::nullopt;
 }
 
-[[nodiscard]] inline bool is_directory(const std::string& path) noexcept {
+[[nodiscard]] inline std::optional<gid_t> symlink_gid(const std::string& path) noexcept {
   struct stat s;
-  if (stat(path.c_str(), &s) == 0) {
-    return S_ISDIR(s.st_mode);
+  if (lstat(path.c_str(), &s) == 0) {
+    return s.st_gid;
   }
-  return false;
+  return std::nullopt;
 }
 
 [[nodiscard]] inline bool is_owned(const std::string& path, uid_t uid) noexcept {
-  struct stat s;
-  if (stat(path.c_str(), &s) == 0) {
-    return s.st_uid == uid;
-  }
-  return false;
+  return pqrs::filesystem::uid(path) == uid;
 }
 
-[[nodiscard]] inline std::string dirname(const std::string& path) {
-  size_t pos = impl::get_dirname_position(path);
-  if (pos == 0) {
-    return ".";
-  }
-  return path.substr(0, pos);
-}
-
-[[nodiscard]] inline std::optional<std::string> realpath(const std::string& path) {
-  std::array<char, PATH_MAX> resolved_path;
-  if (!::realpath(path.c_str(), resolved_path.data())) {
-    return std::nullopt;
-  }
-  return std::string(resolved_path.data());
-}
-
-[[nodiscard]] inline std::optional<mode_t> file_access_permissions(const std::string& path) noexcept {
-  struct stat s;
-  if (stat(path.c_str(), &s) != 0) {
-    return std::nullopt;
-  }
-  return s.st_mode & ACCESSPERMS;
-}
-
-[[nodiscard]] inline std::optional<off_t> file_size(const std::string& path) noexcept {
-  struct stat s;
-  if (stat(path.c_str(), &s) != 0) {
-    return std::nullopt;
-  }
-  return s.st_size;
-}
-
-inline bool create_directory_with_intermediate_directories(const std::string& path, mode_t mode) {
-  std::vector<std::string> parents;
-  auto directory = path;
-  while (!exists(directory)) {
-    parents.push_back(directory);
-    directory = dirname(directory);
-  }
-
-  for (auto it = std::rbegin(parents); it != std::rend(parents); std::advance(it, 1)) {
-    if (mkdir(it->c_str(), mode) != 0) {
-      return false;
-    }
-  }
-
-  if (!is_directory(path)) {
-    return false;
-  }
-
-  return (chmod(path.c_str(), mode) == 0);
-}
-
-inline void copy(const std::string& from_file_path,
-                 const std::string& to_file_path) {
-  std::ifstream ifstream(from_file_path);
-  if (!ifstream) {
-    return;
-  }
-  std::ofstream ofstream(to_file_path);
-  if (!ofstream) {
-    return;
-  }
-  ofstream << ifstream.rdbuf();
+[[nodiscard]] inline bool is_symlink_owned(const std::string& path, uid_t uid) noexcept {
+  return pqrs::filesystem::symlink_uid(path) == uid;
 }
 
 } // namespace pqrs::filesystem
