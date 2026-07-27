330de68f03657a642d944ab8ddf690516b369c1b
Takayama Fumihiko <tekezo@pqrs.org>
2026-07-28 08:36:35 +0900

    Remove killall Karabiner-Menu, killall Karabiner-NotificationWindow

--- src/scripts/uninstall.sh
+++ src/scripts/uninstall.sh
@@ -7,11 +7,9 @@ bash '/Library/Application Support/org.pqrs/Karabiner-Elements/uninstall_core.sh
 bash '/Library/Application Support/org.pqrs/Karabiner-DriverKit-VirtualHIDDevice/scripts/uninstall/remove_files.sh'
 
 killall Karabiner-Core-Service
-killall karabiner_console_user_server
+killall Karabiner-Console-User-Server
 killall Karabiner-Elements
 killall Karabiner-EventViewer
-killall Karabiner-Menu
 killall Karabiner-MultitouchExtension
-killall Karabiner-NotificationWindow
 
 exit 0
