f00a36513e87cdeb01732938b29efb1f31216ad7
Takayama Fumihiko <tekezo@pqrs.org>
2026-07-29 22:04:05 +0900

    Update src/core/console_user_server/swift/Bridging-Header.h

--- src/core/console_user_server/swift/Bridging-Header.h
+++ src/core/console_user_server/swift/Bridging-Header.h
@@ -1,2 +1,4 @@
+#include "console_user_server/runtime.h"
+#include "console_user_server/ui_bridge.h"
 #include "pqrs/osx/process_info/impl/Bridging-Header.h"
 #include "pqrs/osx/workspace/impl/Bridging-Header.h"
