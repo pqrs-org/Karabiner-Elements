0afab84792f13933da4ddd11bcd4cbd69447803f
Takayama Fumihiko <tekezo@pqrs.org>
2026-08-01 21:50:03 +0900

    Remove libkrbn dependency in appendix/get_manipulator_environment

--- appendix/get_manipulator_environment/main.cpp
+++ appendix/get_manipulator_environment/main.cpp
@@ -1,35 +1,13 @@
-#include "../../src/lib/libkrbn/include/libkrbn/libkrbn.h"
+#include "core_service_daemon_client.hpp"
+#include "dispatcher_utility.hpp"
+#include "environment_variable_utility.hpp"
+#include "json_utility.hpp"
+#include "run_loop_thread_utility.hpp"
 #include <iostream>
 #include <pqrs/thread_wait.hpp>
 #include <thread>
 
 namespace {
-void status_changed_callback() noexcept {
-  switch (libkrbn_core_service_daemon_client_get_status()) {
-    case libkrbn_core_service_daemon_client_status_connected:
-      std::cerr << "core_service_daemon_client connected" << std::endl;
-
-      libkrbn_core_service_daemon_client_async_get_manipulator_environment();
-
-      break;
-
-    case libkrbn_core_service_daemon_client_status_connect_failed:
-      std::cerr << "core_service_daemon_client connect_failed" << std::endl;
-      break;
-
-    case libkrbn_core_service_daemon_client_status_closed:
-      std::cerr << "core_service_daemon_client closed" << std::endl;
-      break;
-
-    case libkrbn_core_service_daemon_client_status_none:
-      break;
-  }
-}
-
-void manipulator_environment_received_callback(const char* json_string) noexcept {
-  std::cout << json_string << std::endl;
-}
-
 auto global_wait = pqrs::make_thread_wait();
 } // namespace
 
@@ -41,25 +19,45 @@ int main() {
             << "the process must be code-signed with the same Team ID as Karabiner-Core-Service.app." << std::endl
             << std::endl;
 
-  libkrbn_initialize();
-  libkrbn_load_custom_environment_variables();
+  auto scoped_dispatcher_manager = krbn::dispatcher_utility::initialize_dispatchers();
+  auto scoped_run_loop_thread_manager = krbn::run_loop_thread_utility::initialize_scoped_run_loop_thread_manager(
+      pqrs::cf::run_loop_thread::failure_policy::exit);
+
+  auto environment_variables = krbn::environment_variable_utility::load_custom_environment_variables();
+  krbn::environment_variable_utility::log(environment_variables);
 
   signal(SIGINT, [](int) noexcept {
     global_wait->notify();
   });
 
-  libkrbn_enable_core_service_daemon_client();
+  auto client = std::make_unique<krbn::core_service_daemon_client>();
 
-  libkrbn_register_core_service_daemon_client_status_changed_callback(status_changed_callback);
-  libkrbn_register_core_service_daemon_client_manipulator_environment_received_callback(manipulator_environment_received_callback);
+  client->connected.connect([&client] {
+    std::cerr << "core_service_daemon_client connected" << std::endl;
+    client->async_get_manipulator_environment();
+  });
 
-  libkrbn_core_service_daemon_client_async_start();
+  client->connect_failed.connect([](auto&&) {
+    std::cerr << "core_service_daemon_client connect_failed" << std::endl;
+  });
+
+  client->closed.connect([] {
+    std::cerr << "core_service_daemon_client closed" << std::endl;
+  });
+
+  client->received.connect([](auto&& operation_type, auto&& json) {
+    if (operation_type == krbn::operation_type::manipulator_environment) {
+      std::cout << krbn::json_utility::dump(json.at("manipulator_environment")) << std::endl;
+    }
+  });
+
+  client->async_start();
 
   std::this_thread::sleep_for(std::chrono::seconds(1));
 
   global_wait->wait_notice();
 
-  libkrbn_terminate();
+  client = nullptr;
 
   std::cout << "finished" << std::endl;
 
