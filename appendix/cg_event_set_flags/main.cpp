1dbdf59d38d226c4536df83f9abf03e506c4feb6
Takayama Fumihiko <tekezo@pqrs.org>
2026-08-01 12:58:23 +0900

    Update appendix/cg_event_set_flags to use pqrs::osx::cg_event_tap

--- appendix/cg_event_set_flags/main.cpp
+++ appendix/cg_event_set_flags/main.cpp
@@ -1,22 +1,26 @@
 #include <CoreGraphics/CoreGraphics.h>
 #include <iostream>
-#include <pqrs/cf/cf_ptr.hpp>
+#include <memory>
+#include <pqrs/osx/cg_event_tap.hpp>
 
 namespace {
-pqrs::cf::cf_ptr<CFMachPortRef> event_tap;
+std::unique_ptr<pqrs::osx::cg_event_tap> event_tap;
 
 CGEventRef _Nullable callback(CGEventTapProxy _Nullable proxy,
                               CGEventType type,
                               CGEventRef _Nullable event,
                               void* _Nonnull refcon) noexcept {
   if (type == kCGEventTapDisabledByTimeout) {
-    CGEventTapEnable(event_tap.get(),
-                     true);
+    if (event_tap &&
+        !event_tap->enable()) {
+      std::cerr << "Failed to re-enable event tap." << std::endl;
+    }
     return event;
   }
 
   if (type == kCGEventRightMouseDown) {
-    exit(0);
+    CFRunLoopStop(CFRunLoopGetMain());
+    return event;
   }
 
   if (event) {
@@ -43,29 +47,22 @@ int main() {
               CGEventMaskBit(kCGEventOtherMouseUp) |
               CGEventMaskBit(kCGEventOtherMouseDragged);
 
-  event_tap = pqrs::cf::adopt_cf_ptr(CGEventTapCreate(kCGHIDEventTap,
-                                                      kCGHeadInsertEventTap,
-                                                      kCGEventTapOptionDefault,
-                                                      mask,
-                                                      callback,
-                                                      nullptr));
-  if (event_tap) {
-    if (auto run_loop_source = pqrs::cf::adopt_cf_ptr(CFMachPortCreateRunLoopSource(kCFAllocatorDefault,
-                                                                                    event_tap.get(),
-                                                                                    0))) {
-      CFRunLoopAddSource(CFRunLoopGetMain(),
-                         run_loop_source.get(),
-                         kCFRunLoopCommonModes);
-      CGEventTapEnable(event_tap.get(),
-                       true);
-
-      std::cout << std::endl;
-      std::cout << std::endl;
-      std::cout << "Set mouse event flags to command." << std::endl;
-      std::cout << "You can exit by right click." << std::endl;
-      std::cout << std::endl;
-      std::cout << std::endl;
-    }
+  event_tap = std::make_unique<pqrs::osx::cg_event_tap>(
+      pqrs::cf::adopt_cf_ptr(CGEventTapCreate(kCGHIDEventTap,
+                                              kCGHeadInsertEventTap,
+                                              kCGEventTapOptionDefault,
+                                              mask,
+                                              callback,
+                                              nullptr)));
+  if (event_tap->valid() &&
+      event_tap->attach_to_run_loop(pqrs::cf::cf_ptr(CFRunLoopGetMain())) &&
+      event_tap->enable()) {
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << "Set mouse event flags to command." << std::endl;
+    std::cout << "You can exit by right click." << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
   }
 
   CFRunLoopRun();
