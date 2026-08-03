d0176761bc9ff09cc2ff120ab07c54e3689afb41
Takayama Fumihiko <tekezo@pqrs.org>
2026-08-04 08:01:14 +0900

    Remove old comments

--- src/apps/Updater/src/KarabinerUpdaterApp.swift
+++ src/apps/Updater/src/KarabinerUpdaterApp.swift
@@ -2,16 +2,6 @@ import AppKit
 import SwiftUI
 import os
 
-// Note #1:
-// The icon shown in Sparkle's dialog is generally taken from the file specified by CFBundleIconFile.
-// As a result, the icon overridden by AppIconSwitcher is not reflected.
-// The default icon is always used instead.
-
-// Note #2:
-// If an update is available, the Updater may attempt to restart during the update process.
-// When using libkrbn, this can cause the process to abort where libkrbn_terminate is not called,
-// Therefore, please avoid using libkrbn in Updater.
-
 @main
 struct KarabinerUpdaterApp: App {
   @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
