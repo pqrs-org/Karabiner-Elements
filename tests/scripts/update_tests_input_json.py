eea1d12f59e1374093bb4e22e8efe0bef40cbe28
Takayama Fumihiko <tekezo@pqrs.org>
2026-09-13 20:32:06 +0900

    Format python scripts

--- tests/scripts/update_tests_input_json.py
+++ tests/scripts/update_tests_input_json.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
-'''Script to batch update `input_event_queue/*.json`'''
+"""Script to batch update `input_event_queue/*.json`"""
 
 import sys
 import json
 
 
 def migrate(paths):
-    '''
+    """
     - Remove event_origin
-    '''
+    """
 
     for path in paths:
-        with open(path, encoding='utf-8') as file:
+        with open(path, encoding="utf-8") as file:
             j = json.load(file)
             for entry in j:
-                if 'event_origin' in entry:
-                    del entry['event_origin']
+                if "event_origin" in entry:
+                    del entry["event_origin"]
 
-            with open(path, 'w', encoding='utf-8') as output:
+            with open(path, "w", encoding="utf-8") as output:
                 json.dump(j, output, sort_keys=True, indent=4)
 
 
