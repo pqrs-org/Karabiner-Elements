52483341e88fd637ef0be35d17764240238e47f4
Takayama Fumihiko <tekezo@pqrs.org>
2026-09-23 19:38:20 +0900

    mouse_motion_and_wheel_to_key_needs_virtual_hid_pointing.json -> mouse_motion_and_wheel_to_key_needs_virtual_hid_pointing.jsonc

--- tests/src/manipulator/src/manipulator_manager_test.hpp
+++ tests/src/manipulator/src/manipulator_manager_test.hpp
@@ -125,7 +125,7 @@ void run_manipulator_manager_test() {
   };
 
   "mouse_motion_and_wheel_to_key.needs_virtual_hid_pointing"_test = [] {
-    for (const auto& test : krbn::unit_testing::json_helper::load_jsonc("json/mouse_motion_and_wheel_to_key_needs_virtual_hid_pointing.json")) {
+    for (const auto& test : krbn::unit_testing::json_helper::load_jsonc("json/mouse_motion_and_wheel_to_key_needs_virtual_hid_pointing.jsonc")) {
       auto parameters = std::make_shared<krbn::core_configuration::details::complex_modifications_parameters>();
       auto m = krbn::manipulator::manipulator_factory::make_manipulator(test.at("input"), parameters);
       krbn::manipulator::manipulator_manager manager;
